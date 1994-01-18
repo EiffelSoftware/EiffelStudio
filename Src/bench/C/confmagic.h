4ce0de49c899ff789880ee97dc7cd3828080b8cc
grator <grator@eiffel.com>
1994-01-18 16:48:48 +0000

    Initial revision
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@469 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/bench/C/confmagic.h
@@ -0,0 +1,45 @@
+/*
+ * This file was produced by running metaconfig and is intended to be included
+ * after config.h and after all the other needed includes have been dealt with.
+ *
+ * This file may be empty, and should not be edited. Rerun metaconfig instead.
+ * If you wish to get rid of this magic, remove this file and rerun metaconfig
+ * without the -M option.
+ *
+ *  $Id$
+ */
+
+#ifndef _confmagic_h_
+#define _confmagic_h_
+
+#ifndef HAS_BCMP
+#ifndef bcmp
+#define bcmp(s,d,l) memcmp((s),(d),(l))
+#endif
+#endif
+
+#ifndef HAS_BCOPY
+#ifndef bcopy
+#define bcopy(s,d,l) memcpy((d),(s),(l))
+#endif
+#endif
+
+#ifndef HAS_BZERO
+#ifndef bzero
+#define bzero(s,l) memset((s),0,(l))
+#endif
+#endif
+
+#ifndef HAS_INDEX
+#ifndef index
+#define index strchr
+#endif
+#endif
+
+#ifndef HAS_INDEX
+#ifndef rindex
+#define rindex strrchr
+#endif
+#endif
+
+#endif

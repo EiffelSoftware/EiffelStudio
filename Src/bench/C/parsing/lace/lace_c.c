413fd977d8c97f0025ca3e706f52f30f3f6aa14b
grator <grator@eiffel.com>
1994-10-28 21:45:55 +0000

    lex specific stuff is done in lace_l.l => #ifdef FLEX_SCANNER ... parsing from string has been removed
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@1252 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/parsing/lace/lace_c.c
+++ Src/bench/C/parsing/lace/lace_c.c
@@ -22,9 +22,6 @@ extern Malloc_t malloc();
  * Declarations 
  */
 
-int string_mode;	/* Is the parser reading a string? */
-int to_parse_size;	/* Size of `to_parse' in string mode */
-char *to_parse;		/* Current string being parsed by yacc (if string_mode)*/
 char *file_name;
 
 static char *id_string;		/* Eiffel string buffer (instance of ID_SD) */
@@ -82,8 +79,7 @@ char *filename;
 								 * in last token */
 	extern int end_position;	/* Position in text of the last character in
 								 * last token */
-	extern char xxsbuf[];
-	extern char *xxsptr;
+	extern void reset_lace_lex_parser();
 
 	extern char *yacc_file_name;
 #ifdef YYDEBUG
@@ -94,7 +90,6 @@ char *filename;
 	int cancel;				/* Parsing cancelity */
 
 	/* Initialization */
-	string_mode = 0;
 	xxlineno = 1;
 	start_position = 0;
 	end_position = 0;
@@ -109,8 +104,7 @@ char *filename;
 	xxdebug = 1;
 #endif
 
-	/* Reset lex parser */
-	xxsptr = xxsbuf;
+	reset_lace_lex_parser();
 
 	/*
 	 * `id_pchar' is a reference on the attribute `area' of instance
@@ -125,64 +119,6 @@ char *filename;
 		print_err_msg(stderr, "Syntax analysis canceled\n");
 		exit(1);
 	}
-	return rn_ast;
-}
-
-char *lp_string(toparse,filename)
-char *toparse;
-char *filename;
-{
-	/* Parse Lace source string `to_parse'.
-	 * Return root node fo the corresponding abstract syntax tree
-	 */
-
-	extern int xxparse();	/* Yacc parsing */
-	extern int xxlineno;	/* Lex line number */
-	extern int start_position;	/* Position in text of the first character
-								 * in last token */
-	extern int end_position;	/* Position in text of the last character
-								 * in last token */
-	extern char *yacc_file_name;
-	extern char *xxsptr;
-	extern char xxsbuf[];
-#ifdef YYDEBUG
-	extern int xxdebug;
-#endif
-
-	char buf[100];			/* Error message */
-	int cancel;				/* Parsing cancelity */
-
-	/* Initialization */
-	to_parse_size = strlen(toparse);
-	string_mode = 1;
-	to_parse = toparse;
-	xxlineno = 1;
-	yacc_file_name = filename;
-	
-	/* Stack initialization */
-	object_top = 0;
-	count_top - -1;
-	bzero (count_stack, count_size * sizeof(int));
-
-	/* Reset lex */
-	xxsptr = xxsbuf;
-
-	/* Parsing */
-#ifdef YYDEBUG
-	xxdebug = 1;
-#endif
-	start_position = 0;
-	end_position = 0;
-	cancel = xxparse();
-
-	/* Cancelation if error */	
-	if (cancel) {
-		/* Error during parsing */
-		rn_ast = (char *) 0;
-	}
-#ifdef DEBUG
-	printf("Finished with object_top = %d\n", object_top);
-#endif
 	return rn_ast;
 }
 

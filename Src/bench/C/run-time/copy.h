c8bd8ceea25c2e2a57054468077a86444664a3f0
grator <grator@eiffel.com>
1994-12-02 20:19:13 +0000

    definition of `safe_bcopy'
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@1500 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/copy.h
+++ Src/bench/C/run-time/copy.h
@@ -27,4 +27,14 @@ public void ecopy();			/* Standard copy of a normal Eiffel object */
 public void xcopy();			/* Expanded copy with possible exception */
 public void spcopy();			/* Standard copy of a special object */
 
+#ifdef HAS_SAFE_BCOPY
+#define safe_bcopy(s,d,l) bcopy((s),(d),(l))
+#elif HAS_SAFE_MEMCPY
+#define safe_bcopy(s,d,l) memcpy((d),(s),(l))
+#elif HAS_MEMMOVE
+#define safe_bcopy(s,d,l) memmove((d),(s),(l))
+#else
+You must define your own version of safe_bcopy
+#endif
+
 #endif

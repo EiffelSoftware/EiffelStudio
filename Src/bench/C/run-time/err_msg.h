09da16d8873a9f69f0c3e92d25b0ba62d3ab0d0b
grator <grator@eiffel.com>
1995-03-21 23:42:41 +0000

    #ifdef for print_err_msg simplified
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@2217 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/err_msg.h
+++ Src/bench/C/run-time/err_msg.h
@@ -6,23 +6,11 @@
 #ifndef _err_msg_h
 #define _err_msg_h
 
-#ifndef __WINDOWS_386__
-#include <stdio.h>
-#define print_err_msg fprintf
-#else
 #ifdef __WINDOWS_386__
 extern int print_err_msg(FILE *err, char *StrFmt, ...);
 #else
 #include <stdio.h>
-#include <stdarg.h>
-int print_err_msg (FILE *err, char *StrFmt, ...)
-{
-	va_list ap;
-
-	va_start (ap, StrFmt);
-	return vprintf (StrFmt, ap);
-}
-#endif
+#define print_err_msg fprintf
 #endif
 
 #endif

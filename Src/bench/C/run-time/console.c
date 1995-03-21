fdc30031d3c1b962dafa4253aafbc0f85954b469
grator <grator@eiffel.com>
1995-03-21 23:42:10 +0000

    prototypes only for Windows
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@2216 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/console.c
+++ Src/bench/C/run-time/console.c
@@ -10,18 +10,30 @@
 
 #include "file.h"
 
+#ifdef __WINDOWS_386__
 extern void eif_console_putint (long);
 extern void eif_console_putreal (double);
 extern void eif_console_putdouble (double);
 extern void eif_console_putchar (char);
 extern void eif_console_pustring (char *, int);
+extern long eif_console_readline (char *, int, int);
+extern long eif_console_readword (char *, int, int);
+extern long eif_console_readstream (char *, int);
+#else
+extern void eif_console_putint ();
+extern void eif_console_putreal ();
+extern void eif_console_putdouble ();
+extern void eif_console_putchar ();
+extern void eif_console_pustring ();
+extern long eif_console_readline ();
+extern long eif_console_readword ();
+extern long eif_console_readstream ();
+#endif
+
 extern long eif_console_readint ();
 extern char eif_console_readchar ();
 extern double eif_console_readdouble ();
 extern float eif_console_readreal ();
-extern long eif_console_readline (char *, int, int);
-extern long eif_console_readword (char *, int, int);
-extern long eif_console_readstream (char *, int);
 
 public EIF_POINTER file_def(file)
 int file;

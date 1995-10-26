3ae44fc91f52753385f5cd595759e8b5ce791c6a
davidm <davidm@eiffel.com>
1995-10-26 14:25:57 +0000

    the exception trace appears as a Dialog
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@2973 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/err_msg.h
+++ Src/bench/C/run-time/err_msg.h
@@ -8,8 +8,11 @@
 
 #include <stdio.h>
 
-#ifdef EIF_WIN_31
+#ifdef EIF_WINDOWS
 extern int print_err_msg(FILE *err, char *StrFmt, ...);
+extern int save_exception_trace;
+extern char *exception_trace_string;
+extern void show_trace();
 #else
 #define print_err_msg fprintf
 #endif

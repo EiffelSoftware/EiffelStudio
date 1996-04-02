a1eb1be0fe3bf6302a09a683454b8f0400d67d40
dinov <dinov@eiffel.com>
1996-04-02 15:54:40 +0000

    added extern `last_color_alloc_status'
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@4460 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/mel/Clib/pixel.h
+++ Src/library/mel/Clib/pixel.h
@@ -1,9 +1,13 @@
 /*
- *
  * Constants used to code errors when allocating colors.
- *
  */
 
 #define BAD_SCREEN_REFERENCE	-1
 #define BAD_COLOR_NAME			-2
 #define NO_FREE_CELL_AVAILABLE  -3
+
+/*
+ * Variable used to store error status
+ */
+
+extern EIF_INTEGER last_color_alloc_status;

838bd97ba87ebc341b173ffc8360be74be7b3cce
patricek <patricek@eiffel.com>
1996-04-05 14:35:21 +0000

    new macros: `cwel_msg_set_hwnd', `cwel_msg_set_message', `cwel_msg_set_wparam', `cwel_msg_set_lparam'
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@4611 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/wel/spec/windows/include/msg.h
+++ Src/library/wel/spec/windows/include/msg.h
@@ -9,10 +9,15 @@
 #	include <wel.h>
 #endif
 
-#define cwel_msg_get_hwnd(_ptr_) ((EIF_POINTER) ((MSG *) _ptr_)->hwnd)
-#define cwel_msg_get_message(_ptr_) ((EIF_INTEGER) ((MSG *) _ptr_)->message)
-#define cwel_msg_get_wparam(_ptr_) ((EIF_INTEGER) ((MSG *) _ptr_)->wParam)
-#define cwel_msg_get_lparam(_ptr_) ((EIF_INTEGER) ((MSG *) _ptr_)->lParam)
+#define cwel_msg_set_hwnd(_ptr_, _value_) (((MSG *) _ptr_)->hwnd = (HWND) (_value_))
+#define cwel_msg_set_message(_ptr_, _value_) (((MSG *) _ptr_)->message = (UINT) (_value_))
+#define cwel_msg_set_wparam(_ptr_, _value_) (((MSG *) _ptr_)->wParam = (WPARAM) (_value_))
+#define cwel_msg_set_lparam(_ptr_, _value_) (((MSG *) _ptr_)->lParam = (LPARAM) (_value_))
+
+#define cwel_msg_get_hwnd(_ptr_) (((MSG *) _ptr_)->hwnd)
+#define cwel_msg_get_message(_ptr_) (((MSG *) _ptr_)->message)
+#define cwel_msg_get_wparam(_ptr_) (((MSG *) _ptr_)->wParam)
+#define cwel_msg_get_lparam(_ptr_) (((MSG *) _ptr_)->lParam)
 
 #endif /* __WEL_MSG__ */
 

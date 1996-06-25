7d2661c451c2b395691db7c43cb3b5644dc63346
xavierl <xavierl@eiffel.com>
1996-06-25 13:56:47 +0000

    public, private, shared => rt_public, rt_private, rt_shared (C++ conflict) protection of declarations for C++ compilation
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@5708 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/equal.h
+++ Src/bench/C/run-time/equal.h
@@ -13,6 +13,10 @@
 #ifndef _equal_h_
 #define _equal_h_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "portable.h"
 
 #ifndef TRUE
@@ -27,11 +31,15 @@
  * Routine declarations
  */
 
-public int xequal();			/* Equality with no conformance constraint */
-public int eequal();			/* Standard equality on standard objects */
-public int spequal();			/* Standard equality on special objects */
-public int eiso();				/* Standard isomorphism on normal objects */
-public int spiso();				/* Standard isomorphism on special objects */
-public int ediso();				/* Standard recursive isomorphism */
+rt_public int xequal();			/* Equality with no conformance constraint */
+rt_public int eequal();			/* Standard equality on standard objects */
+rt_public int spequal();			/* Standard equality on special objects */
+rt_public int eiso();				/* Standard isomorphism on normal objects */
+rt_public int spiso();				/* Standard isomorphism on special objects */
+rt_public int ediso();				/* Standard recursive isomorphism */
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif

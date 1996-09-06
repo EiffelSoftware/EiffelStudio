eda31c5df5a121be8bc22247bbca26a9e65f314c
terryt <terryt@eiffel.com>
1996-09-06 16:57:11 +0000

    warning added at the top (header file must be included only once)
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@6456 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/curserver.h
+++ Src/bench/C/run-time/curserver.h
@@ -1,3 +1,15 @@
+/****************************************************************
+
+                  I M P O R T A N T    N O T I C E
+                  --------------------------------
+
+    The include file is and only is included once by an application,
+because it defines the globals used by concurrent run-time.
+At present, we let "server.c" in concurrency include it. So no other
+file can include the head files again.
+
+****************************************************************/ 
+
 #ifndef _CONCURRENT_SERVER_
 #define _CONCURRENT_SERVER_
 

2a3d03bb62a8312fe967fa9f89712255b46ea982
xavierl <xavierl@eiffel.com>
1996-09-25 14:51:37 +0000

    MAXG => MAX_GENERICS
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@6537 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/rtlimits.h
+++ Src/bench/C/run-time/rtlimits.h
@@ -13,7 +13,7 @@
 #ifndef _rtlimits_h_
 #define _rtlimits_h_
 
-#define MAXG            4		/* Maximum number of generic parameters */
+#define MAX_GENERICS      4		/* Maximum number of generic parameters */
 
 #define IDLENGTH        512		/* Maximum length for identifiers */
 #define STRINGLENGTH    5120	/* Maximum manifest string length (parsing) */

3111fa2e34e9467cfa0b2cec5cc21072fea0dd94
zorans <zorans@eiffel.com>
1996-11-04 16:02:37 +0000

    *** empty log message ***
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@7083 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/bench/C/run-time/eif_threads.h
@@ -0,0 +1,23 @@
+/*
+
+ ######     #    ######           #####  #    #  #####   ######    ##    #####	  ####            ####
+ #          #    #                  #    #    #  #    #  #        #  #   #    #	 #               #    #
+ #####      #    #####              #    ######  #    #  #####   #    #  #    #	  ####           #
+ #          #    #                  #    #    #  #####   #       ######  #    #	      #   ###    #
+ #          #    #                  #    #    #  #   #   #       #    #  #    #	 #    #   ###    #    #
+ ######     #    #      #######     #    #    #  #    #  ######  #    #  #####	  ####    ###     ####
+
+	Thread management routines.
+
+*/
+
+#ifdef EIF_THREADS
+#ifndef _eif_threads_h_
+#define _eif_threads_h_
+
+#ifndef EIF_REENTRANT		/* Make sure we have reentrant run-time */
+#define EIF_REENTRANT
+#endif
+
+#endif	/* _eif_threads_h_ */
+#endif	/* EIF_THREADS */

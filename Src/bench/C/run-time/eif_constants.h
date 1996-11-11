34f07343ab1766e7a2858633d7007c492a8b08c0
zorans <zorans@eiffel.com>
1996-11-11 17:17:06 +0000

    *** empty log message ***
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@7143 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/bench/C/run-time/eif_constants.h
@@ -0,0 +1,22 @@
+/*
+
+ ######     #    ######           ####    ####   #    #   ####    #####    ##	 #    #   #####   ####           #    #
+ #          #    #               #    #  #    #  ##   #  #          #     #  #	 ##   #     #    #               #    #
+ #####      #    #####           #       #    #  # #  #   ####      #    #    #	 # #  #     #     ####           ######
+ #          #    #               #       #    #  #  # #       #     #    ######	 #  # #     #         #   ###    #    #
+ #          #    #               #    #  #    #  #   ##  #    #     #    #    #	 #   ##     #    #    #   ###    #    #
+ ######     #    #      #######   ####    ####   #    #   ####      #    #    #	 #    #     #     ####    ###    #    #
+
+	Constants used in global variable definitions.
+
+*/
+
+#ifndef _eif_constants_h_
+#define _eif_constants_h_
+
+/* garcol */
+#define AGE_BITS	4					/* How many bits are used to store age */
+#define TENURE_MAX		(1<<AGE_BITS)	/* Non reached age */
+
+
+#endif	/* _eif_constants_h_ */
---
+++ Src/bench/C/run-time/eif_types.h
@@ -0,0 +1,23 @@
+/*
+
+ ######     #    ######           #####   #   #  #####   ######   ####		   #    #
+ #          #    #                  #      # #   #    #  #       #			   #    #
+ #####      #    #####              #       #    #    #  #####    ####		   ######
+ #          #    #                  #       #    #####   #            #   ###  #    #
+ #          #    #                  #       #    #       #       #    #   ###  #    #
+ ######     #    #      #######     #       #    #       ######   ####    ###  #    #
+
+	Structures and types defining global variables.
+
+ */
+
+#ifndef _eif_types_h_
+#define _eif_types_h_
+
+/* wbench */
+struct bounds { 			/* Structure used to record min/max dtypes */
+	int16 min;
+	int16 max;
+};
+
+#endif	 /* _eif_types_h */

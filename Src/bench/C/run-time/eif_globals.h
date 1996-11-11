3b55ff8edcde24c0e8c3c254d15dceecfaa881ff
zorans <zorans@eiffel.com>
1996-11-11 17:12:17 +0000

    new macros
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@7138 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_globals.h
+++ Src/bench/C/run-time/eif_globals.h
@@ -1,11 +1,11 @@
 /*
 
- ######     #    ######           ####   #        ####   #####     ##    #		  ####            ####
+ ######     #    ######           ####   #        ####   #####     ##    #		  ####           #    #
  #          #    #               #    #  #       #    #  #    #   #  #   #		 #               #    #
- #####      #    #####           #       #       #    #  #####   #    #  #		  ####           #
- #          #    #               #  ###  #       #    #  #    #  ######  #		      #   ###    #
+ #####      #    #####           #       #       #    #  #####   #    #  #		  ####           ######
+ #          #    #               #  ###  #       #    #  #    #  ######  #		      #   ###    #    #
  #          #    #               #    #  #       #    #  #    #  #    #  #		 #    #   ###    #    #
- ######     #    #      #######   ####   ######   ####   #####   #    #  ######	  ####    ###     ####
+ ######     #    #      #######   ####   ######   ####   #####   #    #  ######	  ####    ###    #    #
 
 	Global variables handling.
 
@@ -16,26 +16,48 @@
 
 #include "eif_threads.h"		/* Make sure we're definig global variables as requested */
 
+#include "portable.h"
+#include "eif_types.h"
+#include "eif_constants.h"
+
 #ifdef EIF_REENTRANT
 
-#define EIF_EXTERN_GLOBAL(type,name)
-#define EIF_GLOBAL(scope,type,name)		#define name eif_globals->eif_##name
-#define EIF_THR_CONTEXT_ALONE	EIF_GLOBALS		struct EIF_GLOBALS *eif_globals
-#define EIF_THR_CONTEXT			EIF_THR_CONTEXT_ALONE,
+#define EIF_DECL_GLOBAL(x)
+#define EIF_CONTEXT_NOARG	eif_global_context_t	*eif_globals
+#define EIF_CONTEXT			EIF_CONTEXT_NOARG,
+#define EIF_STATIC_OPT
 
-typedef struct tagEIF_GLOBALS {		/* Structure containing all global variables to the run-time */
-	char *account;				/* Array of traversed dyn types */
-} EIF_GLOBALS;
 
-rt_public EIF_GLOBALS *eif_thr_init(void) {
-}
+typedef struct tag_eif_globals 		/* Structure containing all global variables to the run-time */
+{
+		/* debug */
+	int breakpoint_number;			/* used for application interrupt */
+
+
+		/* garcol */
+	uint32 age_table[TENURE_MAX];	/* Number of objects/age */
+
+		/* interp */
+	int argnum;						/* Number of arguments */
+
+		/* wbench */
+	struct bounds *bounds_tab;		/* Bounds of the various classes */
+
+
+} eif_global_context_t;
+
+#define breakpoint_number	(eif_globals->breakpoint_number)	/* used only in debug.c */
+#define age_table	(eif_globals->age_table)		/* global to garcol.c */
+#define argnum		(eif_globals->argnum)			/* global to interp.c */
+#define bounds_tab	(eif_globals->bounds_tab)		/* appears only in wbench.c */
 
 #else
 
-#define EIF_EXTERN_GLOBAL(type,name) extern scope type name;
-#define EIF_GLOBAL(scope,type,name) scope type name;
-#define EIF_THR_CONTEXT_ALONE void
-#define EIF_THR_CONTEXT
+#define EIF_DECL_GLOBAL(x) x
+#define EIF_CONTEXT_NOARG void
+#define EIF_CONTEXT
+#define EIF_STATIC_OPT static
+
 
 #endif	/* EIF_REENTRANT */
 

b9dfdb9abc7c68df32b328104bc7d9200ba27def
zorans <zorans@eiffel.com>
1996-11-25 18:02:10 +0000

    new macros to create threads, get mutexes, ... for POSIX, WIN32, SOLARIS i.e. portable macros
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@7360 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_threads.h
+++ Src/bench/C/run-time/eif_threads.h
@@ -19,23 +19,96 @@
 #define EIF_REENTRANT
 #endif
 
-#ifdef POSIX_THREADS
-#define EIF_THREAD_T pthread_t
-#define EIF_MUTEX_T pthread_mutex_t
-#else
-#ifdef WIN32
-#define EIF_THREAD_T HANDLE
-#define EIF_MUTEX_T			/* ? */
-#else
-#ifdef SOLARIS_THREADS
-#define EIF_THREAD_T thread_t
-#define EIF_MUTEXT_T mutex_t
-#endif
-#endif
-#endif
 
-extern eif_global_context_t *eif_get_new_context(EIF_THREAD_T);
+#ifdef POSIX_THREADS
+
+/*-----------------------*/
+/*---  POSIX Threads  ---*/
+/*-----------------------*/
+
+#include <pthread.h>
+
+
+#elif defined WIN32
+
+/*-------------------------------*/
+/*---  WINDOWS 95/NT Threads  ---*/
+/*-------------------------------*/
+
+#include <windows.h>
+#include <process.h>
+
+#define EIF_THR_ENTRY_TYPE		void
+#define EIF_THR_ENTRY_ARG_TYPE	void *
+
+#define EIF_THR_CREATION_FLAGS
+
+#define EIF_THR_TYPE						HANDLE
+#define EIF_THR_ERRCODE_TYPE				unsigned long
+#define EIF_THR_CREATE(entry,arg,tid,errcode)	errcode=_beginthread((entry),0,(EIF_THR_ENTRY_ARG_TYPE)(arg)); tid=(HANDLE)errcode
+#define EIF_THR_EXIT(arg)					_endthread()
+#define EIF_THR_JOIN(which)
+#define EIF_THR_VALID_ERRCODE(errcode)		(errcode!=-1)
+
+#define EIF_MUTEX_TYPE						CRITICAL_SECTION
+#define EIF_MUTEX_ERRCODE_TYPE				int
+#define EIF_MUTEX_CREATE(m,errcode)			InitializeCriticalSection(&m)
+#define EIF_MUTEX_LOCK(m,errcode)			EnterCriticalSection(&m)
+#define EIF_MUTEX_UNLOCK(m,errcode)			LeaveCriticalSection(&m)
+#define EIF_MUTEX_DESTROY(m,errcode)		DeleteCriticalSection(&m)
+#define EIF_MUTEX_VALID_ERRCODE(errcode)	(1)
+
+#define EIF_TSD_TYPE					DWORD
+#define EIF_TSD_VAL_TYPE				void *
+#define EIF_TSD_ERRCODE_TYPE			DWORD
+#define EIF_TSD_CREATE(key,errcode)		key=TlsAlloc(); errcode=(key==0xFFFFFFFF)
+#define EIF_TSD_SET(key,val,errcode)	errcode=(TlsSetValue(key,(EIF_TSD_VAL_TYPE)val)==FALSE)
+#define EIF_TSD_GET(key,val,errcode)	val=TlsGetValue(key); errcode=(GetLastError()!=NO_ERROR)
+#define EIF_TSD_DESTROY(key,errcode)	errcode=(TlsFree()==FALSE)
+#define	EIF_TSD_VALID_ERRCODE(errcode)	(!errcode)
+
+
+#elif defined SOLARIS_THREADS
+
+/*-------------------------*/
+/*---  SOLARIS Threads  ---*/
+/*-------------------------*/
+
+#include <thread.h>
+
+
+#define EIF_THR_ENTRY_TYPE		void *
+#define EIF_THR_ENTRY_ARG_TYPE	void *
+
+#define EIF_THR_CREATION_FLAGS	THR_NEW_LWP
+
+#define EIF_THR_TYPE						thread_t
+#define EIF_THR_ERRCODE_TYPE				int
+#define EIF_THR_CREATE(entry,arg,tid,errcode)	errcode=thr_create(NULL,0,(entry),(EIF_THR_ENTRY_ARG_TYPE)(arg),EIF_THR_CREATION_FLAGS,&tid)
+#define EIF_THR_EXIT(arg)					thr_exit(arg)
+#define EIF_THR_JOIN(which)					thr_join(0,(which),NULL)
+#define EIF_THR_VALID_ERRCODE(errcode)		(!errcode)
+
+#define EIF_MUTEX_TYPE						mutex_t
+#define EIF_MUTEX_ERRCODE_TYPE				int
+#define EIF_MUTEX_CREATE(m,errcode)			errcode=mutex_init(&(m),USYNC_THREAD,NULL)
+#define EIF_MUTEX_LOCK(m,errcode)			errcode=mutex_lock(&(m))
+#define EIF_MUTEX_UNLOCK(m,errcode)			errcode=mutex_unlock(&(m))
+#define EIF_MUTEX_DESTROY(m,errcode)
+#define EIF_MUTEX_VALID_ERRCODE(errcode)	!(errcode)
+
+#define EIF_TSD_TYPE					thread_key_t
+#define EIF_TSD_VAL_TYPE				void *
+#define EIF_TSD_ERRCODE_TYPE			int
+#define EIF_TSD_CREATE(key,errcode)		errcode=thr_keycreate(&(key),NULL)
+#define EIF_TSD_SET(key,val,errcode)	errcode=thr_setspecific((key),(EIF_TSD_VAL_TYPE)(val))
+#define EIF_TSD_GET(key,val,errcode)	errcode=thr_getspecific((key),&(val))
+#define EIF_TSD_DESTROY(key,errcode)
+#define	EIF_TSD_VALID_ERRCODE(errcode)	!(errcode)
+
+#endif	/* end of POSIX, WIN32, SOLARIS... */
 
 
 #endif	/* _eif_threads_h_ */
 #endif	/* EIF_THREADS */
+

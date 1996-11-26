489ded90f107e5f6f044e0028e1f5581b4b1ebf7
dinov <dinov@eiffel.com>
1996-11-26 13:56:04 +0000

    updated makefile to produce net.lib
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@7379 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/net/Clib_kr/makefile.m
+++ Src/library/net/Clib_kr/makefile.m
@@ -9,9 +9,9 @@ RM = del
 SOURCES =  $(LSRCS)
 OBJECTS =  $(OBJS) $(WOBJS)
 
-all:: network.lmb wnetwork.lmb
+all:: net.lib
 
-.c.obm:
+.c.obj:
 	$(CC) -Fo$@ -c $(CFLAGS) $<
 
 SMODE = network.c network_r.c
@@ -19,56 +19,21 @@ SMODE = network.c network_r.c
 LSRCS = $(SMODE)
 
 OBJS = \
-	network.obm \
-	network_r.obm
-
-WOBJS = \
-	wnetwork.obm \
-	wnetwork_r.obm
-
-wnetwork.c: network.c
-	$(RM) wnetwork.c
-	$(LN) network.c wnetwork.c
-
-
-wnetwork.obm: wnetwork.c bitmask.h
-	$(CC) -Fo$@ -c $(CFLAGS) -DWORKBENCH $*.c
-
-
-wnetwork_r.c: network_r.c
-	$(RM) wnetwork_r.c
-	$(LN) network_r.c wnetwork_r.c
-
-wnetwork_r.obm: wnetwork_r.c
-	$(CC) -Fo$@ -c $(CFLAGS) -DWORKBENCH $*.c
-
+	network.obj \
+	network_r.obj
 
 local_clean:: remove
 
-all:: network.lmb
+all:: net.lib
 
 local_realclean::
-	$(RM) network.lmb
+	$(RM) net.lib
 
-network.lmb: $(OBJS)
+net.lib: $(OBJS)
 	$(RM) $@
-	$(AR) /OUT:$@ network_r.obm network.obm wsock32.lib
+	$(AR) /OUT:$@ $(OBJS) wsock32.lib
 	$(RANLIB) $@
 	if not exist ..\spec mkdir ..\spec
 	if not exist ..\spec\w32msc mkdir ..\spec\w32msc
 	if not exist ..\spec\w32msc\lib mkdir ..\spec\w32msc\lib
-	copy network.lmb ..\spec\w32msc\lib\network.lib
-
-all:: wnetwork.lmb
-
-local_realclean::
-	$(RM) wnetwork.lmb
-
-wnetwork.lmb: $(WOBJS)
-	$(RM) $@
-	$(AR) /OUT:$@ wnetwork_r.obm wnetwork.obm wsock32.lib
-	$(RANLIB) $@
-	if not exist ..\spec mkdir ..\spec
-	if not exist ..\spec\w32msc mkdir ..\spec\w32msc
-	if not exist ..\spec\w32msc\lib mkdir ..\spec\w32msc\lib
-	copy wnetwork.lmb ..\spec\w32msc\lib\wnetwork.lib
+	copy net.lib ..\spec\w32msc\lib\net.lib

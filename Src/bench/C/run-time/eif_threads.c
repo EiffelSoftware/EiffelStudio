6e47399f70353646f2042a3dcecb7ad9f38609c4
zorans <zorans@eiffel.com>
1997-01-16 12:57:16 +0000

    Corrected declarations, reviewed initialization part
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@7607 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_threads.c
+++ Src/bench/C/run-time/eif_threads.c
@@ -14,26 +14,29 @@
 #include "eiffel.h"
 #include "eif_threads.h"
 #include "eif_globals.h"
-#include "except.h"				/* for panic () */
 
 #ifdef EIF_THREADS
 
-rt_public void eif_thr_panic(char *);
-rt_public void eif_init_root_thread(void);
-rt_public void eif_register_thread(void);
-rt_private void eif_init_context(eif_global_context_t *eif_globals);
+/* Structure used to give two arguments to a new thread,
+   with only one used -- the pointer on this structure.
+   This structure is only needed locally */
 
 typedef struct {
 	EIF_OBJ current;
 	EIF_PROC routine;
 } start_routine_ctxt_t;
 
-rt_public void eif_thr_create (EIF_OBJ current_obj, EIF_PROC init_func);
+rt_public void eif_thr_panic(char *);
+rt_public void eif_thr_init_root(void);
+rt_public void eif_thr_register(void);
+rt_public void eif_thr_create(EIF_OBJ current_obj, EIF_PROC init_func);
+
+rt_private void eif_init_context(eif_global_context_t *eif_globals);
 rt_private void eif_thr_entry(start_routine_ctxt_t *routine_ctxt);
 
 rt_public EIF_TSD_TYPE eif_global_key;
+rt_public EIF_MUTEX_TYPE eif_rmark_mutex;
 rt_private eif_global_context_t globals_buffer;
-rt_private EIF_MUTEX_TYPE globals_buffer_mutex;
 
 
 rt_public void eif_thr_panic(char *msg) {
@@ -42,52 +45,21 @@ rt_public void eif_thr_panic(char *msg) {
 	exit(0);
 }
 
-rt_public void eif_init_root_thread(void) {
-	int i;
-
-	printf("Entered eif_init_root_thread (size of context=%d)\n",sizeof(eif_global_context_t));
-/*	if (eif_global_key)
-		eif_thr_panic("global key already allocated !");
-*/
-
-	printf("No global key yet.. attempting to create one\n");
-
-	if (i=thr_keycreate(&eif_global_key,NULL)) {
-		printf("keycreate errcode=%d\n",i);
-		exit(1);
-	}
-/*	EIF_TSD_CREATE(eif_global_key,i,"could not create global key for initial thread");
-*/
-
-	printf("TSD created... trying mutex\n");
-	EIF_MUTEX_CREATE(globals_buffer_mutex,"could not create mutex for context buffer");
-
-	printf("Mutex created... initalizing data\n");
-	eif_register_thread();
+rt_public void eif_thr_init_root(void) {
+	EIF_TSD_CREATE(eif_global_key, "could not create global key for initial thread");
+	EIF_MUTEX_CREATE(eif_rmark_mutex,"could not create mutex for recursive marking");
+	eif_thr_register();
 }
 
-rt_public void eif_register_thread(void) {
+rt_public void eif_thr_register(void) {
 	eif_global_context_t *eif_globals;
 
-	printf("Locking mutex... ");
-	EIF_MUTEX_LOCK(globals_buffer_mutex,"problem while trying to lock mutex on context buffer");
-
-	printf("OK\n");
-	eif_globals = &globals_buffer;
-	eif_init_context(eif_globals);
-
-	EIF_TSD_SET(eif_global_key,eif_globals,"could not bind a specific value to a thread");
-
-	eif_globals = (eif_global_context_t *)cmalloc(sizeof(eif_global_context_t));
+	eif_globals = (eif_global_context_t *)malloc(sizeof(eif_global_context_t));
 	if (!eif_globals)
-		panic("Could not get memory for thread context");
+		eif_thr_panic("Could not get memory for thread context");
 
-	bcopy((char *)&globals_buffer, (char *)eif_globals, sizeof(eif_global_context_t));
-
+	eif_init_context(eif_globals);
 	EIF_TSD_SET(eif_global_key,eif_globals,"could not bind a specific value to a thread");
-
-	EIF_MUTEX_UNLOCK(globals_buffer_mutex,"problem while unlocking mutex on context buffer");
-
 }
 
 
@@ -106,21 +78,23 @@ rt_public void eif_init_context(eif_global_context_t *eif_globals) {
 
 		/* malloc.c */
 	gen_scavenge = GS_SET;
-	printf("Initalization OK\n");
 }
 
 
 void eif_thr_create (EIF_OBJ current_obj, EIF_PROC init_func)
 {
-	start_routine_ctxt_t routine_ctxt;
+	start_routine_ctxt_t *routine_ctxt;
 	EIF_THR_TYPE tid;
 
-	routine_ctxt.current = eif_adopt(current_obj);
-	routine_ctxt.routine = init_func;
+	routine_ctxt = (start_routine_ctxt_t *)malloc(sizeof(start_routine_ctxt_t));
+	if (!routine_ctxt)
+		eif_thr_panic("No memory for thread info\n");
+	routine_ctxt->current = eif_adopt(current_obj);
+	routine_ctxt->routine = init_func;
 
 	EIF_THR_CREATE(
-		eif_thr_entry, 
-		(EIF_THR_ENTRY_ARG_TYPE)(&routine_ctxt), 
+		eif_thr_entry,
+		(EIF_THR_ENTRY_ARG_TYPE)(routine_ctxt),
 		tid,
 		"Cannot create thread\n");
 }

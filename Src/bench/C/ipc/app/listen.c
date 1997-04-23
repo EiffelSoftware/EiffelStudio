f14d727b96c60f4a7e79815689054c3423de2d30
Emmanuel Stapf <manus@eiffel.com>
1997-04-23 20:18:32 +0000

    Changes to reflect the new `ipc' directory structure (Merged extra/win32/ipc with ipc/).
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@7753 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/ipc/app/listen.c
+++ Src/bench/C/ipc/app/listen.c
@@ -8,7 +8,10 @@
  ######     #     ####      #    ######  #    #   ###     ####
 
 	Wide listening on all opened file descriptors (read).
-*/
+
+	For Windows:
+		A #ifdef removed as the nature of the select statement is predetermined.
+ */
 
 #include "config.h"
 #include "portable.h"
@@ -17,8 +20,14 @@
 #include "select.h"
 #include "ewbio.h"
 #include "stream.h"
+#include "listen.h"
 
+#ifdef EIF_WIN32
+extern STREAM *sp;
+extern HANDLE global_ewbin, global_ewbout, global_event_r, global_event_w;
+#else
 extern int ised;		/* Socket used to chat with the daemon */
+#endif
 
 rt_public void wide_listen(void)
 {
@@ -30,25 +39,53 @@ rt_public void wide_listen(void)
 	 * routine whenever data is available there.
 	 */
 
+#ifdef EIF_WIN32
+	if (-1 == add_input(sp, (HANDLE_FN) arqsthandle)) {
+#else
 	if (-1 == add_input(EWBIN, arqsthandle)) {
+#endif
+
 #ifdef USE_ADD_LOG
 		add_log(4, "add_input: %s (%s)", s_strerror(), s_strname());
 #endif
 		dexit(1);
 	}
 
+#ifdef USE_ADD_LOG
+	add_log(12, "in listen");
+#endif
+
 	/* After having selected, we scan all our files to make sure none of them
 	 * has been removed from the selection process. If at least one is missing,
 	 * we are exiting immediately.
 	 */
 
+#ifdef EIF_WIN32
+
+	while (0 <= do_select(0)) {
+#ifdef USE_ADD_LOG
+	add_log(12, "in while do_select");
+#endif
+		if (!has_input(sp)) {			/* Socket connection broken? */
+#ifdef USE_ADD_LOG
+	add_log(12, "in !has_input which is what we want");
+#endif
+			return;						/* Anyway, abort processing */
+		}
+	}
+#ifdef USE_ADD_LOG
+	add_log(12, "out of listen");
+#endif
+
+#else
+
 	while (0 < do_select((struct timeval *) 0)) {
 		if (!has_input(EWBIN))			/* Socket connection broken? */
 			return;						/* Anyway, abort processing */
 	}
+#endif
 
 #ifdef USE_ADD_LOG
 	add_log(12, "do_select: %s (%s)", s_strerror(), s_strname());
 #endif
 }
-

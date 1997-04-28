4fd690ca7798750ed9d400bc78315230917598e7
Emmanuel Stapf <manus@eiffel.com>
1997-04-28 17:00:53 +0000

    Removed the reference to `show_trace' since this routine doesn't exist anymore. Changed the #ifdef EIF_WINDOWS in #ifdef EIF_WIN32
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@7779 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/err_msg.h
+++ Src/bench/C/run-time/err_msg.h
@@ -13,9 +13,8 @@ extern "C" {
 #include "eif_globals.h"
 #include <stdio.h>
 
-#ifdef EIF_WINDOWS
+#ifdef EIF_WIN32
 extern int print_err_msg(FILE *err, char *StrFmt, ...);
-extern void show_trace();		/* %%zs undefined */
 #else
 #define print_err_msg fprintf
 #endif

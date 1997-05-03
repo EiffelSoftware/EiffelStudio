5b9799ccd02a072c1fe6838adc2dd5b44ad20ca7
Emmanuel Stapf <manus@eiffel.com>
1997-05-03 18:47:04 +0000

    Added what need to be in the C-Header file of econsole.c
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@7802 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/console/econsole.h
+++ Src/bench/C/console/econsole.h
@@ -1,3 +1,22 @@
 /*
-	ECONSOLE.H
+	EIF_CONS.H - a console for Win32.
+
 */
+
+#include "eiffel.h"
+
+extern void eif_console_next_line(void);
+extern void eif_console_putint(EIF_INTEGER);
+extern void eif_console_putreal(EIF_DOUBLE);
+extern void eif_console_putdouble(EIF_DOUBLE);
+extern void eif_console_putchar(EIF_CHARACTER);
+extern void eif_console_pustring(EIF_POINTER, EIF_INTEGER);
+extern EIF_INTEGER eif_console_readline(EIF_POINTER, EIF_INTEGER, EIF_INTEGER);
+extern EIF_REAL eif_console_readreal();
+extern EIF_DOUBLE eif_console_readdouble();
+extern EIF_CHARACTER eif_console_readchar();
+extern EIF_INTEGER eif_console_readint();
+extern EIF_INTEGER eif_console_readword(EIF_POINTER, EIF_INTEGER, EIF_INTEGER);
+extern EIF_INTEGER eif_console_readstream(EIF_POINTER, EIF_INTEGER);
+extern EIF_BOOLEAN eif_console_eof();
+#

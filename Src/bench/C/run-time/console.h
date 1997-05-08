546b04419534c80643c3e96f6ca2a3f31bb7a96d
Emmanuel Stapf <manus@eiffel.com>
1997-05-08 20:18:23 +0000

    Changed to reflect the new `econsole.h'.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@7852 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/console.h
+++ Src/bench/C/run-time/console.h
@@ -23,13 +23,8 @@ extern "C" {
 
 #include "file.h"
 
-#ifdef EIF_WINDOWS
-#include "eif_cons.h"	/* extra/mswin/console or extra/win32/console */
-#endif
-
 #ifdef EIF_WIN32
-extern void eif_console_next_line(void);	/* extra/win32/console/econsole.c */
-extern void eif_console_putstring(char *s, long len);	/* extra/win32/console/econsole.c , should be (BYTE, long) */
+#include "econsole.h"
 #else
 extern void eif_console_next_line(void);
 #endif

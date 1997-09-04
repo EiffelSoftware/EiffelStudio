1ed8736f1e868daed57df5fff202522c965f69c3
Emmanuel Stapf <manus@eiffel.com>
1997-09-04 19:37:43 +0000

    Remove the STDC ifdef
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@8105 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/plug.h
+++ Src/bench/C/run-time/plug.h
@@ -44,15 +44,9 @@ extern int str_dtype;				/* Dynamic type for string */
 extern int arr_dtype;				/* Dynamic type for ARRAY[ANY] */
 extern int32 disp_rout_id;			/* Dispose routine id */ 
 
-#ifdef __STDC__
 extern void (*eif_strmake)(char *, EIF_INTEGER);	/* STRING creation feature */
 extern void (*eif_strset)(char *, EIF_INTEGER);		/* STRING `set_count' feature */
 extern void (*eif_arrmake)(char *, EIF_INTEGER, EIF_INTEGER);	/* STRING creation feature */
-#else
-extern void (*eif_strmake)();	/* STRING creation feature */
-extern void (*eif_strset)();		/* STRING `set_count' feature */
-extern void (*eif_arrmake)();	/* STRING creation feature */
-#endif
 
 extern char *argarr(int argc, char **argv);				/* ARRAY[STRING] creation from command line arguments */
 

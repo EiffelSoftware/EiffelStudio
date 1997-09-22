818a772fb450578751adc51751f7670e6cb83256
paulv <paulv@eiffel.com>
1997-09-22 23:34:36 +0000

    Declaration of eif_alloc_init
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@8182 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/main.h
+++ Src/bench/C/run-time/main.h
@@ -26,11 +26,11 @@ extern void system_mod_init (void);	/* Module Initialization (from einit.c) */
 
 extern void dinterrupt(void);
 extern void dserver(void);
+extern void eif_alloc_init();
 extern int in_assertion;
 extern char *ename;				/* Name of the Eiffel program running */
 extern int cc_for_speed;		/* Optimized for speed or for memory */
 extern int scount;				/* Maximum dtype */
-
 #ifdef __cplusplus
 }
 #endif

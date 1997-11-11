2b6613d0ccf72bec21e97af199e4dc61ea62b03c
Emmanuel Stapf <manus@eiffel.com>
1997-11-11 20:08:39 +0000

    Changed the Makefile to generate the DLL for DESC_DLL
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@8496 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/desc/makefile-win.sh
+++ Src/bench/C/desc/makefile-win.sh
@@ -2,34 +2,27 @@ MTL=MkTypLib.exe
 CPP= $cc
 RSC= $rc
 
-OUTDIR=.\$platform
-INTDIR=.\$platform
-
-ALL : $(OUTDIR)-ise_desc.dll 
-
-$(OUTDIR) : 
-    if not exist $(OUTDIR)\nul mkdir $(OUTDIR)
+ALL : ise_desc.dll 
 
 MTL_PROJ=-nologo -D "NDEBUG" -win32 
-CPP_PROJ=-nologo -ML -W3 -GX -YX -O2 -I "..\..\.."\
+CPP_PROJ=-nologo -ML -W3 -GX -YX -O2 -I.. \
  -D "NDEBUG" -D "WIN32" -D "_WINDOWS" -D "-P" \
  -Fo$(INTDIR)- -c 
-CPP_OBJS=.\w32msc-
 
 LINK32=link.exe
-LINK32_FLAGS=kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib	\
- advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib -NOLOGO	\
- -SUBSYSTEM:windows -DLL -INCREMENTAL:no -PDB:$(OUTDIR)\"ise_desc.pdb" -MACHINE:I386	\
- -OUT:$(OUTDIR)\"ise_desc.dll" -IMPLIB:$(OUTDIR)\"ise_desc.lib" 
+LINK32_FLAGS= \
+	kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib	\
+	advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib -NOLOGO	\
+	-SUBSYSTEM:windows -DLL -INCREMENTAL:no -OUT:ise_desc.dll -IMPLIB:ise_desc.lib 
 DEF_FILE=
-LINK32_OBJS= $(INTDIR)\ise_desc.obj
+LINK32_OBJS= ise_desc.obj
 
-$(OUTDIR)\ise_desc.dll : $(OUTDIR)  $(DEF_FILE) $(LINK32_OBJS)
+ise_desc.dll : $(DEF_FILE) $(LINK32_OBJS)
     $(LINK32) @<<
   $(LINK32_FLAGS) $(LINK32_OBJS)
 <<
 
-.c{$(CPP_OBJS)}.obj:
+.c$(CPP_OBJS)}.obj:
    $(CPP) $(CPP_PROJ) $<  
 
 # Begin Source File
@@ -37,6 +30,6 @@ $(OUTDIR)\ise_desc.dll : $(OUTDIR)  $(DEF_FILE) $(LINK32_OBJS)
 SOURCE=.\ise_desc.c
 DEP_DESC_=.\ise_desc.h
 
-$(INTDIR)\ise_desc.obj :  $(SOURCE)  $(DEP_DESC_) $(INTDIR)
-
+ise_desc.obj :  $(SOURCE)  $(DEP_DESC_)
+	$(CPP) $(CPP_PROJ) ise_desc.c
 

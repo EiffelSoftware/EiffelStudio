d33f6d09287b948a365b1eb7fd410d8bfe0a4d8c
Emmanuel Stapf <manus@eiffel.com>
1997-12-08 06:14:28 +0000

    Added the new class to find a text in a RICH_EDIT control. Added the constants and the *.h files
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@8756 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/library/wel/spec/windows/include/wel_find_argument.h
@@ -0,0 +1,39 @@
+/*
+ * WEL_FIND_ARGUMENT.H
+ */
+
+#ifndef __WEL_FIND_ARGUMENT__
+#define __WEL_FIND_ARGUMENT__
+
+#ifndef __WEL__
+#	include <wel.h>
+#endif
+
+#ifndef __WEL_RICHEDIT__
+#   include <redit.h>
+#endif
+
+#define cwel_findargument_set_range(_ptr_, _value_) (((FINDTEXTEX *) _ptr_)->chrg = *((CHARRANGE*) (_value_)))
+#define cwel_findargument_set_string_to_find(_ptr_, _value_) (((FINDTEXTEX *) _ptr_)->lpstrText = (LPSTR) (_value_))
+#define cwel_findargument_initialize_range_out_min(_ptr_) (((FINDTEXTEX *) _ptr_)->chrgText.cpMin = (LONG)0)
+#define cwel_findargument_initialize_range_out_max(_ptr_) (((FINDTEXTEX *) _ptr_)->chrgText.cpMax = (LONG)0)
+
+#define cwel_findargument_get_range(_ptr_) (&(((FINDTEXTEX *) _ptr_)->chrg))
+#define cwel_findargument_get_string_to_find(_ptr_) ((((FINDTEXTEX *) _ptr_)->lpstrText))
+#define cwel_findargument_get_range_out(_ptr_) (&(((FINDTEXTEX *) _ptr_)->chrgText))
+
+#endif /* __WEL_FIND_ARGUMENT__ */
+
+/*
+--|-------------------------------------------------------------------------
+--| Windows Eiffel Library: library of reusable components for ISE Eiffel.
+--| Copyright (C) 1995-1997, Interactive Software Engineering, Inc.
+--| All rights reserved. Duplication and distribution prohibited.
+--|
+--| 270 Storke Road, ISE Building, second floor, Goleta, CA 93117 USA
+--| Telephone 805-685-1006
+--| Fax 805-685-6869
+--| Information e-mail <info@eiffel.com>
+--| Customer support e-mail <support@eiffel.com>
+--|-------------------------------------------------------------------------
+*/

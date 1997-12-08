4089e5df363f0aeeb5612dcaae1fba26720a399f
Emmanuel Stapf <manus@eiffel.com>
1997-12-08 06:48:13 +0000

    Cosmetics
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@8759 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_threads.c
+++ Src/bench/C/run-time/eif_threads.c
@@ -359,11 +359,11 @@ rt_public void eif_thr_exit(void)
 #endif
 	EIF_MUTEX_UNLOCK(eif_thr_context->children_mutex, "Unlock parent mutex");
 
-	reclaim ();						/* Free all allocated memory chunks */
+	reclaim ();							/* Free all allocated memory chunks */
 	eif_free (eif_thr_context->tid);	/* Thread id of the current thread */
 	eif_free (eif_thr_context);			/* Thread context passed by parent */
 	eif_free (eif_globals);				/* Global variables specific to the current
-									 * thread of the run-time */
+										 * thread of the run-time */
 
 	EIF_THR_EXIT(0);
 	EIF_END_GET_CONTEXT

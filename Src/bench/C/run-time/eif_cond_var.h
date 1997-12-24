56428becc2c7395396835b85a4ad63fab7adf463
Jocelyn Fiat <jfiat@eiffel.com>
1997-12-24 19:39:08 +0000

    New file for the condition variables (windows only).
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@8917 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/bench/C/run-time/eif_cond_var.h
@@ -0,0 +1,42 @@
+#ifndef _eif_cond_var_h_
+#define _eif_cond_var_h_
+
+
+#include <windows.h>
+#include <process.h>
+
+typedef struct
+{
+  long waiters_;
+  /* Number of waiting threads. */
+
+  CRITICAL_SECTION  waiters_lock_;
+  /* Serialize access to the waiters count. */
+
+  HANDLE sema_;
+  /* Semaphore used to queue up threads waiting */
+  /* for the condition to become signaled.      */
+
+  HANDLE waiters_done_;
+  /* An auto reset event used by the broadcast/signal */
+  /* thread to wait for all the waiting thread(s) to  */
+  /* wake up and be released from the semaphore.      */
+
+  size_t was_broadcast_;
+  /* Keeps track of whether we were broadcasting      */
+  /* or signaling.  This allows us to optimize the    */
+  /* code if we're just signaling.                    */
+} pthread_cond_t;
+
+
+/* Use a Win32 Mutex type for the POSIX pthread_mutex_t; */
+typedef HANDLE pthread_mutex_t;
+
+typedef void pthread_condattr_t ;
+extern int pthread_cond_init (pthread_cond_t *,const pthread_condattr_t * ) ;
+extern int pthread_cond_wait (pthread_cond_t *, pthread_mutex_t );
+extern int pthread_cond_broadcast (pthread_cond_t *);
+extern int pthread_cond_signal (pthread_cond_t *cv);
+extern int pthread_cond_destroy (pthread_cond_t *cv) ;
+
+#endif  /* _eif_cond_var_h_ */

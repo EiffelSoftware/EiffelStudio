a604b2f399932edbb9182d9f5ea92c7280bc93ff
Emmanuel Stapf <manus@eiffel.com>
1998-01-11 18:59:24 +0000

    Changed the way to compute the percentage time in the profiler.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@8931 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_option.h
+++ Src/bench/C/run-time/eif_option.h
@@ -85,6 +85,14 @@ extern int is_debug(int st_type, char *key);		/* Debug level query */
 
 extern EIF_INTEGER prof_enabled;	  /* Is the Eiffel profiler on */
 
+#ifdef HAS_GETRUSAGE	/* Variable to get the total execution time */
+extern struct prof_rusage init_date;
+#elif defined(HAS_TIMES)
+extern double init_date;
+#elif defined(EIF_WIN32)
+extern SYSTEMTIME init_date;
+#endif /* HAS_GERUSAGE */
+
 /* #define prof_enabled    EIF_TEST(prof_enabled & IN_ACE_FILE) */      /* Has the profiler been enabled in the ACE file? */
 #define prof_recording	EIF_TEST(prof_enabled & PROF_RECORDING)   /* Is the profile currently recording? */
 

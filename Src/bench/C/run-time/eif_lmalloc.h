a437432d3f9cf03806cc51090cee9b348613f153
davids <davids@eiffel.com>
1998-02-16 00:38:46 +0000

    VMS port: add prototypes for eif_calloc, eif_realloc, eif_free
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@9208 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_lmalloc.h
+++ Src/bench/C/run-time/eif_lmalloc.h
@@ -16,7 +16,10 @@
 extern "C" {
 #endif
 
-extern Malloc_t eif_malloc(register unsigned int nbytes);
+extern Malloc_t eif_malloc (register unsigned int nbytes);
+extern Malloc_t eif_calloc (unsigned int nelem, unsigned int elsize) ;
+extern Malloc_t eif_realloc (void *ptr, unsigned int nbytes) ;
+extern void eif_free (void *ptr) ;
 
 #ifdef __cplusplus
 }

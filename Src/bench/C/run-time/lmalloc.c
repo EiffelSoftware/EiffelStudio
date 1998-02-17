0375a0de181706843872dc77e371ebd2679c9488
davids <davids@eiffel.com>
1998-02-17 00:49:39 +0000

    VMS port: include VMS-specific runtime functions
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@9219 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/lmalloc.c
+++ Src/bench/C/run-time/lmalloc.c
@@ -18,6 +18,12 @@
   forget to remove it in the delivery to HP (the HP's engineers have their
   own malloc, ... we do not have, so we call our malloc in the C library).
   Manuelt.
+
+  Additionally, this file contains some VMS platform specific functions.
+  They are here (rather than in misc.c) because this module can be 
+  included in a non-Eiffel application (ebench daemon) without lots of
+  side effects.
+  David Schwartz
 */
 
 #include "eif_config.h"
@@ -162,3 +168,134 @@ void eif_free(register void *ptr)
 #endif /* EIF_THREADS */	
 }
 
+
+
+
+
+#ifdef EIF_VMS
+
+#ifdef EIF_VMS_OLD
+/*	**********************************************	    */
+/*	***   VMS specific function definitions.   ***	    */
+/*	**********************************************	    */
+
+#include <starlet.h>		/* vms system services */
+#include <descrip.h>		/* descriptor data structures */
+
+/* VMS version of "putenv" (note - VMS V7 onward has a putenv (and a	*/
+/* setenv as well); I'm not sure precisely what they do (that is, how	*/
+/* they are is implemented).  This one creates a logical name		*/
+/* definition in the PROCESS table (so that it's private to the current	*/
+/* process and any subprocesses that are subsequently created) in USER	*/
+/* mode (so that it doesn't outlive the currently running image).  This	*/
+/* behavior is more akin to unix putenv, which doesn't permit a		*/
+/* subprocess to modify a parent's environment.  This routine was	*/
+/* pinched from the C/ipc/ipcshared/ipcvms.c version.			*/
+
+#include lnmdef				/* sys$crelnm, etc. LNM$ symbols */
+
+rt_public int eif_rt_putenv (const char *envstring)
+{
+    VMS_STS st;
+    char *eq;
+    DX_BLD(nam_d,0,0);
+    const $DESCRIPTOR(tabnam_d, "LNM$PROCESS");
+    ITEMLIST lnm_list = { ITEM(LNM$_STRING, NULL, 0, NULL), ITEMLIST_END };
+
+    /* the argument must be a string of the form <name>=<value>		*/
+    eq = strchr(envstring, '=');	/* find the =			*/
+    if (!eq) return -1;			/* argument is invalid		*/
+    DXPTR(nam_d) = (char*)envstring;	/* name is part up to (but not	*/
+    DXLEN(nam_d) = eq - envstring;	/* including) the "="		*/
+    lnm_list[0].bufadr = ++eq;		/* value is the rest following	*/
+    lnm_list[0].buflen = strlen(eq);	/* the "="			*/
+    st = sys$crelnm(0, (void*)&tabnam_d, &nam_d, 0, &lnm_list);
+    if (VMS_FAILURE(st)) {
+#ifdef DEBUG
+	printerr(EVMSERR, "eif_rt_putenv: error %%x%x from sys$crelnm(%s)\n-- %s",
+		st, envstring, strerror(EVMSERR,st));
+#endif /* DEBUG */
+	return -1;
+    }
+    return 0;
+} /* end putenv() */
+
+
+rt_public int eif_rt_setenv(const char *name, const char *val, int overwrite) {
+    VMS_STS st;
+    DX_BLD(nam_d,0,0);
+    const $DESCRIPTOR(tabnam_d, "LNM$PROCESS");
+    ITEMLIST lnm_list = { ITEM(LNM$_STRING, NULL, 0, NULL), ITEMLIST_END };
+
+    /* **FIXME** - dss --- don't create name if overwrite == 0 and already exists */
+    DXPTR(nam_d) = (char*)name;
+    DXLEN(nam_d) = strlen(name);
+    lnm_list[0].bufadr = (char*)val;
+    lnm_list[0].buflen = strlen(val);
+    st = sys$crelnm(0, (void*)&tabnam_d, &nam_d, 0, &lnm_list);
+#ifdef DEBUG
+    if (VMS_FAILURE(st)) {
+	printerr(EVMSERR, "eif_rt_setenv: error %%x%x from sys$crelnm(%s)=%s\n-- %s",
+		st, name,val, strerror(EVMSERR,st));
+	return -1;
+    }
+#endif /* DEBUG */
+    return ( st & 1) - 1;
+} /* end setenv() */
+
+
+int eif_rt_unlink (const char *name)
+{
+    int err;
+    err = remove(name);
+    return err;
+}
+
+
+#else  /* EIF_VMS_OLD */
+rt_public int eif_rt_putenv (const char *envstring) {  
+    extern int DECC$PUTENV (const char *envstring) ;
+    return DECC$PUTENV (envstring);  
+}
+
+rt_public int eif_rt_setenv(const char *name, const char *val, int overwrite) {  
+    extern int DECC$SETENV (const char *name, const char *val, int overwrite) ;
+    return DECC$SETENV (name, val, overwrite);  
+}
+
+rt_public int eif_rt_unlink (const char *name) {
+    extern int DECC$UNLINK ( const char*name) ;
+    return DECC$UNLINK (name);  
+}
+
+#endif  /* EIF_VMS_OLD */
+
+#ifdef TEST
+#include <stdio.h>
+#include <ssdef.h>
+#include <errno.h>
+#include <starlet.h>		/* for sys$getmsg() */
+
+main () {
+	char			buff[256];
+	int			cond;
+	int			flags = 0xf;
+	char			mesg[133];
+	$DESCRIPTOR(message_text,mesg);
+	int			mesglen;
+	register		status;
+
+	printf("\n\nEnter putenv string as   name=value :  ");
+	(void)fflush(stdout);
+	(void)gets(buff);
+	if (buff[0] != '\0') {
+		cond = putenv(buff);		
+		printf("Return value is %d.\n",(int)cond);
+		status = SYS$GETMSG(cond,&mesglen,&message_text,flags,0);
+		printf("\n<%d> %s\n",status,mesg);
+/*		lib$stop(cond); */
+	}
+}
+#endif	/* TEST */
+
+#endif /* EIF_VMS */

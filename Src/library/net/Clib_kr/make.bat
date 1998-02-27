07b38f1e2d99d60595ff1bdb4aa777092bc92e8e
Generic Eiffel Software User <svn@eiffel.com>
1998-02-27 22:58:51 +0000

    Removed Clib_kr
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@9264 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/net/Clib_kr/make.bat
+++
@@ -1,9 +0,0 @@
-set include=c:\bc45\include
-set library=c:\bc45\lib
-C:\bc45\bin\make -f makefile.b
-set include=d:\msvc20\include
-set library=d:\msvc20\lib
-nmake -f makefile.m
-set include=C:\watcom\h;c:\watcom\h\nt
-set library=c:\watcom\lib
-wmake -f makefile.w

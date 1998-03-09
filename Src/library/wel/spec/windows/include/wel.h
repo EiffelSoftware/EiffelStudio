ec44cdc0c8d59881c9b2f9ec3f325665e65dddee
Emmanuel Stapf <manus@eiffel.com>
1998-03-09 17:40:22 +0000

    Changed the c_flag_set function
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@9288 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/wel/spec/windows/include/wel.h
+++ Src/library/wel/spec/windows/include/wel.h
@@ -30,7 +30,7 @@
 
 #define c_set_flag(_flags_, _mask_) ((((EIF_INTEGER) _flags_) | ((EIF_INTEGER) _mask_)))
 #define c_clear_flag(_flags_, _mask_) ((((EIF_INTEGER) _flags_) & ((EIF_INTEGER) ~_mask_)))
-#define c_flag_set(_flags_, _mask_) (((((EIF_INTEGER) _flags_) & ((EIF_INTEGER) _mask_)) ? 1 : 0))
+#define c_flag_set(_flags_, _mask_) ((((((EIF_INTEGER) _flags_) & ((EIF_INTEGER) _mask_)) == (EIF_INTEGER) _mask_ ) ? 1 : 0))
 
 #define c_mouse_message_x(_lparam_) ((short) LOWORD(_lparam_))
 #define c_mouse_message_y(_lparam_) ((short) HIWORD(_lparam_))

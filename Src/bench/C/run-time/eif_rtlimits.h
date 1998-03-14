054c6079449f0e502cf7e9b20ec54cd748a0d5e3
Emmanuel Stapf <manus@eiffel.com>
1998-03-14 21:40:42 +0000

    Increased the number of generics from 4 to 8
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@9642 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_rtlimits.h
+++ Src/bench/C/run-time/eif_rtlimits.h
@@ -13,7 +13,7 @@
 #ifndef _eif_rtlimits_h_
 #define _eif_rtlimits_h_
 
-#define MAX_GENERICS      4		/* Maximum number of generic parameters */
+#define MAX_GENERICS      8		/* Maximum number of generic parameters */
 
 #define IDLENGTH        512		/* Maximum length for identifiers */
 #define STRINGLENGTH    5120	/* Maximum manifest string length (parsing) */

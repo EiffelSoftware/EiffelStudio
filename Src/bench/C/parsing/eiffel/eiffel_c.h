909f58f571c1dc94f60f7fbd279e1a39a0472e25
Emmanuel Stapf <manus@eiffel.com>
1998-03-25 00:52:36 +0000

    Cosmetics Added a parameter to create_generic in order to pass the creation routines list for a formal generic parameters
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@9782 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/parsing/eiffel/eiffel_c.h
+++ Src/bench/C/parsing/eiffel/eiffel_c.h
@@ -15,27 +15,27 @@
 /*
  * Eiffel object (AS nodes) creation 
  */
-extern char		*create_class(char *cl_name, char deferred, char expanded, char separate, char *indexes, char *generics, char *obsolete, char *parents, char *creators, char *features, char *invariant, char *cl_list, int end_pos),
-				*create_id(char *s),
-				*create_char(char *a_char),
-				*create_bool(int b),
-				*create_int(char *an_int, int sign),
-				*create_real(char *a_real, int sign),
-				*create_string(char *s),
-				*create_type_class(char *id, char *generics),
-				*create_feature_as(char *names, char *declaration, int start_pos, int end_pos),
-				*create_exp_class_type(char *id, char *generics),
-				*create_separate_class_type(char *id, char *generics),
-				*create_feature_name(int dyn_type, char *name, char is_frozen),
-				*create_fclause_as(char *clients, char *feature_list, int start_pos),
-				*create_routine_as(char *obsolete, int start_pos, char *precs, char *locals, char *body, char *posts, char *rescue);
+extern char *create_class(char *, char, char, char, char *, char *, char *, char *, char *, char *, char *, char *, int);
+extern char *create_id(char *);
+extern char *create_char(char *);
+extern char *create_bool(int);
+extern char *create_int(char *, int);
+extern char *create_real(char *, int);
+extern char *create_string(char *);
+extern char *create_type_class(char *, char *);
+extern char *create_feature_as(char *, char *, int, int);
+extern char *create_exp_class_type(char *, char *);
+extern char *create_separate_class_type(char *, char *);
+extern char *create_feature_name(int, char *, char);
+extern char *create_fclause_as(char *, char *, int);
+extern char *create_routine_as(char *, int, char *, char *, char *, char *, char *);
 
 /*
  * Formal generic parameter managment
  */
 
-extern void		generic_inc(void);
-extern char		*create_generic(char *gen_name, char *constrained);
+extern void generic_inc(void);
+extern char *create_generic (char *, char *, char *);
 
 /*
  * Miscellaneous

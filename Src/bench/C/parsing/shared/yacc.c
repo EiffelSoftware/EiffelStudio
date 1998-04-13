b975faa9c65fd2c5604e1cdc2394125e2a1a0fe9
Emmanuel Stapf <manus@eiffel.com>
1998-04-13 23:46:50 +0000

    Cosmetics
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@9869 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/parsing/shared/yacc.c
+++ Src/bench/C/parsing/shared/yacc.c
@@ -400,13 +400,13 @@ char is_feature_as(char *obj)
 {
 	/* Is `obj' an instance of FEATURE_AS ? */
 
-	return Dtype(obj) == feature_as_dtype ? '\01' : '\0';
+	return (Dtype(obj) == feature_as_dtype) ? '\01' : '\0';
 }
 
 char is_invariant_as(char *obj)
 {
 	/* Is `obj' an instance of INVARIANT_AS ? */
 
-    return Dtype(obj) == invariant_as_dtype ? '\01' : '\0';
+    return (Dtype(obj) == invariant_as_dtype) ? '\01' : '\0';
 }
 

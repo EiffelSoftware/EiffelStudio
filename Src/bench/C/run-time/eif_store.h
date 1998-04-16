19412efc7a99f876efc770a18e28274c546a669c
Emmanuel Stapf <manus@eiffel.com>
1998-04-16 23:31:02 +0000

    Changed the signature of function rt_init_store (we will need to update it in EiffelNet as well) Added the declaration of the make_header_func
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@9902 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_store.h
+++ Src/bench/C/run-time/eif_store.h
@@ -70,14 +70,22 @@ extern int end_of_buffer;
 /* compression */
 extern char * cmps_general_buffer;
 
+extern void (*make_header_func)(void);
+
 extern void (*store_write_func)(void);
 extern void store_write(void);
 
 extern int (*char_write_func)(char *, int);
 extern int char_write(char *pointer, int size);
 
-extern void rt_init_store(void (*store_function) (void), int (*char_write_function)
-(char *, int), int buf_size);
+extern void rt_init_store(
+	void (*store_function) (void),
+	int (*char_write_function)(char *, int),
+	void (*flush_buffer_function) (void),
+	void (*st_write_function) (char *),
+	void (*make_header_function) (void),
+	int accounting_type,
+	int buf_size);
 extern void rt_reset_store(void);
 
 extern long get_offset(uint32 o_type, uint32 attrib_num);          /* get offset of attrib in object*/

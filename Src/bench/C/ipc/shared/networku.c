c697e0e6f572a2c81b3dfa549e5d159669757d98
Emmanuel Stapf <manus@eiffel.com>
1998-04-16 22:52:35 +0000

    Inline some macros by using macros
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@9896 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/ipc/shared/networku.c
+++ Src/bench/C/ipc/shared/networku.c
@@ -7,27 +7,29 @@
 	See UNIX man pages for the details of the functions and their purpose.
 */
 
-unsigned short ntohs(unsigned short x)
-{
-	return (((x & 0x00ff) << 8) |
-			((x & 0xff00) >> 8));
+#include "networku.h"
+
+#define NTOHL(x)	(((x & 0x000000ffU) << 24) | \
+					((x & 0x0000ff00U) <<  8) |	\
+					((x & 0x00ff0000U) >>  8) |	\
+					((x & 0xff000000U) >> 24))
+
+#define NTOHS(x)	(((x & 0x00ff) << 8) |	\
+					((x & 0xff00) >> 8))	
+
+unsigned short ntohs(unsigned short x) {
+	return NTOHS(x);
 }
 
-long ntohl(long x)
-{
-	return (((x & 0x000000ffU) << 24) |
-			((x & 0x0000ff00U) <<  8) |
-			((x & 0x00ff0000U) >>  8) |
-			((x & 0xff000000U) >> 24));
+unsigned short htons(short x) {
+	return NTOHS(x);
 }
 
-unsigned short htons(short x)
-{
-	return ntohs(x);
+long ntohl(long x) {
+	return NTOHL(x);
 }
 
-
-long htonl(long x)
-{
-	return ntohl(x);
+long htonl(long x) {
+	return NTOHL(x);
 }
+

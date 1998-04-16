7fefbbe7dd564aa222faead49629242bae69368f
Emmanuel Stapf <manus@eiffel.com>
1998-04-16 23:12:07 +0000

    Changed the status of gc_stat, gc_info, gc_infod from extern to RT_LNK since they are exported in a DLL
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@9901 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_memory.h
+++ Src/bench/C/run-time/eif_memory.h
@@ -29,9 +29,9 @@ RT_LNK void mem_pset(long int value);
 RT_LNK void mem_stat(long int type);
 RT_LNK long mem_info(long int field);
 RT_LNK void gc_mon(char flag);
-extern void gc_stat(long int type);	/* Initialize the GC statistics buffer */
-extern long gc_info(long int field);
-extern double gc_infod(long int field);
+RT_LNK void gc_stat(long int type);	/* Initialize the GC statistics buffer */
+RT_LNK long gc_info(long int field);
+RT_LNK double gc_infod(long int field);
 RT_LNK char gc_ison(void);
 RT_LNK void eif_set_max_mem(EIF_INTEGER); /* Set max memory RT can allocate */
 RT_LNK EIF_INTEGER eif_get_max_mem(void); /* Return max_mem */

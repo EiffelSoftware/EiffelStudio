6f4c73d87719049d5499f31e05abcb6c676c7cca
Emmanuel Stapf <manus@eiffel.com>
1998-04-17 00:10:46 +0000

    Added a header file for networku.c
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@9909 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/bench/C/ipc/shared/networku.h
@@ -0,0 +1,9 @@
+#ifndef _networku_h_
+#define _networku_h_
+
+extern unsigned short ntohs(unsigned short x);
+extern unsigned short htons(short x);
+extern long ntohl(long x);
+extern long htonl(long x);
+
+#endif

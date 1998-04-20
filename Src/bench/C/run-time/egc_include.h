6bb3003a0101ce3161901eddaf19580bcfff7a39
Jocelyn Fiat <jfiat@eiffel.com>
1998-04-20 20:43:18 +0000

    This file is use for the generated C code, it is not part of the run-time.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@9911 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/bench/C/run-time/egc_include.h
@@ -0,0 +1,40 @@
+#ifndef _EGC_INCLUDE_H_
+#define _EGC_INCLUDE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "eif_struct.h"
+
+	extern struct ctable egc_ce_type_init ;
+	extern struct ctable egc_ce_gtype_init ;
+	extern struct cnode egc_fsystem_init [] ;
+	extern struct conform *egc_fco_table_init [] ;
+	extern void egc_system_mod_init_init (void) ;
+
+#ifdef WORKBENCH
+	extern fnptr egc_frozen_init [];
+	extern int egc_fpatidtab_init [] ;
+	extern struct eif_opt egc_foption_init [] ;
+	extern fnptr **egc_address_table_init;
+	extern uint32 egc_fdispatch_init [] ;
+	extern struct p_interface egc_fpattern_init [] ;
+	
+	extern void egc_einit_init (void);
+	extern void egc_tabinit_init (void) ;
+	
+	extern int32 *egc_fcall_init [] ;
+	extern struct rout_info egc_forg_table_init [] ;
+	extern int16 egc_fdtypes_init [] ;
+#else
+	extern struct ctable egc_ce_rname_init[];
+	extern long egc_fnbref_init[];
+	extern long egc_fsize_init[]; 
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

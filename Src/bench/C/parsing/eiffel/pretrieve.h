e382e43770a511521db433306ee276f47cc13607
Emmanuel Stapf <manus@eiffel.com>
1998-05-13 20:38:36 +0000

    Added some header files to make it more C ansi compliant
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@10101 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/bench/C/parsing/eiffel/pretrieve.h
@@ -0,0 +1,8 @@
+#ifndef _pretrieve_h_
+#define _pretrieve_h_
+
+rt_public char *partial_retrieve (EIF_INTEGER f_desc, size_t file_size, long position, long nb_obj);
+rt_public char *retrieve_all(EIF_INTEGER f_desc, size_t file_size, long position);
+rt_public void parsing_retrieve_initialize (void);
+
+#endif
---
+++ Src/bench/C/parsing/eiffel/pstore.h
@@ -0,0 +1,9 @@
+#ifndef _pstore_h_
+#define _pstore_h_
+
+#include "eif_struct.h"
+
+rt_public long store_append (EIF_INTEGER f_desc, char *o, fnptr mid, fnptr nid, char *s);
+rt_public void parsing_store_initialize (void);
+
+#endif

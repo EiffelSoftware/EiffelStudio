8c46e00e257200c6ab8a060f57172591dfef0120
Emmanuel Stapf <manus@eiffel.com>
1998-05-14 05:55:43 +0000

    Cosmetics Removed the regeneration of parser.c at each C compilation
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@10158 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/parsing/eiffel/makefile-win.sh
+++ Src/bench/C/parsing/eiffel/makefile-win.sh
@@ -33,54 +33,52 @@ all:: weiffel.lib
 weiffel.lib: $(WOBJECTS)
 	$link_wline
 
-parser.c : parser.cwn
+parser.c: parser.cwn
+	copy parser.hwn parser.h
 	copy parser.cwn parser.c
 
-parser.h : parser.hwn
-	copy parser.hwn parser.h
-
-wpstore.c : pstore.c
+wpstore.c: pstore.c
 	copy pstore.c wpstore.c
 
-wpstore.obj : wpstore.c
+wpstore.obj: wpstore.c
 	$(CC) -c -DWORKBENCH $(JCFLAGS) wpstore.c
 
-parser.h : parser.c
+parser.h: parser.c
 
-lexic.obj : parser.h
+lexic.obj: parser.h
 
-keyword.obj : parser.h
-eif_err.obj : parser.h
+keyword.obj: parser.h
+eif_err.obj: parser.h
 
-eiffel_c.obj : eiffel_c.c
-eiffel_c.obj : eiffel_c.h
-eif_err.obj : eif_err.c
-encode.obj : encode.c
-pretrieve.obj : pretrieve.c
-date.obj : date.c
-keyword.obj : keyword.c
-byte.obj : byte.c
-offset.obj : offset.c
-parser.obj : eiffel_c.h
-parser.obj : parser.c
-infix.obj : infix.c
-prefix.obj : prefix.c
-pstore.obj : pstore.c
-eiffel_c.obj : eiffel_c.c
-eiffel_c.obj : eiffel_c.h
-eif_err.obj : eif_err.c
-encode.obj : encode.c
-pretrieve.obj : pretrieve.c
-date.obj : date.c
-keyword.obj : keyword.c
-byte.obj : byte.c
-offset.obj : offset.c
-parser.obj : eiffel_c.h
-parser.obj : parser.y
-infix.obj : infix.c
-prefix.obj : prefix.c
-pstore.obj : pstore.c
-lexic.obj : lexic.c
-parser.obj : eiffel_c.h
-parser.obj : parser.c
+eiffel_c.obj: eiffel_c.c
+eiffel_c.obj: eiffel_c.h
+eif_err.obj: eif_err.c
+encode.obj: encode.c
+pretrieve.obj: pretrieve.c
+date.obj: date.c
+keyword.obj: keyword.c
+byte.obj: byte.c
+offset.obj: offset.c
+parser.obj: eiffel_c.h
+parser.obj: parser.c
+infix.obj: infix.c
+prefix.obj: prefix.c
+pstore.obj: pstore.c
+eiffel_c.obj: eiffel_c.c
+eiffel_c.obj: eiffel_c.h
+eif_err.obj: eif_err.c
+encode.obj: encode.c
+pretrieve.obj: pretrieve.c
+date.obj: date.c
+keyword.obj: keyword.c
+byte.obj: byte.c
+offset.obj: offset.c
+parser.obj: eiffel_c.h
+parser.obj: parser.y
+infix.obj: infix.c
+prefix.obj: prefix.c
+pstore.obj: pstore.c
+lexic.obj: lexic.c
+parser.obj: eiffel_c.h
+parser.obj: parser.c
 

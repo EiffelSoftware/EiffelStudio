2b46f029dda0dc882c3a0483b412a9f75ee1f80e
Emmanuel Stapf <manus@eiffel.com>
1998-05-14 18:35:19 +0000

    First implementation of the storing mechanism and retrieving mechanism using s STREAM
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@10159 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/parsing/eiffel/pretrieve.c
+++ Src/bench/C/parsing/eiffel/pretrieve.c
@@ -25,17 +25,44 @@
 #include "unistd.h"
 #endif
 
-char *partial_retrieve(EIF_INTEGER f_desc, long position, long nb_obj)
+/* Public features */
+/* rt_public char *partial_retrieve (EIF_INTEGER f_desc, long position, long nb_obj);
+rt_public char *retrieve_all(EIF_INTEGER f_desc, long position); */
+rt_public char *partial_retrieve (EIF_INTEGER f_desc, size_t file_size, long position, long nb_obj);
+rt_public char *retrieve_all(EIF_INTEGER f_desc, size_t file_size, long position);
+
+/* Private features */
+rt_private void stream_buffer_initialization (EIF_INTEGER file_desc, size_t file_size, long position);
+rt_private int parsing_char_read (char *, int);
+rt_private int parsing_stream_read (char *pointer, int size);	/* store in stream */
+rt_private char *parsing_stream_buffer;
+rt_private int parsing_stream_position;
+rt_private int parsing_stream_size;
+rt_private int file_descriptor;
+rt_private int file_position;
+
+/* Implementation */
+
+/* rt_public char *partial_retrieve(EIF_INTEGER f_desc, long position, long nb_obj) */
+rt_public char *partial_retrieve(EIF_INTEGER f_desc, size_t file_size, long position, long nb_obj)
 {
 	EIF_GET_CONTEXT
 	/* Return `nb_obj' retrieved in file `file_ptr' read at `position'. */
 	char *result;
 
-	rt_init_retrieve(retrieve_read_with_compression, char_read, 0);
+/*	parsing_stream_position = 0; */
+	file_descriptor = (int) f_desc;
+	if (lseek (f_desc, position, SEEK_SET) == -1)
+		esys ();
+
+	rt_init_retrieve(
+		retrieve_read_with_compression,
+		parsing_char_read,
+		262144);
 
 	rt_kind = '\0';
-	r_fides = (int)f_desc;
-	if (lseek(r_fides, position, SEEK_SET) == -1) esys();	/* bail out */
+/* 	stream_buffer_initialization (f_desc, file_size, position); */
+
 	allocate_gen_buffer();
 	result = rt_nmake(nb_obj);			/* Retrieve `nb_obj' objects */
 	ht_free(rt_table);                  /* Free hash table descriptor */
@@ -47,18 +74,27 @@ char *partial_retrieve(EIF_INTEGER f_desc, long position, long nb_obj)
 	EIF_END_GET_CONTEXT
 }
 
-char *retrieve_all(EIF_INTEGER f_desc, long position)
+/* rt_public char *retrieve_all(EIF_INTEGER f_desc, long position) */
+rt_public char *retrieve_all(EIF_INTEGER f_desc, size_t file_size, long position)
 {
 	EIF_GET_CONTEXT
 	/* Return object graph retrieved in file `file_ptr' read at
 	 * position. */
 	char *result;
 
-	rt_init_retrieve(retrieve_read_with_compression, char_read, 0);
+/*	parsing_stream_position = 0; */
+	file_descriptor = (int)f_desc;
+	if (lseek(file_descriptor, position, SEEK_SET) == -1)
+		esys();   /* bail out */
+
+	rt_init_retrieve(
+		retrieve_read_with_compression,
+		parsing_char_read,
+		262144L);
 
 	rt_kind = '\0';
-	r_fides = (int)f_desc;
-	if (lseek(r_fides, position, SEEK_SET) == -1) esys();	/* bail out */
+/*	stream_buffer_initialization (f_desc, file_size, position); */
+
 	allocate_gen_buffer();
 	result = rt_make();
 	ht_free(rt_table);					/* Free hash table descriptor */
@@ -68,4 +104,33 @@ char *retrieve_all(EIF_INTEGER f_desc, long position)
 
 	return result;
 	EIF_END_GET_CONTEXT
+}
+
+rt_private int parsing_char_read (char *pointer, int size)
+{
+	return read(file_descriptor, pointer, size);
+}
+
+int parsing_stream_read(char *pointer, int size)
+{
+	memcpy (pointer, (parsing_stream_buffer + parsing_stream_position), size);
+	parsing_stream_position += size;
+	return size;
+}
+
+rt_private void stream_buffer_initialization (EIF_INTEGER file_desc, size_t file_size, long position) {
+	long number_left = file_size - position;
+	long number_read;
+
+	if (number_left > parsing_stream_size) {
+		parsing_stream_size = number_left; 
+		parsing_stream_buffer = realloc (parsing_stream_buffer, parsing_stream_size);
+	}
+
+	if (lseek (file_desc, position, SEEK_SET) == -1)
+		esys ();
+
+	number_read = read (file_desc, parsing_stream_buffer, number_left);
+	if ((number_read <= 0) || (number_read != number_left))
+			eio ();
 }

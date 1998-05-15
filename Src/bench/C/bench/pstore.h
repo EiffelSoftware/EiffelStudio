62cfb4f6f04096f3603857dda319a546b29fb97e
Emmanuel Stapf <manus@eiffel.com>
1998-05-15 00:48:28 +0000

    Now the storable mechanism is using a buffer before to write on a disk.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@10160 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/bench/pstore.h
+++ Src/bench/C/bench/pstore.h
@@ -4,6 +4,6 @@
 #include "eif_struct.h"
 
 rt_public long store_append (EIF_INTEGER f_desc, char *o, fnptr mid, fnptr nid, char *s);
-rt_public void parsing_store_initialize (void);
+rt_public void parsing_store_initialize (long size);
 
 #endif

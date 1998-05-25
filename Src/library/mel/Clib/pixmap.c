6d227ab42fab76d8210b5155b4a938af65af7cbf
davids <davids@eiffel.com>
1998-05-25 21:18:57 +0000

    VMS port: workaround XReadBitmapFile bug on VMS
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@10250 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/mel/Clib/pixmap.c
+++ Src/library/mel/Clib/pixmap.c
@@ -1,4 +1,11 @@
+/* mel/Clib/pixmap.c */
+/* $Id$ */
+
 #include "mel.h"
+
+#ifdef EIF_VMS
+#include <unistd.h>
+#endif
 
 EIF_POINTER x_read_bitmap_from_file (EIF_POINTER a_screen, char *filename, 
 	EIF_INTEGER *a_status, EIF_INTEGER *a_width, EIF_INTEGER *a_height, 
@@ -19,8 +26,26 @@ EIF_POINTER x_read_bitmap_from_file (EIF_POINTER a_screen, char *filename,
 			&bitmap, &pixmap_x_hot,
 			&pixmap_y_hot);
 
-	*a_status = (EIF_INTEGER) pixmap_status;
+#ifdef EIF_VMS
+	/* workaround bug in VMS implementation of XReadBitmapFile - it may not work with VMS filespecs */
+	if (pixmap_status == BitmapOpenFailed && !access(filename, R_OK)) {
+	    char unix_filename[FILENAME_MAX +1];
+	    const char* tmp = decc$translate_vms (filename);
+	    int err = (int)tmp;
+	    if (err != 0 && err != -1) {	/* if filename translated to unix successfully */
+		strcpy (unix_filename, tmp);	/* save the translated filename */
+		if ( strcmp (filename, unix_filename) )
+		    /* this call should match the one above, except for the unix_filename argument */
+		    pixmap_status = XReadBitmapFile (DisplayOfScreen((Screen *) a_screen), 
+				    RootWindowOfScreen ((Screen *) a_screen),
+				    unix_filename, &pixmap_width, &pixmap_height, 
+				    &bitmap, &pixmap_x_hot,
+				    &pixmap_y_hot);
+	    }
+	}
+#endif /* EIF_VMS */
 
+	*a_status = (EIF_INTEGER) pixmap_status;
 	if (pixmap_status != BitmapSuccess) 
 		return (EIF_POINTER) NULL;
 
@@ -29,5 +54,5 @@ EIF_POINTER x_read_bitmap_from_file (EIF_POINTER a_screen, char *filename,
 	*x1 = (EIF_INTEGER) pixmap_x_hot;
 	*y1 = (EIF_INTEGER) pixmap_y_hot;
 	return (EIF_POINTER) bitmap;
-			
+
 }

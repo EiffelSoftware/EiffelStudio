c459ef1859ac541485375cc7a3292ba2f8b15b15
Jocelyn Fiat <jfiat@eiffel.com>
1998-05-28 21:48:21 +0000

    Corrected a RT_LINK into RT_LNK.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@10258 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_misc.h
+++ Src/bench/C/run-time/eif_misc.h
@@ -41,10 +41,10 @@ RT_LNK EIF_INTEGER eschar_size(void);
 RT_LNK EIF_INTEGER esreal_size(void);
 RT_LNK EIF_INTEGER esint_size(void);
 RT_LNK EIF_INTEGER esdouble_size(void);
-RT_LINK EIF_INTEGER eif_system (char *s);
-RT_LINK EIF_INTEGER eif_putenv (EIF_OBJ v, EIF_OBJ k);
-RT_LINK EIF_OBJ eif_getenv(EIF_OBJ k);
-RT_LINK EIF_INTEGER eif_safe_putenv (EIF_OBJ v, EIF_OBJ k); /* Safe Eiffel putenv dealing only with env. var. */
+RT_LNK EIF_INTEGER eif_system (char *s);
+RT_LNK EIF_INTEGER eif_putenv (EIF_OBJ v, EIF_OBJ k);
+RT_LNK EIF_OBJ eif_getenv(EIF_OBJ k);
+RT_LNK EIF_INTEGER eif_safe_putenv (EIF_OBJ v, EIF_OBJ k); /* Safe Eiffel putenv dealing only with env. var. */
 
 RT_LNK char *arycpy(char *area, EIF_INTEGER i, EIF_INTEGER j, EIF_INTEGER k);
 

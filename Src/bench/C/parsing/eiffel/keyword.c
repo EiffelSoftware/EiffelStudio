b4014979d3e012f0aa7cb98997ef41792d8e3d39
Emmanuel Stapf <manus@eiffel.com>
1998-06-19 05:51:19 +0000

    Introduce the new way how to create eiffel object with the `create' keyword. And also introduce a first version of creation expression.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@10433 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/parsing/eiffel/keyword.c
+++ Src/bench/C/parsing/eiffel/keyword.c
@@ -1,7 +1,5 @@
-/* C code produced by gperf version 1.9.1 (modified by Raphael Manfredi) */
-/* Command-line: gperf -a -p -t -l -k 1,\$,3 eiffel.gperf  */
-
-
+/* C code produced by gperf version 2.5 (GNU C++ version) */
+/* Command-line: gperf -a -p -t -l -k 1,$,3 eiffel.gperf  */
 /* Lex will see the function "in_word_set" which will return a pointer
  * to a token structure. Due to gperf constraints, the structure has to
  * be declared here and in lex and cannot be held in an include file--RAM.
@@ -13,46 +11,45 @@ struct token {
 	int yaccval;	/* Value returned to yacc */
 };
 
+#define TOTAL_KEYWORDS 59
 #define MIN_WORD_LENGTH 2
 #define MAX_WORD_LENGTH 9
 #define MIN_HASH_VALUE 2
-#define MAX_HASH_VALUE 184
-/*
-   58 keywords
-  183 is the maximum key range
-*/
+#define MAX_HASH_VALUE 173
+/* maximum key range = 172, duplicates = 0 */
 
-static int
+static unsigned int
 hash (register const char *str, register int len)
 {
-  static unsigned char hash_table[] =
+  static unsigned char asso_values[] =
     {
-     184, 184, 184, 184, 184, 184, 184, 184, 184, 184,
-     184, 184, 184, 184, 184, 184, 184, 184, 184, 184,
-     184, 184, 184, 184, 184, 184, 184, 184, 184, 184,
-     184, 184, 184, 184, 184, 184, 184, 184, 184, 184,
-     184, 184, 184, 184, 184, 184, 184, 184, 184, 184,
-     184, 184, 184, 184, 184, 184, 184, 184, 184, 184,
-     184, 184, 184, 184, 184, 184, 184, 184, 184, 184,
-     184, 184, 184, 184, 184, 184, 184, 184, 184, 184,
-     184, 184, 184, 184, 184, 184, 184, 184, 184, 184,
-     184, 184, 184, 184, 184, 184, 184,  47,  30,  41,
-      60,   0,  56,  35,   5,   0, 184,   5,   5,   0,
-       5,  22,  30,   0,  20,   0,   6,  37,  35,   0,
-      40,   5, 184, 184, 184, 184, 184, 184,
+     174, 174, 174, 174, 174, 174, 174, 174, 174, 174,
+     174, 174, 174, 174, 174, 174, 174, 174, 174, 174,
+     174, 174, 174, 174, 174, 174, 174, 174, 174, 174,
+     174, 174, 174, 174, 174, 174, 174, 174, 174, 174,
+     174, 174, 174, 174, 174, 174, 174, 174, 174, 174,
+     174, 174, 174, 174, 174, 174, 174, 174, 174, 174,
+     174, 174, 174, 174, 174, 174, 174, 174, 174, 174,
+     174, 174, 174, 174, 174, 174, 174, 174, 174, 174,
+     174, 174, 174, 174, 174, 174, 174, 174, 174, 174,
+     174, 174, 174, 174, 174, 174, 174,  45,  60,  25,
+      55,   0,  15,  20,  15,   0, 174,   5,   5,   5,
+      40,  25,  62,   0,  30,   0,  55,  35,   0,   0,
+      40,   0, 174, 174, 174, 174, 174, 174,
     };
-  register int hval = len ;
+  register int hval = len;
 
   switch (hval)
     {
       default:
       case 3:
-        hval += hash_table[str[2]];
+        hval += asso_values[str[2]];
       case 2:
       case 1:
-        hval += hash_table[str[0]];
+        hval += asso_values[str[0]];
+        break;
     }
-  return hval + hash_table[str[len - 1]] ;
+  return hval + asso_values[str[len - 1]];
 }
 
 struct token *
@@ -61,24 +58,21 @@ in_word_set (register const char *str, register int len)
 
   static unsigned char lengthtable[] =
     {
-      0,  0,  2,  0,  4,  0,  6,  0,  0,  4,  0,  0,  0,  7,
-      4,  4,  0,  6,  7,  8,  3,  0,  0,  0,  0,  0,  6,  7,
-      0,  0,  8,  6,  6,  0,  0,  0,  5,  7,  8,  0,  0,  0,
-      6,  6,  2,  3,  0,  4,  0,  2,  9,  5,  5,  5,  8,  5,
-      5,  0,  2,  9,  3,  4,  6,  0,  0,  0,  5,  4,  7,  0,
-      0,  0,  0,  0,  7,  0,  6,  0,  0,  0,  0,  0,  4,  3,
-      2,  0,  0,  0,  8,  6,  0,  0,  0,  5,  0,  0,  0,  0,
-      8,  0,  0,  5,  0,  8,  0,  8,  0,  0,  0,  0,  7,  0,
-      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  3,  0,  0,
-      0,  0,  0,  0,  5,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  0,  0,  3,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  3,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  8,
+      0,  0,  2,  0,  4,  0,  6,  0,  0,  0,  0,  0,  0,  0,
+      4,  0,  0,  2,  0,  0,  0,  6,  0,  0,  0,  5,  0,  0,
+      0,  0,  0,  6,  0,  8,  0,  5,  6,  7,  0,  0,  5,  6,
+      0,  0,  4,  0,  0,  2,  0,  4,  5,  0,  0,  0,  4,  0,
+      0,  2,  3,  0,  5,  0,  7,  0,  9,  0,  6,  7,  8,  7,
+      8,  0,  0,  8,  0,  5,  6,  7,  0,  0,  0,  0,  2,  8,
+      0,  0,  6,  0,  0,  0,  5,  6,  7,  8,  4,  0,  4,  5,
+      8,  4,  5,  9,  0,  3,  0,  0,  0,  0,  6,  0,  0,  0,
+      0,  3,  0,  0,  0,  7,  0,  0,  0,  0,  0,  6,  0,  8,
+      0,  0,  0,  0,  0,  0,  0,  8,  0,  0,  0,  0,  3,  0,
+      5,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  3,
+      0,  0,  0,  0,  3,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+      0,  0,  0,  0,  0,  3,
     };
-
-
-  static struct token  wordlist[] =
+  static struct token wordlist[] =
     {
       {"",}, {"",}, 
       {"is", 				TE_IS},
@@ -86,99 +80,106 @@ in_word_set (register const char *str, register int len)
       {"else", 			TE_ELSE},
       {"",}, 
       {"ensure", 			TE_ENSURE},
-      {"",}, {"",}, 
-      {"when", 			TE_WHEN},
-      {"",}, {"",}, {"",}, 
-      {"inspect", 		TE_INSPECT},
+      {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, 
       {"like", 			TE_LIKE},
-      {"then", 			TE_THEN},
-      {"",}, 
-      {"select", 			TE_SELECT},
-      {"inherit", 		TE_INHERIT},
-      {"external", 		TE_EXTERNAL},
-      {"not", 			TE_NOT},
-      {"",}, {"",}, {"",}, {"",}, {"",}, 
-      {"rescue", 			TE_RESCUE},
-      {"require", 		TE_REQUIRE},
       {"",}, {"",}, 
-      {"obsolete", 		TE_OBSOLETE},
-      {"rename", 			TE_RENAME},
-      {"result", 			TE_RESULT},
-      {"",}, {"",}, {"",}, 
-      {"retry", 			TE_RETRY},
-      {"implies", 		TE_IMPLIES},
-      {"separate", 		TE_SEPARATE},
-      {"",}, {"",}, {"",}, 
-      {"export", 			TE_EXPORT},
-      {"unique", 			TE_UNIQUE},
-      {"or", 				TE_OR},
-      {"bit", 			TE_BIT},
-      {"",}, 
-      {"true", 			TE_TRUE},
-      {"",}, 
-      {"as", 				TE_AS},
-      {"invariant", 		TE_INVARIANT},
-      {"check", 			TE_CHECK},
-      {"alias", 			TE_ALIAS},
-      {"until", 			TE_UNTIL},
-      {"creation", 		TE_CREATION},
-      {"strip", 			TE_STRIP},
-      {"local", 			TE_LOCAL},
-      {"",}, 
       {"if", 				TE_IF},
-      {"precursor", 		TE_PRECURSOR},
-      {"all", 			TE_ALL},
-      {"loop", 			TE_LOOP},
+      {"",}, {"",}, {"",}, 
       {"elseif", 			TE_ELSEIF},
       {"",}, {"",}, {"",}, 
       {"false", 			TE_FALSE},
-      {"once", 			TE_ONCE},
-      {"variant", 		TE_VARIANT},
       {"",}, {"",}, {"",}, {"",}, {"",}, 
-      {"current", 		TE_CURRENT},
+      {"create", 			TE_CREATION},
       {"",}, 
-      {"prefix", 			TE_PREFIX},
-      {"",}, {"",}, {"",}, {"",}, {"",}, 
-      {"from", 			TE_FROM},
-      {"xor", 			TE_XOR},
-      {"do", 				TE_DO},
-      {"",}, {"",}, {"",}, 
-      {"redefine", 		TE_REDEFINE},
-      {"frozen", 			TE_FROZEN},
-      {"",}, {"",}, {"",}, 
-      {"class", 			TE_CLASS},
-      {"",}, {"",}, {"",}, {"",}, 
-      {"expanded", 		TE_EXPANDED},
+      {"obsolete", 		TE_OBSOLETE},
+      {"",}, 
+      {"check", 			TE_CHECK},
+      {"rescue", 			TE_RESCUE},
+      {"require", 		TE_REQUIRE},
       {"",}, {"",}, 
+      {"local", 			TE_LOCAL},
+      {"unique", 			TE_UNIQUE},
+      {"",}, {"",}, 
+      {"when", 			TE_WHEN},
+      {"",}, {"",}, 
+      {"as", 				TE_AS},
+      {"",}, 
+      {"from", 			TE_FROM},
+      {"alias", 			TE_ALIAS},
+      {"",}, {"",}, {"",}, 
+      {"once", 			TE_ONCE},
+      {"",}, {"",}, 
+      {"or", 				TE_OR},
+      {"all", 			TE_ALL},
+      {"",}, 
       {"infix", 			TE_INFIX},
       {"",}, 
-      {"indexing", 		TE_INDEXING},
+      {"inspect", 		TE_INSPECT},
       {"",}, 
-      {"undefine", 		TE_UNDEFINE},
-      {"",}, {"",}, {"",}, {"",}, 
+      {"invariant", 		TE_INVARIANT},
+      {"",}, 
+      {"select", 			TE_SELECT},
       {"feature", 		TE_FEATURE},
-      {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, 
+      {"external", 		TE_EXTERNAL},
+      {"implies", 		TE_IMPLIES},
+      {"separate", 		TE_SEPARATE},
+      {"",}, {"",}, 
+      {"creation", 		TE_CREATION},
+      {"",}, 
+      {"class", 			TE_CLASS},
+      {"rename", 			TE_RENAME},
+      {"inherit", 		TE_INHERIT},
+      {"",}, {"",}, {"",}, {"",}, 
+      {"do", 				TE_DO},
+      {"indexing", 		TE_INDEXING},
+      {"",}, {"",}, 
+      {"frozen", 			TE_FROZEN},
       {"",}, {"",}, {"",}, 
+      {"retry", 			TE_RETRY},
+      {"result", 			TE_RESULT},
+      {"variant", 		TE_VARIANT},
+      {"redefine", 		TE_REDEFINE},
+      {"true", 			TE_TRUE},
+      {"",}, 
+      {"loop", 			TE_LOOP},
+      {"strip", 			TE_STRIP},
+      {"undefine", 		TE_UNDEFINE},
+      {"then", 			TE_THEN},
+      {"until", 			TE_UNTIL},
+      {"precursor", 		TE_PRECURSOR},
+      {"",}, 
+      {"xor", 			TE_XOR},
+      {"",}, {"",}, {"",}, {"",}, 
+      {"prefix", 			TE_PREFIX},
+      {"",}, {"",}, {"",}, {"",}, 
       {"end", 			TE_END},
-      {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, 
+      {"",}, {"",}, {"",}, 
+      {"current", 		TE_CURRENT},
+      {"",}, {"",}, {"",}, {"",}, {"",}, 
+      {"export", 			TE_EXPORT},
+      {"",}, 
+      {"expanded", 		TE_EXPANDED},
+      {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, 
+      {"deferred", 		TE_DEFERRED},
+      {"",}, {"",}, {"",}, {"",}, 
+      {"old", 			TE_OLD},
+      {"",}, 
       {"debug", 			TE_DEBUG},
       {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, 
-      {"",}, {"",}, {"",}, {"",}, {"",}, 
-      {"old", 			TE_OLD},
-      {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, 
-      {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, 
-      {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, 
+      {"",}, {"",}, {"",}, 
+      {"not", 			TE_NOT},
+      {"",}, {"",}, {"",}, {"",}, 
       {"and", 			TE_AND},
       {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, {"",}, 
-      {"",}, {"",}, {"",}, {"",}, 
-      {"deferred", 		TE_DEFERRED},
+      {"",}, {"",}, {"",}, {"",}, {"",}, 
+      {"bit", 			TE_BIT},
     };
 
   if (len <= MAX_WORD_LENGTH && len >= MIN_WORD_LENGTH)
     {
       register int key = hash (str, len);
 
-      if (key <= MAX_HASH_VALUE && key >= MIN_HASH_VALUE)
+      if (key <= MAX_HASH_VALUE && key >= 0)
         {
           register char *s = wordlist[key].name;
 

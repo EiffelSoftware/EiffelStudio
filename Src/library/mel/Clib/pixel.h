a83692353e4d3d65b12121ea671406fa157202e3
Emmanuel Stapf <manus@eiffel.com>
1998-08-19 03:55:07 +0000

    Added new macros which enables the user to set the closeness choice of color when opening a bitmap.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@11074 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/mel/Clib/pixel.h
+++ Src/library/mel/Clib/pixel.h
@@ -20,6 +20,24 @@ extern EIF_INTEGER last_color_alloc_status;
 #define set_xpm_attributes_valuemask(_ptr_, _valuemask_) \
 		(((XpmAttributes *)_ptr_)->valuemask = _valuemask_)
 
+#define set_xpm_attributes_alloc_close_colors(_ptr_, _cmode_) \
+		(((XpmAttributes *)_ptr_)->alloc_close_colors = (Bool) (_cmode_!=0))
+	
+#define set_xpm_attributes_colors_closeness(_ptr_, _degree_) \
+		(((XpmAttributes *)_ptr_)->closeness = _degree_)
+
+#define set_xpm_attributes_red_closeness(_ptr_, _degree_) \
+		(((XpmAttributes *)_ptr_)->red_closeness = _degree_) 
+	
+#define set_xpm_attributes_blue_closeness(_ptr_, _degree_) \
+		(((XpmAttributes *)_ptr_)->blue_closeness = _degree_) 
+
+#define set_xpm_attributes_green_closeness(_ptr_, _degree_) \
+		(((XpmAttributes *)_ptr_)->green_closeness = _degree_) 
+
+#define set_xpm_attributes_exactcolors(_ptr_, _cmode_) \
+		(((XpmAttributes *)_ptr_)->exactColors = (Bool) (_cmode_!=0))
+	
 #define xpm_attributes_value_mask(_ptr_) (_ptr_)->valuemask
 #define xpm_attributes_height(_ptr_) (_ptr_)->height
 #define xpm_attributes_width(_ptr_) (_ptr_)->width
@@ -27,4 +45,10 @@ extern EIF_INTEGER last_color_alloc_status;
 #define xpm_attributes_y_hotspot(_ptr_) (_ptr_)->y_hotspot
 #define xpm_attributes_colormap(_ptr_) (_ptr_)->colormap
 #define xpm_attributes_ncolors(_ptr_) (_ptr_)->ncolors
+#define xpm_attributes_red_closeness(_ptr_) (_ptr_)->red_closeness
+#define xpm_attributes_blue_closeness(_ptr_) (_ptr_)->blue_closeness
+#define xpm_attributes_green_closeness(_ptr_) (_ptr_)->green_closeness
+#define xpm_attributes_alloc_close_colors(_ptr_) (Bool) ((_ptr_)->alloc_close_colors) == True ? 1 : 0
+	
+
 

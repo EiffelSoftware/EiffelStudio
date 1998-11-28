95cfa5036566a375374ef9c022a95262678743f1
Emmanuel Stapf <manus@eiffel.com>
1998-11-28 00:49:16 +0000

    Fixed a bug when we are going from the winter to summer time (and from summer to winter), this will avoid useless recompilation of an entire Eiffel system.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@12115 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/parsing/eiffel/date.c
+++ Src/bench/C/parsing/eiffel/date.c
@@ -14,6 +14,8 @@
 #include "eif_macros.h"
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/timeb.h>
+#include <time.h>
 
 #ifdef EIF_WIN32
 /*#define NOGDI*/
@@ -25,16 +27,37 @@ EIF_INTEGER eif_date(char *path)
 	/* Return last modification time of file of path `path' */
 
 	static struct stat info;
+	static struct timeb current_time;
 
-	return (-1 == stat(path,&info)) ? (EIF_INTEGER) 0L : (EIF_INTEGER) info.st_mtime;
+	ftime (&current_time);
+	if (current_time.dstflag == 1)
+			/* We are not in the same daylight saving zone (here we are in summer) and thus in order
+			 * to save in the default daylight saving zone (the winter) we need to remove 3600 seconds
+			 * from the given time */
+		return (-1 == stat(path,&info)) ? (EIF_INTEGER) 0L : ((EIF_INTEGER) info.st_mtime - 3600);
+	else
+			/* Since we are in the default daylight saving zone, we just use the value given by the
+			 * operating system */
+		return (-1 == stat(path,&info)) ? (EIF_INTEGER) 0L : (EIF_INTEGER) info.st_mtime;
 }
 
-EIF_BOOLEAN eif_directory_has_changed(char *path, EIF_INTEGER date)
+EIF_BOOLEAN eif_file_has_changed(char *path, EIF_INTEGER date)
 {
 	/* Check to see if the directory `path' has changed after `date' */
 
 	static struct stat info;
+	static struct timeb current_time;
 
-	return (-1 == stat(path,&info)) ? EIF_TRUE : EIF_TEST(date != (EIF_INTEGER) info.st_mtime);
+	ftime (&current_time);
+	if (current_time.dstflag == 1)
+			/* We are not in the same daylight saving zone (here we are in summer) and thus in order
+			 * to save in the default daylight saving zone (the winter) we need to remove 3600 seconds
+			 * from the given time */
+		return (stat(path,&info) == -1) ? EIF_TRUE :  EIF_TEST(date != ((EIF_INTEGER) info.st_mtime - 3600));
+	else
+			/* Since we are in the default daylight saving zone, we just use the value given by the
+			 * operating system */
+		return (stat(path,&info) == -1) ? EIF_TRUE :  EIF_TEST(date != (EIF_INTEGER) info.st_mtime);
+
 }
 

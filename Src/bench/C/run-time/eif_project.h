634a7536f43b5c85062d92ded5770d91e72279df
Emmanuel Stapf <manus@eiffel.com>
1998-12-21 01:07:13 +0000

    Removed `egc_tupmake' which was useless
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@12387 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_project.h
+++ Src/bench/C/run-time/eif_project.h
@@ -30,7 +30,6 @@ extern "C" {
 	RT_LNK void (*egc_strmake)(EIF_REFERENCE, EIF_INTEGER);	/* STRING creation feature */
 	RT_LNK void (*egc_strset)(EIF_REFERENCE, EIF_INTEGER);	/* STRING `set_count' feature */
 	RT_LNK void (*egc_arrmake)(EIF_REFERENCE, EIF_INTEGER, EIF_INTEGER);/* ARRAY creation feature */
-	RT_LNK void (*egc_tupmake)(EIF_REFERENCE);	/* TUPLE creation feature */
 	RT_LNK void (*egc_routdisp)(char *, char *, char *, char *, EIF_INTEGER);		/* ROUTINE `set_rout_disp' feature */
 	RT_LNK int egc_str_dtype;				/* Dynamic type for string */
 	RT_LNK int egc_arr_dtype;				/* Dynamic type for ARRAY[ANY] */

dfb535ac3847ae5de55afc5246e4933eefb27a51
Emmanuel Stapf <manus@eiffel.com>
1998-12-22 06:19:43 +0000

    New header file used for the generation of shared libraries.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@12401 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/bench/C/run-time/egc_dynlib.h
@@ -0,0 +1,37 @@
+/********************
+ *** EGC_DYNLIB.H ***
+ ********************/
+
+#ifndef _EGC_DYNLIB_H_
+#define _EGC_DYNLIB_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	
+#include "eif_cecil.h"
+#include "eif_eiffel.h"
+	
+	
+	extern void init_rt(void);
+	extern void reclaim_rt(void);
+	
+#define DYNAMIC_LIB_RT_INITIALIZE(x)\
+	char **l ; \
+	char **ol = (char **) 0; \
+	init_rt(); \
+	l = loc_set.st_top;  \
+	RTLI(x);
+		
+#define DYNAMIC_LIB_RT_END RTLE;	
+			
+#define DYNAMIC_LIB_RT_RECLAIM(x) \
+			reclaim_rt(); 
+				
+			
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+

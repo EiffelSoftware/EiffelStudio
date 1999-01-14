4160e4cfbc75a956b3b63ce78a4e3984a441243b
Emmanuel Stapf <manus@eiffel.com>
1999-01-14 16:36:51 +0000

    Added the RT_LNK macro for each of the exported function of `gen_conf.c' which can be used through a DLL.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@12611 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_gen_conf.h
+++ Src/bench/C/run-time/eif_gen_conf.h
@@ -6,74 +6,74 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-				
+
 /* Initialize module */
-extern void eif_gen_conf_init (int);
+RT_LNK void eif_gen_conf_init (int);
 
 /* Creation type of a gen. feat. in final mode */
-extern int16 eif_final_id (int16, int16 *ttable, int16 **gttable, char *object);
+RT_LNK int16 eif_final_id (int16, int16 *ttable, int16 **gttable, char *object);
 
 /* Converts an array of type ids to a single id */
-extern int16 eif_compound_id (int16 *cache, char *object, int16 dyn_type, int16 *typearr);
+RT_LNK int16 eif_compound_id (int16 *cache, char *object, int16 dyn_type, int16 *typearr);
 
 /* Type of an actual generic parameter */
-extern int16 eif_gen_param (int16 stype, char *object, int pos, char *is_exp, long *nr_bits);
+RT_LNK int16 eif_gen_param (int16 stype, char *object, int pos, char *is_exp, long *nr_bits);
 
 /* Number of generic parameters of an object */
-extern int eif_gen_count (char *object);
+RT_LNK int eif_gen_count (char *object);
 
 /* New object with same type as i-th (`posï) generic of `objectï */
-extern char *eif_gen_create (char *object, int pos);
+RT_LNK char *eif_gen_create (char *object, int pos);
 
 /* Register a bit type of size `sizeï */
-extern int16 eif_register_bit_type (long size);
+RT_LNK int16 eif_register_bit_type (long size);
 
 /* Type of ARRAY [type] */
-extern int16 eif_typeof_array_of (int16 type);
+RT_LNK int16 eif_typeof_array_of (int16 type);
 
 /* Full type name of an object as STRING object */
-extern char *eif_gen_typename (char *obj);
+RT_LNK char *eif_gen_typename (char *obj);
 
 /* CID which creates a given type */
-extern int16 *eif_gen_cid (int16);
+RT_LNK int16 *eif_gen_cid (int16);
 
 /* Generic id list from external sources (retrieve) */
-extern int16 eif_gen_id_from_cid (int16 *, int *);
+RT_LNK int16 eif_gen_id_from_cid (int16 *, int *);
 
 /* Conformance test */
-extern int eif_gen_conf (int16, int16);
+RT_LNK int eif_gen_conf (int16, int16);
 
 /* Type of the i-th generic parameter */
-extern int16 eif_gen_param_id (int16 stype, char *obj, int pos);
+RT_LNK int16 eif_gen_param_id (int16 stype, char *obj, int pos);
 
 /* TUPLEs */
 
 /* Typecode for generic parameter */
-extern char eif_gen_typecode (char *obj, int pos);
+RT_LNK char eif_gen_typecode (char *obj, int pos);
 
 /* Uniform TUPLE? */
 
-extern char eif_gen_is_uniform (char *obj, char code);
+RT_LNK char eif_gen_is_uniform (char *obj, char code);
 
 /* ROUTINEs */
 
 /* Target and argument type codes as STRING object */
-extern char *eif_gen_typecode_str (char *obj);
+RT_LNK char *eif_gen_typecode_str (char *obj);
 
 /* Parent tables */
 
-extern struct eif_par_types **eif_par_table;
-extern int eif_par_table_size;
+RT_LNK struct eif_par_types **eif_par_table;
+RT_LNK int eif_par_table_size;
 
 /* Auxiliary parent tables (dynamic extension) */
-extern struct eif_par_types **eif_par_table2;
-extern int eif_par_table2_size;
+RT_LNK struct eif_par_types **eif_par_table2;
+RT_LNK int eif_par_table2_size;
 
 /* Type map: compound->compiler generated id */
-extern int16 *eif_cid_map;
+RT_LNK int16 *eif_cid_map;
 
 /* Inverse RTUD Table to reverse the effect of RTUD */
-extern int16 *rtud_inv;
+RT_LNK int16 *rtud_inv;
 
 /* Maximum nr. of entries in a compound typeid array */
 #define MAX_CID_SIZE    200

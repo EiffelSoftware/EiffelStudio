88e7a26c73f45d6c12919ffd500de42335ee8567
Emmanuel Stapf <manus@eiffel.com>
1999-01-15 16:30:08 +0000

    Changed a completely incorrect signature using Eiffel objects where it should have been plain C string.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@12623 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_misc.h
+++ Src/bench/C/run-time/eif_misc.h
@@ -42,8 +42,8 @@ RT_LNK EIF_INTEGER esreal_size(void);
 RT_LNK EIF_INTEGER esint_size(void);
 RT_LNK EIF_INTEGER esdouble_size(void);
 RT_LNK EIF_INTEGER eif_system (char *s);
-RT_LNK EIF_INTEGER eif_putenv (EIF_OBJ v, EIF_OBJ k);
-RT_LNK EIF_OBJ eif_getenv(EIF_OBJ k);
+RT_LNK EIF_INTEGER eif_putenv (char * v, char * k);
+RT_LNK char * eif_getenv(char * k);
 RT_LNK EIF_INTEGER eif_safe_putenv (EIF_OBJ v, EIF_OBJ k); /* Safe Eiffel putenv dealing only with env. var. */
 
 RT_LNK char *arycpy(char *area, EIF_INTEGER i, EIF_INTEGER j, EIF_INTEGER k);

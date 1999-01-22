f9b93646c575b58252798bc04c894f19b2cf0806
aitkaci <aitkaci@eiffel.com>
1999-01-22 18:46:04 +0000

    Two new structure used in the notification messages of the tree_vies.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@12856 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/library/wel/spec/windows/include/tvdispinfo.h
@@ -0,0 +1,29 @@
+/*
+ * TVDISPINFO.H
+ */
+
+#ifndef __WEL_TV_DISPINFO__
+#define __WEL_TV_DISPINFO__
+
+#ifndef __WEL_COMMONCONTROLS__
+#	include <cctrl.h>
+#endif
+
+#define cwel_tv_dispinfo_get_hdr(_ptr_) (&(((TV_DISPINFO *) _ptr_)->hdr))
+#define cwel_tv_dispinfo_get_item(_ptr_) (&(((TV_DISPINFO *) _ptr_)->item))
+
+#endif /* __WEL_TV_DISPINFO__ */
+
+/*
+--|-------------------------------------------------------------------------
+--| Windows Eiffel Library: library of reusable components for ISE Eiffel.
+--| Copyright (C) 1995-1997, Interactive Software Engineering, Inc.
+--| All rights reserved. Duplication and distribution prohibited.
+--|
+--| 270 Storke Road, Suite 7, Goleta, CA 93117 USA
+--| Telephone 805-685-1006
+--| Fax 805-685-6869
+--| Information e-mail <info@eiffel.com>
+--| Customer support e-mail <support@eiffel.com>
+--|-------------------------------------------------------------------------
+*/

83f91f18e39d7e4eb4d3197a65413ab01cf8e2c8
Emmanuel Stapf <manus@eiffel.com>
1999-01-28 23:02:09 +0000

    Fixed a bug which occurs only when using CDE (from what we know so far). This is due to the fact, that CDDE returns a XFontSet instead of a XFontStruct as expected by the Eiffel code from `font_struct' of MEL_FONT_LIST_ENTRY.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@13036 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/mel/Clib/font.c
+++ Src/library/mel/Clib/font.c
@@ -13,3 +13,15 @@ EIF_POINTER xm_font_list_init_font_context (EIF_POINTER a_font_list)
 
 	return NULL;
 }
+
+EIF_POINTER x_build_font_from_set (EIF_POINTER p)
+{
+	int result;
+	XFontStruct **font_list;
+	char **name_list;
+
+	result = XFontsOfFontSet ((XFontSet) p, &font_list, &name_list);	
+	
+	return (EIF_POINTER) font_list [0];
+}
+

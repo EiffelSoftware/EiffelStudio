c2b75fc77b5091b853c277e5bc26fc7a45897d90
manuelt <manuelt@eiffel.com>
1999-03-04 23:57:05 +0000

    Initial revision.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@13630 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/examples/cecil/C2Eiffel/string/main.c
@@ -0,0 +1,65 @@
+/* CECIL program converting a C string into an Eiffel String */
+#include <stdio.h>
+#include "eif_setup.h"
+#include "eif_eiffel.h"
+#include "version.h"	/* For 4.3 -> 4.2 compatibility */
+
+
+int main (int argc,char **argv,char **envp) {
+
+	char s [256];
+	unsigned char c;
+	int i = 0;
+
+	
+	EIF_PROCEDURE p_put_string;	/* `put_string' from STD_FILES */
+	EIF_REFERENCE_FUNCTION f_io;		/* `io' from GENERAL (function) */ 
+	EIF_REFERENCE o_str;	/* Eiffel String */
+	EIF_OBJECT obj; 			/* Protected indirection to o_str */
+	EIF_REFERENCE o_io;		/* `io' from GENERAL (object) */
+	EIF_OBJECT i_io;		/* safe indirection to `io' from GENERAL (object) */
+
+	EIF_INITIALIZE (failure)	/* Initialization of Eiffel run-time */
+
+	p_put_string = eif_procedure ("put_string", eif_type_id ("STD_FILES"));
+
+		/* `io' is a once function. On the C side it is a function and
+		 * an object. We call it on `root_obj' which is the root-object
+		 * of the Eiffel system.
+		 */
+	f_io = eif_reference_function ("io", eif_type_id ("STD_FILES")); 
+	o_io = (f_io) (root_obj); /* Call `io' (on root_obj, which is the root 
+							   * object).`io' can be called on any object, 
+							   * since every type inherits from GENERAL */
+
+	i_io = eif_protect (o_io);	/* Protect `io' */
+	
+
+	printf ("Enter a string to convert in Eiffel string:\n");
+		
+	while ((c= (unsigned char) getchar ()) != '\n') {
+		/* Read the line, build the C string */
+		s [i++] = c;
+	}
+	s [i] = '\0';
+
+
+	o_str = RTMS (s);	/* Eiffel Run-time macro, that converts a C string
+						 * into an Eiffel String */
+	obj = eif_protect (o_str);	/* protect the return value of RTMS, we will
+							     * access to the Eiffel string through
+								 * eif_access (obj)	*/
+		/* Call to `io.put_string'. Note that `print' can be called instead
+	     * without any initialization of `io' */	
+	(p_put_string) (eif_access (i_io), eif_access (obj));/* Call `io.put_string (o_str)' */
+	printf ("\n");
+	eif_wean (obj);		/* Unprotect Eiffel string  */
+	eif_wean (i_io);	/* Unprotect `io' */
+		
+	EIF_DISPOSE_ALL		/* Reclaim object allocated by Eiffel run-time. */
+
+	return 0;
+
+
+}
+	
---
+++ Src/examples/cecil/C2Eiffel/string/root_class.e
@@ -0,0 +1,10 @@
+class
+	ROOT_CLASS
+creation
+	make
+feature
+	make is
+		do
+			io.put_string ("Test for CECIL%N")
+		end
+end -- class ROOT_CLASS

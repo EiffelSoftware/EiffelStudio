677af8e2839bef32b320ad37248dc3b4a57642ec
Emmanuel Stapf <manus@eiffel.com>
1999-05-11 02:19:50 +0000

    Fixed some ANSI conformance bug Fixed a Borland compatibility problem
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@14370 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/time/Clib/datetime.c
+++ Src/library/time/Clib/datetime.c
@@ -1,16 +1,16 @@
 #include <sys/types.h>
 #include <stdio.h>
-#include <eif_eiffel.h>
-#include <time.h>
 #include <sys/timeb.h>
+#include <time.h>
+#include "eif_eiffel.h"
 
 #ifdef EIF_WIN32
-	struct _timeb date_time_fine;
-#else
-	struct timeb date_time_fine;
-#endif
-
 #ifdef EIF_BORLAND
+	struct timeb date_time_fine;
+#else
+	struct _timeb date_time_fine;
+#endif
+#else
 	struct timeb date_time_fine;
 #endif
 
@@ -18,10 +18,13 @@ struct tm *date_time;
 	
 void c_get_date_time () 
 {
-	
 	const time_t *tmp;
 #ifdef EIF_WIN32
+#ifdef EIF_BORLAND
+	ftime (&date_time_fine);
+#else
 	_ftime (&date_time_fine);
+#endif
 #else
 	ftime (&date_time_fine);
 #endif
@@ -64,7 +67,8 @@ EIF_INTEGER c_millisecond_now ()
 }
 
 EIF_INTEGER c_make_date (EIF_INTEGER d, EIF_INTEGER m, EIF_INTEGER y)
-{   int compact_date = 0;
+{
+	int compact_date;
 	compact_date = ((int) d << 24);
 	compact_date = compact_date + ((int) m << 16);
 	compact_date = compact_date + y;
@@ -86,32 +90,6 @@ EIF_INTEGER c_year (EIF_INTEGER compact_date)
 	return ((EIF_INTEGER) (compact_date&0x0000ffff));
 }
 
-c_set_day (EIF_INTEGER d, EIF_INTEGER *compact_date)
-{
-	*compact_date = *compact_date&0x00ffffff;
-	*compact_date = *compact_date + ((int) (d<<24));
-}
-
-c_set_month (EIF_INTEGER m, EIF_INTEGER *compact_date)
-{
-	*compact_date = *compact_date&0xff00ffff;
-	*compact_date = *compact_date + ((int) (m<<16));
-}
-
-c_set_year (EIF_INTEGER y, EIF_INTEGER *compact_date)
-{
-	*compact_date = *compact_date&0xffff0000;
-	*compact_date = *compact_date + ((int) (y));
-}
-
-EIF_INTEGER c_make_time (EIF_INTEGER h, EIF_INTEGER m, EIF_INTEGER s)
-{   int compact_time = 0;
-	compact_time = ((int) h << 16);
-	compact_time = compact_time + ((int) m << 8);
-	compact_time = compact_time + s;
-	return compact_time;
-}
-
 EIF_INTEGER c_hour (EIF_INTEGER compact_time)
 {
 	return ((EIF_INTEGER) (compact_time&0xff0000)>>16);
@@ -127,19 +105,45 @@ EIF_INTEGER c_second (EIF_INTEGER compact_time)
 	return ((EIF_INTEGER) (compact_time&0x0000ff));
 }
 
-EIF_INTEGER c_set_hour (EIF_INTEGER h, EIF_INTEGER *compact_time)
+void c_set_day (EIF_INTEGER d, EIF_INTEGER *compact_date)
+{
+	*compact_date = *compact_date&0x00ffffff;
+	*compact_date = *compact_date + ((int) (d<<24));
+}
+
+void c_set_month (EIF_INTEGER m, EIF_INTEGER *compact_date)
+{
+	*compact_date = *compact_date&0xff00ffff;
+	*compact_date = *compact_date + ((int) (m<<16));
+}
+
+void c_set_year (EIF_INTEGER y, EIF_INTEGER *compact_date)
+{
+	*compact_date = *compact_date&0xffff0000;
+	*compact_date = *compact_date + ((int) (y));
+}
+
+EIF_INTEGER c_make_time (EIF_INTEGER h, EIF_INTEGER m, EIF_INTEGER s)
+{   int compact_time;
+	compact_time = ((int) h << 16);
+	compact_time = compact_time + ((int) m << 8);
+	compact_time = compact_time + s;
+	return compact_time;
+}
+
+void c_set_hour (EIF_INTEGER h, EIF_INTEGER *compact_time)
 {
 	*compact_time = *compact_time&0x00ffff;
 	*compact_time = *compact_time + ((int) (h<<16));
 }
 
-EIF_INTEGER c_set_minute (EIF_INTEGER m, EIF_INTEGER *compact_time)
+void c_set_minute (EIF_INTEGER m, EIF_INTEGER *compact_time)
 {
 	*compact_time = *compact_time&0xff00ff;
 	*compact_time = *compact_time + ((int) (m<<8));
 }
 
-EIF_INTEGER c_set_second (EIF_INTEGER s, EIF_INTEGER *compact_time)
+void c_set_second (EIF_INTEGER s, EIF_INTEGER *compact_time)
 {
 	*compact_time = *compact_time&0xffff00;
 	*compact_time = *compact_time + ((int) (s));

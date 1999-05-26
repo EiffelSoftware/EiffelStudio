cd45d6bd5608928dd00ce12a6777d28c1944dd1a
aitkaci <aitkaci@eiffel.com>
1999-05-26 16:17:58 +0000

    Two new classes structures needed by the folder selection standard dialog.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@14625 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/library/wel/spec/windows/include/choose_folder.h
@@ -0,0 +1,45 @@
+/*
+ * BIF.H
+ */
+
+#ifndef __WEL_BROWSEINFO__
+#define __WEL_BROWSEINFO__
+
+#ifndef __SHLOBJ__
+#define __SHLOBJ__
+#include <shlobj.h>
+#endif
+
+#define cwel_browse_info_set_hwndowner(_ptr_, _value_) (((BROWSEINFO *) _ptr_)->hwndOwner = (HWND) (_value_))
+#define cwel_browse_info_set_pidlroot (_ptr_, _value_) (((BROWSEINFO *) _ptr_)->pidlRoot = (LPCITEMIDLIST) (_value_))
+#define cwel_browse_info_set_pszdisplayname(_ptr_, _value_) (((BROWSEINFO *) _ptr_)->pszDisplayName = (LPSTR) (_value_))
+#define cwel_browse_info_set_lpsztitle(_ptr_, _value_) (((BROWSEINFO *) _ptr_)->lpszTitle = (LPCSTR) (_value_))
+#define cwel_browse_info_set_ulflags(_ptr_, _value_) (((BROWSEINFO *) _ptr_)->ulFlags = (UINT) (_value_))
+#define cwel_browse_info_set_lpfn(_ptr_, _value_) (((BROWSEINFO *) _ptr_)->lpfn = (BFFCALLBACK) (_value_))
+#define cwel_browse_info_set_lparam(_ptr_, _value_) (((BROWSEINFO *) _ptr_)->lParam = (LPARAM) (_value_))
+#define cwel_browse_info_set_iimage(_ptr_, _value_) (((BROWSEINFO *) _ptr_)->iImage = (int) (_value_))
+
+#define cwel_browse_info_get_hwndowner(_ptr_) ((((BROWSEINFO *) _ptr_)->hwndOwner))
+#define cwel_browse_info_get_pidlroot(_ptr_) ((((BROWSEINFO *) _ptr_)->pidlRoot))
+#define cwel_browse_info_get_pszdisplayname(_ptr_) ((((BROWSEINFO *) _ptr_)->pszDisplayName))
+#define cwel_browse_info_get_lpszTitle(_ptr_) ((((BROWSEINFO *) _ptr_)->lpszTitle))
+#define cwel_browse_info_get_ulflags(_ptr_) ((((BROWSEINFO *) _ptr_)->ulFlags))
+#define cwel_browse_info_get_lpfn(_ptr_) ((((BROWSEINFO *) _ptr_)->lpfn))
+#define cwel_browse_info_get_lparam(_ptr_) ((((BROWSEINFO *) _ptr_)->lParam))
+#define cwel_browse_info_get_iimage(_ptr_) ((((BROWSEINFO *) _ptr_)->iImage))
+
+#endif /* __WEL_BROWSEINFO__ */
+
+/*
+--|-------------------------------------------------------------------------
+--| Windows Eiffel Library: library of reusable components for ISE Eiffel.
+--| Copyright (C) 1995-1999, Interactive Software Engineering, Inc.
+--| All rights reserved. Duplication and distribution prohibited.
+--|
+--| 270 Storke Road, Suite 7, Goleta, CA 93117 USA
+--| Telephone 805-685-1006
+--| Fax 805-685-6869
+--| Information e-mail <info@eiffel.com>
+--| Customer support e-mail <support@eiffel.com>
+--|-------------------------------------------------------------------------
+*/

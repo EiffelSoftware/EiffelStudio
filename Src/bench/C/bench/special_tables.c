f8bba627d539c59ea5e97b5c2eede217f93a2907
Emmanuel Stapf <manus@eiffel.com>
1999-06-10 20:02:29 +0000

    Added some debug statements and fixed an undeclared declaration of `malloc'.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@14838 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/bench/special_tables.c
+++ Src/bench/C/bench/special_tables.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "special_tables.h"
 
 /* Feature computed has a polymorphic call in current context*/
 #define IS_POLYMORPHIC	0x03
-
 
 /* Feature computed has a static call in current context*/
 #define NOT_POLYMORPHIC	0x02
@@ -11,21 +11,45 @@
 /* Feature which is deferred */
 #define IS_DEFERRED		0x01
 
+#ifdef DEBUG
+struct poly_debug {
+	int size;
+	unsigned char* area;
+};
+#endif
+
 /* Allocate table */
 unsigned char *table_malloc (int nb_classes) {
 	unsigned char *area;
 	int logical_size;
+#ifdef DEBUG
+	struct poly_debug *my_poly_table;
+	my_poly_table = (struct poly_debug *) malloc (sizeof (struct poly_debug));
+#endif
 
 	logical_size = ((nb_classes >> 2) + 1) * sizeof (unsigned char);
 	area = (unsigned char *) malloc (logical_size);
 	area = (unsigned char *) memset ((void *) area, 0, logical_size);
+
+#ifdef debug
+	my_poly_table->area = area;
+	my_poly_table->size = logical_size;
+	return (unsigned char *) my_poly_table;
+#else
 	return area;
+#endif
 }
 
 void table_free (unsigned char *area)
 {
-	if (area)
+	if (area) {
+#ifdef DEBUG
+		free (((struct poly_debug *)area)->area);
+		free ((struct poly_debug *) area);
+#else
 		free (area);
+#endif
+	}
 }
 	
 #define set_status(area,type_id,flag)	\
@@ -52,6 +76,11 @@ void put_value (unsigned char *area, int type_id, EIF_BOOLEAN v)
 		set_status (area, type_id, IS_POLYMORPHIC);
 	else
 		set_status (area, type_id, NOT_POLYMORPHIC);
+
+#ifdef DEBUG
+	if ((type_id >> 2) > (((struct poly_debug *) area)->size))
+		printf ("Access error: size max is %d, accessed item is %d\n", (((struct poly_debug *) area)->size, type_id >> 2));
+#endif
 }
 
 
@@ -59,6 +88,11 @@ EIF_INTEGER get_value (unsigned char *area, int type_id)
 {
 	register unsigned char item = area [type_id >> 2];
 	register int offset = (type_id & 0x00000003) << 1;
+
+#ifdef DEBUG
+	if ((type_id >> 2) > (((struct poly_debug *) area)->size))
+		printf ("Access error: size max is %d, accessed item is %d\n", (((struct poly_debug *) area)->size, type_id >> 2));
+#endif
 
 	if (item & (1 << (offset + 1))) {
 		if (item & (1 << offset))

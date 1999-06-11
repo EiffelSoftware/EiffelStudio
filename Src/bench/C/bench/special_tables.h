6c9987a4da3e5e708fc4e8330f19b7eed8009a44
Emmanuel Stapf <manus@eiffel.com>
1999-06-11 22:46:07 +0000

    Improved the DEBUG flag so that we can have more information stored in the C arrays to be able to track any out of bound access.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@14857 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/bench/special_tables.h
+++ Src/bench/C/bench/special_tables.h
@@ -1,14 +1,25 @@
 #include "eif_eiffel.h"
 
-unsigned char *table_malloc (int nb_classes);
+#ifdef DEBUG
+struct poly_debug {
+	int size;
+	unsigned char* area;
+};
+
+#define AREA_TYPE struct poly_debug *
+#else
+#define AREA_TYPE unsigned char *
+#endif
+
+AREA_TYPE table_malloc (int nb_classes);
 /* initialise the table of a poly_table */
 /* arguments: maximum number of class_type_id in current polymorphic table */
 
-void table_free (unsigned char *area);
+void table_free (AREA_TYPE area);
 /* free the structure */
 
-void put_value (unsigned char *area, int type_id, EIF_BOOLEAN v);
+void put_value (AREA_TYPE area, int type_id, EIF_BOOLEAN v);
 /* put the flag `v' corresponding to the type_id */
 
-EIF_INTEGER get_value (unsigned char *area, int type_id);
+EIF_INTEGER get_value (AREA_TYPE  area, int type_id);
 /* give a boolean saying if the current call is a polymorphic one or not */

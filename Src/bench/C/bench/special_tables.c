3b54d20e282ec6557accca0fbb6cfabdfbe3500b
Emmanuel Stapf <manus@eiffel.com>
1999-06-13 23:14:09 +0000

    Removed some debug statement since they are now useless (we found the problem, which was that we can have some accesses outside the array because we did not allocate enough space at the beginning. The main reason is that `nb_classes' correspond to the greatest type_id of the class which redefine a certain `routine_id' but it does not mean that it is the highest `type_id' that we can access.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@14869 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/bench/special_tables.c
+++ Src/bench/C/bench/special_tables.c
@@ -15,10 +15,9 @@
 AREA_TYPE table_malloc (int nb_classes) {
 	unsigned char *area;
 	int logical_size;
-#ifdef DEBUG
 	struct poly_debug *my_poly_table;
+
 	my_poly_table = (struct poly_debug *) malloc (sizeof (struct poly_debug));
-#endif
 
 	logical_size = ((nb_classes >> 2) + 1) * sizeof (unsigned char);
 	area = (unsigned char *) malloc (logical_size);
@@ -28,24 +27,16 @@ AREA_TYPE table_malloc (int nb_classes) {
 
 	area = (unsigned char *) memset ((void *) area, 0, logical_size);
 
-#ifdef DEBUG
 	my_poly_table->area = area;
 	my_poly_table->size = logical_size;
 	return my_poly_table;
-#else
-	return area;
-#endif
 }
 
 void table_free (AREA_TYPE area)
 {
 	if (area) {
-#ifdef DEBUG
 		free (((struct poly_debug *)area)->area);
 		free ((struct poly_debug *) area);
-#else
-		free (area);
-#endif
 	}
 }
 	
@@ -68,43 +59,26 @@ void put_value (AREA_TYPE area, int type_id, EIF_BOOLEAN v)
 		 * Then we shift IS_POLYMORPHIC or NOT_POLYMORPHIC to be in front of the
 		 * correct offset in area[index] and we add the new value to area[index]
 		 */
-#ifdef DEBUG
-#define area area->area
-#endif
+	register int size = type_id >> 2;
+
+		/* In some cases, the area is too small, and we need to do a reallocation */
+	if ((size) > area->size) {
+		area->area = (unsigned char *) realloc ((void *) area->area, size + 1);
+		area->size = size + 1;
+	}
+
 	if (v == EIF_TRUE)
 			/* It is a polymorphic call */
-		set_status (area, type_id, IS_POLYMORPHIC);
+		set_status (area->area, type_id, IS_POLYMORPHIC);
 	else
-		set_status (area, type_id, NOT_POLYMORPHIC);
-
-#ifdef DEBUG
-#undef area
-#endif
-
-#ifdef DEBUG
-	if ((type_id >> 2) > (area->size))
-		printf ("Access error: size max is %d, accessed item is %d\n", area->size, type_id >> 2);
-#endif
+		set_status (area->area, type_id, NOT_POLYMORPHIC);
 }
 
 
 EIF_INTEGER get_value (AREA_TYPE area, int type_id)
 {
-#ifdef DEBUG
-#define area area->area
-#endif
-
-	register unsigned char item = area [type_id >> 2];
+	register unsigned char item = area->area [type_id >> 2];
 	register int offset = (type_id & 0x00000003) << 1;
-
-#ifdef DEBUG
-#undef area
-#endif
-
-#ifdef DEBUG
-	if ((type_id >> 2) > (area->size))
-		printf ("Access error: size max is %d, accessed item is %d\n", area->size, type_id >> 2);
-#endif
 
 	if (item & (1 << (offset + 1))) {
 		if (item & (1 << offset))

0c879b517c1fbde6275e4dd4979e6ec4319d485e
marina <marina@eiffel.com>
1999-06-21 21:28:52 +0000

    Modified Files:  	E_statstg.h  Added Files:  	ecom_rt_globals.h  ----------------------------------------------------------------------
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@15012 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/library/com/spec/windows/include/ecom_rt_globals.h
@@ -0,0 +1,19 @@
+//--------------------------------------------------------------------------
+//
+//  EiffelCOM
+//  Copyright (C) Interactive Software Engineering, 1998.
+//
+//  File:		ecom_rt_globals.h
+//
+//  Contents: 	Global variables used by EiffelCOM wizard
+//
+//--------------------------------------------------------------------------
+
+#ifndef __ECOM_RUNTIME_GLOBALS_H_INC__
+#define __ECOM_RUNTIME_GLOBALS_H_INC__
+
+#include "ecom_exception.h"
+
+extern Formatter  f;
+
+#endif // !__ECOM_RUNTIME_GLOBALS_H_INC__

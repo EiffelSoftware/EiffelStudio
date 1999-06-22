c7f4728086eedf6ca7bba3582a606a75e1e4a8a7
marina <marina@eiffel.com>
1999-06-22 22:59:37 +0000

    Modified Files:  	E_statstg.h ecom_rt_globals.h  Added Files:  	E_Currency.h E_Decimal.h E_Hresult.h  ----------------------------------------------------------------------
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@15031 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/library/com/spec/windows/include/E_Currency.h
@@ -0,0 +1,42 @@
+//--------------------------------------------------------------------------
+//
+//  EiffelCOM
+//  Copyright (C) Interactive Software Engineering, 1998.
+//
+//  File:		E_Currency.h
+//
+//  Contents:	Declaration of E_Currency macros.
+//
+//
+//--------------------------------------------------------------------------
+//
+#ifndef __ECOM_E_CURRENCY_H_INC__
+#define __ECOM_E_CURRENCY_H_INC__
+
+#include <objbase.h>
+#include <oleauto.h>
+#include "eif_eiffel.h"
+
+#define ccom_currency_round(_ptr_, _value_, _ptr_2) VarCyRound( *((CY*)_ptr_), _value_, (CY*)_ptr_2)
+#define ccom_currency_negative(_ptr_, _ptr_2) VarCyNeg( *((CY*)_ptr_), (CY*)_ptr_2)
+#define ccom_currency_integer(_ptr_, _ptr_2) VarCyInt( *((CY*)_ptr_), (CY*)_ptr_2)
+#define ccom_currency_fix(_ptr_, _ptr_2) VarCyFix( *((CY*)_ptr_), (CY*)_ptr_2)
+#define ccom_currency_absolute(_ptr_, _ptr_2) VarCyAbs( *((CY*)_ptr_), (CY*)_ptr_2)
+#define ccom_currency_set_high_bits(_ptr_, _value_) (((CY*)_ptr_)->Hi=(unsigned long)_value_)
+#define ccom_currency_set_low_bits(_ptr_, _value_) (((CY*)_ptr_)->Lo=(long)_value_)
+#define ccom_currency_high_bits(_ptr_) (((CY*)_ptr_)->Hi)
+#define ccom_currency_low_bits(_ptr_) (((CY*)_ptr_)->Lo)
+#define ccom_currency_add(_ptr_, _ptr_2,_ptr_3) VarCyAdd( *((CY*)_ptr_), *((CY*)_ptr_2),(CY*)_ptr_3)
+#define ccom_currency_multiply(_ptr_, _ptr_2,_ptr_3) VarCyMul( *((CY*)_ptr_), *((CY*)_ptr_2),(CY*)_ptr_3)
+#define ccom_currency_multiply_by_4bytes_integer(_ptr_, _ptr_2,_ptr_3) VarCyMulI4( *((CY*)_ptr_), *((CY*)_ptr_2),(CY*)_ptr_3)
+#define ccom_currency_subtract(_ptr_, _ptr_2,_ptr_3) VarCySub( *((CY*)_ptr_), *((CY*)_ptr_2),(CY*)_ptr_3)
+
+// Zero value
+void ccom_currency_value_zero (CY * a_value);
+
+// One value
+void ccom_currency_value_one (CY * a_value);
+
+EIF_REFERENCE ccom_currency_convert_to_eiffel_currency (CY * a_value);
+ 
+#endif // !__ECOM_E_CURRENCY_H_INC__

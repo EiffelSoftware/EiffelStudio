6040148ebf149942818cde4c68d93d7ae7183ff2
raphaels <raphaels@eiffel.com>
1999-07-21 23:37:04 +0000

    Corrected signature of ccom_is_equal_guid
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@15556 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/com/spec/windows/include/E_guid.h
+++ Src/library/com/spec/windows/include/E_guid.h
@@ -18,7 +18,7 @@
 #include "eif_eiffel.h"
 
 #define ccom_string_to_guid(_pwchar_,_p_guid_) (CLSIDFromString ((LPOLESTR)_pwchar_, (LPCLSID)_p_guid_))
-#define ccom_is_equal_guid(_guid1_,_guid2_) ((IsEqualGUID((GUID *)_guid1_,(GUID *)_guid2_) == FALSE)? EIF_FALSE : EIF_TRUE)
+#define ccom_is_equal_guid(_guid1_,_guid2_) ((IsEqualGUID((REFGUID)_guid1_,(REFGUID)_guid2_) == FALSE)? EIF_FALSE : EIF_TRUE)
 EIF_POINTER ccom_guid_to_wide_string (GUID * guid);
 
 #endif // !__ECOM_E_GUID_H_INC__

a48c50a3e38fdf150886b13036203d8198e8ea14
Emmanuel Stapf <manus@eiffel.com>
1999-07-30 17:28:37 +0000

    Removed `econfm' from the runtime and therefore we removed all occurences of it in the header file and DEF files for Windows DLLs.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@15826 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_plug.h
+++ Src/bench/C/run-time/eif_plug.h
@@ -107,7 +107,6 @@ extern int dynamic_dtype;	/* Dynamic type of DYNAMIC */
 	(((EIF_REFERENCE) obj1 == (EIF_REFERENCE) 0) || ((EIF_REFERENCE) obj2 == (EIF_REFERENCE) 0))? EIF_FALSE: \
 		(Dtype(obj1) == Dtype(obj2))
 
-RT_LNK int econfm(int ancestor, int heir);	/* Conformance query for assignment attempt */
 RT_LNK long sp_count(EIF_REFERENCE spobject);		/* Count of a special object */
 RT_LNK void chkinv(EIF_REFERENCE obj, int where);	/* Invariant control call */
 

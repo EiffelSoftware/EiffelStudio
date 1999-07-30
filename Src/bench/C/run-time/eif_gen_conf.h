eaf9a3b9c9f9bceef5de5423829412a45e265a30
Emmanuel Stapf <manus@eiffel.com>
1999-07-30 18:11:16 +0000

    Use `EIF_REFERENCE' instead of `char *' to show that we are handling Eiffel objects. Fixed some warning regarding some missing casts Initializaed a local variable `matched' which could have been used without assignments Removed the feature `eif_gen_create_from_cid' since it was not used anywhere.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@15828 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_gen_conf.h
+++ Src/bench/C/run-time/eif_gen_conf.h
@@ -13,19 +13,19 @@ extern "C" {
 RT_LNK void eif_gen_conf_init (int);
 
 /* Creation type of a gen. feat. in final mode */
-RT_LNK int16 eif_final_id (int16, int16 *ttable, int16 **gttable, char *object);
+RT_LNK int16 eif_final_id (int16, int16 *ttable, int16 **gttable, EIF_REFERENCE object);
 
 /* Converts an array of type ids to a single id */
-RT_LNK int16 eif_compound_id (int16 *cache, char *object, int16 dyn_type, int16 *typearr);
+RT_LNK int16 eif_compound_id (int16 *cache, EIF_REFERENCE object, int16 dyn_type, int16 *typearr);
 
 /* Type of an actual generic parameter */
-RT_LNK int16 eif_gen_param (int16 stype, char *object, int pos, char *is_exp, long *nr_bits);
+RT_LNK int16 eif_gen_param (int16 stype, EIF_REFERENCE object, int pos, char *is_exp, long *nr_bits);
 
 /* Number of generic parameters of an object */
-RT_LNK int eif_gen_count (char *object);
+RT_LNK int eif_gen_count (EIF_REFERENCE object);
 
 /* New object with same type as i-th (`posï) generic of `objectï */
-RT_LNK char *eif_gen_create (char *object, int pos);
+RT_LNK EIF_REFERENCE eif_gen_create (EIF_REFERENCE object, int pos);
 
 /* Register a bit type of size `sizeï */
 RT_LNK int16 eif_register_bit_type (long size);
@@ -34,7 +34,7 @@ RT_LNK int16 eif_register_bit_type (long size);
 RT_LNK int16 eif_typeof_array_of (int16 type);
 
 /* Full type name of an object as STRING object */
-RT_LNK char *eif_gen_typename (char *obj);
+RT_LNK char *eif_gen_typename (EIF_REFERENCE obj);
 
 /* Dynamic Type id of an object of type `type_string' */
 RT_LNK EIF_TYPE_ID eif_type_id (char *type_string);
@@ -49,21 +49,21 @@ RT_LNK int16 eif_gen_id_from_cid (int16 *, int *);
 RT_LNK int eif_gen_conf (int16, int16);
 
 /* Type of the i-th generic parameter */
-RT_LNK int16 eif_gen_param_id (int16 stype, char *obj, int pos);
+RT_LNK int16 eif_gen_param_id (int16 stype, EIF_REFERENCE obj, int pos);
 
 /* TUPLEs */
 
 /* Typecode for generic parameter */
-RT_LNK char eif_gen_typecode (char *obj, int pos);
+RT_LNK char eif_gen_typecode (EIF_REFERENCE obj, int pos);
 
 /* Uniform TUPLE? */
 
-RT_LNK char eif_gen_is_uniform (char *obj, char code);
+RT_LNK char eif_gen_is_uniform (EIF_REFERENCE obj, char code);
 
 /* ROUTINEs */
 
 /* Target and argument type codes as STRING object */
-RT_LNK char *eif_gen_typecode_str (char *obj);
+RT_LNK EIF_REFERENCE eif_gen_typecode_str (EIF_REFERENCE obj);
 
 /* Parent tables */
 

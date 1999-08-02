570d0de27f54aa8c0e6d01136db6f369aca3ebf5
Emmanuel Stapf <manus@eiffel.com>
1999-08-02 22:53:19 +0000

    Removed `str_len' and replaced it by `strlen'. Changed all the occurences of `char' or `char *' by `EIF_CHARACTER' or `EIF_CHARACTER *' so that we can define EIF_CHARACTER the way we want to.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@15990 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_str.h
+++ Src/bench/C/run-time/eif_str.h
@@ -19,38 +19,39 @@ extern "C" {
 
 #include "eif_portable.h"
 
-RT_LNK int str_left(register char *str, int length);
-RT_LNK void str_ljustify(register char *str, int length, int capacity);
-RT_LNK void str_rjustify(register char *str, int length, int capacity);
-RT_LNK void str_cjustify(register char *str, int length, int capacity);
-RT_LNK int str_right(register char *str, int length);
-RT_LNK int str_search(char *str, char c, int start, int len);
-RT_LNK int str_last_search (char *str, char c, int start_index_from_end);
-RT_LNK void str_replace(char *str, char *new_str, int string_length, int new_len, int start, int end);
-RT_LNK void str_insert(char *str, char *new_str, int string_length, int new_len, int idx);
-RT_LNK EIF_INTEGER str_code(EIF_CHARACTER *str, EIF_INTEGER i);
-RT_LNK void str_blank(char *str, int n);
-RT_LNK void str_fill(char *str, int n, char c);
-RT_LNK void str_tail(register char *str, register int n, int l);
-RT_LNK void str_take(char *str, char *new_str, long int start, long int end);
-RT_LNK void str_lower(register char *str, int l);
-RT_LNK void str_lower(register char *str, int l);
-RT_LNK void str_upper(register char *str, int l);
-RT_LNK int str_cmp(register char *str1, register char *str2, int l1, int l2);
-RT_LNK void str_cpy(char *to, char *from, int len);
-RT_LNK void str_cprepend(char *str, char c, int l);
-RT_LNK void str_append(char *str, char *new_str, int string_length, int new_len);
-RT_LNK void str_rmchar(char *str, int l, int i);
-RT_LNK int str_rmall(char *str, char c, int l);
-RT_LNK void str_mirror(register char *str, register char *new_str, register int len);
-RT_LNK void str_reverse(register char *str, int len);
-RT_LNK long str_atoi(char *str, int length);
-RT_LNK float str_ator(char *str, int length);
-RT_LNK double str_atod(char *str, int length);
-RT_LNK EIF_BOOLEAN str_isi(char *str, EIF_INTEGER length);
-RT_LNK EIF_BOOLEAN str_isr(char *str, EIF_INTEGER length);
-RT_LNK EIF_BOOLEAN str_isd(char *str, EIF_INTEGER length);
-RT_LNK long str_len(char *str);
+/* Numeric code of 'i'-th EIF_CHARACTER in 'str' */
+#define str_code(str,i) (EIF_INTEGER) ((EIF_CHARACTER *) (str))[((EIF_INTEGER)(i))-1]
+
+RT_LNK int str_left(register EIF_CHARACTER *str, int length);
+RT_LNK void str_ljustify(register EIF_CHARACTER *str, int length, int capacity);
+RT_LNK void str_rjustify(register EIF_CHARACTER *str, int length, int capacity);
+RT_LNK void str_cjustify(register EIF_CHARACTER *str, int length, int capacity);
+RT_LNK int str_right(register EIF_CHARACTER *str, int length);
+RT_LNK int str_search(EIF_CHARACTER *str, EIF_CHARACTER c, int start, int len);
+RT_LNK int str_last_search (EIF_CHARACTER *str, EIF_CHARACTER c, int start_index_from_end);
+RT_LNK void str_replace(EIF_CHARACTER *str, EIF_CHARACTER *new_str, int string_length, int new_len, int start, int end);
+RT_LNK void str_insert(EIF_CHARACTER *str, EIF_CHARACTER *new_str, int string_length, int new_len, int idx);
+RT_LNK void str_blank(EIF_CHARACTER *str, int n);
+RT_LNK void str_fill(EIF_CHARACTER *str, int n, EIF_CHARACTER c);
+RT_LNK void str_tail(register EIF_CHARACTER *str, register int n, int l);
+RT_LNK void str_take(EIF_CHARACTER *str, EIF_CHARACTER *new_str, long int start, long int end);
+RT_LNK void str_lower(register EIF_CHARACTER *str, int l);
+RT_LNK void str_lower(register EIF_CHARACTER *str, int l);
+RT_LNK void str_upper(register EIF_CHARACTER *str, int l);
+RT_LNK int str_cmp(register EIF_CHARACTER *str1, register EIF_CHARACTER *str2, int l1, int l2);
+RT_LNK void str_cpy(EIF_CHARACTER *to, EIF_CHARACTER *from, int len);
+RT_LNK void str_cprepend(EIF_CHARACTER *str, EIF_CHARACTER c, int l);
+RT_LNK void str_append(EIF_CHARACTER *str, EIF_CHARACTER *new_str, int string_length, int new_len);
+RT_LNK void str_rmchar(EIF_CHARACTER *str, int l, int i);
+RT_LNK int str_rmall(EIF_CHARACTER *str, EIF_CHARACTER c, int l);
+RT_LNK void str_mirror(register EIF_CHARACTER *str, register EIF_CHARACTER *new_str, register int len);
+RT_LNK void str_reverse(register EIF_CHARACTER *str, int len);
+RT_LNK long str_atoi(EIF_CHARACTER *str, int length);
+RT_LNK float str_ator(EIF_CHARACTER *str, int length);
+RT_LNK double str_atod(EIF_CHARACTER *str, int length);
+RT_LNK EIF_BOOLEAN str_isi(EIF_CHARACTER *str, EIF_INTEGER length);
+RT_LNK EIF_BOOLEAN str_isr(EIF_CHARACTER *str, EIF_INTEGER length);
+RT_LNK EIF_BOOLEAN str_isd(EIF_CHARACTER *str, EIF_INTEGER length);
 
 #ifdef __cplusplus
 }

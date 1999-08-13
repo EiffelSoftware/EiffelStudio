7703899cdd069623487f43fb6fd61b1e125dd9b9
Emmanuel Stapf <manus@eiffel.com>
1999-08-13 17:54:30 +0000

    Fixed some type definitions: use of EIF_REFERENCE, EIF_POINTER instead of char * Fixed some non-protected objects bugs.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@16309 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_rout_obj.h
+++ Src/bench/C/run-time/eif_rout_obj.h
@@ -11,12 +11,11 @@ extern "C" {
    argument tuple `args', open map `omap' and closed map `cmap'
 */
 
-extern char *rout_obj_create (int16 dftype, char *rout_disp, char *args, char *omap, char *cmap);
+extern EIF_REFERENCE rout_obj_create (int16 dftype, EIF_POINTER rout_disp, EIF_REFERENCE args, EIF_REFERENCE omap, EIF_REFERENCE cmap);
 
 /* Union structure for arguments */
 
 typedef union {
-
 	EIF_BOOLEAN     barg;
 	EIF_CHARACTER   carg;
 	EIF_DOUBLE      darg;
@@ -24,28 +23,27 @@ typedef union {
 	EIF_POINTER     parg;
 	EIF_REAL        farg;
 	EIF_REFERENCE   rarg;
-
 } EIF_ARG_UNION;
 
 /* Argument structure (alloc/free) */
 
-extern char *rout_obj_new_args (int count);
-extern void rout_obj_free_args (char *);
+extern EIF_POINTER rout_obj_new_args (EIF_INTEGER count);
+extern void rout_obj_free_args (EIF_POINTER);
 
 /* Calls */
 
-extern void rout_obj_call_function (char *cur, char *res, char *rout, char *args);
+extern void rout_obj_call_function (EIF_REFERENCE cur, EIF_REFERENCE res, EIF_POINTER rout, EIF_POINTER args);
 
 /* Macros */
 
 #define rout_obj_call_procedure(r,a) ((void(*)(char *, char *, EIF_ARG_UNION *))(r))(((EIF_ARG_UNION*)(a))[0].rarg, (char *)(((EIF_ARG_UNION*)(a))+1), (EIF_ARG_UNION *)0)
 
-#define RBVAL(v) (((char*)(v))==0 ? EIF_FALSE : *((EIF_BOOLEAN *)v))
-#define RCVAL(v) (((char*)(v))==0 ? (EIF_CHARACTER) 0 : *((EIF_CHARACTER *)v))
-#define RDVAL(v) (((char*)(v))==0 ? (EIF_DOUBLE) 0.0 : *((EIF_DOUBLE *)v))
-#define RFVAL(v) (((char*)(v))==0 ? (EIF_REAL) 0.0 : *((EIF_REAL *)v))
-#define RIVAL(v) (((char*)(v))==0 ? (EIF_INTEGER) 0 : *((EIF_INTEGER *)v))
-#define RPVAL(v) (((char*)(v))==0 ? (EIF_POINTER) 0 : *((EIF_POINTER *)v))
+#define RBVAL(v) (((EIF_REFERENCE)(v))== (EIF_REFERENCE) 0 ? EIF_FALSE : *((EIF_BOOLEAN *)v))
+#define RCVAL(v) (((EIF_REFERENCE)(v))== (EIF_REFERENCE) 0 ? (EIF_CHARACTER) 0 : *((EIF_CHARACTER *)v))
+#define RDVAL(v) (((EIF_REFERENCE)(v))== (EIF_REFERENCE) 0 ? (EIF_DOUBLE) 0.0 : *((EIF_DOUBLE *)v))
+#define RFVAL(v) (((EIF_REFERENCE)(v))== (EIF_REFERENCE) 0 ? (EIF_REAL) 0.0 : *((EIF_REAL *)v))
+#define RIVAL(v) (((EIF_REFERENCE)(v))== (EIF_REFERENCE) 0 ? (EIF_INTEGER) 0 : *((EIF_INTEGER *)v))
+#define RPVAL(v) (((EIF_REFERENCE)(v))== (EIF_REFERENCE) 0 ? (EIF_POINTER) 0 : *((EIF_POINTER *)v))
 
 #define rout_obj_putb(a,i,v) (((EIF_ARG_UNION *)(a))[i].barg = RBVAL(v))
 #define rout_obj_putc(a,i,v) (((EIF_ARG_UNION *)(a))[i].carg = RCVAL(v))
@@ -53,7 +51,7 @@ extern void rout_obj_call_function (char *cur, char *res, char *rout, char *args
 #define rout_obj_putf(a,i,v) (((EIF_ARG_UNION *)(a))[i].farg = RFVAL(v))
 #define rout_obj_puti(a,i,v) (((EIF_ARG_UNION *)(a))[i].iarg = RIVAL(v))
 #define rout_obj_putp(a,i,v) (((EIF_ARG_UNION *)(a))[i].parg = RPVAL(v))
-#define rout_obj_putr(a,i,v) (((EIF_ARG_UNION *)(a))[i].rarg = (EIF_REFERENCE)v)
+#define rout_obj_putr(a,i,v) (((EIF_ARG_UNION *)(a))[i].rarg = (EIF_REFERENCE) v)
 
 #ifdef __cplusplus
 }

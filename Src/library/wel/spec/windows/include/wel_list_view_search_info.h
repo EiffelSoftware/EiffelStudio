82175fe33ddc94673e3df165b558f1ed7eed5808
rogers <rogers@eiffel.com>
1999-08-20 23:49:08 +0000

    Bug Fixed.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@16719 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/wel/spec/windows/include/wel_list_view_search_info.h
+++ Src/library/wel/spec/windows/include/wel_list_view_search_info.h
@@ -8,13 +8,13 @@
 #define cwel_list_view_search_info_flags(_ptr_) (((LV_FINDINFO*) _ptr_)->flags)
 #define cwel_list_view_search_info_target(_ptr_) (((LV_FINDINFO*) _ptr_)->psz)
 #define cwel_list_view_search_info_lparam(_ptr_) (((LV_FINDINFO*) _ptr_)->lParam)
-#define cwel_list_view_search_info_starting_position(_ptr_) (((LV_FINDINFO*) _ptr_)->pt)
+#define cwel_list_view_search_info_starting_position(_ptr_) &(((LV_FINDINFO*) _ptr_)->pt)
 #define cwel_list_view_search_info_direction(_ptr_) (((LV_FINDINFO*) _ptr_)->vkDirection)
 
 #define cwel_list_view_search_info_set_flags(_ptr_, _value_) (((LV_FINDINFO *) _ptr_)->flags = (UINT) (_value_))
 #define cwel_list_view_search_info_set_target(_ptr_, _value_) (((LV_FINDINFO *) _ptr_)->psz = (LPCTSTR) (_value_))
 #define cwel_list_view_search_info_set_lparam(_ptr_, _value_) (((LV_FINDINFO *) _ptr_)->lParam = (LPARAM) (_value_))
-#define cwel_list_view_search_info_set_starting_position(_ptr_, _value_) (((LV_FINDINFO *) _ptr_)->pt = (POINT ) (_value_))
+#define cwel_list_view_search_info_set_starting_position(_ptr_, _value_) (((LV_FINDINFO *) _ptr_)->pt =  (*_value_))
 #define cwel_list_view_search_info_set_direction(_ptr_, _value_) (((LV_FINDINFO *) _ptr_)->vkDirection = (UINT) (_value_))
 
 #endif /* __WEL_LIST_VIEW_SEARCH_INFO__ */

9ba0d9e301bea6844e18bb62b678fa1392d326b3
chee <chee@eiffel.com>
1999-09-24 02:04:06 +0000

    Debug
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@17501 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/com/spec/windows/include/ecom_generated_rt_globals.h
+++ Src/library/com/spec/windows/include/ecom_generated_rt_globals.h
@@ -3,7 +3,7 @@
 //  EiffelCOM
 //  Copyright (C) Interactive Software Engineering, 1998.
 //
-//  File:		ecom_rt_globals.h
+//  File:		ecom_generated_rt_globals.h
 //
 //  Contents: 	Global variables used by EiffelCOM wizard
 //
@@ -12,7 +12,9 @@
 #ifndef __ECOM_GENERATED_RUNTIME_GLOBALS_H_INC__
 #define __ECOM_GENERATED_RUNTIME_GLOBALS_H_INC__
 
-#include "ecom_runtime_e_c.h"
+#include "ecom_rt_globals.h"
+#include "ecom_generated_ec.h"
+#include "ecom_generated_ce.h"
 
 extern ecom_generated_ec rt_generated_ec;
 extern ecom_generated_ce rt_generated_ce;

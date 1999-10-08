8ddd761c64ffb0ee46033e8bbbd5a1c6d362e4ec
Emmanuel Stapf <manus@eiffel.com>
1999-10-08 02:37:52 +0000

    Adapted to generate special multithreaded mode libraries. Does not need the non workbench mode since it works only in workbench mode.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@17949 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/ipc/shared/makefile-win.sh
+++ Src/bench/C/ipc/shared/makefile-win.sh
@@ -3,11 +3,13 @@ OUTDIR= .
 INDIR= .
 CC=$cc
 RM=del
+OUTPUT_CMD= $output_cmd
 LIBRUN = $(TOP)\run-time
 LIBIDR = $(TOP)\idrs
 DPFLAGS = -I$(TOP) -I$(LIBRUN) -I$(LIBIDR) -I.
 CFLAGS = $(DPFLAGS) -DWORKBENCH
 JCFLAGS = $(CFLAGS) $ccflags $optimize
+JMTCFLAGS = $(CFLAGS) $mtccflags $optimize
 
 OBJECTS = \
 	com.$obj \
@@ -25,17 +27,72 @@ OBJECTS = \
 	rqst_idrs.$obj \
 	uu.$obj
 
+MT_OBJECTS = \
+	MTcom.$obj \
+	MTidentify.$obj \
+	MTlock.$obj \
+	MTlogfile.$obj \
+	MTnetwork.$obj \
+	MTnetworku.$obj \
+	MTselect.$obj \
+	MTshword.$obj \
+	MTstack.$obj \
+	MTstream.$obj \
+	MTsystem.$obj \
+	MTtransfer.$obj \
+	MTrqst_idrs.$obj \
+	MTuu.$obj
+
 .c.$obj:
 	$(CC) -c $(JCFLAGS) $<
 
-all:: ipc.$lib
+all:: ipc.$lib mtipc.$lib
 
 ipc.$lib: $(OBJECTS)
 	$link_line
 
-com.$obj: eif_logfile.h
-logfile.$obj: eif_logfile.h
-network.$obj: eif_logfile.h
-select.$obj: eif_logfile.h
-transfer.$obj: eif_logfile.h
-identify.$obj: eif_logfile.h
+mtipc.$lib: $(MT_OBJECTS)
+	$link_mtline
+
+MTcom.$obj: com.c
+	$(CC) -c $(JMTCFLAGS) $? $(OUTPUT_CMD)$@
+
+MTidentify.$obj: identify.c
+	$(CC) -c $(JMTCFLAGS) $? $(OUTPUT_CMD)$@
+
+MTlock.$obj: lock.c
+	$(CC) -c $(JMTCFLAGS) $? $(OUTPUT_CMD)$@
+
+MTlogfile.$obj: logfile.c
+	$(CC) -c $(JMTCFLAGS) $? $(OUTPUT_CMD)$@
+
+MTnetwork.$obj: network.c
+	$(CC) -c $(JMTCFLAGS) $? $(OUTPUT_CMD)$@
+
+MTnetworku.$obj: networku.c
+	$(CC) -c $(JMTCFLAGS) $? $(OUTPUT_CMD)$@
+
+MTselect.$obj: select.c
+	$(CC) -c $(JMTCFLAGS) $? $(OUTPUT_CMD)$@
+
+MTshword.$obj: shword.c
+	$(CC) -c $(JMTCFLAGS) $? $(OUTPUT_CMD)$@
+
+MTstack.$obj: stack.c
+	$(CC) -c $(JMTCFLAGS) $? $(OUTPUT_CMD)$@
+
+MTstream.$obj: stream.c
+	$(CC) -c $(JMTCFLAGS) $? $(OUTPUT_CMD)$@
+
+MTsystem.$obj: system.c
+	$(CC) -c $(JMTCFLAGS) $? $(OUTPUT_CMD)$@
+
+MTtransfer.$obj: transfer.c
+	$(CC) -c $(JMTCFLAGS) $? $(OUTPUT_CMD)$@
+
+MTrqst_idrs.$obj: rqst_idrs.c
+	$(CC) -c $(JMTCFLAGS) $? $(OUTPUT_CMD)$@
+
+MTuu.$obj: uu.c
+	$(CC) -c $(JMTCFLAGS) $? $(OUTPUT_CMD)$@
+

8504b3a152d60eb44b587189486cffe1bef835f5
Emmanuel Stapf <manus@eiffel.com>
1999-10-08 02:49:52 +0000

    New Windows Makefiles which generates multithreaded version of the library required to produce a multithreaded run-time. Avoiding the need to change the configuration file all the time.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@17950 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/ipc/app/makefile-win.sh
+++ Src/bench/C/ipc/app/makefile-win.sh
@@ -2,10 +2,21 @@ TOP = ..\..
 OUTDIR = .
 INDIR = .
 CC = $cc
+OUTPUT_CMD = $output_cmd
+CFLAGS = -I$(TOP) -I..\shared -I$(LIBRUN) -I$(LIBIDR)
 JCFLAGS = $(CFLAGS) $ccflags $optimize -DWORKBENCH
+JMTCFLAGS = $(CFLAGS) $mtccflags $optimize -DWORKBENCH
 MAKE = $make
 MV = copy
 RM = del
+# Where shared archive is located (path and name)
+LIBDIR = ..\shared
+LIBRUN = $(TOP)\run-time
+LIBIDR = $(TOP)\idrs
+LIBIDRNAME = idr.$lib
+LIBNAME = ipc.$lib
+LIBMTIDRNAME = mtidr.$lib
+LIBMTNAME = mtipc.$lib
 
 .c.$obj:
 	$(CC) -c $(JCFLAGS) $<
@@ -17,18 +28,26 @@ OBJECTS = \
 	app_server.$obj \
 	$(LIBDIR)\$(LIBNAME)
 
-# Where shared archive is located (path and name)
-LIBDIR = ..\shared
-LIBRUN = $(TOP)\run-time
-LIBIDR = $(TOP)\idrs
-LIBIDRNAME = idr.$lib
-LIBNAME = ipc.$lib
-LIBARCH = $(LIBDIR)\$(LIBNAME)
-LIBIDRARCH = $(LIBIDR)\$(LIBIDRNAME)
-CFLAGS = -I$(TOP) -I..\shared -I$(LIBRUN) -I$(LIBIDR)
+MT_OBJECTS = \
+	MTapp_listen.$obj \
+	MTapp_proto.$obj \
+	MTapp_server.$obj \
+	$(LIBDIR)\$(LIBMTNAME)
 
-all: network.$lib
+all: network.$lib mtnetwork.$lib
 
 network.$lib: $(OBJECTS) $(LIBDIR)\$(LIBNAME)
 	$link_line
+
+mtnetwork.$lib: $(MT_OBJECTS) $(LIBDIR)\$(LIBMTNAME)
+	$link_mtline
+
+MTapp_listen.$obj: app_listen.c
+	$(CC) -c $(JMTCFLAGS)  $? $(OUTPUT_CMD)$@
+
+MTapp_proto.$obj: app_proto.c
+	$(CC) -c $(JMTCFLAGS)  $? $(OUTPUT_CMD)$@
+
+MTapp_server.$obj: app_server.c
+	$(CC) -c $(JMTCFLAGS)  $? $(OUTPUT_CMD)$@
 

55c0d44ce69b1753c122559dbd1aaa966b3c143c
Emmanuel Stapf <manus@eiffel.com>
1999-10-09 01:17:37 +0000

    Fixed some warnings on Windows at the C compilation run-time.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@17984 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_threads.c
+++ Src/bench/C/run-time/eif_threads.c
@@ -609,7 +609,7 @@ rt_private EIF_THR_ENTRY_TYPE eif_thr_entry (EIF_THR_ENTRY_ARG_TYPE arg)
 		EIF_END_GET_CONTEXT
 	}
 	eif_thr_exit ();
-#ifndef SOLARIS_THREADS
+#if (!defined SOLARIS_THREADS && !defined EIF_WIN32)
 	return (EIF_THR_ENTRY_TYPE) 0;	/* 	NOTREACHED. */
 #else
 	/* On solaris, EIF_ENTRY_TYPE is void: there is no Null void. */

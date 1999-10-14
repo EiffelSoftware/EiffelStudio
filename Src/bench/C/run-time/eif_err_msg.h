b42ff072d9ac78eb04d282ddac6f42a6acee3d2c
manuelt <manuelt@eiffel.com>
1999-10-14 23:54:14 +0000

    C++ conformance (move paragrah `extern "C"')
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@18029 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_err_msg.h
+++ Src/bench/C/run-time/eif_err_msg.h
@@ -6,14 +6,14 @@
 #ifndef _err_msg_h
 #define _err_msg_h
 
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 #include "eif_globals.h"
 #include "eif_file.h"
 #include <stdio.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 #ifdef EIF_WIN32
 extern int print_err_msg(FILE *err, char *StrFmt, ...);
 extern char *exception_trace_string;

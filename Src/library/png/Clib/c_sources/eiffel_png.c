bc070af96e082e37b4312815dc96bf8dd6f848a9
pascalf <pascalf@eiffel.com>
1999-10-20 01:16:14 +0000

    ko
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@18087 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/png/Clib/c_sources/eiffel_png.c
+++ Src/library/png/Clib/c_sources/eiffel_png.c
@@ -27,4 +27,13 @@ rt_public EIF_POINTER c_medium_bold_font () {
 rt_public EIF_POINTER c_giant_font () {
 	return (EIF_POINTER) gdFontGiant;
 }
+rt_public EIF_INTEGER c_get_red (gdImagePtr img,int _i_) {
+	return (EIF_INTEGER) gdImageRed(img,_i_);
+}
+rt_public EIF_INTEGER c_get_blue (gdImagePtr img,int _i_) {
+	return (EIF_INTEGER) gdImageBlue(img,_i_);
+}
+rt_public EIF_INTEGER c_get_green (gdImagePtr img,int _i_) {
+	return (EIF_INTEGER) gdImageGreen(img,_i_);
+}
 

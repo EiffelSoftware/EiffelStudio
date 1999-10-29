32a26f2136110e4d462b2738ef875246885a742d
Emmanuel Stapf <manus@eiffel.com>
1999-10-29 19:45:08 +0000

    Removed specific call to EIF_BORLAND since it is not needed anymore with Borland C++ Builder 4.0
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@18322 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/wel/spec/windows/include/cctrl.h
+++ Src/library/wel/spec/windows/include/cctrl.h
@@ -17,19 +17,6 @@
 extern "C" {
 #endif
 
-#ifdef EIF_BORLAND
-#define UDS_HOTTRACK		0x0100
-#define TCIF_STATE			0x0010
-#define TCS_VERTICAL		0x0080
-#define TCS_SCROLLOPPOSITE	0x0001
-#define TCS_RIGHT			0x0002
-#define TCS_BOTTOM			0x0002
-#define TCS_HOTTRACK		0x0040
-#define LVCF_IMAGE			0x0010
-#define PBS_SMOOTH			0x01
-#define PBS_VERTICAL		0x04
-#endif
-
 #ifdef __cplusplus
 }
 #endif

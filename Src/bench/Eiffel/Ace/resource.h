17e1c6f48363b148a1db039340ae855361000dff
Emmanuel Stapf <manus@eiffel.com>
1999-11-01 03:36:01 +0000

    Changed the dialog window to show a progress bar for the trial period.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@18343 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/Eiffel/Ace/resource.h
+++ Src/bench/Eiffel/Ace/resource.h
@@ -15,13 +15,21 @@
 #define ID_CB_DRIVE                     103
 #define ID_LB_DIR                       104
 #define IDD_REPLACEDIALOG               108
+#define IDD_PROMPT                      201
+#define IDB_LOGO                        202
+#define IDD_REGISTER                    203
 #define Prg_compilation_progress        1000
+#define IDC_PROGRESS                    1000
 #define Txt_degree                      1001
+#define IDC_USERNAME                    1001
 #define Txt_entity                      1002
 #define Txt_current_degree              1003
 #define Txt_nbr_to_go                   1004
 #define Txt_current_entity              1005
 #define Txt_current_nbr_to_go           1006
+#define IDC_REGISTER                    2000
+#define IDC_USER                        2001
+#define IDC_KEY                         2002
 #define ID_FINDNEXT                     3000
 #define ID_REPLACEALL                   3002
 #define IDC_FINDEDIT                    3007
@@ -35,15 +43,7 @@
 #define ID_FINDSTATIC                   3017
 #define ID_REPLACESTATIC                3018
 #define Txt_percentage                  3019
-
-#define IDD_PROMPT                      201
-#define IDB_LOGO                        202
-#define IDD_REGISTER                    203
-#define IDC_REGISTER                    2000
-#define IDC_USER                        2001
-#define IDC_KEY                         2002
-
-#define IDC_STATIC						-1
+#define IDC_STATIC                      -1
 
 // Next default values for new objects
 // 
@@ -52,7 +52,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        101
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif

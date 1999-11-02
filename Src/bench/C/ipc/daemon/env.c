ffc29f9916362a906b061c297829660a033241f5
Emmanuel Stapf <manus@eiffel.com>
1999-11-02 04:09:56 +0000

    Changed the way we look at the environment variables when launching ebench. First we look in the environment variable, if not found, we look in the registry keys at `HKEY_CURRENT_USER' then in `HKEY_LOCAL_MACHINE'.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@18413 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/ipc/daemon/env.c
+++ Src/bench/C/ipc/daemon/env.c
@@ -18,53 +18,74 @@ char *win_eif_getenv (char *k, char *app)
 	If app is not set use the modulename of the executing module.
 */
 {
-	char *key, *lower_k;
-	static char buf[1024];
-	int appl_len, key_len;
-	char modulename [PATH_MAX + 1];
-	HKEY hkey;
-	DWORD bsize;
+	char *result = getenv (k);
 
-	GetModuleFileName (NULL, modulename, PATH_MAX);
+	if (!result)
+		return result;
+	else {
+		char *key, *lower_k;
+		static char buf[1024];
+		int appl_len, key_len;
+		char modulename [PATH_MAX + 1];
+		HKEY hkey;
+		DWORD bsize;
+	
+		GetModuleFileName (NULL, modulename, PATH_MAX);
+	
+		if (app == NULL)
+			appl_len = strrchr (modulename, '.') - strrchr (modulename, '\\') -1;
+		else
+			appl_len = strlen (app);
+		key_len = strlen (k);
+		if ((key = (char *) calloc (appl_len + 57+key_len, 1)) == NULL)
+			return result;
+	
+		if ((lower_k = (char *) calloc (key_len+1, 1)) == NULL) {
+			free (key);
+			return result;
+		}
+	
+		strcpy (lower_k, k);
+		CharLowerBuff (lower_k, key_len);
+	
+		strcpy (key, "Software\\ISE\\Eiffel45\\");
+		if (app == NULL)
+			strncat (key, strrchr(modulename, '\\')+1, appl_len);
+		else
+			strcat (key, app);
+	
+		bsize = 1024;
+		if (RegOpenKeyEx (HKEY_CURRENT_USER, key, 0, KEY_READ, &hkey) != ERROR_SUCCESS) {
+			if (RegOpenKeyEx (HKEY_LOCAL_MACHINE, key, 0, KEY_READ, &hkey) != ERROR_SUCCESS) {
+				free (key);
+				free (lower_k);
+				return result;
+			}
+			if (RegQueryValueEx (hkey, lower_k, NULL, NULL, buf, &bsize) != ERROR_SUCCESS) {
+				free (key);
+				free (lower_k);
+				RegCloseKey (hkey);
+				return result;
+			}
+		} else {
+			if (RegQueryValueEx (hkey, lower_k, NULL, NULL, buf, &bsize) != ERROR_SUCCESS) {
+				if (RegOpenKeyEx (HKEY_LOCAL_MACHINE, key, 0, KEY_READ, &hkey) != ERROR_SUCCESS) {
+					free (key);
+					free (lower_k);
+					return result;
+				}
+				if (RegQueryValueEx (hkey, lower_k, NULL, NULL, buf, &bsize) != ERROR_SUCCESS) {
+					free (key);
+					free (lower_k);
+					RegCloseKey (hkey);
+					return result;
+				}
+			}
+		}
 
-	if (app == NULL)
-		appl_len = strrchr (modulename, '.') - strrchr (modulename, '\\') -1;
-	else
-		appl_len = strlen (app);
-	key_len = strlen (k);
-	if ((key = (char *) calloc (appl_len + 57+key_len, 1)) == NULL)
-		return getenv (k);
-
-	if ((lower_k = (char *) calloc (key_len+1, 1)) == NULL) {
-		free (key);
-		return getenv (k);
-	}
-
-	strcpy (lower_k, k);
-	CharLowerBuff (lower_k, key_len);
-
-	strcpy (key, "Software\\ISE\\Eiffel45\\");
-	if (app == NULL)
-		strncat (key, strrchr(modulename, '\\')+1, appl_len);
-	else
-		strcat (key, app);
-
-	if (RegOpenKeyEx (HKEY_LOCAL_MACHINE, key, 0, KEY_READ, &hkey) != ERROR_SUCCESS) {
-		free (key);
-		free (lower_k);
-		return getenv (k);
-	}
-
-	bsize = 1024;
-	if (RegQueryValueEx (hkey, lower_k, NULL, NULL, buf, &bsize) != ERROR_SUCCESS) {
 		free (key);
 		free (lower_k);
 		RegCloseKey (hkey);
-		return getenv (k);
+		return (char *) buf;
 	}
-
-	free (key);
-	free (lower_k);
-	RegCloseKey (hkey);
-	return (char *) buf;
 }

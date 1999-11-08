d42ed763188adc282572fd16fadec9a739a863aa
raphaels <raphaels@eiffel.com>
1999-11-08 20:55:15 +0000

    com examples
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@18633 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/examples/com/wizard/string_manipulator/client/resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by String_manipulator_client.rc
+//
+#define Set_string_button               3
+#define Replace_substring_button        4
+#define Prune_button                    6
+#define String_manipulator_client_dialog_box 101
+#define Set_string_edit                 1003
+#define Current_string_display          1004
+#define Prune_all_edit                  1005
+#define Replace_substring_index_1_edit  1006
+#define Replace_substring_index_2_edit  1007
+#define Replace_substring_edit          1008
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif

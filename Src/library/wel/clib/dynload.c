e240e23554a574aee04ac81da888fc18ed5aa62a
pichery <pichery@eiffel.com>
2000-03-14 22:37:29 +0000

    Added Function dynamic loading, and Windows version control.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@20761 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/library/wel/clib/dynload.c
@@ -0,0 +1,192 @@
+/*****************************************************************************/
+/* Filename: dynload.c  [ associated WEL class: numerous ]                   */
+/* Author  : arnaud PICHERY [ aranud@mail.dotcom.fr ]                        */
+/*****************************************************************************/
+/* Used to check for the existence of a function on the current windows      */
+/* platform (some functions are available on Windows 98 but not on 95..)     */
+/* Wrapper to dynamically call a function                                    */
+/*****************************************************************************/
+#include "Windows.h"
+#include "shlwapi.h"
+#include "eif_portable.h"
+#include "wel_dynload.h"
+
+/*---------------------------------------------------------------------------*/
+/* Return the address of the specified function. The function is specified   */
+/* through its name and the name of the module (.dll) where it is located    */
+/*                                                                           */
+/* a typical call to this function would be:                                 */
+/*  ptr = cwin_get_function_address("kernel.dll", "GetFreeDiskSpaceEx");     */
+/*---------------------------------------------------------------------------*/
+EIF_POINTER cwin_get_function_address(
+		char *pszModuleName,		// Name of the module where the function is
+		char *pszFunctionName		// Name of the function to retrieve.
+		)
+	{
+	FARPROC pFunctionAddress;		// Address of the function
+	HMODULE hModule;
+	
+	/* Load the library */
+	hModule = LoadLibrary(pszModuleName);
+	if (hModule==NULL)
+		{
+		/* Unable to load the library...we give up */
+		return NULL;
+		}
+		
+	/* Retrieve Address of the function */
+	pFunctionAddress = GetProcAddress(hModule, pszFunctionName);
+
+	/* Free the library */
+	FreeLibrary(hModule);
+	
+	/* Return the retrieved address (it can be NULL if GetProcAddress returned so) */
+	return (EIF_POINTER)pFunctionAddress;
+	}
+
+/*---------------------------------------------------------------------------*/
+/* The MaskBlt function combines the color data for the source and           */
+/* destination bitmaps using the specified mask and raster operation.        */
+/*---------------------------------------------------------------------------*/
+void cwin_mask_blt(
+		FARPROC pMaskBltFuncAddr,
+		HDC hdcDest,     // handle to destination DC
+		int nXDest,      // x-coord of destination upper-left corner
+		int nYDest,      // y-coord of destination upper-left corner 
+		int nWidth,      // width of source and destination
+		int nHeight,     // height of source and destination
+		HDC hdcSrc,      // handle to source DC
+		int nXSrc,       // x-coord of upper-left corner of source
+		int nYSrc,       // y-coord of upper-left corner of source
+		HBITMAP hbmMask, // handle to monochrome bit mask
+		int xMask,       // horizontal offset into mask bitmap
+		int yMask,       // vertical offset into mask bitmap
+		DWORD dwRop      // raster operation code
+		)
+	{
+	pMaskBltFuncAddr(
+		hdcDest, nXDest, nYDest, nWidth, nHeight, 
+		hdcSrc, nXSrc, nYSrc,
+		hbmMask, xMask, yMask,
+		dwRop
+		);
+	}
+
+/*---------------------------------------------------------------------------*/
+/* Return the version number of the specified DLL. The version number is     */
+/* packed.                                                                   */
+/*                                                                           */
+/* a typical call to this function would be:                                 */
+/*  if(cwin_get_dll_version("comctl32.dll") >= PACKVERSION(4,71))            */
+/*---------------------------------------------------------------------------*/
+DWORD cwin_get_dll_version(char *pszDllName)
+	{
+	HINSTANCE hinstDll;
+	DWORD dwVersion = 0;
+
+	/* Load the dll library into memory in case it was not */
+	hinstDll = LoadLibrary(pszDllName);
+
+	if(hinstDll)
+		{
+		FARPROC pDllGetVersion;
+
+		pDllGetVersion = GetProcAddress(hinstDll, "DllGetVersion");
+
+		/* Because some DLLs may not implement this function, you
+		 * must test for it explicitly. Depending on the particular 
+		 * DLL, the lack of a DllGetVersion function may
+		 * be a useful indicator of the version.
+		 */
+		if(pDllGetVersion)
+			{
+			DLLVERSIONINFO dvi;
+			HRESULT hr;
+
+			ZeroMemory(&dvi, sizeof(dvi));
+			dvi.cbSize = sizeof(dvi);
+
+			hr = (*pDllGetVersion)(&dvi);
+
+			if(SUCCEEDED(hr))
+				{
+				dwVersion = PACKVERSION(dvi.dwMajorVersion, dvi.dwMinorVersion);
+				}
+			}
+
+		/* Unload the library if it was not loaded before the call to LoadLibrary */
+		FreeLibrary(hinstDll);
+		}
+	return dwVersion;
+	}
+
+/*---------------------------------------------------------------------------*/
+/* Return the version number of one of the "shell & common controls" dll:    */ 
+/*   - shell32.dll                                                           */
+/*   - commctrl32.dll                                                        */
+/*   - shlwapi.dll                                                           */
+/* The version number is packed.                                             */
+/*                                                                           */
+/* a typical call to this function would be:                                 */
+/*  if(cwin_get_shellcomctrl_dll_version("comctl32.dll")>=PACKVERSION(4,71)) */
+/*---------------------------------------------------------------------------*/
+DWORD cwin_get_shellcomctrl_dll_version(char *pszDllName)
+	{
+	DWORD dwVersion = cwin_get_dll_version(pszDllName);
+	
+	if (dwVersion > 0)
+		return dwVersion;
+	else
+		{
+		// DllGetVersion is not supported --> Win95 (4.00) or Win95+IE4 (4.70)
+		void *pInitCommonCtrlEx_addr = NULL;
+
+		pInitCommonCtrlEx_addr = cwin_get_function_address("Comctl32.dll", "InitCommonControlsEx");
+		if (pInitCommonCtrlEx_addr == NULL)
+			{
+			// InitCommonCtrlEx do NOT exist on the system --> Win95 alone
+			return PACKVERSION(4,0);
+			}
+		else
+			{
+			// InitCommonCtrlEx do exist on the system --> Win95 + IE3
+			return PACKVERSION(4,70);
+			}
+		}
+	}
+
+/*---------------------------------------------------------------------------*/
+/* Return the version number of the "shell32.dll" DLL. The version number is */
+/* packed.                                                                   */
+/*                                                                           */
+/* a typical call to this function would be:                                 */
+/*  if(cwin_get_shell32dll_version() >= PACKVERSION(4,71))                   */
+/*---------------------------------------------------------------------------*/
+DWORD cwin_get_shell32dll_version(void)
+	{
+	return cwin_get_shellcomctrl_dll_version("Shell32.dll");
+	}
+
+/*---------------------------------------------------------------------------*/
+/* Return the version number of the "Comctl32.dll" DLL. The version number   */
+/* is packed.                                                                */
+/*                                                                           */
+/* a typical call to this function would be:                                 */
+/*  if(cwin_get_comctl32dll_version() >= PACKVERSION(4,71))                  */
+/*---------------------------------------------------------------------------*/
+DWORD cwin_get_comctl32dll_version(void)
+	{
+	return cwin_get_shellcomctrl_dll_version("Comctl32.dll");
+	}
+
+/*---------------------------------------------------------------------------*/
+/* Return the version number of the "Shlwapi.dll" DLL. The version number is */
+/* packed.                                                                   */
+/*                                                                           */
+/* a typical call to this function would be:                                 */
+/*  if(cwin_get_shlwapidll_version() >= PACKVERSION(4,71))                   */
+/*---------------------------------------------------------------------------*/
+DWORD cwin_get_shlwapidll_version(void)
+	{
+	return cwin_get_shellcomctrl_dll_version("Shlwapi.dll");
+	}

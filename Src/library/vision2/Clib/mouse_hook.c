907668dc375c4b2ef5002d43b3fb485d10582848
pichery <pichery@eiffel.com>
2000-03-23 18:54:34 +0000

    Work on Windows hooks
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@21128 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/vision2/Clib/mouse_hook.c
+++ Src/library/vision2/Clib/mouse_hook.c
@@ -62,3 +62,64 @@ void c_ev_unhook_mouse()
 	hMouseHook = NULL;
 	}
 
+/*---------------------------------------------------------------------------*/
+/* FUNC: MouseProc                                                           */
+/* ARGS: nCode: hook code                                                    */
+/*       wParam: message identifier                                          */
+/*       lParam: mouse coordinates                                           */
+/*---------------------------------------------------------------------------*/
+/* The MouseProc hook procedure is an application-defined or library-defined */
+/* callback function used with the SetWindowsHookEx function. The system     */
+/* calls this function whenever an application calls the GetMessage or       */
+/* PeekMessage function and there is a mouse message to be processed.        */
+/*                                                                           */
+/* The HOOKPROC type defines a pointer to this callback function. MouseProc  */
+/* is a placeholder for the application-defined or library-defined function  */
+/* name.                                                                     */
+/*---------------------------------------------------------------------------*/
+LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam)
+	{
+	UINT Msg = (UINT)wParam;
+	DWORD dwMousePos;
+	MOUSEHOOKSTRUCT *pInfo = (MOUSEHOOKSTRUCT *) lParam;
+
+
+	/* We should not handle this message */
+	if (hMouseHook == NULL)
+		return 0;
+
+	/* We should not handle this message */
+	if (hHookWindow == NULL ||	/* No window has requested a hook */
+		nCode < 0) ||			/* Windows tell us not to handle this msg */
+		pInfo->hwnd == hHookWindow	/* The target window is the window that */
+									/* has requested the hook, we do not */
+									/* want to duplicate the mouse message */
+		)
+		return CallNextHookEx(hMouseHook, nCode, wParam, lParam);
+
+
+	/* We should handle this mouse message */
+	dwMousePos = MAKELONG(pInfo->pt.x, pInfo->pt.y);
+
+	switch (Msg)
+		{
+		case WM_MOUSEMOVE:
+		case WM_LBUTTONDOWN:
+		case WM_MBUTTONDOWN:
+		case WM_RBUTTONDOWN:
+		case WM_LBUTTONUP:
+		case WM_MBUTTONUP:
+		case WM_RBUTTONUP:
+		case WM_LBUTTONDBLCLK:
+		case WM_MBUTTONDBLCLK:
+		case WM_RBUTTONDBLCLK:
+			SendMessage(hHookWindow, Msg, 0, dwMousePos);
+			return 1;	/* To prevent the system from passing the message */
+						/* to the target window procedure. */
+		}
+
+	/* We have not handled the message, so go to next hook function */
+	return CallNextHookEx(hMouseHook, nCode, wParam, lParam);
+	}
+
+#endif /* EIF_WIN32 */

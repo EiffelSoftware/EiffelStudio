ec3ed54638fcca39ad57366c661d0bf4bc008321
brendel <brendel@eiffel.com>
2000-04-13 23:39:36 +0000

    Removed copying of test_pixmap and Ace.ace, which is stupid. Added make of WEL.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@21784 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/vision2/release/make_msc.bat
+++ Src/library/vision2/release/make_msc.bat
@@ -1,11 +1,8 @@
 @echo off
 echo -- Compiling libraries necessary for EiffelVision2...
 echo.
-cd vision2\test
-move test_pixmap test_pixmap.unix
-copy test_pixmap.win test_pixmap
-move Ace.ace Ace.unix.ace
-copy Ace.win.ace Ace.ace
+cd wel\Clib
+call make_msc.bat
 cd ..\..\libpng
 call make_msc.bat
 cd ..\zlib

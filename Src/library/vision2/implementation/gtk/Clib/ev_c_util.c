64860fca7bc2c73a00499e6fde1d724b21c6c30f
king <king@eiffel.com>
2000-04-18 21:43:23 +0000

    Moved string_pointer_deref definition from header to source
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@21871 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/vision2/implementation/gtk/Clib/ev_c_util.c
+++ Src/library/vision2/implementation/gtk/Clib/ev_c_util.c
@@ -10,6 +10,8 @@
 #include "ev_c_util.h"
 #include "eif_except.h"
 
+char* string_pointer_deref (char** pointer) {return *pointer;}
+
 void* pointer_array_i_th (void** pointer_array, int index)
 {
 	return pointer_array [index];
@@ -93,6 +95,9 @@ void enable_ev_gtk_log (void)
 //------------------------------------------------------------------------------
 //
 // $Log$
+// Revision 1.5  2000/04/18 21:43:23  king
+// Moved string_pointer_deref definition from header to source
+//
 // Revision 1.4  2000/04/18 17:58:01  oconnor
 // Renamed get_pointer_from_array_by_index -> pointer_array_i_th
 // Added string_pointer_deref (pointer: POINTER): POINTER

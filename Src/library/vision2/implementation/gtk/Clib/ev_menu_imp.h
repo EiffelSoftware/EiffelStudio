650e0f0df8ce382977c92cf179627bdee6b8d6b5
brendel <brendel@eiffel.com>
2000-04-25 17:37:54 +0000

    Initial.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@22139 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/library/vision2/implementation/gtk/Clib/ev_menu_imp.h
@@ -0,0 +1,47 @@
+//------------------------------------------------------------------------------
+// ev_menu_imp.h
+//------------------------------------------------------------------------------
+// description: "C function for EV_MENU_IMP."
+// status: "See notice at end of file"
+// date: "$Date$"
+// revision: "$Revision$"
+//------------------------------------------------------------------------------
+
+#ifndef _EV_MENU_IMP_H_INCLUDED_
+#define _EV_MENU_IMP_H_INCLUDED_
+
+#include <gtk/gtk.h>
+
+void
+c_gtk_menu_popup (GtkMenu * menu, gint x, gint y);
+
+#endif
+
+//------------------------------------------------------------------------------
+// EiffelVision2: library of reusable components for ISE Eiffel.
+// Copyright (C) 1986-2000 Interactive Software Engineering Inc.
+// All rights reserved. Duplication and distribution prohibited.
+// May be used only with ISE Eiffel, under terms of user license.
+// Contact ISE for any other use.
+//
+// Interactive Software Engineering Inc.
+// ISE Building, 2nd floor
+// 270 Storke Road, Goleta, CA 93117 USA
+// Telephone 805-685-1006, Fax 805-685-6869
+// Electronic mail <info@eiffel.com>
+// Customer support e-mail <support@eiffel.com>
+// For latest info see award-winning pages: http://www.eiffel.com
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// CVS log
+//------------------------------------------------------------------------------
+//
+// $Log$
+// Revision 1.1  2000/04/25 17:37:54  brendel
+// Initial.
+//
+//
+//------------------------------------------------------------------------------
+// End of CVS log
+//------------------------------------------------------------------------------

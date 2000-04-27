6aa53a42284da68c09f99a8380e93a1e4daf4a41
marina <marina@eiffel.com>
2000-04-27 00:08:04 +0000

    Removed before by mistake.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@22269 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/library/com/spec/windows/include/ecom_font.h
@@ -0,0 +1,39 @@
+/*-----------------------------------------------------------
+No description available.
+-----------------------------------------------------------*/
+
+#ifndef __ECOM_FONT_H__
+#define __ECOM_FONT_H__
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+class Font;
+
+#include "eif_com.h"
+
+#include "eif_eiffel.h"
+
+#include "windows.h"
+
+class Font : public IDispatch
+{
+public:
+	Font () {};
+	~Font () {};
+
+
+protected:
+
+
+private:
+
+
+};
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

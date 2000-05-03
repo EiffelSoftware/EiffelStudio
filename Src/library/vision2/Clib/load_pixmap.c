7a58f2f623ecd111a022d2332ad31feb02a3bc4f
pichery <pichery@eiffel.com>
2000-05-03 00:23:57 +0000

    Added default window pixmap.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@22506 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/vision2/Clib/load_pixmap.c
+++ Src/library/vision2/Clib/load_pixmap.c
@@ -68,8 +68,9 @@ typedef struct TBufferedFile  BufferedFile;
 
 /* Bufferred File definition */
 struct TLoadPixmapCtx {
-		void *pCurrObject;
-		char *pszFileName;
+		void *pCurrObject;		// Current Eiffel Object executed
+		char *pszFileName;		// File name
+		FILE *pFile;			// File Pointer
 		void (*LoadPixmapUpdateObject)(
 				void*, 
 				unsigned int, 
@@ -118,14 +119,65 @@ void 			c_ev_load_windows_file(
 #endif
 
 
+// Default Vision2 icon
+unsigned char default_vision2_icon[833] = {
+		137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 32, 0, 
+		0, 0, 32, 8, 2, 0, 0, 0, 252, 24, 237, 163, 0, 0, 0, 7, 116, 73, 77, 69, 7, 
+		208, 5, 2, 2, 3, 16, 137, 29, 102, 189, 0, 0, 0, 9, 112, 72, 89, 115, 0, 0, 
+		10, 240, 0, 0, 10, 240, 1, 66, 172, 52, 152, 0, 0, 2, 224, 73, 68, 65, 84, 
+		120, 218, 99, 60, 198, 64, 91, 192, 2, 196, 98, 41, 41, 188, 54, 54, 84, 55, 
+		250, 243, 145, 35, 175, 230, 204, 1, 89, 0, 52, 93, 200, 207, 239, 235, 249, 
+		243, 4, 245, 112, 168, 168, 252, 184, 115, 135, 24, 211, 185, 13, 13, 129, 
+		36, 212, 2, 32, 0, 154, 126, 205, 217, 153, 160, 54, 165, 89, 179, 238, 165, 
+		165, 17, 99, 129, 214, 222, 189, 16, 6, 19, 73, 190, 102, 149, 144, 32, 53, 
+		160, 72, 176, 128, 145, 157, 157, 153, 135, 71, 166, 161, 129, 86, 22, 168, 
+		173, 90, 197, 103, 107, 11, 180, 131, 86, 22, 220, 244, 247, 127, 189, 120, 
+		241, 135, 157, 59, 105, 101, 1, 16, 252, 253, 252, 153, 93, 78, 142, 150, 22, 
+		124, 248, 192, 174, 164, 68, 67, 11, 126, 191, 126, 205, 161, 172, 76, 43, 
+		11, 216, 164, 165, 153, 184, 185, 185, 180, 181, 105, 101, 1, 183, 169, 41, 
+		40, 14, 20, 20, 24, 152, 72, 208, 69, 138, 5, 122, 122, 191, 159, 61, 3, 38, 
+		83, 14, 82, 162, 129, 4, 11, 120, 204, 205, 127, 220, 187, 247, 231, 195, 7, 
+		62, 71, 71, 62, 123, 123, 70, 54, 54, 170, 90, 192, 200, 200, 173, 175, 255, 
+		255, 207, 31, 96, 248, 40, 207, 154, 165, 181, 127, 63, 191, 171, 43, 49, 
+		250, 88, 136, 52, 31, 152, 252, 89, 37, 37, 117, 79, 159, 102, 230, 226, 250, 
+		243, 241, 227, 117, 15, 143, 47, 39, 78, 80, 205, 2, 102, 126, 126, 229, 249, 
+		243, 255, 255, 254, 253, 113, 247, 110, 102, 94, 222, 191, 31, 63, 254, 126, 
+		245, 138, 72, 151, 17, 21, 68, 162, 177, 177, 108, 146, 146, 207, 39, 77, 
+		186, 25, 16, 240, 118, 221, 186, 127, 63, 126, 136, 198, 197, 17, 153, 227, 
+		136, 178, 224, 197, 148, 41, 192, 162, 244, 235, 217, 179, 204, 124, 124, 
+		156, 106, 106, 66, 1, 1, 178, 245, 245, 250, 23, 46, 80, 45, 136, 184, 244, 
+		244, 216, 229, 229, 129, 69, 169, 210, 140, 25, 44, 2, 2, 64, 145, 95, 79, 
+		159, 62, 170, 169, 161, 142, 5, 64, 183, 75, 149, 148, 48, 50, 49, 137, 165, 
+		166, 126, 62, 118, 236, 221, 186, 117, 188, 86, 86, 223, 175, 95, 231, 119, 
+		114, 122, 189, 96, 1, 165, 22, 112, 25, 26, 202, 119, 116, 8, 184, 185, 189, 
+		152, 49, 227, 81, 85, 213, 223, 247, 239, 129, 130, 223, 175, 93, 3, 90, 198, 
+		169, 169, 201, 161, 166, 246, 227, 214, 45, 252, 38, 16, 136, 131, 111, 23, 
+		47, 62, 237, 236, 252, 255, 255, 255, 139, 137, 19, 33, 166, 3, 193, 199, 
+		125, 251, 128, 181, 255, 203, 153, 51, 229, 59, 59, 9, 250, 128, 80, 36, 255, 
+		251, 39, 158, 150, 246, 253, 198, 141, 239, 183, 110, 169, 44, 93, 106, 246, 
+		249, 51, 48, 139, 1, 107, 230, 103, 93, 93, 64, 31, 48, 178, 178, 10, 120, 
+		122, 82, 100, 1, 176, 4, 21, 14, 14, 126, 191, 121, 179, 104, 76, 12, 191, 
+		163, 227, 155, 149, 43, 129, 249, 89, 247, 228, 201, 207, 199, 143, 3, 61, 
+		241, 102, 217, 50, 169, 226, 98, 86, 113, 113, 242, 45, 16, 75, 78, 6, 22, 
+		18, 47, 167, 79, 23, 207, 200, 184, 108, 110, 126, 47, 37, 229, 162, 129, 1, 
+		195, 223, 191, 82, 165, 165, 119, 162, 163, 197, 18, 19, 191, 223, 190, 45, 
+		158, 158, 78, 166, 5, 76, 92, 92, 226, 89, 89, 159, 14, 31, 254, 247, 243, 
+		231, 167, 35, 71, 126, 61, 126, 12, 20, 252, 245, 232, 209, 173, 136, 136, 
+		215, 243, 231, 255, 121, 247, 238, 86, 120, 248, 191, 111, 223, 94, 205, 155, 
+		135, 199, 16, 124, 169, 72, 40, 40, 136, 77, 92, 252, 78, 124, 252, 159, 183, 
+		111, 159, 52, 54, 194, 197, 191, 156, 60, 9, 97, 0, 237, 120, 88, 92, 140, 
+		63, 12, 160, 22, 0, 91, 122, 240, 182, 24, 28, 0, 83, 225, 191, 223, 191, 
+		165, 203, 203, 165, 203, 202, 24, 72, 4, 64, 3, 127, 130, 125, 12, 178, 0, 
+		216, 74, 197, 170, 8, 162, 130, 60, 0, 212, 11, 49, 150, 145, 214, 205, 119, 
+		0, 239, 86, 18, 108, 118, 212, 26, 236, 0, 0, 0, 0, 73, 69, 78, 68, 174, 66, 
+		96, 130
+	};
+
 
 /*---------------------------------------------------------------------------*/
 /* FUNC: c_ev_load_pixmap                                                    */
 /* ARGS: - pszFileName: name of the graphical file to load.                  */
-/*---------------------------------------------------------------------------*/
-/* Return a pointer onto raw graphical data (4 bytes/pixels: R/G/B/Alpha)    */
-/* NULL if an error occured while reading the file or if the file format is  */
-/* not recognized.                                                           */
+/*         if NULL, then the default EiffelVision2 icon is loaded            */
 /*---------------------------------------------------------------------------*/
 void c_ev_load_pixmap(
 		void *pCurrObject, 
@@ -144,27 +196,41 @@ void c_ev_load_pixmap(
 	FILE *pFile;
 	unsigned char *pBuffer;
 	BufferedFile stBufFile;
-	unsigned char nFileFormat; /* File format found: one of FILEFORMAT_XXXX */
+	unsigned char nFileFormat; // File format found: one of FILEFORMAT_XXXX
 	LoadPixmapCtx stCtx;
 	
-	/* Open the file */
-	pFile = fopen ((const char *)pszFileName, "rb");
-	if (pFile == NULL)
+	if (pszFileName == NULL)
 		{
-		/* Unable to open the file, return NULL */
-		LoadPixmapUpdateObject(
-			pCurrObject,
-			LOADPIXMAP_ERROR_UNABLE_OPEN_FILE,
-			0,
-			0,
-			0,
-			NULL,
-			NULL
-		);
-		return;
+		FILE *pFile2 = fopen ("C:\\validate.png", "wb");
+		fwrite (default_vision2_icon, 1, sizeof (default_vision2_icon), pFile2);
+		fclose (pFile2);
+
+		// Load the default Vision2 icon
+		pFile = tmpfile(); // create a temporary file
+		fwrite (default_vision2_icon, 1, sizeof (default_vision2_icon), pFile);
+		fseek (pFile, 0, SEEK_SET);
+		}
+	else
+		{
+		// Open the file
+		pFile = fopen ((const char *)pszFileName, "rb");
+		if (pFile == NULL)
+			{
+			// Unable to open the file, return NULL
+			LoadPixmapUpdateObject(
+				pCurrObject,
+				LOADPIXMAP_ERROR_UNABLE_OPEN_FILE,
+				0,
+				0,
+				0,
+				NULL,
+				NULL
+				);
+			return;
+			}
 		}
 
-	/* Setup the buffered file */
+	// Setup the buffered file
 	pBuffer = malloc(BUFFER_SIZE*sizeof(unsigned char));
 	if (pBuffer == NULL)
 		{
@@ -235,6 +301,7 @@ void c_ev_load_pixmap(
 			stCtx.LoadPixmapUpdateObject = LoadPixmapUpdateObject;
 			stCtx.pCurrObject = pCurrObject;
 			stCtx.pszFileName = pszFileName;
+			stCtx.pFile = pFile;
 			c_ev_load_png_file(&stCtx);
 			return;
 
@@ -251,6 +318,9 @@ void c_ev_load_pixmap(
 			);
 			return;
 		}
+	
+	/* close the graphical file or the temporary file */
+	fclose (pFile);
 	}
 
 
@@ -680,6 +750,7 @@ void c_ev_load_png_file(LoadPixmapCtx *pCtx)
 	unsigned char 	*pImage;		// Pointer on a DIB structure
 	unsigned char 	*pAlphaData;	// Pointer on the Alpha data
 	unsigned char 	*pAlphaImage;	// Pointer on a DIB structure
+	unsigned char	bAlphaImage = FALSE;// Is there a mask for this image?
 #ifdef EIF_WIN32
 	unsigned long 	iData;
 	unsigned long 	iAlphaData;
@@ -688,8 +759,12 @@ void c_ev_load_png_file(LoadPixmapCtx *pCtx)
 	unsigned long 	row;			// Current scan line
 	unsigned long	nErrorCode = LOADPIXMAP_ERROR_NOERROR;
 
-	if ((fp = fopen(pCtx->pszFileName, "rb")) == NULL)
+	// Retrieve File Stream
+	fp = pCtx->pFile;
+	if (fp == NULL)
 		return;
+	// reset the file pointer
+	fseek (fp, 0, SEEK_SET);
 
 	/* Create and initialize the png_struct with the desired error handler
 	 * functions.  If you want to use the default stderr and longjump method,
@@ -700,7 +775,7 @@ void c_ev_load_png_file(LoadPixmapCtx *pCtx)
 	png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
 	if (png_ptr == NULL)
 		{
-		fclose(fp);
+		//fclose(fp);
 		return;
 		}
 
@@ -708,7 +783,7 @@ void c_ev_load_png_file(LoadPixmapCtx *pCtx)
 	info_ptr = png_create_info_struct(png_ptr);
 	if (info_ptr == NULL)
 		{
-		fclose(fp);
+		//fclose(fp);
 		png_destroy_read_struct(&png_ptr, (png_infopp)NULL, (png_infopp)NULL);
 		return;
 		}
@@ -721,7 +796,7 @@ void c_ev_load_png_file(LoadPixmapCtx *pCtx)
 		{
 		/* Free all of the memory associated with the png_ptr and info_ptr */
 		png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
-		fclose(fp);
+		//fclose(fp);
 
 		/* If we get here, we had a problem reading the file */
 		return;
@@ -793,7 +868,7 @@ void c_ev_load_png_file(LoadPixmapCtx *pCtx)
 	png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
 
 	/* close the file */
-	fclose(fp);
+	//fclose(fp);
 
 #ifdef EIF_WIN32
 	sRowSize = 4 * ((width * 24 + 31) / 32);
@@ -807,33 +882,33 @@ void c_ev_load_png_file(LoadPixmapCtx *pCtx)
 	iAlphaData = 0;
 
 	/* Create a Windows DIB Header for the color bitmap */
-	*((DWORD *)pData) = 40;			pData += 4;	// Size of header
-	*((DWORD *)pData) = width;		pData += 4;	// width in pixel
-	*((DWORD *)pData) = height;		pData += 4;	// height in pixel
-	*((WORD *)pData) = 1;			pData += 2;	// bit plane
-	*((WORD *)pData) = 24;			pData += 2;	// bits/pixel
-	*((DWORD *)pData) = BI_RGB;		pData += 4;	// compression
-	*((DWORD *)pData) = 0;			pData += 4;	// imageSize
-	*((DWORD *)pData) = 0;			pData += 4;	// XPelPerM
-	*((DWORD *)pData) = 0;			pData += 4;	// YPelPerM
-	*((DWORD *)pData) = 0;			pData += 4;	// ClrUsed
-	*((DWORD *)pData) = 0;			pData += 4;	// ClrImportant
+	*((DWORD *)pData) = 40;			pData += 4;				// Size of header
+	*((DWORD *)pData) = width;		pData += 4;				// width in pixel
+	*((DWORD *)pData) = height;		pData += 4;				// height in pixel
+	*((WORD *)pData) = 1;			pData += 2;				// bit plane
+	*((WORD *)pData) = 24;			pData += 2;				// bits/pixel
+	*((DWORD *)pData) = BI_RGB;		pData += 4;				// compression
+	*((DWORD *)pData) = 0;			pData += 4;				// imageSize
+	*((DWORD *)pData) = 0;			pData += 4;				// XPelPerM
+	*((DWORD *)pData) = 0;			pData += 4;				// YPelPerM
+	*((DWORD *)pData) = 0;			pData += 4;				// ClrUsed
+	*((DWORD *)pData) = 0;			pData += 4;				// ClrImportant
 
 	/* Create a Windows DIB Header for the mask bitmap */
-	*((DWORD *)pAlphaData) = 40;		pAlphaData += 4;/* size of header */
-	*((DWORD *)pAlphaData) = width;		pAlphaData += 4;/* width in pixel */
-	*((DWORD *)pAlphaData) = height;	pAlphaData += 4;/* height in pixel */
-	*((WORD *)pAlphaData) = 1;			pAlphaData += 2;/* bit plane */
-	*((WORD *)pAlphaData) = 1;			pAlphaData += 2;/* bits/pixel */
-	*((DWORD *)pAlphaData) = BI_RGB;	pAlphaData += 4;/* compression */
-	*((DWORD *)pAlphaData) = 0;			pAlphaData += 4;/* imageSize */
-	*((DWORD *)pAlphaData) = 0;			pAlphaData += 4;/* XPelPerM */
-	*((DWORD *)pAlphaData) = 0;			pAlphaData += 4;/* YPelPerM */
-	*((DWORD *)pAlphaData) = 2;			pAlphaData += 4;/* ClrUsed */
-	*((DWORD *)pAlphaData) = 2;			pAlphaData += 4;/* ClrImportant */
+	*((DWORD *)pAlphaData) = 40;		pAlphaData += 4;	// size of header
+	*((DWORD *)pAlphaData) = width;		pAlphaData += 4;	// width in pixel
+	*((DWORD *)pAlphaData) = height;	pAlphaData += 4;	// height in pixel
+	*((WORD *)pAlphaData) = 1;			pAlphaData += 2;	// bit plane
+	*((WORD *)pAlphaData) = 1;			pAlphaData += 2;	// bits/pixel
+	*((DWORD *)pAlphaData) = BI_RGB;	pAlphaData += 4;	// compression
+	*((DWORD *)pAlphaData) = 0;			pAlphaData += 4;	// imageSize
+	*((DWORD *)pAlphaData) = 0;			pAlphaData += 4;	// XPelPerM
+	*((DWORD *)pAlphaData) = 0;			pAlphaData += 4;	// YPelPerM
+	*((DWORD *)pAlphaData) = 2;			pAlphaData += 4;	// ClrUsed
+	*((DWORD *)pAlphaData) = 2;			pAlphaData += 4;	// ClrImportant
 	/* Color Table */
-	*((DWORD *)pAlphaData) = 0x00000000;pAlphaData += 4;/* Index 0 (black) */
-	*((DWORD *)pAlphaData) = 0x00FFFFFF;pAlphaData += 4;/* Index 1 (white) */
+	*((DWORD *)pAlphaData) = 0x00000000;pAlphaData += 4;	// Index 0 (black)
+	*((DWORD *)pAlphaData) = 0x00FFFFFF;pAlphaData += 4;	// Index 1 (white)
 
 
 	for (row = height; row > 0; row--)
@@ -846,7 +921,6 @@ void c_ev_load_png_file(LoadPixmapCtx *pCtx)
 		for (column = 0; column < width; column++)
 			{
 			/* Copy the RGB data */
-			//memcpy(pData + iData, pSrc, 3);
 			*(pData + iData + 0) = *(pSrc + 2);	// B
 			*(pData + iData + 1) = *(pSrc + 1);	// G
 			*(pData + iData + 2) = *(pSrc + 0);	// R
@@ -855,7 +929,10 @@ void c_ev_load_png_file(LoadPixmapCtx *pCtx)
 			if (*(pSrc + 3) > 0x7F)
 				c_ev_set_bit(0, pAlphaData, iAlphaData);
 			else
+				{
 				c_ev_set_bit(1, pAlphaData, iAlphaData);
+				bAlphaImage = TRUE;
+				}
 
 			iData += 3;
 			pSrc += 4;
@@ -878,6 +955,14 @@ void c_ev_load_png_file(LoadPixmapCtx *pCtx)
 			iData++;
 			}
 		}
+	
+	// The mast is empty, remove it
+	if (bAlphaImage == FALSE)
+		{
+		free (pAlphaImage);
+		pAlphaImage = NULL;
+		}
+	
 #else
 	pImage = (unsigned char *) malloc(width * height * 3);
 	pData = pImage;
@@ -923,8 +1008,13 @@ void c_ev_load_png_file(LoadPixmapCtx *pCtx)
 		pAlphaImage
 	);
 
-	free(pImage);
-	free(pAlphaImage);
+	// Free the image occupied by the image & the
+	// mask.
+	if (pImage != NULL)
+		free(pImage);
+	
+	if (pAlphaImage != NULL)
+		free(pAlphaImage);
 
 	/* that's it */
 	return;

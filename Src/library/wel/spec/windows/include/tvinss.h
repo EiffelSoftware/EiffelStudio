8c7d7558707d943258bb009236e6b056cff79887
Emmanuel Stapf <manus@eiffel.com>
2000-06-06 17:11:19 +0000

    Added Borland compatibility
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@22737 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/wel/spec/windows/include/tvinss.h
+++ Src/library/wel/spec/windows/include/tvinss.h
@@ -15,11 +15,11 @@ extern "C" {
 
 #define cwel_insertstruct_set_hparent(_ptr_, _value_) (((TV_INSERTSTRUCT *) _ptr_)->hParent = (HTREEITEM) (_value_))
 #define cwel_insertstruct_set_hinsertafter(_ptr_, _value_) (((TV_INSERTSTRUCT *) _ptr_)->hInsertAfter = (HTREEITEM) (_value_))
-#define cwel_insertstruct_set_item(_ptr_, _value_) (((TV_INSERTSTRUCT *) _ptr_)->item = (*(TV_ITEM *) (_value_)))
+#define cwel_insertstruct_set_item(_ptr_, _value_) (((TV_INSERTSTRUCT *) _ptr_)->DUMMYUNIONNAME.item = (*(TV_ITEM *) (_value_)))
 
 #define cwel_insertstruct_get_hparent(_ptr_) ((((TV_INSERTSTRUCT *) _ptr_)->hParent))
 #define cwel_insertstruct_get_hinsertafter(_ptr_) ((((TV_INSERTSTRUCT *) _ptr_)->hInsertAfter))
-#define cwel_insertstruct_get_item(_ptr_) (&(((TV_INSERTSTRUCT *) _ptr_)->item))
+#define cwel_insertstruct_get_item(_ptr_) (&(((TV_INSERTSTRUCT *) _ptr_)->DUMMYUNIONNAME.item))
 
 #ifdef __cplusplus
 }

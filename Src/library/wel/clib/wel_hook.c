91cef3d93df46addf5d0bf70b418effa89612203
Emmanuel Stapf <manus@eiffel.com>
2000-08-17 00:46:17 +0000

    Cosmetics. Specified the calling convention of exported features `hook_mouse', `unhook_mouse' and `MouseProc' to be `__stdcall' instead of not being specified.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@23054 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/wel/clib/wel_hook.c
+++ Src/library/wel/clib/wel_hook.c
@@ -13,10 +13,10 @@
 
 // Shared Data
 #pragma data_seg(".shared")	// Make a new section that we'll make shared
-HHOOK hMouseHook = 0;		// HHOOK from SetWindowsHook
-HWND hHookWindow = 0;      	// Handle to the window that hook the mouse
-HWND hForegroundWnd_at_hook_time = 0;	// Handle to the foreground window
-										// at hook time.
+HHOOK hMouseHook = NULL;		// HHOOK from SetWindowsHook
+HWND hHookWindow = NULL;      	// Handle to the window that hook the mouse
+HWND hForegroundWnd_at_hook_time = NULL;	// Handle to the foreground window
+											// at hook time.
 #pragma data_seg()			// Back to regular, nonshared data
 
 // Per process data
@@ -28,27 +28,29 @@ HINSTANCE hDllInstance = 0;
 //=============================================================================
 
 /*---------------------------------------------------------------------------*/
-/* FUNC: unhook_mouse                                                        */
+/* FUNC: DllMain                                                             */
+/* ARGS: hinstDLL: handle to DLL module                                      */
+/*       fdwReason: reason for calling function                              */
+/*       lpvReserved: reserved                                               */
 /*---------------------------------------------------------------------------*/
-/* Unhook mouse messages. Call this function when you do not want monitor    */
-/* mouse messages anymore.                                                   */
-/*                                                                           */
-/* Returns FALSE (0) when something wrong happened, TRUE (1) otherwise       */
+/* The DllMain function is an optional method of entry into a dynamic-link   */
+/* library (DLL). If the function is used, it is called by the system when   */
+/* processes and threads are initialized and terminated, or upon calls to    */
+/* the LoadLibrary and FreeLibrary functions.                                */
 /*---------------------------------------------------------------------------*/
-int unhook_mouse()
-	{
-	if (UnhookWindowsHookEx(hMouseHook)==0)
-		{
-		// DWORD nErrorCode = GetLastError();
-		// FIXME: Insert code here to deal with the error */
-		return 0;
-		}
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+	if (fdwReason == DLL_PROCESS_ATTACH) {
+			// We don't need thread notifications for what we're doing.  Thus, get
+			// rid of them, thereby eliminating some of the overhead of this DLL,
+			// which will end up in nearly every GUI process anyhow.
+		DisableThreadLibraryCalls(hinstDLL);
 
-	/* Unhook succeeded, we reset the hook handles */
-	hMouseHook = NULL;
-	hHookWindow = NULL;
-	return 1;
+		hDllInstance = hinstDLL;
 	}
+
+	return TRUE;
+}
 
 /*---------------------------------------------------------------------------*/
 /* FUNC: MouseProc                                                           */
@@ -65,8 +67,8 @@ int unhook_mouse()
 /* is a placeholder for the application-defined or library-defined function  */
 /* name.                                                                     */
 /*---------------------------------------------------------------------------*/
-LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam)
-	{
+__declspec(dllexport) LRESULT WINAPI MouseProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
 	UINT Msg = (UINT)wParam;
 	DWORD dwMousePos;
 	MOUSEHOOKSTRUCT *pInfo = (MOUSEHOOKSTRUCT *) lParam;
@@ -105,8 +107,7 @@ LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam)
 	dwMousePos = MAKELONG(pInfo->pt.x - rect.left, pInfo->pt.y - rect.top);
 
 	// Transform & redirect the mouse message to the target window.
-	switch (Msg)
-		{
+	switch (Msg) {
 		case WM_MOUSEMOVE:
 		case WM_LBUTTONDOWN:
 		case WM_MBUTTONDOWN:
@@ -118,23 +119,22 @@ LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam)
 		case WM_MBUTTONDBLCLK:
 		case WM_RBUTTONDBLCLK:
 		case WM_NCHITTEST:
-			if (pInfo->hwnd != hHookWindow)
-				{
-				// The current window is not the target
-				// window, so we prevent the system to
-				// pass the message to the current window
-				// (return 1), and we send an identical
-				// message to the target window.
+			if (pInfo->hwnd != hHookWindow) {
+					/* The current window is not the target
+					 * window, so we prevent the system to
+					 * pass the message to the current window
+					 * (return 1), and we send an identical
+					 * message to the target window. */
 				SendMessage(hHookWindow, Msg, 0, dwMousePos);
 				return 1;
 				}
 			else
 				{
-				// The current window IS the window that
-				// has requested the hook, we do not
-				// want to duplicate the mouse message
-				// So we ignore this message & tell the system
-				// to pass it to the target window.
+					/* The current window IS the window that
+					 * has requested the hook, we do not want
+					 * to duplicate the mouse message So we
+					 * ignore this message & tell the system
+					 * to pass it to the target window. */
 				return 0;
 				}
 			break;
@@ -171,9 +171,9 @@ LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam)
 			return 1;
 		}
 	
-	// We dont care about this message...do nothing.
+		// We dont care about this message...do nothing.
 	return 0;
-	}
+}
 
 /*---------------------------------------------------------------------------*/
 /* FUNC: hook_mouse                                                          */
@@ -184,60 +184,59 @@ LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam)
 /*                                                                           */
 /* Returns FALSE (0) when something wrong happened, TRUE (1) otherwise       */
 /*---------------------------------------------------------------------------*/
-int hook_mouse(HWND hWnd)
-	{
-	if (hWnd==NULL)
-		{
+__declspec(dllexport) int WINAPI hook_mouse(HWND hWnd)
+{
+	HOOKPROC hkprcMouse;
+
+	if (hWnd == NULL) {
 		MessageBox (NULL, "Invalid hWnd", "wel_hook.dll error", MB_OK | MB_ICONSTOP);
 		return 0;	// Invalid handle to a window, exit.
-		}
+	}
 	
 	// Remember the current foreground window
 	hForegroundWnd_at_hook_time = GetForegroundWindow();
 
+	hkprcMouse = (HOOKPROC)GetProcAddress(hDllInstance, "MouseProc"); 
+
 	// Hook the mouse messages
 	hMouseHook = SetWindowsHookEx (
  		WH_MOUSE,				// hook type
-		(HOOKPROC)MouseProc,	// hook procedure
+		hkprcMouse,			// hook procedure
 		hDllInstance,			// handle to application instance
 		0						// thread identifier
 		);
 
 	// Set the HookWindow if SetWindowsHookEx succeeded
-	if (hMouseHook == NULL)
-		{
+	if (hMouseHook == NULL) {
 		hHookWindow = NULL;
 		MessageBox (NULL, "hMouseHook==NULL", "wel_hook.dll error", MB_OK | MB_ICONSTOP);
 		return 0;
-		}
+	}
 	
 	// Everything went fine 
 	hHookWindow = hWnd;
 	return 1;
+}
+
+/*---------------------------------------------------------------------------*/
+/* FUNC: unhook_mouse                                                        */
+/*---------------------------------------------------------------------------*/
+/* Unhook mouse messages. Call this function when you do not want monitor    */
+/* mouse messages anymore.                                                   */
+/*                                                                           */
+/* Returns FALSE (0) when something wrong happened, TRUE (1) otherwise       */
+/*---------------------------------------------------------------------------*/
+__declspec(dllexport) int WINAPI unhook_mouse()
+{
+	if (UnhookWindowsHookEx(hMouseHook)==0) {
+		// DWORD nErrorCode = GetLastError();
+		// FIXME: Insert code here to deal with the error */
+		return 0;
 	}
 
-/*---------------------------------------------------------------------------*/
-/* FUNC: DllMain                                                             */
-/* ARGS: hinstDLL: handle to DLL module                                      */
-/*       fdwReason: reason for calling function                              */
-/*       lpvReserved: reserved                                               */
-/*---------------------------------------------------------------------------*/
-/* The DllMain function is an optional method of entry into a dynamic-link   */
-/* library (DLL). If the function is used, it is called by the system when   */
-/* processes and threads are initialized and terminated, or upon calls to    */
-/* the LoadLibrary and FreeLibrary functions.                                */
-/*---------------------------------------------------------------------------*/
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
-	{
-	if (fdwReason == DLL_PROCESS_ATTACH)
-		{
-		// We don't need thread notifications for what we're doing.  Thus, get
-		// rid of them, thereby eliminating some of the overhead of this DLL,
-		// which will end up in nearly every GUI process anyhow.
-		DisableThreadLibraryCalls(hinstDLL);
-
-		hDllInstance = hinstDLL;
-		}
-
-	return TRUE;
-	}
+		/* Unhook succeeded, we reset the hook handles */
+	hMouseHook = NULL;
+	hHookWindow = NULL;
+	hForegroundWnd_at_hook_time = NULL;
+	return 1;
+}

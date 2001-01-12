9e2f4f9d1e6778a9ea0a3359d028592910eb4489
Emmanuel Stapf <manus@eiffel.com>
2001-01-12 22:44:40 +0000

    Added C++ protection. Fixed some bad signatures.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@23690 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/encryption/Clib/des.h
+++ Src/library/encryption/Clib/des.h
@@ -10,8 +10,21 @@
 	Declarations for DES routines.
 */
 
+#ifndef _des_h_
+#define _des_h_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern int ise_desinit();		/* Initialize internal DES structures */
 extern void ise_setkey();		/* Set encryption/decryption key */
-extern void ise_endes();		/* Perform DES encryption */
-extern void ise_dedes();		/* Perform DES decryption */
+extern void ise_endes(char *);		/* Perform DES encryption */
+extern void ise_dedes(char *);		/* Perform DES decryption */
 extern void ise_desdone();		/* Free internal structures allocated by ise_desinit */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _des_h_ */

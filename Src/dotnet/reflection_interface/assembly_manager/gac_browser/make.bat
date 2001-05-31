c4f419dcc60401f97ce153059cc4b619b7871ec0
karine <karine@eiffel.com>
2001-05-31 00:39:52 +0000

    Now generates signed assembly.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@25146 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/dotnet/reflection_interface/assembly_manager/gac_browser/make.bat
+++ Src/dotnet/reflection_interface/assembly_manager/gac_browser/make.bat
@@ -1,1 +1,3 @@
-csc /t:library /out:ISE.AssemblyManager.WindowsDirectoryExtractor.dll WindowsDirectoryExtractor.cs
+csc /debug /t:module WindowsDirectoryExtractor.cs
+sn -k Key
+al windowsdirectoryextractor.netmodule /keyfile:Key /out:ISE.AssemblyManager.WindowsDirectoryExtractor.dll

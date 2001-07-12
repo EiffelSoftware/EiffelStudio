34480af3a09da6fade9c9846fd6817fded81fa27
marina <marina@eiffel.com>
2001-07-12 17:16:40 +0000

    Added handling of the crach of C compiler during the test.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@26288 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/com_wizard/testing/test_client.bat
+++ Src/com_wizard/testing/test_client.bat
@@ -8,6 +8,6 @@ if exist D:\testing\%1  rd /q /s D:\testing\%1
 
 mkdir D:\testing\%1
 
-c:\com_wizard_cmd\EIFGEN\W_code\com_wizard_cmd.exe -new_com_project -com_file %2 -destination D:\testing\%1 -in_process -compile_c -compile_eiffel -stop_on_error -output_none -not_spawn_ebench
+d:\com_wizard_cmd\EIFGEN\W_code\com_wizard_cmd.exe -new_com_project -com_file %2 -destination D:\testing\%1 -in_process -compile_c -compile_eiffel -stop_on_error -output_none -not_spawn_ebench
 
 call test_status.bat %1 testing client
--- Src/com_wizard/testing/test_status2.bat
+++ Src/com_wizard/testing/test_status2.bat
@@ -7,12 +7,7 @@ if exist D:\%2\%1\%3\eifgen\w_code\%1.exe (
  		if exist D:\%2\%1\%3\eifgen\w_code\%1.dll (
 			rd /q /s D:\%2\%1
  		) else (
- 			if exist D:\%2\%1\%3\eifgen\w_code (
-				cd D:\%2\%1\%3\eifgen\w_code
-				if exist Makefile nmake
-				cd D:\com_wizard_cmd
- 			)
- 			call test_status2.bat %1 %2 %3
+ 			echo "failed" >> D:\%2\%1_failed
  		)
  	)
 )

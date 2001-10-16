825ffe6df941af14789fc7f0548fbe93fcc07ed4
Emmanuel Stapf <manus@eiffel.com>
2001-10-16 16:52:05 +0000

    New version number.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@28153 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/Eiffel/eiffel/com_il_generation/Core/runtime_assembly_info.cs
+++ Src/bench/Eiffel/eiffel/com_il_generation/Core/runtime_assembly_info.cs
@@ -26,7 +26,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the value or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("5.0.35.1")]
+[assembly: AssemblyVersion("5.1.5.1")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 

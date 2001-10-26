0cd72bc4f162f98747ba53c62501f6b564f3bf6d
Emmanuel Stapf <manus@eiffel.com>
2001-10-26 23:40:44 +0000

    Made example compilable under 5.1 on Windows.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@28499 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/examples/cecil/Eiffel2C/array/fext.c
+++ Src/examples/cecil/Eiffel2C/array/fext.c
@@ -4,6 +4,8 @@
 void c_print (EIF_POINTER ptr) {
 	int i;
 
+	printf ("\nDisplaying from C \n");
+
 	for (i = 0; i < 10; i++) {
 		printf ("@%d = %ld\n", i+1, (EIF_INTEGER) *((EIF_INTEGER *)ptr+i));
 	}

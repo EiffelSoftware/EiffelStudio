c427de76beeaed9c07424fa59f015068ca78efa7
Emmanuel Stapf <manus@eiffel.com>
2001-12-06 00:59:28 +0000

    New class to hold information about the feature we want to generate before its associated XXBuilder (XX=Attribute or Method) is created. After the creation of the XXBuilder the information stored in EiffelMethod is discarded and we save a lot of space.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@29523 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/bench/Eiffel/eiffel/com_il_generation/Core/feature_info.cs
@@ -0,0 +1,97 @@
+/* 
+indexing
+	description: "Info about a feature that can be discarded as soon as
+		a MethodBuilder or an AttributeBuilder is created. Having this
+		saves a lot of space in memory since the information was never
+		used after the Builder was created."
+	date: "$Date$"
+	revision: "$Revision$"
+*/
+
+using System;
+
+internal class FEATURE_INFO {
+	
+	public FEATURE_INFO (int arg_count) {
+		parameter_names = new string [arg_count];
+		feature_id = EiffelReflectionEmit.NoValue;
+	}
+
+	public string real_name;
+	public string eiffel_name;
+	public string name() {
+		if (real_name == null)
+			return eiffel_name;
+		else
+			return real_name;
+	}
+
+	// Parameter Names
+	public string[] parameter_names;
+	
+	// Is feature deferred?
+	public bool is_deferred;
+
+	// Is feature redefined/implemented from parent?
+	public bool is_redefined;
+
+	// Is feature frozen?
+	public bool is_frozen;
+
+	// Is feature a static?
+	public bool is_static;
+
+	// Is feature an invariant?
+	public bool IsInvariant;
+
+	// Belongs to interface
+	public bool is_interface_routine;
+	
+	// Feature identifier in class
+	public int feature_id;
+
+	// Set `real_name' with `a_feature_name'.
+	public void set_real_name( string a_feature_name ) {
+		real_name = a_feature_name;
+	}
+
+	// Set `eiffel_name' with `a_feature_name'.
+	public void set_eiffel_name( string a_feature_name ) {
+		eiffel_name = a_feature_name;
+	}
+
+	// Set `is_deferred' with `val'.
+	public void set_is_deferred (bool val) {
+		is_deferred = val;
+	}
+
+	// Set `is_redefined' with `val'.
+	public void set_is_redefined (bool val) {
+		is_redefined = val;
+	}
+
+	// Set `is_frozen' with `val'.
+	public void set_is_frozen (bool val) {
+		is_frozen = val;
+	}
+
+	// Set `is_static' with `val'.
+	public void set_is_static (bool val) {
+		is_static = val;
+	}
+
+	// Set `IsInvariant' with `val'.
+	public void SetIsInvariant (bool val) {
+		IsInvariant = val;
+	}
+
+	// Set `is_interface_routine' with `ID'.
+	public void set_is_interface_routine (bool val) {
+		is_interface_routine = val;
+	}
+
+	// Set `feature_id' with `ID'.
+	public void set_feature_id( int ID ) {
+		feature_id = ID;
+	}
+}

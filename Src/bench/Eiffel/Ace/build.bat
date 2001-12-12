1ad4cae6fa488ac9641dbedee19b3e5409e694a4
Emmanuel Stapf <manus@eiffel.com>
2001-12-12 01:24:17 +0000

    Added some protections.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@29630 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/Eiffel/Ace/build.bat
+++ Src/bench/Eiffel/Ace/build.bat
@@ -1,5 +1,10 @@
 rem Finalizing new compiler
 
+echo off
+
+if .%1. == .. goto usage
+if .%2. == .. goto usage
+
 cdd %1
 rd /q /s EIFGEN
 
@@ -10,6 +15,7 @@ cdd EIFGEN\F_code
 %ISE_EIFFEL%\studio\spec\windows\bin\finish_freezing.exe -silent
 
 cdd %ISE_EIFFEL%\studio\spec\windows\bin
+del old_ec.exe
 ren ec.exe old_ec.exe
 copy %1\EIFGEN\F_code\ec.exe .
 
@@ -21,3 +27,12 @@ rd /q /s EIFGEN
 cdd EIFGEN\W_code
 
 %ISE_EIFFEL%\studio\spec\windows\bin\finish_freezing.exe -silent
+
+goto end
+
+:usage
+echo build.bat final_directory workbench_directory
+
+:end
+
+rem Build is complete 

e258bdb4ccbbaa491c9461e314bbd677838a6e96
Emmanuel Stapf <manus@eiffel.com>
2001-12-25 03:41:12 +0000

    Removed useless C externals.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@29917 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/bench/byte.c
+++ Src/bench/C/bench/byte.c
@@ -4,22 +4,6 @@
 #include <stdio.h>
 #include <string.h>
 
-long ca_ssiz(void);			/* Short integer size */
-
-void ca_put(char *area, char c, long int i)
-{
-	/* Put character `c' at position `i' in character array `area'. */
-	
-	area[i] = c;
-}
-
-char ca_item(char *area, long int i)
-{
-	/* Item at position `i' in character array `area'. */
-	
-	return area[i];
-}
-
 void ca_copy(char *from, char *to, long int nbitems, long int at)
 {
 	/* Copy `nbitems' character from `from' into `to'. */
@@ -150,61 +134,10 @@ void ca_wbit(char *area, char *bit, long int strchr, long int blength)
 	ca_wuint32(area,val,index + (nb_packs-1)*sizeof(uint32));
 }
 
-long ca_ssiz(void)
-{
-	/* Short integer size. */
-
-	return (long) sizeof(short);
-}
-
-long ca_sint32(void)
-{
-	/* int32 integer size. */
-
-	return (long) sizeof(int32);
-}
-
-long ca_suint32(void)
-{
-	/* uint32 integer size. */
-
-	return (long) sizeof(uint32);
-}
-
 void ca_store(char *area, long int siz, FILE *fil)
 {
 	if (fwrite(area, sizeof(char), siz, fil) != (size_t) siz)
 		xraise(EN_IO);
-}
-
-long c_char_size(void)
-{
-	return (long) sizeof(char);
-}
-
-long c_long_size(void)
-{
-	return (long) sizeof(long);
-}
-
-long c_float_size(void)
-{
-	return (long) sizeof(float);
-}
-
-long c_double_size(void)
-{
-	return (long) sizeof(double);
-}
-
-long c_pointer_size(void)
-{
-	return (long) sizeof(char *(*)());
-}
-
-long c_reference_size(void)
-{
-	return (long) sizeof(char *);
 }
 
 void write_int (FILE *file, long int val)

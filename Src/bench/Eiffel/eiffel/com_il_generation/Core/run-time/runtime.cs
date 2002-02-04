3ce8c19a44351d78957f664f3ca8e8c308a2aa3f
Emmanuel Stapf <manus@eiffel.com>
2002-02-04 23:26:30 +0000

    Added first implementation of `is_eiffel_string', `is_eiffel_array', `eiffel_string' and `eiffel_array' to help the Eiffel debugger engine to display those objects nicely in the VS debugger.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@30542 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/Eiffel/eiffel/com_il_generation/Core/run-time/runtime.cs
+++ Src/bench/Eiffel/eiffel/com_il_generation/Core/run-time/runtime.cs
@@ -7,6 +7,9 @@ indexing
 */
 	
 using System;
+using System.Text;
+using System.Reflection;
+
 namespace ISE.Runtime {
 
 	public abstract class ANY {
@@ -22,88 +25,158 @@ namespace ISE.Runtime {
 	public delegate void EV_PIXMAP_IMP_DELEGATE (int error_code, int data_type, int pixmap_width,
 		int pixmap_height, IntPtr rgb_data, IntPtr alpha_data);
 
-	public class RUN_TIME
-	{
-		public static bool in_assertion;
-				// Flag used during assertion checking to make sure
-				// that assertions are not checked within an assertion
-				// checking.
+public class RUN_TIME
+{
+/*
+feature -- Access
+*/
+	public static bool in_assertion;
+			// Flag used during assertion checking to make sure
+			// that assertions are not checked within an assertion
+			// checking.
 
-		public static string assertion_tag;
-				// Tag of last checked assertion
+	public static string assertion_tag;
+			// Tag of last checked assertion
 
 
-		public static bool is_assertion_checked = true;
-				// Are assertions checked?
+	public static bool is_assertion_checked = true;
+			// Are assertions checked?
 
-		public static bool check_assert (bool val) {
-				// Enable or disable checking of assertions?
-			bool tmp = is_assertion_checked;
-			is_assertion_checked = val;
-			return tmp;
-		}
-
-		public static String generator (object o)
-			// Generator class name of `o'.
-		{
-			String Result;
-
-			if (o is EIFFEL_TYPE_INFO) {
-					// This is a generated Eiffel type, we extract
-					// stored type.
-				Result = ((EIFFEL_TYPE_INFO) o).____class_name ();
-			} else {
-				Result = o.GetType().Name;
-			}
-			return Result;
-		}
-
-		public static String generating_type (object o)
-			// Generating type name of `o'.
-		{
-			EIFFEL_DERIVATION der;
-			String Result;
-
-			if (o is EIFFEL_TYPE_INFO) {
-					// This is a generated Eiffel type.
-				EIFFEL_TYPE_INFO info = (EIFFEL_TYPE_INFO) o;
-				der = info.____type ();
-				if (der == null) {
-						// Not a generic class, we extract stored name.
-					Result = info.____class_name ();
-				} else {
-					Result = der.type_name ();
-				}
-			} else {
-				Result = o.GetType().Name;
-			}
-			return Result;
-		}
-
-		public static int generic_parameter_count (object o)
-			// Number of generic Parameter if any.
-		{
-			int Result = 0;
-			if (o is EIFFEL_TYPE_INFO) {
-				Result = ((EIFFEL_TYPE_INFO) o).____type ().nb_generics;
-			}
-			return Result;
-		}
+	public static bool check_assert (bool val) {
+			// Enable or disable checking of assertions?
+		bool tmp = is_assertion_checked;
+		is_assertion_checked = val;
+		return tmp;
 	}
 
-	public class EXCEPTION_MANAGER
+/*
+feature -- Status report
+*/
+	public static String generator (object o)
+		// Generator class name of `o'.
 	{
-		public EXCEPTION_MANAGER()
-		{
+		String Result;
+
+		if (o is EIFFEL_TYPE_INFO) {
+				// This is a generated Eiffel type, we extract
+				// stored type.
+			Result = ((EIFFEL_TYPE_INFO) o).____class_name ();
+		} else {
+			Result = o.GetType().Name;
 		}
-
-		public static Exception last_exception;
-				// Last raised exception in `rescue' clause.
-
-		public static void raise (Exception e) {
-			throw e;
-		}
-
+		return Result;
 	}
+
+	public static String generating_type (object o)
+		// Generating type name of `o'.
+	{
+		EIFFEL_DERIVATION der;
+		EIFFEL_TYPE_INFO info;
+		String Result;
+
+		if (o is EIFFEL_TYPE_INFO) {
+				// This is a generated Eiffel type.
+			info = (EIFFEL_TYPE_INFO) o;
+			der = info.____type ();
+			if (der == null) {
+					// Not a generic class, we extract stored name.
+				Result = info.____class_name ();
+			} else {
+				Result = der.type_name ();
+			}
+		} else {
+			Result = o.GetType().Name;
+		}
+		return Result;
+	}
+
+	public static int generic_parameter_count (object o)
+		// Number of generic Parameter if any.
+	{
+		int Result = 0;
+		if (o is EIFFEL_TYPE_INFO) {
+			Result = ((EIFFEL_TYPE_INFO) o).____type ().nb_generics;
+		}
+		return Result;
+	}
+
+	public static Boolean is_eiffel_string (object o)
+		// Is `o' an instance of an Eiffel STRING.
+	{
+		EIFFEL_DERIVATION der;
+		EIFFEL_TYPE_INFO info;
+		Boolean Result = false;
+
+		if (o is EIFFEL_TYPE_INFO) {
+			info = (EIFFEL_TYPE_INFO) o;
+			der = info.____type ();
+			if (der == null) {
+					// Not a generic class, possibly a good candidate for STRING.
+				Result = info.____class_name ().Equals ("STRING");
+			}
+		}
+		return Result;
+	}
+
+	public static Boolean is_eiffel_array (object o)
+		// Is `o' an instance of an Eiffel ARRAY.
+	{
+		EIFFEL_DERIVATION der;
+		EIFFEL_TYPE_INFO info;
+		Boolean Result = false;
+
+		if (o is EIFFEL_TYPE_INFO) {
+			info = (EIFFEL_TYPE_INFO) o;
+			der = info.____type ();
+			if (der == null) {
+					// Not a generic class, possibly a good candidate for ARRAY.
+				Result = info.____class_name ().Equals ("ARRAY");
+			}
+		}
+		return Result;
+	}
+
+	public static String eiffel_string (object o)
+		// System.String representation of `o' if it is an Eiffel STRING instance.
+	{
+		String Result = null;
+		Type eiffel_string_type;
+		FieldInfo string_builder_info;
+		StringBuilder builder;
+
+		if (is_eiffel_string (o)) {
+			eiffel_string_type = o.GetType();	
+			string_builder_info = eiffel_string_type.GetField ("$$internal_string_builder");
+			builder = (StringBuilder) string_builder_info.GetValue (o);
+			Result = builder.ToString();
+		}
+
+		return Result;
+	}
+
+	public static Array eiffel_array (object o)
+		// System.Array representation of `o' if it is an Eiffel ARRAY instance.
+	{
+		Array Result = null;
+		Type eiffel_array_type, eiffel_special_type;
+		FieldInfo area_info, native_info;
+		Object special;
+
+		if (is_eiffel_array (o)) {
+				// Get SPECIAL object
+			eiffel_array_type = o.GetType();	
+			area_info = eiffel_array_type.GetField ("$$area");
+			special = area_info.GetValue (o);
+
+				// Get System.Array
+			eiffel_special_type = special.GetType();
+			native_info = eiffel_special_type.GetField ("$$native_array");
+			Result = (Array) native_info.GetValue (special);
+		}
+
+		return Result;
+	}
+	
+}
 }
 

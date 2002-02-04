217821d1079aaa67889c259a8c56a0044115f409
Emmanuel Stapf <manus@eiffel.com>
2002-02-04 22:52:05 +0000

    Added a specific class for exception handling (used to be in runtime.cs)
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@30540 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/bench/Eiffel/eiffel/com_il_generation/Core/run-time/exception.cs
@@ -0,0 +1,27 @@
+/*
+indexing
+	description: "To manage exceptions from the Eiffel side"
+	date: "$Date$"
+	revision: "$Revision$"
+*/
+	
+using System;
+namespace ISE.Runtime {
+
+public class EXCEPTION_MANAGER {
+/*
+feature -- Access
+*/
+	public static Exception last_exception;
+		// Last raised exception in `rescue' clause.
+
+/*
+feature -- Exceptions
+*/
+	public static void raise (Exception e)
+		// Throw an exception `e'.
+	{
+		throw e;
+	}
+
+}

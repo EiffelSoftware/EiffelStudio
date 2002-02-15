f0fbfc819ecd87335bac3bdd6f570a9defd65d61
marina <marina@eiffel.com>
2002-02-15 02:37:32 +0000

    Implementation of IEiffelSystemClusters interface.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@30679 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/bench/com_compiler/Clib/ecom_eiffel_compiler_ieiffelsystemclusters_s.h
@@ -0,0 +1,115 @@
+/*-----------------------------------------------------------
+Eiffel System Clusters.  Help file: 
+-----------------------------------------------------------*/
+
+#ifndef __ECOM_EIFFEL_COMPILER_IEIFFELSYSTEMCLUSTERS_S_H__
+#define __ECOM_EIFFEL_COMPILER_IEIFFELSYSTEMCLUSTERS_S_H__
+#ifdef __cplusplus
+extern "C" {
+
+
+#ifndef __ecom_eiffel_compiler_IEiffelSystemClusters_FWD_DEFINED__
+#define __ecom_eiffel_compiler_IEiffelSystemClusters_FWD_DEFINED__
+namespace ecom_eiffel_compiler
+{
+class IEiffelSystemClusters;
+}
+#endif
+
+}
+#endif
+
+#include "eif_com.h"
+
+#include "eif_eiffel.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef __ecom_eiffel_compiler_IEnumClusterProp_FWD_DEFINED__
+#define __ecom_eiffel_compiler_IEnumClusterProp_FWD_DEFINED__
+namespace ecom_eiffel_compiler
+{
+class IEnumClusterProp;
+}
+#endif
+
+
+
+#ifndef __ecom_eiffel_compiler_IEiffelClusterProperties_FWD_DEFINED__
+#define __ecom_eiffel_compiler_IEiffelClusterProperties_FWD_DEFINED__
+namespace ecom_eiffel_compiler
+{
+class IEiffelClusterProperties;
+}
+#endif
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#ifndef __ecom_eiffel_compiler_IEiffelSystemClusters_INTERFACE_DEFINED__
+#define __ecom_eiffel_compiler_IEiffelSystemClusters_INTERFACE_DEFINED__
+namespace ecom_eiffel_compiler
+{
+class IEiffelSystemClusters : public IDispatch
+{
+public:
+	IEiffelSystemClusters () {};
+	~IEiffelSystemClusters () {};
+
+	/*-----------------------------------------------------------
+	Cluster tree.
+	-----------------------------------------------------------*/
+	virtual STDMETHODIMP cluster_tree(  /* [out, retval] */ ecom_eiffel_compiler::IEnumClusterProp * * return_value ) = 0;
+
+
+	/*-----------------------------------------------------------
+	Save changes.
+	-----------------------------------------------------------*/
+	virtual STDMETHODIMP store( void ) = 0;
+
+
+	/*-----------------------------------------------------------
+	Add a cluster to the project.
+	-----------------------------------------------------------*/
+	virtual STDMETHODIMP add_cluster(  /* [in] */ BSTR cluster_name, /* [in] */ BSTR parent_name, /* [in] */ BSTR cluster_path ) = 0;
+
+
+	/*-----------------------------------------------------------
+	Remove a cluster from the project.
+	-----------------------------------------------------------*/
+	virtual STDMETHODIMP remove_cluster(  /* [in] */ BSTR cluster_name ) = 0;
+
+
+	/*-----------------------------------------------------------
+	Cluster properties.
+	-----------------------------------------------------------*/
+	virtual STDMETHODIMP cluster_properties(  /* [in] */ BSTR cluster_name, /* [out, retval] */ ecom_eiffel_compiler::IEiffelClusterProperties * * return_value ) = 0;
+
+
+	/*-----------------------------------------------------------
+	Cluster properties.
+	-----------------------------------------------------------*/
+	virtual STDMETHODIMP cluster_properties_by_id(  /* [in] */ ULONG cluster_id, /* [out, retval] */ ecom_eiffel_compiler::IEiffelClusterProperties * * return_value ) = 0;
+
+
+
+protected:
+
+
+private:
+
+
+};
+}
+#endif
+}
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

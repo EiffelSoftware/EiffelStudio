2f3874110abc7dc49db470b2ee8a45df909e0a81
Emmanuel Stapf <manus@eiffel.com>
2002-02-20 20:07:03 +0000

    Added new custom attribute `not_cls_compliant_attr'. Instead of defining the predefined CA as static functions, they are not defined as static properties, that way we do not need the extra () in the client code to call those features.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@30767 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/Eiffel/eiffel/com_il_generation/Core/ca.cs
+++ Src/bench/Eiffel/eiffel/com_il_generation/Core/ca.cs
@@ -15,49 +15,75 @@ namespace ISE.Compiler {
 
 internal class CA
 {
-	// Debugger CAs to hide dummy callbacks routine
-	internal static CustomAttributeBuilder internal_debugger_step_through_attr;
-	internal static CustomAttributeBuilder internal_debugger_hidden_attr;
-	internal static CustomAttributeBuilder internal_suppress_check_attr;
-
-	internal static CustomAttributeBuilder debugger_step_through_attr ()
+	internal static CustomAttributeBuilder debugger_step_through_attr {
 		// Once: Predefined Custom attribute to force debugger
 		// to skip certain routines.
-	{
-		if (internal_debugger_step_through_attr == null) {
-			Type type = Type.GetType ("System.Diagnostics.DebuggerStepThroughAttribute");
-			ConstructorInfo constructor = type.GetConstructor (Type.EmptyTypes);
-			internal_debugger_step_through_attr =
-				new CustomAttributeBuilder (constructor, new object [0] {});
+		get {
+			if (internal_debugger_step_through_attr == null) {
+				Type type = Type.GetType ("System.Diagnostics.DebuggerStepThroughAttribute");
+				ConstructorInfo constructor = type.GetConstructor (Type.EmptyTypes);
+				internal_debugger_step_through_attr =
+					new CustomAttributeBuilder (constructor, new object [0] {});
+			}
+			return internal_debugger_step_through_attr;
 		}
-		return internal_debugger_step_through_attr;
 	}
 
-	internal static CustomAttributeBuilder debugger_hidden_attr ()
+	internal static CustomAttributeBuilder debugger_hidden_attr {
 		// Once: Predefined Custom attribute to force debugger
 		// to hide certain routines.
-	{
-		if (internal_debugger_hidden_attr == null) {
-			Type type = Type.GetType ("System.Diagnostics.DebuggerHiddenAttribute");
-			ConstructorInfo constructor = type.GetConstructor (Type.EmptyTypes);
-			internal_debugger_hidden_attr =
-				new CustomAttributeBuilder (constructor, new object [0] {});
+		get {
+			if (internal_debugger_hidden_attr == null) {
+				Type type = Type.GetType ("System.Diagnostics.DebuggerHiddenAttribute");
+				ConstructorInfo constructor = type.GetConstructor (Type.EmptyTypes);
+				internal_debugger_hidden_attr =
+					new CustomAttributeBuilder (constructor, new object [0] {});
+			}
+			return internal_debugger_hidden_attr;
 		}
-		return internal_debugger_hidden_attr;
 	}
 
-	internal static CustomAttributeBuilder suppress_check_attr ()
+	internal static CustomAttributeBuilder suppress_check_attr {
 		// Once: Predefined Custom attribute to force .net runtime
 		// not to perform a stack walk when calling a C external.
-	{
-		if (internal_suppress_check_attr == null) {
-			Type type = Type.GetType ("System.Security.SuppressUnmanagedCodeSecurityAttribute");
-			ConstructorInfo constructor = type.GetConstructor (Type.EmptyTypes);
-			internal_suppress_check_attr =
-				new CustomAttributeBuilder (constructor, new object [0] {});
+		get {
+			if (internal_suppress_check_attr == null) {
+				Type type = Type.GetType ("System.Security.SuppressUnmanagedCodeSecurityAttribute");
+				ConstructorInfo constructor = type.GetConstructor (Type.EmptyTypes);
+				internal_suppress_check_attr =
+					new CustomAttributeBuilder (constructor, new object [0] {});
+			}
+			return internal_suppress_check_attr;
 		}
-		return internal_suppress_check_attr;
 	}
+
+	internal static CustomAttributeBuilder not_cls_compliant_attr {
+		// Once: Predefined Custom attribute to tell .NET runtime
+		// that current member that holds the CA is not CLS compliant.
+		get {
+			if (internal_not_cls_compliant_attr == null) {
+				Type type = Type.GetType ("System.CLSCompliantAttribute");
+				ConstructorInfo constructor = type.GetConstructor (
+					new Type [1] {Type.GetType ("System.Boolean")});
+				internal_not_cls_compliant_attr =
+					new CustomAttributeBuilder (constructor, new object [1] {false});
+			}
+			return internal_not_cls_compliant_attr;
+		}
+	}
+
+/*
+feature {NONE} -- Implementation
+*/
+	private static CustomAttributeBuilder internal_debugger_step_through_attr;
+	private static CustomAttributeBuilder internal_debugger_hidden_attr;
+		// Debugger CAs to hide dummy callbacks routine
+
+	private static CustomAttributeBuilder internal_suppress_check_attr;
+		// To speed up access to C externals.
+
+	private static CustomAttributeBuilder internal_not_cls_compliant_attr;
+		// Mark current member to be not CLS compliant.
 
 } // end of CA
 

2d9bac5770cf633f3adec3b94a850a08ce262faa
Emmanuel Stapf <manus@eiffel.com>
2002-03-02 00:31:24 +0000

    First Eiffel custom attributes used in code generation to show the real Eiffel class name in the Eiffel way.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@30908 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/bench/Eiffel/eiffel/com_il_generation/Core/run-time/custom_attributes/eiffel_class_name_attribute.cs
@@ -0,0 +1,32 @@
+/*
+indexing
+	description: "Custom attributes to save real Eiffel class name in metadata."
+	date: "$Date$"
+	revision: "$Revision$"
+*/
+
+using System;
+
+namespace ISE.Runtime
+{
+
+[CLSCompliant(false)]
+[AttributeUsage (AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+public class EIFFEL_CLASS_NAME_ATTRIBUTE : Attribute
+{
+/*
+feature -- Initialization
+*/
+	public EIFFEL_CLASS_NAME_ATTRIBUTE(String name)
+	{
+		class_name = name;
+	}
+
+/*
+feature -- Access
+*/
+	public String class_name;
+		// Name of class associated to Current.
+}
+
+} // class EIFFEL_CLASS_NAME_ATTRIBUTE

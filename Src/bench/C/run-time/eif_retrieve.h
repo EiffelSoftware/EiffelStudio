3e4808fb216b923a243e8f54be2549bd1b42028a
Emmanuel Stapf <manus@eiffel.com>
2002-03-13 18:24:32 +0000

    Improved signatures for stream storing/retrieving.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@31033 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_retrieve.h
+++ Src/bench/C/run-time/eif_retrieve.h
@@ -63,7 +63,7 @@ struct rt_struct {
  * Eiffel calls
  */
 RT_LNK char *eretrieve(EIF_INTEGER file_desc);		/* Retrieve object store in file */
-RT_LNK EIF_REFERENCE stream_eretrieve(char **, long, long, EIF_INTEGER *);	/* Retrieve object store in stream */
+RT_LNK EIF_REFERENCE stream_eretrieve(EIF_POINTER *, EIF_INTEGER, EIF_INTEGER, EIF_INTEGER *);	/* Retrieve object store in stream */
 RT_LNK char *portable_retrieve(int (*char_read_function)(char *, int));
 
 

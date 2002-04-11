a9ec110064745812a1419deb68c8007a63edbf2d
Emmanuel Stapf <manus@eiffel.com>
2002-04-11 20:33:02 +0000

    Use `EIF_REFERENCE' instead of `void *' to show that we are dealing with an Eiffel object.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@31424 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/wel/spec/windows/include/wel_enum_child_windows.h
+++ Src/library/wel/spec/windows/include/wel_enum_child_windows.h
@@ -15,7 +15,7 @@ extern "C" {
 /* FUNC: cwel_enum_child_windows                                             */
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
-void cwel_enum_child_windows_procedure (void *pCurrObject, void *fnptr, HWND hWndParent);
+void cwel_enum_child_windows_procedure (EIF_REFERENCE pCurrObject, void *fnptr, HWND hWndParent);
 
 #ifdef __cplusplus
 }

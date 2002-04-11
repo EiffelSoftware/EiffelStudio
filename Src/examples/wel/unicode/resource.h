f5afeb61bcfa6ae25637f87e7c31d1b189051908
neilc <neilc@eiffel.com>
2002-04-11 20:15:48 +0000

    Modified resource
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@31419 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/examples/wel/unicode/resource.h
+++ Src/examples/wel/unicode/resource.h
@@ -1,24 +1,20 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Developer Studio generated include file.
-// Used by unicode_demo.rc
+// Used by unicode.rc
 //
-#define Idmainmenu                      101
-#define Idshapefull                     40001
-#define Idshapehalf                     40002
-#define Idnative                        40003
-#define Idalphanumeric                  40004
-#define Idsoftkeyboard                  40005
-#define ID_IMEOPTIONS_CONVERSIONMODE_NOCONVERSION 40006
-#define ID_IMEOPTIONS_CONVERSIONMODE_EUDC 40007
-#define ID_IMEOPTIONS_CONVERSIONMODE_SYMBOL 40008
-#define ID_IMEOPTIONS_CONVERSIONMODE_HANJA 40009
+#define IDR_MENU1                       101
+#define Idsoftkeyboard_constant         40001
+#define Idshapehalf_constant            40003
+#define Idshapefull_constant            40004
+#define Idnative_constant               40007
+#define Idalphanumeric_constant         40008
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40010
+#define _APS_NEXT_COMMAND_VALUE         40009
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
 #endif

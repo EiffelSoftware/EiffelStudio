0d138ebb8f68d4899dcb02e0ab8bdaad275623b7
Emmanuel Stapf <manus@eiffel.com>
2002-04-20 03:28:00 +0000

    Fixed first incorrect implementation of min and max.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@31659 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_helpers.h
+++ Src/bench/C/run-time/eif_helpers.h
@@ -42,54 +42,54 @@ rt_private EIF_DOUBLE eif_abs_double (EIF_DOUBLE d) {
 
 /* Max computation */
 rt_private EIF_INTEGER_8 eif_max_int8 (EIF_INTEGER_8 i, EIF_INTEGER_8 j) {
-	return (i > j ? j : j);
+	return (i > j ? i : j);
 }
 rt_private EIF_INTEGER_16 eif_max_int16 (EIF_INTEGER_16 i, EIF_INTEGER_16 j) {
-	return (i > j ? j : j);
+	return (i > j ? i : j);
 }
 rt_private EIF_INTEGER_32 eif_max_int32 (EIF_INTEGER_32 i, EIF_INTEGER_32 j) {
-	return (i > j ? j : j);
+	return (i > j ? i : j);
 }
 rt_private EIF_INTEGER_64 eif_max_int64 (EIF_INTEGER_64 i, EIF_INTEGER_64 j) {
-	return (i > j ? j : j);
+	return (i > j ? i : j);
 }
 rt_private EIF_CHARACTER eif_max_char (EIF_CHARACTER i, EIF_CHARACTER j) {
-	return (i > j ? j : j);
+	return (i > j ? i : j);
 }
 rt_private EIF_WIDE_CHAR eif_max_wide_char (EIF_WIDE_CHAR i, EIF_WIDE_CHAR j) {
-	return (i > j ? j : j);
+	return (i > j ? i : j);
 }
 rt_private EIF_REAL eif_max_real (EIF_REAL i, EIF_REAL j) {
-	return (i > j ? j : j);
+	return (i > j ? i : j);
 }
 rt_private EIF_DOUBLE eif_max_DOUBLE (EIF_DOUBLE i, EIF_DOUBLE j) {
-	return (i > j ? j : j);
+	return (i > j ? i : j);
 }
 
 /* Min computation */
 rt_private EIF_INTEGER_8 eif_min_int8 (EIF_INTEGER_8 i, EIF_INTEGER_8 j) {
-	return (i < j ? j : j);
+	return (i < j ? i : j);
 }
 rt_private EIF_INTEGER_16 eif_min_int16 (EIF_INTEGER_16 i, EIF_INTEGER_16 j) {
-	return (i < j ? j : j);
+	return (i < j ? i : j);
 }
 rt_private EIF_INTEGER_32 eif_min_int32 (EIF_INTEGER_32 i, EIF_INTEGER_32 j) {
-	return (i < j ? j : j);
+	return (i < j ? i : j);
 }
 rt_private EIF_INTEGER_64 eif_min_int64 (EIF_INTEGER_64 i, EIF_INTEGER_64 j) {
-	return (i < j ? j : j);
+	return (i < j ? i : j);
 }
 rt_private EIF_CHARACTER eif_min_char (EIF_CHARACTER i, EIF_CHARACTER j) {
-	return (i < j ? j : j);
+	return (i < j ? i : j);
 }
 rt_private EIF_WIDE_CHAR eif_min_wide_char (EIF_WIDE_CHAR i, EIF_WIDE_CHAR j) {
-	return (i < j ? j : j);
+	return (i < j ? i : j);
 }
 rt_private EIF_REAL eif_min_real (EIF_REAL i, EIF_REAL j) {
-	return (i < j ? j : j);
+	return (i < j ? i : j);
 }
 rt_private EIF_DOUBLE eif_min_DOUBLE (EIF_DOUBLE i, EIF_DOUBLE j) {
-	return (i < j ? j : j);
+	return (i < j ? i : j);
 }
 
 

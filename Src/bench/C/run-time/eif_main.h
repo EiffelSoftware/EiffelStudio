eefaaf806695459e578b10c1a6c743420651ac69
Emmanuel Stapf <manus@eiffel.com>
2002-05-31 23:54:01 +0000

    Moved some declaration from `eif_econsole.h' in current header file, that it makes more sense to have them here.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@32318 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_main.h
+++ Src/bench/C/run-time/eif_main.h
@@ -15,6 +15,10 @@
 #include "eif_portable.h"
 #include "eif_globals.h"
 
+#ifdef EIF_WIN32
+#include <windows.h>
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -22,6 +26,16 @@ extern "C" {
 RT_LNK long EIF_once_count;			/* Nr. of once routines */
 RT_LNK int scount;					/* Maximum dtype */
 RT_LNK void eif_alloc_init(void);
+
+#ifdef EIF_WIN32
+RT_LNK void get_argcargv (int *argc, char ***argv);
+RT_LNK void free_argv (char ***argv);
+RT_LNK HANDLE ghInstance;
+RT_LNK HINSTANCE eif_hInstance;
+RT_LNK HINSTANCE eif_hPrevInstance;
+RT_LNK LPSTR eif_lpCmdLine;
+RT_LNK int eif_nCmdShow;
+#endif
 
 #ifdef __cplusplus
 }

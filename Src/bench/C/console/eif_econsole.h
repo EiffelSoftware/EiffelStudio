cc5622819ce40d312ecee24c90af4cca64d1887d
Emmanuel Stapf <manus@eiffel.com>
2002-05-31 23:54:47 +0000

    Removed useless declaration since we have now removed all the declared features from `econsole.c' and that some of them have been moved into `eif_main.h'.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@32319 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/console/eif_econsole.h
+++ Src/bench/C/console/eif_econsole.h
@@ -1,5 +1,5 @@
 /*
-	EIF_CONS.H - a console for Win32.
+	eif_econsole.h - a console for Win32.
 
 */
 
@@ -12,30 +12,8 @@
 extern "C" {
 #endif
 
-RT_LNK void get_argcargv (int *argc, char ***argv);
-RT_LNK void free_argv (char ***argv);
-RT_LNK HANDLE ghInstance;
-RT_LNK HINSTANCE eif_hInstance;
-RT_LNK HINSTANCE eif_hPrevInstance;
-RT_LNK LPSTR eif_lpCmdLine;
-RT_LNK int eif_nCmdShow;
-
 extern void eif_show_console(void);					/* Show the DOS console if needed */
 extern void eif_console_cleanup (EIF_BOOLEAN);
-extern void eif_console_next_line(void);
-extern void eif_console_putint(EIF_INTEGER);
-extern void eif_console_putreal(EIF_REAL);
-extern void eif_console_putdouble(EIF_DOUBLE);
-extern void eif_console_putchar(EIF_CHARACTER);
-extern void eif_console_putstring(EIF_CHARACTER *, EIF_INTEGER);
-extern EIF_INTEGER eif_console_readline(EIF_CHARACTER *, EIF_INTEGER, EIF_INTEGER);
-extern EIF_REAL eif_console_readreal(void);
-extern EIF_DOUBLE eif_console_readdouble(void);
-extern EIF_CHARACTER eif_console_readchar(void);
-extern EIF_INTEGER eif_console_readint(void);
-extern EIF_INTEGER eif_console_readword(EIF_CHARACTER *, EIF_INTEGER, EIF_INTEGER);
-extern EIF_INTEGER eif_console_readstream(EIF_CHARACTER *, EIF_INTEGER);
-extern EIF_BOOLEAN eif_console_eof(void);
 
 #ifdef __cplusplus
 }

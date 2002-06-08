90b13547676ad216483c3cd32a9eeba0a3384496
neilc <neilc@eiffel.com>
2002-06-08 00:54:51 +0000

    Added new functions to call ISE.VS.FusionSupport.dll to get dotnet GAC assembly information
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@32395 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/Eiffel/library/cli_writer/Clib/cli_writer.cpp
+++ Src/bench/Eiffel/library/cli_writer/Clib/cli_writer.cpp
@@ -1,4 +1,5 @@
 #include "cli_writer.h"
+#include "vs_support.h"
 #include <objbase.h>
 #include <cor.h>
 #include <CorSym.h>
@@ -38,6 +39,163 @@ rt_public void com_initialize ()
 
 	CHECK (hr, "CoInitialize failed");
 }
+
+/*
+feature -- Fusion Support API
+*/
+
+//FusionSupport------------------------------------------------------------------------------
+
+rt_public EIF_POINTER new_fusion_support ()
+	/* Create new instance of IFusionSupport */
+{
+	HRESULT hr;
+	IFusionSupport *ifs;
+
+	hr = CoCreateInstance (CLSID_FusionSupport, NULL,
+		CLSCTX_INPROC_SERVER, IID_IFusionSupport, (void **) & ifs);
+
+	CHECK (hr, "Could not create IFusionSupport")
+
+	return ifs;
+}
+
+rt_public EIF_POINTER c_get_gac_assemblies (EIF_POINTER i_fusion_support)
+	/* Call `GetGacAssemblies()' */
+{
+	IFusionSupport * fusion_support = (IFusionSupport *) i_fusion_support;
+	IEnumAssemblies *iea;
+	HRESULT hr;
+
+	hr = fusion_support->GetGacAssemblies ((IEnumAssemblies **) &iea);
+
+	CHECK (hr, "Could not get IEnumAssemblies")
+
+	return iea;
+}
+
+rt_public EIF_BOOLEAN c_signed (EIF_POINTER i_fusion_support, EIF_POINTER a_path)
+	/* Call `GetGacAssemblies()' */
+{
+	IFusionSupport * fusion_support = (IFusionSupport *) i_fusion_support;
+	BOOL is_signed;
+	HRESULT hr;
+	BSTR pBStr;
+
+	pBStr = SysAllocString ((LPWSTR) a_path);
+
+	hr = fusion_support->IsAssemblySigned (pBStr, (VARIANT_BOOL*) &is_signed);
+
+	SysFreeString (pBStr);
+
+	CHECK (hr, "Could not get IsAssemblySigned")
+
+	return (EIF_BOOLEAN) is_signed;
+}
+
+rt_public EIF_POINTER c_get_assembly_info_from_assembly (EIF_POINTER i_fusion_support, EIF_POINTER a_path)
+	/* Call `GetGacAssemblies()' */
+{
+	IFusionSupport * fusion_support = (IFusionSupport *) i_fusion_support;
+	IAssemblyInfo *iai;
+	HRESULT hr;
+	BSTR pBStr;
+
+	pBStr = SysAllocString ((LPWSTR) a_path);
+
+	hr = fusion_support->GetAssemblyInfoFromAssembly (pBStr, (IAssemblyInfo **) &iai);
+
+	SysFreeString (pBStr);
+
+	CHECK (hr, "Could not get IsAssemblySigned")
+
+	return iai;
+}
+
+//IEnumAssemblies----------------------------------------------------------------------------
+
+rt_public EIF_POINTER c_ith (EIF_POINTER i_enum_assemblies, EIF_INTEGER index)
+	/* Call `IEnumAssemblies->IthItem(index)' */
+{
+	IEnumAssemblies * enum_assemblies = (IEnumAssemblies *) i_enum_assemblies;
+	IAssemblyInfo *iai;
+	HRESULT hr;
+
+	hr = enum_assemblies->IthItem (index, (IAssemblyInfo **) &iai);
+
+	CHECK (hr, "Could not get IAssembyInfo")
+
+	return iai;
+}
+
+rt_public EIF_INTEGER c_count (EIF_POINTER i_enum_assemblies)
+	/* Call `IEnumAssemblies->Count()' */
+{
+	IEnumAssemblies * enum_assemblies = (IEnumAssemblies *) i_enum_assemblies;
+	long count;
+	HRESULT hr;
+
+	hr = enum_assemblies->get_Count (&count);
+
+	CHECK (hr, "Could not get Count")
+
+	return count;
+}
+
+//IAssemblyInfo----------------------------------------------------------------------------------
+
+rt_public EIF_INTEGER c_name (EIF_POINTER i_assembly_info, EIF_POINTER a_str)
+	/* Call `IAssemblyInfo->assembly_name() */
+{
+	IAssemblyInfo* assembly_info = (IAssemblyInfo *) i_assembly_info;
+	HRESULT hr;
+
+	hr = assembly_info->AssemblyName ((LPWSTR *) a_str);
+
+	CHECK (hr, "Could not get Name")
+
+	return hr;
+}
+
+rt_public EIF_INTEGER c_version (EIF_POINTER i_assembly_info, EIF_POINTER a_str)
+	/* Call `IAssemblyInfo->assembly_version() */
+{
+	IAssemblyInfo* assembly_info = (IAssemblyInfo *) i_assembly_info;
+	HRESULT hr;
+
+	hr = assembly_info->Version ((LPWSTR *) a_str);
+
+	CHECK (hr, "Could not get Version")
+
+	return hr;
+}
+
+rt_public EIF_INTEGER c_culture (EIF_POINTER i_assembly_info, EIF_POINTER a_str)
+	/* Call `IAssemblyInfo->assembly_culture() */
+{
+	IAssemblyInfo* assembly_info = (IAssemblyInfo *) i_assembly_info;
+	HRESULT hr;
+
+	hr = assembly_info->Culture ((LPWSTR *) a_str);
+
+	CHECK (hr, "Could not get Culture")
+
+	return hr;
+}
+
+rt_public EIF_INTEGER c_public_key_token (EIF_POINTER i_assembly_info, EIF_POINTER a_str)
+	/* Call `IAssemblyInfo->assembly_public_key_token() */
+{
+	IAssemblyInfo* assembly_info = (IAssemblyInfo *) i_assembly_info;
+	HRESULT hr;
+
+	hr = assembly_info->PublicKeyToken ((LPWSTR *) a_str);
+
+	CHECK (hr, "Could not get Public Key Token")
+
+	return hr;
+}
+
 
 /*
 feature -- Unmanaged Metadata API

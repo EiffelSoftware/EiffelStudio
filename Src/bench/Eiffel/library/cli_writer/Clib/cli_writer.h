2eecf02e59b6492ccaf5bba12d8cb23b5df17633
neilc <neilc@eiffel.com>
2002-06-10 20:20:52 +0000

    Removed unneeded C code to COM calls
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@32411 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/Eiffel/library/cli_writer/Clib/cli_writer.h
+++ Src/bench/Eiffel/library/cli_writer/Clib/cli_writer.h
@@ -10,15 +10,6 @@ extern "C" {
 
 extern void com_initialize ();
 extern EIF_POINTER new_fusion_support ();
-extern EIF_POINTER c_get_gac_assemblies (EIF_POINTER);
-extern EIF_BOOLEAN c_signed (EIF_POINTER , EIF_POINTER);
-extern EIF_POINTER c_get_assembly_info_from_assembly (EIF_POINTER, EIF_POINTER);
-extern EIF_POINTER c_ith (EIF_POINTER, EIF_INTEGER);
-extern EIF_INTEGER c_count (EIF_POINTER);
-extern EIF_INTEGER c_name (EIF_POINTER, EIF_POINTER);
-extern EIF_INTEGER c_version (EIF_POINTER, EIF_POINTER);
-extern EIF_INTEGER c_culture (EIF_POINTER, EIF_POINTER);
-extern EIF_INTEGER c_public_key_token (EIF_POINTER, EIF_POINTER);
 extern EIF_POINTER new_md_dispenser ();
 extern EIF_POINTER c_define_scope_for_md_emit (EIF_POINTER);
 extern EIF_POINTER c_query_assembly_emit (EIF_POINTER);

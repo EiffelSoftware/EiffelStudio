20df8998c579c2e65c74a1141c76ca956ced3988
Emmanuel Stapf <manus@eiffel.com>
2002-06-12 17:18:33 +0000

    Adde `_invariant' feature to EIFFEL_TYPE_INFO, used for invariant checking.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@32449 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/Eiffel/eiffel/com_il_generation/Core/run-time/gen_conf.cs
+++ Src/bench/Eiffel/eiffel/com_il_generation/Core/run-time/gen_conf.cs
@@ -227,9 +227,29 @@ feature -- Settings
 
 [CLSCompliantAttribute (false)]
 public interface EIFFEL_TYPE_INFO {
+/*
+feature -- Assertion checking
+*/
+	void _invariant ();
+		/* Checks invariant of current class, throw an exception
+		 * when invariant is not satisfied.
+		 */
+/*
+feature -- Access
+*/
 	EIFFEL_DERIVATION ____type();
+		/* Get generic type if any. */
+
 	String ____class_name();
+		/* Name of current object's generating class
+		 * (base class of the type of which it is a direct instance)
+		 */
+/*
+feature -- Settings
+*/
 	void ____set_type(EIFFEL_DERIVATION type);
+		/* Set `____type' with `type'. */
+
 }
 
 }

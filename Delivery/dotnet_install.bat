5b3f05e18e5b7e00cf6b8cfdf83769171980b8a2
Emmanuel Stapf <manus@eiffel.com>
2002-06-14 00:14:57 +0000

    Updated installation of .NET components.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@32485 8089f293-4706-0410-a29e-feb5c42a2edf

--- Delivery/dotnet_install.bat
+++ Delivery/dotnet_install.bat
@@ -9,30 +9,10 @@ cd studio\spec\windows\bin
 
 rem Registering ISE runtime for .NET
 
-call gacutil -silent -nologo -u ise_runtime
-call ngen -silent -nologo -delete ise_runtime
-
-call gacutil -silent -nologo -i ise_runtime.dll
+call gacutil -silent -nologo -if ise_runtime.dll
 call ngen -silent -nologo ise_runtime.dll
 
-rem Registering ISE generator for .NET
-call gacutil -silent -nologo -u EiffelCompiler
-call ngen -silent -nologo -delete EiffelCompiler
-
-call gacutil -silent -nologo -i EiffelCompiler.dll
-call ngen -silent -nologo EiffelCompiler.dll
-call regasm -silent -nologo EiffelCompiler.dll
-
 cd ..\..\..\..
-
-rem Assembly Manager install
-rem call dotnet_install_ami.bat
-
-rem Registering Eiffel Assembly Cache
-rem cd dotnet\assembly_manager
-rem call regasm -silent -nologo ISE.Reflection.EiffelComponents.dll
-rem call regasm -silent -nologo ISE.Reflection.CodeGenerator.dll
-rem cd ..\..
 
 @echo on
 rem Installation of .NET components terminated.

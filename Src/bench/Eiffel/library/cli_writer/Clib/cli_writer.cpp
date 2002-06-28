e4551df91bbd0254e574984b69734dc0dfab802a
paulb <paulb@eiffel.com>
2002-06-28 01:25:46 +0000

    Updated for new GUID for latest version of the emitter
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@32603 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/Eiffel/library/cli_writer/Clib/cli_writer.cpp
+++ Src/bench/Eiffel/library/cli_writer/Clib/cli_writer.cpp
@@ -1,5 +1,6 @@
 #include "cli_writer.h"
 #include "vs_support.h"
+#include "ise_cache_manager.h"
 #include <objbase.h>
 #include <cor.h>
 #include <CorSym.h>
@@ -9,13 +10,13 @@ extern "C" {
 #endif
 
 rt_private char message [1024];
-	/* Error message for exceptions */
+  /* Error message for exceptions */
 
 rt_private void raise_error (HRESULT hr, char *msg);
-	/* Raise error */
+  /* Raise error */
 
 #ifdef ASSERTIONS
-#define CHECK(hr,msg)	if (hr) raise_error (hr, msg);
+#define CHECK(hr,msg) if (hr) raise_error (hr, msg);
 #else
 #define CHECK(hr,msg)
 #endif
@@ -25,19 +26,42 @@ feature -- COM specific
 */
 
 rt_private void raise_error (HRESULT hr, char *msg)
-	/* Raise an Eiffel exception */
+  /* Raise an Eiffel exception */
 {
-	sprintf (message, "0x%x: %s", hr, msg);
-	eraise (message, EN_PROG);
+  sprintf (message, "0x%x: %s", hr, msg);
+  eraise (message, EN_PROG);
 }
 
 rt_public void com_initialize ()
-	/* Initialize COM runtime */
+  /* Initialize COM runtime */
 {
-	HRESULT hr;
-	hr = CoInitialize (NULL);
+  HRESULT hr;
+  hr = CoInitialize (NULL);
 
-	CHECK (hr, "CoInitialize failed");
+  CHECK (hr, "CoInitialize failed");
+}
+
+// ISE_COM_CACHE_MANAGER---------------------------------------------------------------------
+
+rt_public EIF_POINTER new_ise_cache_manager ()
+  /* Create new instance of ISE_Cache_ISE_COM_CACHE_MANAGER */
+{
+  HRESULT hr;
+  ISE_Cache_ISE_COM_CACHE_MANAGER *pICM = NULL;
+
+  hr = CoCreateInstance (CLSID_ISE_Cache_Impl_ISE_COM_CACHE_MANAGER,
+                         NULL,
+                         CLSCTX_INPROC_SERVER,
+                         IID_ISE_Cache_ISE_COM_CACHE_MANAGER,
+                         reinterpret_cast<void **>(&pICM));
+
+  CHECK (hr, "Could not instansiate COM Object ISE_Cache_ISE_COM_CACHE_MANAGER")
+
+  hr = pICM->initialize ();
+
+  CHECK (hr, "Failure on initialize")
+
+  return pICM;
 }
 
 /*
@@ -47,31 +71,31 @@ feature -- Fusion Support API
 //FusionSupport------------------------------------------------------------------------------
 
 rt_public EIF_POINTER new_fusion_support ()
-	/* Create new instance of IFusionSupport */
+  /* Create new instance of IFusionSupport */
 {
-	HRESULT hr;
-	IFusionSupport *ifs;
+  HRESULT hr;
+  IFusionSupport *ifs;
 
-	hr = CoCreateInstance (CLSID_FusionSupport, NULL,
-		CLSCTX_INPROC_SERVER, IID_IFusionSupport, (void **) & ifs);
+  hr = CoCreateInstance (CLSID_FusionSupport, NULL,
+    CLSCTX_INPROC_SERVER, IID_IFusionSupport, (void **) & ifs);
 
-	CHECK (hr, "Could not create IFusionSupport")
+  CHECK (hr, "Could not create IFusionSupport")
 
-	return ifs;
+  return ifs;
 }
 
 rt_public EIF_POINTER c_get_bstr (EIF_POINTER a_uni_string)
-  	/* Get a BSTR from a LPWSTR */
+    /* Get a BSTR from a LPWSTR */
 {
-  	BSTR pBStr = SysAllocString ((LPWSTR) a_uni_string);
+    BSTR pBStr = SysAllocString ((LPWSTR) a_uni_string);
 
-   	return pBStr;
+    return pBStr;
 }
 
 rt_public void c_free_bstr (EIF_POINTER a_bstr)
-	/* Free memory associated with 'a_bstr' */
+  /* Free memory associated with 'a_bstr' */
 {
-	SysFreeString ( (BSTR) a_bstr);
+  SysFreeString ( (BSTR) a_bstr);
 }
 
 /*
@@ -79,46 +103,46 @@ feature -- Unmanaged Metadata API
 */
 
 rt_public EIF_POINTER new_md_dispenser ()
-	/* Create new instance of IMetaDataDispenser */
+  /* Create new instance of IMetaDataDispenser */
 {
-	HRESULT hr;
-	IMetaDataDispenser *imdd;
+  HRESULT hr;
+  IMetaDataDispenser *imdd;
 
-	hr = CoCreateInstance (CLSID_CorMetaDataDispenser, NULL,
-		CLSCTX_INPROC_SERVER, IID_IMetaDataDispenser, (void **) & imdd);
+  hr = CoCreateInstance (CLSID_CorMetaDataDispenser, NULL,
+    CLSCTX_INPROC_SERVER, IID_IMetaDataDispenser, (void **) & imdd);
 
-	CHECK (hr, "Could not create IMetaDataDisepenser")
+  CHECK (hr, "Could not create IMetaDataDisepenser")
 
-	return imdd;
+  return imdd;
 }
 
 rt_public EIF_POINTER c_define_scope_for_md_emit (EIF_POINTER md_dispenser)
-	/* Call `DefineScope (CLSID_CorMetaDataRuntime, 0, IID_IMetaDataEmit, (IUnknown **) &imde)' */
+  /* Call `DefineScope (CLSID_CorMetaDataRuntime, 0, IID_IMetaDataEmit, (IUnknown **) &imde)' */
 {
-	IMetaDataDispenser *dispenser = (IMetaDataDispenser *) md_dispenser;
-	IMetaDataEmit *imde;
-	HRESULT hr;
+  IMetaDataDispenser *dispenser = (IMetaDataDispenser *) md_dispenser;
+  IMetaDataEmit *imde;
+  HRESULT hr;
 
-	hr = dispenser->DefineScope (CLSID_CorMetaDataRuntime,
-			0, IID_IMetaDataEmit, (IUnknown **) &imde);
+  hr = dispenser->DefineScope (CLSID_CorMetaDataRuntime,
+      0, IID_IMetaDataEmit, (IUnknown **) &imde);
 
-	CHECK (hr, "Could not get IMetaDataEmit")
+  CHECK (hr, "Could not get IMetaDataEmit")
 
-	return imde;
+  return imde;
 }
 
 rt_public EIF_POINTER c_query_assembly_emit (EIF_POINTER md_emit)
-	/* Call `QueryInterface(IID_IMetaDataAssemblyEmit, (void **)&imda)' */
+  /* Call `QueryInterface(IID_IMetaDataAssemblyEmit, (void **)&imda)' */
 {
-	IMetaDataEmit *emit = (IMetaDataEmit *) md_emit;
-	IMetaDataAssemblyEmit *imda;
-	HRESULT hr;
+  IMetaDataEmit *emit = (IMetaDataEmit *) md_emit;
+  IMetaDataAssemblyEmit *imda;
+  HRESULT hr;
 
-	hr = emit->QueryInterface(IID_IMetaDataAssemblyEmit, (void **)&imda);
+  hr = emit->QueryInterface(IID_IMetaDataAssemblyEmit, (void **)&imda);
 
-	CHECK (hr, "Could not get IMetaDataAssemblyEmit");
+  CHECK (hr, "Could not get IMetaDataAssemblyEmit");
 
-	return imda;
+  return imda;
 }
 
 /*
@@ -126,17 +150,17 @@ feature -- Debuggin information
 */
 
 rt_public EIF_POINTER new_sym_writer ()
-	/* Create new instance of ISymUnmanagedWriter */
+  /* Create new instance of ISymUnmanagedWriter */
 {
-	HRESULT hr;
-	ISymUnmanagedWriter *writer;
+  HRESULT hr;
+  ISymUnmanagedWriter *writer;
 
-	hr = CoCreateInstance (CLSID_CorSymWriter, NULL,
-		CLSCTX_INPROC_SERVER, IID_ISymUnmanagedWriter, (void **) & writer);
+  hr = CoCreateInstance (CLSID_CorSymWriter, NULL,
+    CLSCTX_INPROC_SERVER, IID_ISymUnmanagedWriter, (void **) & writer);
 
-	CHECK (hr, "Could not create ISymUnmanagedWriter")
+  CHECK (hr, "Could not create ISymUnmanagedWriter")
 
-	return writer;
+  return writer;
 }
 
 #ifdef _cplusplus

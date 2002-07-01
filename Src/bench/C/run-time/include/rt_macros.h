b6d42f8c70a574608458de7c2e1dec31821e842a
Emmanuel Stapf <manus@eiffel.com>
2002-07-01 16:52:15 +0000

    Added RT_SPECIAL_INFO, RT_SPECIAL_COUNT and RT_SPECIAL_ELEM_SIZE and the corresponding _WITH_ZONE to quickly get access to internals of SPECIAL objects. (useful for writing assertions).
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@32619 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/include/rt_macros.h
+++ Src/bench/C/run-time/include/rt_macros.h
@@ -36,6 +36,29 @@ extern "C" {
 #define RT_GC_WEAN_N(n)
 #endif
 
+/* Macro used to get info about SPECIAL objects. */
+/* RT_SPECIAL_INFO, RT_SPECIAL_INFO_WITH_ZONE returns pointer to where `count' and `element_size'
+ *    of a special objects is stored.
+ * RT_SPECIAL_COUNT, RT_SPECIAL_COUNT_WITH_ZONE returns `count' of special objects.
+ * RT_SPECIAL_ELEM_SIZE, RT_SPECIAL_ELEM_SIZE_WITH_ZONE returns `element_size' of items in special objects.
+ */
+#define RT_SPECIAL_INFO(spec) \
+	((spec) + (HEADER(spec)->ov_size & B_SIZE) - LNGPAD_2)
+#define RT_SPECIAL_INFO_WITH_ZONE(spec,zone) \
+	((spec) + ((zone)->ov_size & B_SIZE) - LNGPAD_2)
+
+#define RT_SPECIAL_COUNT(spec) \
+	(*(EIF_INTEGER *) RT_SPECIAL_INFO(spec))
+#define RT_SPECIAL_COUNT_WITH_ZONE(spec,zone) \
+	(*(EIF_INTEGER *) RT_SPECIAL_INFO_WITH_ZONE(spec,zone))
+
+#define RT_SPECIAL_ELEM_SIZE(spec) \
+	(*(EIF_INTEGER *) (RT_SPECIAL_INFO(spec) + sizeof(EIF_INTEGER))
+#define RT_SPECIAL_ELEM_SIZE_WITH_ZONE(spec,zone) \
+	(*(EIF_INTEGER *) RT_SPECIAL_INFO_WITH_ZONE(spec,zone) + sizeof(EIF_INTEGER))
+
+
+/* Macro used to protect concurrent running of GC. */
 #ifdef EIF_THREADS
 #define EIF_GC_MUTEX_LOCK	EIF_MUTEX_LOCK(eif_gc_mutex, "Could not lock GC mutex")
 #define EIF_GC_MUTEX_UNLOCK	EIF_MUTEX_UNLOCK(eif_gc_mutex, "Could not unlock GC mutex")

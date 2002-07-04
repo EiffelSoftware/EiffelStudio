ee150dfd7f9e5c2b39bb2eb0181235e0b6f03560
Emmanuel Stapf <manus@eiffel.com>
2002-07-04 05:35:47 +0000

    No need for C code now, everything is done in Eiffel in new C_DATE class.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@32708 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/time/Clib/make_msc.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ------------------------------------------------------------------------
-echo Compiling datetime.lib
-echo ------------------------------------------------------------------------
-nmake /NOLOGO /C /S /f makefile.msc > make.log
-
-echo. 
-echo Compilation done.
-echo The result of the compilation is available in "make.log"

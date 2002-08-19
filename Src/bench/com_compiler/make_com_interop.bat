73fda4ffc4476aead649a8947799b548dccca58d
raphaels <raphaels@eiffel.com>
2002-08-19 17:46:01 +0000

    Renamed ISE.Compiler.exe into cec.exe
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@33764 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/com_compiler/make_com_interop.bat
+++ Src/bench/com_compiler/make_com_interop.bat
@@ -1,19 +1,19 @@
 @echo off
-@rem Creates necessary files to use ISE.Compiler.exe as .net assembly
-@rem ESI.Compiler.exe must be compiled!
+@rem Creates necessary files to use cec.exe as .net assembly
+@rem cec.exe must be compiled!
 
 @rem create workbench version of the compiler
 
 cd ace_file\EIFGEN\W_CODE
 echo ### Creating Workbench Version ###
 echo .
-tlbimp /out:ISE.ComCompilerWrapper.dll /namespace:ISE.ComCompilerWrapper /asmversion:5.2.0.0 /keyfile:"%EIFFEL_SRC%\dotnet\helpers\isekey.snk" ISE.Compiler.exe /nologo
+tlbimp /out:ISE.ComCompilerWrapper.dll /namespace:ISE.ComCompilerWrapper /asmversion:5.2.0.0 /keyfile:"%EIFFEL_SRC%\dotnet\helpers\isekey.snk" cec.exe /nologo
 gacutil -if ISE.ComCompilerWrapper.dll /nologo
 echo ### Workbench Version Created ###
 cd ..\F_CODE
 echo ### Creating Finalized Version ###
 echo .
-tlbimp /out:ISE.Final.ComCompilerWrapper.dll /namespace:ISE.ComCompilerWrapper /asmversion:5.2.0.0 /keyfile:"%EIFFEL_SRC%\dotnet\helpers\isekey.snk" ISE.Compiler.exe /nologo
+tlbimp /out:ISE.Final.ComCompilerWrapper.dll /namespace:ISE.ComCompilerWrapper /asmversion:5.2.0.0 /keyfile:"%EIFFEL_SRC%\dotnet\helpers\isekey.snk" cec.exe /nologo
 gacutil -if ISE.Final.ComCompilerWrapper.dll /nologo
 echo .
 echo ### Finalized Version Created ###

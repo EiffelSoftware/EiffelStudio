cfb7ad301f67631428ee5604925e0b6c8681b90c
Emmanuel Stapf <manus@eiffel.com>
2002-09-03 05:46:45 +0000

    Removed Gobo from repository.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@34335 8089f293-4706-0410-a29e-feb5c42a2edf

--- free_add_ons/gobo/test/all/common/test_eiffel_parser.sh
+++
@@ -1,7 +0,0 @@
-#!/local/bin/bash
-
-./eiffel_parser 2 $GOBO/test/all/common/sample.e > tmp1.txt 2>&1
-if [ -s tmp1.txt ]; then
-	echo "./eiffel_parser 2 \$GOBO/test/all/common/sample.e"
-	cat tmp1.txt
-fi

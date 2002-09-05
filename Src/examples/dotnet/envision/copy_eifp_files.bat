025cc214d4f88f1f916047632453c25f6432a440
julieng <julieng@eiffel.com>
2002-09-05 03:20:12 +0000

    Copy appropriate eipf file located in eifp_files directory in example directory.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@34376 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/examples/dotnet/envision/copy_eifp_files.bat
@@ -0,0 +1,39 @@
+echo off
+
+copy .\eifp_files\ado.eifp .\samples\ado\ado.eifp
+
+copy .\eifp_files\calculator.eifp .\samples\calculator\calculator.eifp
+
+copy .\eifp_files\data.eifp .\samples\data\data_grid.eifp
+
+copy .\eifp_files\monitor.eifp .\samples\threading\monitor\monitor.eifp
+copy .\eifp_files\pools.eifp .\samples\threading\pools\pools.eifp
+copy .\eifp_files\timers.eifp .\samples\threading\timers\timers.eifp
+
+copy .\eifp_files\accelerator.eifp .\samples\vision2\accelerator\accelerator.eifp
+copy .\eifp_files\cursor.eifp .\samples\vision2\cursor\cursor.eifp
+copy .\eifp_files\gauges.eifp .\samples\vision2\gauges\gauges.eifp
+copy .\eifp_files\standard_dialogs.eifp .\samples\vision2\standard_dialogs\standard_dialogs.eifp
+copy .\eifp_files\viewport.eifp .\samples\vision2\viewport\viewport.eifp
+copy .\eifp_files\widgets.eifp .\samples\vision2\widgets\widgets.eifp
+
+copy .\eifp_files\step1.eifp .\samples\wel\step1\step1.eifp
+copy .\eifp_files\step2.eifp .\samples\wel\step2\step2.eifp
+copy .\eifp_files\step3.eifp .\samples\wel\step3\step3.eifp
+copy .\eifp_files\step4.eifp .\samples\wel\step4\step4.eifp
+copy .\eifp_files\step5.eifp .\samples\wel\step5\step5.eifp
+copy .\eifp_files\step6.eifp .\samples\wel\step6\step6.eifp
+copy .\eifp_files\step7.eifp .\samples\wel\step7\step7.eifp
+copy .\eifp_files\step8.eifp .\samples\wel\step8\step8.eifp
+copy .\eifp_files\xcell.eifp .\samples\wel\xcell\xcell.eifp
+
+copy .\eifp_files\date_time_picker.eifp .\samples\winforms\date_time_picker\date_time_picker.eifp
+copy .\eifp_files\gdi.eifp .\samples\winforms\gdi\gdi.eifp
+copy .\eifp_files\hello_world_dialog.eifp .\samples\winforms\hello_world_dialog\hello_world_dialog.eifp
+copy .\eifp_files\hello_world_window.eifp .\samples\winforms\hello_world_window\hello_world_window.eifp
+copy .\eifp_files\mdi.eifp .\samples\winforms\mdi\mdi.eifp
+copy .\eifp_files\menu.eifp .\samples\winforms\menu\menu.eifp
+copy .\eifp_files\progress_bar.eifp .\samples\winforms\progress_bar\progress_bar.eifp
+copy .\eifp_files\simple_window.eifp .\samples\winforms\simple_window\simple_window.eifp
+copy .\eifp_files\tree_view.eifp .\samples\winforms\tree_view\tree_view.eifp
+copy .\eifp_files\window_calculator.eifp .\samples\winforms\window_calculator\window_calculator.eifp

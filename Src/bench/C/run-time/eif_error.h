8669c978430d954cca0f56992ab117691c21669c
Emmanuel Stapf <manus@eiffel.com>
2002-11-17 05:38:08 +0000

    Provide an equivalent to `eraise' for IL code generation (But behavior is not yet identical). Also definied `eio', `esys' and `eise_io' the same way.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@34878 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_error.h
+++ Src/bench/C/run-time/eif_error.h
@@ -12,13 +12,21 @@
 #ifndef _eif_error_h_
 #define _eif_error_h_
 
+#include "eif_except.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#ifdef EIF_IL_DLL
+#define esys()	eraise("Operating system error", EN_SYS)
+#define eio()	eraise("I/O error", EN_IO)
+#define eise_io(tag)	eraise(tag, EN_ISE_IO)
+#else
 extern void esys(void);				/* Raise 'Operating system error' exception */
 extern void eio(void);				/* Raise an 'I/O error' exception with default tag */
 extern void eise_io(char *tag);		/* Raise an Eiffel 'I/O error' exception with `tag' */
+#endif
 extern char *error_tag(int code);	/* English description out of errno code */
 
 #ifdef HAS_SYS_ERRLIST

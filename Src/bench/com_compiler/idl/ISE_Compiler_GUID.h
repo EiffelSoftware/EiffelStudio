3519c2bea5c45b06b2deba493add999c1fce6d36
paulb <paulb@eiffel.com>
2002-11-22 18:36:59 +0000

    All GUID for ISE.Compiler.IDL
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@34896 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/bench/com_compiler/idl/ISE_Compiler_GUID.h
@@ -0,0 +1,9 @@
+// ISE.Compiler.idl GUID declarations
+
+#ifndef __ISE_COMPILER_H__
+#define __ISE_COMPILER_H__
+
+#define GUID_IEiffelException D4E4EFBA-B090-420A-86A2-AF0E646D6DC4
+#define GUID_IEiffelExceptionManager 7A6CFC75-81FC-408F-A882-2C766F8300C4
+
+#endif

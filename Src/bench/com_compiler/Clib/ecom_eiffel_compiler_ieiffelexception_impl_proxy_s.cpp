666b55e03df8741d299b0cd9cec114d54d2b5e47
paulb <paulb@eiffel.com>
2002-11-22 18:40:43 +0000

    New classes generated by COM Wizard
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@34901 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/bench/com_compiler/Clib/ecom_eiffel_compiler_ieiffelexception_impl_proxy_s.cpp
@@ -0,0 +1,163 @@
+/*-----------------------------------------------------------
+Implemented `IEiffelException' Interface.
+-----------------------------------------------------------*/
+
+#include "ecom_eiffel_compiler_IEiffelException_impl_proxy_s.h"
+static const IID IID_IEiffelException_ = {0xd4e4efba,0xb090,0x420a,{0x86,0xa2,0xaf,0x0e,0x64,0x6d,0x6d,0xc4}};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+ecom_eiffel_compiler::IEiffelException_impl_proxy::IEiffelException_impl_proxy( IUnknown * a_pointer )
+{
+	HRESULT hr, hr2;
+	hr = CoInitializeEx (NULL, COINIT_APARTMENTTHREADED);
+	if (FAILED (hr))
+	{
+		if ((HRESULT_FACILITY (hr)  ==  FACILITY_ITF) && (HRESULT_CODE (hr) > 1024) && (HRESULT_CODE (hr) < 1053))
+			com_eraise (rt_ec.ccom_ec_lpstr (eename(HRESULT_CODE (hr) - 1024), NULL),HRESULT_CODE (hr) - 1024);
+		com_eraise (f.c_format_message (hr), EN_PROG);
+	};
+	hr = a_pointer->QueryInterface(IID_IUnknown, (void **)&p_unknown);
+	if (FAILED (hr))
+	{
+		if ((HRESULT_FACILITY (hr)  ==  FACILITY_ITF) && (HRESULT_CODE (hr) > 1024) && (HRESULT_CODE (hr) < 1053))
+			com_eraise (rt_ec.ccom_ec_lpstr (eename(HRESULT_CODE (hr) - 1024), NULL),HRESULT_CODE (hr) - 1024);
+		com_eraise (f.c_format_message (hr), EN_PROG);
+	};
+
+	hr = a_pointer->QueryInterface(IID_IEiffelException_, (void **)&p_IEiffelException);
+	if (FAILED (hr))
+	{
+		if ((HRESULT_FACILITY (hr)  ==  FACILITY_ITF) && (HRESULT_CODE (hr) > 1024) && (HRESULT_CODE (hr) < 1053))
+			com_eraise (rt_ec.ccom_ec_lpstr (eename(HRESULT_CODE (hr) - 1024), NULL),HRESULT_CODE (hr) - 1024);
+		com_eraise (f.c_format_message (hr), EN_PROG);
+	};
+
+};
+/*----------------------------------------------------------------------------------------------------------------------*/
+
+ecom_eiffel_compiler::IEiffelException_impl_proxy::~IEiffelException_impl_proxy()
+{
+	p_unknown->Release ();
+	if (p_IEiffelException!=NULL)
+		p_IEiffelException->Release ();
+	CoUninitialize ();
+};
+/*----------------------------------------------------------------------------------------------------------------------*/
+
+EIF_REFERENCE ecom_eiffel_compiler::IEiffelException_impl_proxy::ccom_inner_exception(  )
+
+/*-----------------------------------------------------------
+	Get inner exception
+-----------------------------------------------------------*/
+{
+	HRESULT hr;
+	if (p_IEiffelException == NULL)
+	{
+		hr = p_unknown->QueryInterface (IID_IEiffelException_, (void **)&p_IEiffelException);
+	if (FAILED (hr))
+	{
+		if ((HRESULT_FACILITY (hr)  ==  FACILITY_ITF) && (HRESULT_CODE (hr) > 1024) && (HRESULT_CODE (hr) < 1053))
+			com_eraise (rt_ec.ccom_ec_lpstr (eename(HRESULT_CODE (hr) - 1024), NULL),HRESULT_CODE (hr) - 1024);
+		com_eraise (f.c_format_message (hr), EN_PROG);
+	};
+	};
+	ecom_eiffel_compiler::IEiffelException * ret_value = 0;
+	
+	hr = p_IEiffelException->inner_exception( &ret_value);
+	if (FAILED (hr))
+	{
+		if ((HRESULT_FACILITY (hr)  ==  FACILITY_ITF) && (HRESULT_CODE (hr) > 1024) && (HRESULT_CODE (hr) < 1053))
+			com_eraise (rt_ec.ccom_ec_lpstr (eename(HRESULT_CODE (hr) - 1024), NULL),HRESULT_CODE (hr) - 1024);
+		com_eraise (f.c_format_message (hr), EN_PROG);
+	};
+	
+	
+	EIF_REFERENCE eiffel_result = eif_protect ((EIF_REFERENCE)grt_ce_ISE.ccom_ce_pointed_interface_7 (ret_value));
+	return eif_wean (eiffel_result);
+};
+/*----------------------------------------------------------------------------------------------------------------------*/
+
+EIF_REFERENCE ecom_eiffel_compiler::IEiffelException_impl_proxy::ccom_message(  )
+
+/*-----------------------------------------------------------
+	Get exception message
+-----------------------------------------------------------*/
+{
+	HRESULT hr;
+	if (p_IEiffelException == NULL)
+	{
+		hr = p_unknown->QueryInterface (IID_IEiffelException_, (void **)&p_IEiffelException);
+	if (FAILED (hr))
+	{
+		if ((HRESULT_FACILITY (hr)  ==  FACILITY_ITF) && (HRESULT_CODE (hr) > 1024) && (HRESULT_CODE (hr) < 1053))
+			com_eraise (rt_ec.ccom_ec_lpstr (eename(HRESULT_CODE (hr) - 1024), NULL),HRESULT_CODE (hr) - 1024);
+		com_eraise (f.c_format_message (hr), EN_PROG);
+	};
+	};
+	BSTR ret_value = 0;
+	
+	hr = p_IEiffelException->message( &ret_value);
+	if (FAILED (hr))
+	{
+		if ((HRESULT_FACILITY (hr)  ==  FACILITY_ITF) && (HRESULT_CODE (hr) > 1024) && (HRESULT_CODE (hr) < 1053))
+			com_eraise (rt_ec.ccom_ec_lpstr (eename(HRESULT_CODE (hr) - 1024), NULL),HRESULT_CODE (hr) - 1024);
+		com_eraise (f.c_format_message (hr), EN_PROG);
+	};
+	
+	
+	EIF_REFERENCE eiffel_result = eif_protect ((EIF_REFERENCE)rt_ce.ccom_ce_bstr (ret_value));
+	rt_ce.free_memory_bstr (ret_value);
+	return eif_wean (eiffel_result);
+};
+/*----------------------------------------------------------------------------------------------------------------------*/
+
+EIF_INTEGER ecom_eiffel_compiler::IEiffelException_impl_proxy::ccom_exception_code(  )
+
+/*-----------------------------------------------------------
+	Retrieve exception type
+-----------------------------------------------------------*/
+{
+	HRESULT hr;
+	if (p_IEiffelException == NULL)
+	{
+		hr = p_unknown->QueryInterface (IID_IEiffelException_, (void **)&p_IEiffelException);
+	if (FAILED (hr))
+	{
+		if ((HRESULT_FACILITY (hr)  ==  FACILITY_ITF) && (HRESULT_CODE (hr) > 1024) && (HRESULT_CODE (hr) < 1053))
+			com_eraise (rt_ec.ccom_ec_lpstr (eename(HRESULT_CODE (hr) - 1024), NULL),HRESULT_CODE (hr) - 1024);
+		com_eraise (f.c_format_message (hr), EN_PROG);
+	};
+	};
+	long ret_value = 0;
+	
+	hr = p_IEiffelException->exception_code( &ret_value);
+	if (FAILED (hr))
+	{
+		if ((HRESULT_FACILITY (hr)  ==  FACILITY_ITF) && (HRESULT_CODE (hr) > 1024) && (HRESULT_CODE (hr) < 1053))
+			com_eraise (rt_ec.ccom_ec_lpstr (eename(HRESULT_CODE (hr) - 1024), NULL),HRESULT_CODE (hr) - 1024);
+		com_eraise (f.c_format_message (hr), EN_PROG);
+	};
+	
+	
+	EIF_INTEGER eiffel_result =  (EIF_INTEGER)ret_value;
+	return (eiffel_result);
+};
+/*----------------------------------------------------------------------------------------------------------------------*/
+
+EIF_POINTER ecom_eiffel_compiler::IEiffelException_impl_proxy::ccom_item()
+
+/*-----------------------------------------------------------
+	IUnknown interface
+-----------------------------------------------------------*/
+{
+	return (EIF_POINTER)p_unknown;
+};
+/*----------------------------------------------------------------------------------------------------------------------*/
+
+
+#ifdef __cplusplus
+}
+#endif
---
+++ Src/bench/com_compiler/Clib/ecom_eiffel_compiler_ieiffelexception_impl_stub_s.cpp
@@ -0,0 +1,164 @@
+/*-----------------------------------------------------------
+Implemented `IEiffelException' Interface.
+-----------------------------------------------------------*/
+
+#include "ecom_eiffel_compiler_IEiffelException_impl_stub_s.h"
+static int return_hr_value;
+
+static const IID IID_IEiffelException_ = {0xd4e4efba,0xb090,0x420a,{0x86,0xa2,0xaf,0x0e,0x64,0x6d,0x6d,0xc4}};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+ecom_eiffel_compiler::IEiffelException_impl_stub::IEiffelException_impl_stub( EIF_OBJECT eif_obj )
+{
+	ref_count = 0;
+	eiffel_object = eif_adopt (eif_obj);
+	type_id = eif_type (eiffel_object);
+	
+};
+/*----------------------------------------------------------------------------------------------------------------------*/
+
+ecom_eiffel_compiler::IEiffelException_impl_stub::~IEiffelException_impl_stub()
+{
+	EIF_PROCEDURE eiffel_procedure;
+	eiffel_procedure = eif_procedure ("set_item", type_id);
+
+	(FUNCTION_CAST (void, (EIF_REFERENCE, EIF_POINTER))eiffel_procedure) (eif_access (eiffel_object), NULL);
+	eif_wean (eiffel_object);
+};
+/*----------------------------------------------------------------------------------------------------------------------*/
+
+STDMETHODIMP ecom_eiffel_compiler::IEiffelException_impl_stub::inner_exception(  /* [out, retval] */ ecom_eiffel_compiler::IEiffelException * * a_result )
+
+/*-----------------------------------------------------------
+	Get inner exception
+-----------------------------------------------------------*/
+{
+	ECATCH;
+
+	
+	EIF_REFERENCE_FUNCTION eiffel_function = 0;
+	eiffel_function = eif_reference_function ("inner_exception", type_id);
+	EIF_REFERENCE tmp_value = 0;
+	if (eiffel_function != NULL)
+		tmp_value = (FUNCTION_CAST (EIF_REFERENCE, (EIF_REFERENCE))eiffel_function) (eif_access (eiffel_object));
+	else
+		tmp_value = eif_field (eif_access (eiffel_object), "inner_exception", EIF_REFERENCE);
+	if (tmp_value != NULL)
+	{
+		EIF_OBJECT tmp_object = eif_protect (tmp_value);
+		*a_result = grt_ec_ISE.ccom_ec_pointed_interface_7 (eif_access (tmp_object));
+		eif_wean (tmp_object);
+	}
+	else
+		*a_result = NULL;
+	
+	END_ECATCH;
+	return S_OK;
+};
+/*----------------------------------------------------------------------------------------------------------------------*/
+
+STDMETHODIMP ecom_eiffel_compiler::IEiffelException_impl_stub::message(  /* [out, retval] */ BSTR * a_result )
+
+/*-----------------------------------------------------------
+	Get exception message
+-----------------------------------------------------------*/
+{
+	ECATCH;
+
+	
+	EIF_REFERENCE_FUNCTION eiffel_function = 0;
+	eiffel_function = eif_reference_function ("message", type_id);
+	EIF_REFERENCE tmp_value = 0;
+	if (eiffel_function != NULL)
+		tmp_value = (FUNCTION_CAST (EIF_REFERENCE, (EIF_REFERENCE))eiffel_function) (eif_access (eiffel_object));
+	else
+		tmp_value = eif_field (eif_access (eiffel_object), "message", EIF_REFERENCE);
+	if (tmp_value != NULL)
+	{
+		EIF_OBJECT tmp_object = eif_protect (tmp_value);
+		*a_result = rt_ec.ccom_ec_bstr (eif_access (tmp_object));
+		eif_wean (tmp_object);
+	}
+	else
+		*a_result = NULL;
+	
+	END_ECATCH;
+	return S_OK;
+};
+/*----------------------------------------------------------------------------------------------------------------------*/
+
+STDMETHODIMP ecom_eiffel_compiler::IEiffelException_impl_stub::exception_code(  /* [out, retval] */ long * a_result )
+
+/*-----------------------------------------------------------
+	Retrieve exception type
+-----------------------------------------------------------*/
+{
+	ECATCH;
+
+	
+	EIF_INTEGER_FUNCTION eiffel_function = 0;
+	eiffel_function = eif_integer_function ("exception_code", type_id);
+	EIF_INTEGER tmp_value = 0;
+	if (eiffel_function != NULL)
+		tmp_value = (FUNCTION_CAST (EIF_INTEGER, (EIF_REFERENCE))eiffel_function) (eif_access (eiffel_object));
+	else
+		tmp_value = eif_field (eif_access (eiffel_object), "exception_code", EIF_INTEGER);
+	*a_result = (long)tmp_value;
+	
+	END_ECATCH;
+	return S_OK;
+};
+/*----------------------------------------------------------------------------------------------------------------------*/
+
+STDMETHODIMP_(ULONG) ecom_eiffel_compiler::IEiffelException_impl_stub::Release()
+
+/*-----------------------------------------------------------
+	Decrement reference count
+-----------------------------------------------------------*/
+{
+	UnlockModule ();
+	LONG res = InterlockedDecrement (&ref_count);
+	if (res  ==  0)
+	{
+		delete this;
+	}
+	return res;
+};
+/*----------------------------------------------------------------------------------------------------------------------*/
+
+STDMETHODIMP_(ULONG) ecom_eiffel_compiler::IEiffelException_impl_stub::AddRef()
+
+/*-----------------------------------------------------------
+	Increment reference count
+-----------------------------------------------------------*/
+{
+	LockModule ();
+	return InterlockedIncrement (&ref_count);
+};
+/*----------------------------------------------------------------------------------------------------------------------*/
+
+STDMETHODIMP ecom_eiffel_compiler::IEiffelException_impl_stub::QueryInterface( REFIID riid, void ** ppv )
+
+/*-----------------------------------------------------------
+	Query Interface
+-----------------------------------------------------------*/
+{
+	if (riid == IID_IUnknown)
+		*ppv = static_cast<ecom_eiffel_compiler::IEiffelException*>(this);
+	else if (riid == IID_IEiffelException_)
+		*ppv = static_cast<ecom_eiffel_compiler::IEiffelException*>(this);
+	else
+		return (*ppv = 0), E_NOINTERFACE;
+
+	reinterpret_cast<IUnknown *>(*ppv)->AddRef ();
+	return S_OK;
+};
+/*----------------------------------------------------------------------------------------------------------------------*/
+
+
+#ifdef __cplusplus
+}
+#endif

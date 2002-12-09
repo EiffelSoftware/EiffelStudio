4708af7268a229e0d5c2d765a68158c70063cc7b
paulb <paulb@eiffel.com>
2002-12-09 23:16:59 +0000

    Changed all GUID's for COM Compiler. Updates ISE_Compiler_GUID.h to define all UUID's used in ISE.Compiler.idl for easy management
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@35035 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/com_compiler/idl/ISE_Compiler_GUID.h
+++ Src/bench/com_compiler/idl/ISE_Compiler_GUID.h
@@ -1,9 +1,70 @@
-// ISE.Compiler.idl GUID declarations
+////////////////////////////////////////////////////////////////////////////////////////
+//
+// ISE.Compiler.idl UUID declarations
+// (c)2002 Eiffel Software
+//
+////////////////////////////////////////////////////////////////////////////////////////
 
 #ifndef __ISE_COMPILER_H__
 #define __ISE_COMPILER_H__
 
-#define GUID_IEiffelException D4E4EFBA-B090-420A-86A2-AF0E646D6DC4
-#define GUID_IEiffelExceptionManager 7A6CFC75-81FC-408F-A882-2C766F8300C4
+//
+// Library GUID
+//
+
+#define UUID_LibEiffelCompiler                      06b5d7c0-2c7d-4d1c-a98b-4599bdcdfa58
+
+//
+// CoClass GUID
+//
+
+#define UUID_CEiffelProject                         5b4a07bb-ba73-4d00-b0fe-352fc4127b9e
+#define UUID_CEiffelCompiler                        ca54edd7-9fde-4260-9055-9ab7e92122cd
+#define UUID_CEiffelCompletionInfo                  bca3e550-3e39-4a44-a034-38f8b422d8df
+
+//
+// Interface GUID
+//
+
+#define UUID_IEiffelException                       fc94930c-06f1-448e-a221-9c702b9df685
+#define UUID_IEiffelProject                         3afea5ea-1aed-489b-9e8a-e35342582b2b
+#define UUID_IEiffelCompletionInfo                  8630e639-edae-46b7-880b-27d68e7184fa
+#define UUID_IEiffelCompletionEntry                 1da1355d-24b4-49a9-9cbc-6aff6aeb6de3
+#define UUID_IEiffelCompiler                        51b87f1b-a2e4-4f29-8891-af2654b50b6b
+#define UUID_IEiffelCompilerEvents                  75b32e73-a00e-4bcf-9a7a-13d41e6359b4
+#define UUID_IEiffelSystemBrowser                   afb76625-6f73-442c-9cae-1b981414050d
+#define UUID_IEiffelClusterDescriptor               09cc4e23-ceac-44b5-b525-61ed0cef0075
+#define UUID_IEiffelClassDescriptor                 db9131a8-26ca-44b5-9ee9-3a92bbbaebb0
+#define UUID_IEiffelFeatureDescriptor               75550181-6a69-43d5-aa40-51eec3e2d19f
+#define UUID_IEiffelProjectProperties               635bcc83-fd96-40ce-933e-5351e1b3e294
+#define UUID_IEiffelClusterProperties               64f0d395-2e13-4fdf-b82a-09bfcb46beea
+#define UUID_IEiffelSystemExternals                 ea511e88-0ff6-407b-8956-fb416626b62a
+#define UUID_IEiffelSystemAssemblies                9cf82ea0-b5f9-4110-a758-241a3bc95d52
+#define UUID_IEiffelAssemblyProperties              d58175ff-7ec7-43a4-aef4-d963ec2f9641
+#define UUID_IEiffelSystemClusters                  cd49d55e-5467-4058-a999-1d35b905764e
+#define UUID_IEnumEiffelClass                       61e3d67a-4c96-49d3-8236-85765e67c315
+#define UUID_IEnumCluster                           7a252ad5-c033-4f33-98a2-551d8451b8c4
+#define UUID_IEnumFeature                           dc003e49-6c17-4be2-bd81-3d3540b738b1
+#define UUID_IEnumClusterProp                       c5b13566-8a44-40e1-a090-3e5cc36ca72d
+#define UUID_IEnumClusterExcludes                   3ec7ebb4-bb85-42fe-adc1-b301a8a0b79c
+#define UUID_IEnumIncludePaths                      95f826bf-331d-4f84-8053-e1b4ded312d8
+#define UUID_IEnumObjectFiles                       99ec369f-3cc2-4b78-a9a3-cba25388f182
+#define UUID_IEnumAssembly                          b8a7d9f4-b23b-45a9-b3b1-2e0e004ffb8d
+#define UUID_IEnumCompletionEntry                   7081edbd-9335-48d5-b75f-401f525afb76
+#define UUID_IEnumParameter                         e3ab1afb-730c-498f-a168-418ee482a2d7
+#define UUID_IEiffelParameterDescriptor             7857e992-d5eb-434b-9d21-ec3e4ff0058f
+#define UUID_IEiffelHTMLDocGenerator                86270519-790f-48cb-8869-db06184f97b4
+#define UUID_IEiffelHTMLDocEvents                   b120763e-ed26-4ded-aafb-21fa8b28e879
+
+//
+// Enum GUID
+//
+
+#define UUID_EnumEIF_ENTITY_IMAGES                  032cbd06-0f87-4022-8312-d39ba4f13400
+#define UUID_EnumEIF_FEATURE_TYPES                  5c676a60-6829-444a-846b-da5b9787a1fd
+#define UUID_EnumEIF_EXCEPTIONS                     07f5d6be-fbd7-45b9-bfdb-2ce98417d347
+#define UUID_EnumEIF_PROJECT_TYPES                  168468fb-dff4-46b6-aeec-de988135025b
+#define UUID_EnumEIF_ASSERTIONS                     af5ce2a1-488d-410d-a920-db5a4921c1c1
+#define UUID_EnumEIF_CLUSTER_NAMESPACE_GENERATION   1cb05808-5b25-4dbf-b02e-0a50107a99f9
 
 #endif

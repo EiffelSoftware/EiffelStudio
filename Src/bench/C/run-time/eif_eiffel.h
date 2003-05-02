b431bd9d18ed1cc632427ca7a659d00a7f20a795
Emmanuel Stapf <manus@eiffel.com>
2003-05-02 02:35:29 +0000

    Moved `EIF_IS_WINDOWS' and `EIF_IS_VMS' from `eif_commands.h' to `eif_eiffel.h'   so that we can have the queries `is_windows', `is_vms' and `is_unix' in   PLATFORM class from EiffelBase kernel.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@37073 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_eiffel.h
+++ Src/bench/C/run-time/eif_eiffel.h
@@ -39,6 +39,21 @@
 #include "eif_curextern.h"
 #endif
 
+/* Platform definition */
+/* Windows definition */
+#ifdef EIF_WINDOWS
+#define EIF_IS_WINDOWS EIF_TRUE
+#else
+#define EIF_IS_WINDOWS EIF_FALSE
+#endif
+
+/* VMS definition */
+#ifdef EIF_VMS
+#define EIF_IS_VMS EIF_TRUE
+#else
+#define EIF_IS_VMS EIF_FALSE
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif

da1a6a4cc031c7262f940a779d9d3b83d0e33f65
Emmanuel Stapf <manus@eiffel.com>
2003-05-08 21:06:52 +0000

    Added routines that are only used by the runtime in `rt_except.h'.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@37187 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/bench/C/run-time/include/rt_except.h
@@ -0,0 +1,38 @@
+/*
+
+ #####    #####          ######  #    #   ####   ######  #####    #####         #    #
+ #    #     #            #        #  #   #    #  #       #    #     #           #    #
+ #    #     #            #####     ##    #       #####   #    #     #           ###### 
+ #####      #            #         ##    #       #       #####      #     ###   #    #
+ #   #      #            #        #  #   #    #  #       #          #     ###   #    #
+ #    #     #   #######  ######  #    #   ####   ######  #          #     ###   #    #
+
+ Exception handling C code.
+*/
+
+#ifndef _rt_except_h_
+#define _rt_except_h_
+
+#include <eif_except.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef EIF_WIN32
+extern void set_windows_exception_filter();
+#endif
+
+extern void xraise(EIF_CONTEXT int code);			/* Raise an exception with no tag */
+extern struct ex_vect *exget(register2 struct xstack *stk);	/* Get a new vector on stack */
+extern struct ex_vect *extop(register1 struct xstack *stk);	/* Top of Eiffel stack */
+extern struct ex_vect *exnext(EIF_CONTEXT_NOARG);	/* Read next eif_trace item from bottom */
+extern void ereturn(EIF_CONTEXT_NOARG);			/* Return to lastly recorded rescue entry */
+extern void excatch(jmp_buf *jmp);			/* Set exception catcher from C to interpret */
+extern void exhdlr(EIF_CONTEXT Signal_t (*handler)(int), int sig);			/* Call signal handler */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

f88cc133af270fc760ebea4f120cef948ab56303
Emmanuel Stapf <manus@eiffel.com>
2003-06-05 22:09:54 +0000

    Added Makefile instead of batch file to build scanners and parsers.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@37556 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/common/parser/parser/make_parser.bat
+++
@@ -1,6 +0,0 @@
-%GOBO%\bin\geyacc --version
-%GOBO%\bin\geyacc -x -t EIFFEL_TOKENS -o eiffel_parser.e eiffel.y
-rem %GOBO%\bin\geyacc -x -v eiffel.out -t EIFFEL_TOKENS -o eiffel_parser.e eiffel.y
-%GOBO%\bin\geyacc -x -t EXTERNAL_TOKENS -o external_parser.e external.y
-rem %GOBO%\bin\geyacc -x -v eiffel.out -t EXTERNAL_TOKENS -o external_parser.e external.y
-%GOBO%\bin\geyacc -x -t DUMMY -o cluster_indexing_parser.e cluster_indexing.y
--- Src/common/parser/parser/make_scanner.bat
+++
@@ -1,3 +0,0 @@
-%GOBO%\bin\gelex --version
-%GOBO%\bin\gelex -z eiffel.l
-%GOBO%\bin\gelex -z external.l

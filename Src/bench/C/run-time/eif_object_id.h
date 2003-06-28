e58658eb0f64ebf04f7e8d5ed9c970192c7e4181
Emmanuel Stapf <manus@eiffel.com>
2003-06-28 07:35:08 +0000

    Moved shared object_id data to a private header file `rt_object_id.h'.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@37868 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_object_id.h
+++ Src/bench/C/run-time/eif_object_id.h
@@ -10,9 +10,6 @@
 extern "C" {
 #endif
  
-extern struct stack object_id_stack;	/* Stack where objects referenced through `object_id' are stored
-										 * See class IDENTIFIED */	
-
 RT_LNK EIF_INTEGER eif_object_id(EIF_OBJECT object);			/* Get a new id for the object, assuming it is NOT in the stack*/
 extern EIF_INTEGER eif_general_object_id(EIF_OBJECT object); /* Get an id for the object, sequential search first */
 RT_LNK EIF_REFERENCE eif_id_object(EIF_INTEGER id);		/* returns the object associated with the id */
@@ -26,7 +23,6 @@ RT_LNK void eif_extend_object_id_stack (EIF_INTEGER nb_chunks);	/* extends of `n
 extern void print_object_id_stack (void);
 
 #ifdef CONCURRENT_EIFFEL
-extern struct stack separate_object_id_set; /* keeps track of objects referenced in other processors */
 extern EIF_INTEGER eif_separate_object_id(EIF_OBJECT object);	/* Get an id for the object, sequential search first */
 extern EIF_REFERENCE eif_separate_id_object(EIF_INTEGER id);	/* returns the object associated with the id */
 extern void eif_separate_object_id_free(EIF_INTEGER id);		/* removes the object from the stack */

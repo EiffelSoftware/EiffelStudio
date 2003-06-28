7f12addd5d62bee5cae9534ccc07f12b08bf02ba
Emmanuel Stapf <manus@eiffel.com>
2003-06-28 08:43:16 +0000

    Removed declaration of `ename' since it is now replaced by `egc_system_name'.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@37892 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/include/rt_main.h
+++ Src/bench/C/run-time/include/rt_main.h
@@ -22,7 +22,6 @@ extern long EIF_bonce_count;		/* Nr. of once routines in bytecode */
 extern void once_init (void);		/* Initialization and creation of once keys */
 extern char dinterrupt(void);
 extern void dserver(void);
-extern char *ename;				/* Name of the Eiffel program running */
 extern int eif_no_reclaim;		/* Call reclaim ion termination? */
 extern int cc_for_speed;		/* Optimized for speed or for memory */
 extern char *starting_working_directory;

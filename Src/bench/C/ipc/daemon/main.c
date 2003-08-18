58a1d7ed6ebaca75dc0f02dcfa239f3c1957682c
Emmanuel Stapf <manus@eiffel.com>
2003-08-18 17:15:35 +0000

    Added definition of `ise_printf' when EIF_ASSERTIONS is defined to avoid missing definition for it when in `rt_assert.h' we use `ise_printf' to report errors.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@38559 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/ipc/daemon/main.c
+++ Src/bench/C/ipc/daemon/main.c
@@ -383,6 +383,20 @@ rt_public char *strdup (char *s)
 }
 #endif	/* HAS_STRDUP */
 
+#ifdef EIF_ASSERTIONS
+rt_shared int ise_printf (char *StrFmt, ...)
+	/* To put a breakpoint when an assertion violation occurs. */
+{
+	va_list ap;
+	int r;
+
+	va_start (ap, StrFmt);
+	r = vprintf (StrFmt, ap);
+	va_end (ap);
+
+	return r;
+}
+#endif
 
 #ifdef EIF_WIN32
 

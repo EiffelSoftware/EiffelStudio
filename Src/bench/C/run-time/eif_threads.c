380ce63f3a678346c1fb86c1a487c7046169c663
Emmanuel Stapf <manus@eiffel.com>
2003-08-19 23:44:49 +0000

    Forgot to protect call to `create_scavenge_zone' so that it compiles when not using ISE_GC.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@38600 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_threads.c
+++ Src/bench/C/run-time/eif_threads.c
@@ -166,7 +166,9 @@ rt_public void eif_thr_init_root(void)
 	EIF_LW_MUTEX_CREATE(eif_trace_mutex, "Couldn't create tracemutex");
 	EIF_MUTEX_CREATE(eif_global_once_mutex, "Couldn't create global once mutex");
 	eif_thr_register();
+#ifdef ISE_GC
 	create_scavenge_zones();
+#endif
 #ifdef EIF_WIN32
 	{
 		HMODULE kernel_module = LoadLibrary("kernel32.dll");

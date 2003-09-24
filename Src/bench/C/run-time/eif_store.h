aa4b4773c68e6ee4af5e996cd8fda596e17ebd88
Emmanuel Stapf <manus@eiffel.com>
2003-09-24 19:26:18 +0000

    Moved back #define to public header files.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@39273 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_store.h
+++ Src/bench/C/run-time/eif_store.h
@@ -28,6 +28,22 @@
 extern "C" {
 #endif
 
+/* Different kinds of storage */
+#define BASIC_STORE_3_1			0x00
+#define BASIC_STORE_3_2			0x02
+#define BASIC_STORE_4_0			0x06
+#define GENERAL_STORE_3_1		0x01
+#define GENERAL_STORE_3_2		0x03
+#define GENERAL_STORE_3_3		0x05
+#define GENERAL_STORE_4_0		0x07
+#define INDEPENDENT_STORE_3_2	0x04
+#define INDEPENDENT_STORE_4_0	0x08
+#define INDEPENDENT_STORE_4_3	0x09
+#define INDEPENDENT_STORE_4_4	0x0A
+#define INDEPENDENT_STORE_5_0	0x0B
+/* TODO: Should this be called INDEPENDENT_STORE_5_3? */
+#define RECOVERABLE_STORE_5_3	0x0C
+
 /* Setting of `eif_is_new_independent_format' */
 RT_LNK void eif_set_new_independent_format(EIF_BOOLEAN v);
 

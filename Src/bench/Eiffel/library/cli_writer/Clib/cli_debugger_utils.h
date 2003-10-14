b8b30dcde306dd35e911dd593e3e0cd9fa1db86b
Jocelyn Fiat <jfiat@eiffel.com>
2003-10-14 18:07:11 +0000

    cleaning
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@39634 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/Eiffel/library/cli_writer/Clib/cli_debugger_utils.h
+++ Src/bench/Eiffel/library/cli_writer/Clib/cli_debugger_utils.h
@@ -16,22 +16,8 @@ extern "C" {
 }
 #endif
 
-#define COM_METHOD HRESULT STDMETHODCALLTYPE
 
 #ifdef __cplusplus
-
-//extern EIF_INTEGER QueryInterface_ICorDebugILFrame (ICorDebugFrame* , ICorDebugILFrame** ) ;
-//extern EIF_INTEGER QueryInterface_ICorDebugNativeFrame (ICorDebugFrame* , ICorDebugNativeFrame** ) ;
-//
-//extern EIF_INTEGER QueryInterface_ICorDebugGenericValue (ICorDebugValue* , ICorDebugGenericValue** ) ;
-//extern EIF_INTEGER QueryInterface_ICorDebugReferenceValue (ICorDebugValue* , ICorDebugReferenceValue** ) ;
-//extern EIF_INTEGER QueryInterface_ICorDebugHeapValue (ICorDebugValue* , ICorDebugHeapValue** ) ;
-//extern EIF_INTEGER QueryInterface_ICorDebugObjectValue (ICorDebugValue* , ICorDebugObjectValue** ) ;
-//
-//
-//extern EIF_INTEGER QueryInterface_ICorDebugBoxValue (ICorDebugObjectValue* , ICorDebugBoxValue** ) ;
-//extern EIF_INTEGER QueryInterface_ICorDebugStringValue (ICorDebugObjectValue* , ICorDebugStringValue** ) ;
-//extern EIF_INTEGER QueryInterface_ICorDebugArrayValue (ICorDebugObjectValue* , ICorDebugArrayValue** ) ;
 
 #endif /* __cplusplus */
 

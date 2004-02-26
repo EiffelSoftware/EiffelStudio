c4d60cbcb8d1b3da6eb7613d61a5786936d7fec9
raphaels <raphaels@eiffel.com>
2004-02-26 17:48:03 +0000

    Updated to new project system
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@41557 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/dotnet/codedom_provider/configuration/extract_configuration.bat
+++ Src/dotnet/codedom_provider/configuration/extract_configuration.bat
@@ -1,16 +1,10 @@
 @REM Use this script to extract the configuration files from the current build
 @REM and replace the content of the configuration folder with them.
 
-copy /Y ..\build\ISE.Base\obj\Debug\ace.ace base_debug.ace
-copy /Y ..\build\ISE.Base\obj\Release\ace.ace base_release.ace
 copy /Y ..\build\ISE.Base\ISE.Base.eifp base.eifp
 
-copy /Y ..\build\ISE.CacheBrowser\obj\Debug\ace.ace cache_browser_debug.ace
-copy /Y ..\build\ISE.CacheBrowser\obj\Release\ace.ace cache_browser_release.ace
 copy /Y ..\build\ISE.CacheBrowser\ISE.CacheBrowser.eifp cache_browser.eifp
 
-copy /Y ..\build\ISE.EiffelCodeDomProvider\obj\Debug\ace.ace codedom_provider_debug.ace
-copy /Y ..\build\ISE.EiffelCodeDomProvider\obj\Release\ace.ace codedom_provider_release.ace
 copy /Y ..\build\ISE.EiffelCodeDomProvider\ISE.EiffelCodeDomProvider.eifp codedom_provider.eifp
 
 copy /Y ..\build\codedom_provider.sln codedom_provider.sln

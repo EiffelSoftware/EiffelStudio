c2924c212ded286b4c96599c53b81164ca2fe59e
Emmanuel Stapf <manus@eiffel.com>
2004-03-06 06:31:37 +0000

    Changed constants value of the xx_TYPE ones and removed the ones that are   not used anymore. Updated the EIF_xxx_CODE constant for TUPLE type identification so that they   can fit on 4 bits only.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@41883 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/include/rt_gen_types.h
+++ Src/bench/C/run-time/include/rt_gen_types.h
@@ -13,51 +13,43 @@ extern "C" {
 /* new codes!!                                                      */
 /*------------------------------------------------------------------*/
 
-#define TERMINATOR			-1
-#define CHARACTER_TYPE		-2
-#define BOOLEAN_TYPE		-3
-#define INTEGER_TYPE		-4
-#define REAL_TYPE			-5
-#define DOUBLE_TYPE			-6
-#define BIT_TYPE			-7
-#define POINTER_TYPE		-8
-#define NONE_TYPE			-9
-#define INTERNAL_TYPE		-10
-#define LIKE_ARG_TYPE		-11
-#define LIKE_CURRENT_TYPE	-12
-#define LIKE_PFEATURE_TYPE	-13
-#define LIKE_FEATURE_TYPE	-14
-#define TUPLE_TYPE			-15
-#define INTEGER_8_TYPE		-16
-#define INTEGER_16_TYPE		-17
-#define INTEGER_64_TYPE		-18
-#define WIDE_CHAR_TYPE		-19
-/* Add new types here
-   ...
-   until here.
-*/
-#define FORMAL_TYPE			-32
-#define EXPANDED_LEVEL		-256
+#define TERMINATOR			(int16) 0xFFFF
+#define NONE_TYPE			(int16) 0xFFFE
+#define LIKE_ARG_TYPE		(int16) 0xFFFD
+#define LIKE_CURRENT_TYPE	(int16) 0xFFFC
+#define LIKE_PFEATURE_TYPE	(int16) 0xFFFB
+#define LIKE_FEATURE_TYPE	(int16) 0xFFFA
+#define TUPLE_TYPE			(int16) 0xFFF9
+#define FORMAL_TYPE			(int16) 0xFFF8
+#define MAX_DFTYPE			(uint16) 0xFFF7
+
+/* Offset that needs to be skipped when finding TUPLE_TYPE. It corresponds
+ * to TUPLE_TYPE and nb generic parameters in current tuple type definition. */
+#define TUPLE_OFFSET	2
 
 /*------------------------------------------------------------------*/
-/* One character codes for the basic types and one ('r') for all the*/
+/* One character codes for the basic types and one for all the      */
 /* others. Make sure to assign different letters to new basic types.*/
 /* You must update 'rout_obj.c' after adding new codes!!!           */
 /* You must also update ROUTINE class after adding new codes!!      */
 /*------------------------------------------------------------------*/
 
-#define EIF_BOOLEAN_CODE	'b'
-#define EIF_CHARACTER_CODE	'c'
-#define EIF_DOUBLE_CODE		'd'
-#define EIF_REAL_CODE		'f'
-#define EIF_INTEGER_CODE	'i'
-#define EIF_INTEGER_32_CODE	'i'
-#define EIF_POINTER_CODE	'p'
-#define EIF_REFERENCE_CODE	'r'
-#define EIF_INTEGER_8_CODE	'j'
-#define EIF_INTEGER_16_CODE	'k'
-#define EIF_INTEGER_64_CODE	'l'
-#define EIF_WIDE_CHAR_CODE	'u'
+#define EIF_TUPLE_CODE_MASK 0x0F
+#define EIF_REFERENCE_CODE	0x00
+#define EIF_BOOLEAN_CODE	0x01
+#define EIF_CHARACTER_CODE	0x02
+#define EIF_DOUBLE_CODE		0x03
+#define EIF_REAL_CODE		0x04
+#define EIF_POINTER_CODE	0x05
+#define EIF_INTEGER_8_CODE	0x06
+#define EIF_INTEGER_16_CODE	0x07
+#define EIF_INTEGER_32_CODE	0x08
+#define EIF_INTEGER_64_CODE	0x09
+#define EIF_NATURAL_8_CODE	0x0A
+#define EIF_NATURAL_16_CODE	0x0B
+#define EIF_NATURAL_32_CODE 0x0C
+#define EIF_NATURAL_64_CODE 0x0D
+#define EIF_WIDE_CHAR_CODE	0x0E
 
 /*------------------------------------------------------------------*/
 

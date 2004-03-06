ec7ee0433d810c900db404d4da561fef23e5e338
Emmanuel Stapf <manus@eiffel.com>
2004-03-06 06:25:02 +0000

    Use `rt_struct.h' to get acces to `References' and `Dispose' whose definition has been moved   to `rt_struct.h'.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@41877 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/garcol.c
+++ Src/bench/C/run-time/garcol.c
@@ -39,6 +39,7 @@ doc:<file name="garcol.c" header="eif_garcol.h" version="$Id$" summary="Garbage 
 #include "rt_gen_conf.h"	/* For eif_gen_conf_cleanup () */
 #include "rt_gen_types.h"	/* For tuple marking */
 #include "eif_cecil.h"
+#include "rt_struct.h"
 #include "x2c.h"		/* For macro LNGPAD */
 #ifdef VXWORKS
 #include <string.h>

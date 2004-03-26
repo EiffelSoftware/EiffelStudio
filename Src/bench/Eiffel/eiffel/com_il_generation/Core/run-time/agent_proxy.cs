56088603a86e8107b3faf83a2b6ab466ba7d6d54
Emmanuel Stapf <manus@eiffel.com>
2004-03-26 03:08:10 +0000

    Added a new interface AGENT_PROXY which enables to have more efficient agent   calls. Not this is integrated although the Eiffel code that needs to use   this is not integrated yet. It will be done when we can easily switch from   the old implementation to the new one. Switch is needed as the new one   takes much more memory although it is much faster.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@42400 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/bench/Eiffel/eiffel/com_il_generation/Core/run-time/agent_proxy.cs
@@ -0,0 +1,52 @@
+/*
+indexing
+	description: "Ancestor class for calling agents."
+	date: "$Date$"
+	revision: "$Revision$"
+*/
+	
+using System;
+
+namespace ISE.Runtime {
+
+public class AGENT_PROXY {
+/*
+feature -- Basic operations
+*/
+	public virtual void call (object agent, object[] open_operands)
+			// Call associated routine `agent' with open arguments `open_operands'.
+	{
+		#if ASSERTIONS
+			ASSERTIONS.CHECK ("open_operands_not_void", open_operands != null);
+		#endif
+	}
+
+/*
+feature -- Access
+*/
+
+	public object last_result;
+			// Last result of a function call
+
+	public object item (object agent, object[] open_operands)
+			// Call associated routine `agent' with open arguments `open_operands'.
+	{
+		#if ASSERTIONS
+			ASSERTIONS.CHECK ("open_operands_not_void", open_operands != null);
+		#endif
+		call (agent, open_operands);
+		return last_result;
+	}
+
+/*
+feature -- Settings
+*/
+
+	public virtual void set_closed_arguments (object [] closed_arguments)
+			// Set current object with `closed_arguments'.
+	{
+	}
+}
+}
+
+

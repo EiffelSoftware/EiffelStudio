4c5cf1f946a931b68990f3b540a8710343d60f9c
Emmanuel Stapf <manus@eiffel.com>
2004-03-26 03:12:16 +0000

    Implementation of features of ANY as static routine that can be called on   either .NET objects or Eiffel ones. Added 3 new routines to EIFFEL_TYPE_INFO   in order for us to do the proper dispatch when we handle Eiffel objects. Moved some routines of RUN_TIME to new class ANY (I'll need to continue   the moving to clean this up)
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@42401 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/Eiffel/eiffel/com_il_generation/Core/run-time/eiffel_type_info.cs
+++ Src/bench/Eiffel/eiffel/com_il_generation/Core/run-time/eiffel_type_info.cs
@@ -17,28 +17,49 @@ feature -- Assertion checking
 */
 	[CLSCompliantAttribute (false)]
 	void _invariant ();
-		/* Checks invariant of current class, throw an exception
-		 * when invariant is not satisfied.
-		 */
+		// Checks invariant of current class, throw an exception
+		// when invariant is not satisfied.
+		//
 /*
 feature -- Access
 */
 	[CLSCompliantAttribute (false)]
 	EIFFEL_DERIVATION ____type();
-		/* Get generic type if any. */
+		// Get generic type if any.
 
 	[CLSCompliantAttribute (false)]
 	String ____class_name();
-		/* Name of current object's generating class
-		 * (base class of the type of which it is a direct instance)
-		 */
+		// Name of current object's generating class
+		// (base class of the type of which it is a direct instance)
+
+/*
+feature -- Comparison
+*/
+	[CLSCompliantAttribute (false)]
+	bool ____is_equal (Object other);
+		// Is `other' attached to an object considered
+		// equal to current object?
+
+/*
+feature -- Duplication
+*/
+	[CLSCompliantAttribute (false)]
+	void ____copy (Object other);
+		// Update current object using fields of object attached
+		// to `other', so as to yield equal objects.
+
+	[CLSCompliantAttribute (false)]
+	object ____standard_twin ();
+		// New object field-by-field identical to `other'.
+		// Always uses default copying semantics.
+
 /*
 feature -- Settings
 */
 
 	[CLSCompliantAttribute (false)]
 	void ____set_type(EIFFEL_DERIVATION type);
-		/* Set `____type' with `type'. */
+		// Set `____type' with `type'.
 }
 
 }

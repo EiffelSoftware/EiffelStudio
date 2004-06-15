a5a917986e2ac94b335fad4cb75bf7788c4b1190
Emmanuel Stapf <manus@eiffel.com>
2004-06-15 15:53:01 +0000

    Changed signatures of `st_write', `ist_write' and `gst_write' to take an   extra argument corresponding to the full flags of the object we are storing.   This is needed because some object may have no headers (e.g. expanded without   references stored in a special).
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@43806 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_store.h
+++ Src/bench/C/run-time/eif_store.h
@@ -73,14 +73,14 @@ RT_LNK void rt_init_store(
 	void (*store_function) (void),
 	int (*char_write_function)(char *, int),
 	void (*flush_buffer_function) (void),
-	void (*st_write_function) (EIF_REFERENCE),
+	void (*st_write_function) (EIF_REFERENCE, uint32),
 	void (*make_header_function) (void),
 	int accounting_type);
 RT_LNK void rt_reset_store(void);
 RT_LNK void flush_st_buffer(void);
-RT_LNK void st_write(EIF_REFERENCE object);		/* Write an object in file */
-RT_LNK void ist_write(EIF_REFERENCE object);
-RT_LNK void gst_write(EIF_REFERENCE object);
+RT_LNK void st_write(EIF_REFERENCE object, uint32);		/* Write an object in file */
+RT_LNK void ist_write(EIF_REFERENCE object, uint32);
+RT_LNK void gst_write(EIF_REFERENCE object, uint32);
 RT_LNK void store_write(void);
 RT_LNK void make_header(void);				/* Make header */
 RT_LNK void rmake_header(void);				/* Make header */

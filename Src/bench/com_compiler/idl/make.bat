a81a78d02a1fcb0ad3e29be0909c4240422a93d9
raphaels <raphaels@eiffel.com>
2004-07-01 19:29:52 +0000

    Renamed ISE. files into EiffelSoftware.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@44077 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/com_compiler/idl/make.bat
+++ Src/bench/com_compiler/idl/make.bat
@@ -2,19 +2,19 @@ ECHO OFF
 
 REM Builds and registers cec custom marshaller proxy stub
 
-IF EXIST ISE.CompilerPS.dll regsvr32 -s -u ISE.CompilerPS.dll
+IF EXIST EiffelSoftware.CompilerPS.dll regsvr32 -s -u EiffelSoftware.CompilerPS.dll
 
-IF EXIST ISE.CompilerPS.h DEL ISE.CompilerPS.h
-IF EXIST ISE.CompilerPS_data.c DEL ISE.CompilerPS_data.c
-IF EXIST ISE.CompilerPS_proxy.c DEL ISE.CompilerPS_proxy.c
-IF EXIST ISE.CompilerPS_iid.c DEL ISE.CompilerPS_iid.c
-IF EXIST ISE.CompilerPS.tlb DEL ISE.CompilerPS.tlb
+IF EXIST EiffelSoftware.CompilerPS.h DEL EiffelSoftware.CompilerPS.h
+IF EXIST EiffelSoftware.CompilerPS_data.c DEL EiffelSoftware.CompilerPS_data.c
+IF EXIST EiffelSoftware.CompilerPS_proxy.c DEL EiffelSoftware.CompilerPS_proxy.c
+IF EXIST EiffelSoftware.CompilerPS_iid.c DEL EiffelSoftware.CompilerPS_iid.c
+IF EXIST EiffelSoftware.CompilerPS.tlb DEL EiffelSoftware.CompilerPS.tlb
 IF EXIST *.obj DEL *.obj
 IF EXIST *.pch DEL *.pch
 IF EXIST *.lib DEL *.lib
 IF EXIST *.exp DEL *.exp
 
-CALL midl /h ISE.CompilerPS.h /dlldata ISE.CompilerPS_data.c /iid ISE.CompilerPS_iid.c /proxy ISE.CompilerPS_proxy.c ISE.Compiler.idl
+CALL midl /h EiffelSoftware.CompilerPS.h /dlldata EiffelSoftware.CompilerPS_data.c /iid EiffelSoftware.CompilerPS_iid.c /proxy EiffelSoftware.CompilerPS_proxy.c EiffelSoftware.Compiler.idl
 CALL nmake make_proxystub.msc
 
-regsvr32 -s ISE.CompilerPS.dll
+regsvr32 -s EiffelSoftware.CompilerPS.dll

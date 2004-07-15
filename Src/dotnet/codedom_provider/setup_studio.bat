1aa7b4864250c647a6334137cebdb236f57f5470
raphaels <raphaels@eiffel.com>
2004-07-15 18:12:25 +0000

    Added ability to setup debug or release configuration
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@44261 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/dotnet/codedom_provider/setup_studio.bat
+++ Src/dotnet/codedom_provider/setup_studio.bat
@@ -4,6 +4,24 @@
 
 IF EXIST build_studio RD /Q /S build_studio
 
+IF %1 == "release" GOTO RELEASE
+
+:DEBUG
+MKDIR build_studio_debug
+MKDIR build_studio_debug\EiffelSoftware.EiffelBase
+MKDIR build_studio_debug\EiffelSoftware.EiffelVision2
+MKDIR build_studio_debug\EiffelSoftware.CacheBrowser
+MKDIR build_studio_debug\EiffelSoftware.CodeDom
+MKDIR build_studio_debug\ecd_manager
+
+COPY configuration\base.debug.ace build_studio_debug\EiffelSoftware.EiffelBase\ace.ace
+COPY configuration\vision2.debug.ace build_studio_debug\EiffelSoftware.EiffelVision2\ace.ace
+COPY configuration\cache_browser.debug.ace build_studio_debug\EiffelSoftware.CacheBrowser\ace.ace
+COPY configuration\codedom_provider.debug.ace build_studio_debug\EiffelSoftware.CodeDom\ace.ace
+COPY configuration\manager.debug.ace build_studio_debug\ecd_manager\ace.ace
+GOTO END
+
+:RELEASE
 MKDIR build_studio
 MKDIR build_studio\EiffelSoftware.EiffelBase
 MKDIR build_studio\EiffelSoftware.EiffelVision2
@@ -17,4 +35,5 @@ COPY configuration\cache_browser.ace build_studio\EiffelSoftware.CacheBrowser\ac
 COPY configuration\codedom_provider.ace build_studio\EiffelSoftware.CodeDom\ace.ace
 COPY configuration\manager.ace build_studio\ecd_manager\ace.ace
 
+:END
 ECHO Done setting up EiffelStudio Eiffel CodeDom Provider projects.

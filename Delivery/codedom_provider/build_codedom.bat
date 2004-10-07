2376c31836dd6cca3fb5f4174d70ac6515136903
raphaels <raphaels@eiffel.com>
2004-10-07 16:58:14 +0000

    Now cd to root directory when done.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@45946 8089f293-4706-0410-a29e-feb5c42a2edf

--- Delivery/codedom_provider/build_codedom.bat
+++ Delivery/codedom_provider/build_codedom.bat
@@ -6,6 +6,7 @@ REM *********************************************
 ECHO Building Codedom files
 
 CD checkout\dotnet\codedom_provider
+REM in checkout\dotnet\codedom_provider
 CALL build_studio.bat /release
 IF NOT EXIST build_studio GOTO END
 IF NOT EXIST build_studio\EiffelSoftware.Codedom\EIFGEN\F_code\EiffelSoftware.Codedom.dll GOTO END
@@ -15,16 +16,20 @@ IF NOT EXIST build_studio\EiffelSoftware.EiffelVision2\EIFGEN\F_code\EiffelSoftw
 
 ECHO Copying files
 CD ..\..\..\delivery
+REM in delivery
 MKDIR codedom
 CD codedom
+REM in delivery\codedom
 
 MKDIR manager
 CD manager
+REM in delivery\codedom\manager
 COPY ..\..\..\checkout\dotnet\codedom_provider\build_studio\ecdpman\EIFGEN\F_code\ecdpman.exe .
 MKDIR icons
 XCOPY ..\..\..\checkout\dotnet\codedom_provider\manager\icons icons\ /E
 
 CD ..
+REM in delivery\codedom
 MKDIR configs
 COPY ..\..\default.ecd configs\
 
@@ -37,6 +42,8 @@ COPY ..\..\checkout\dotnet\codedom_provider\build_studio\EiffelSoftware.EiffelVi
 COPY ..\..\checkout\dotnet\codedom_provider\build_studio\EiffelSoftware.EiffelVision2\EIFGEN\F_code\libEiffelSoftware.EiffelVision2.dll bin\
 
 SET CODEDOM_BUILT=1
+
+CD ..\..
 
 :END
 IF "%CODEDOM_BUILT%"=="" ECHO COULD NOT BUILD CODEDOM FILES!!

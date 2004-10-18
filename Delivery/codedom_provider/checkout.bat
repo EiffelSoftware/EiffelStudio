3342f1676acc427c8042618f4d333ff46e5c974b
raphaels <raphaels@eiffel.com>
2004-10-18 23:06:32 +0000

    Added comments to help follow working directory.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@46084 8089f293-4706-0410-a29e-feb5c42a2edf

--- Delivery/codedom_provider/checkout.bat
+++ Delivery/codedom_provider/checkout.bat
@@ -5,6 +5,7 @@ REM *********************************************
 
 ECHO Checking out source files...
 CD checkout
+REM in checkout
 cvs -z3 export -r%COMPILER_RELEASE% Eiffel
 cvs -z3 export -r%COMPILER_RELEASE% runtime
 cvs -z3 export -rHEAD -d eclop free_add_ons/eclop
@@ -13,18 +14,23 @@ cvs -z3 export -r%COMPILER_RELEASE% -d compiler_delivery Delivery/studio
 
 MKDIR tools
 CD tools
+REM in checkout\tools
 cvs -z3 export -rHEAD -d silent_launcher Src/tools/silent_launcher
 cvs -z3 export -rHEAD -d installer_launcher Src/tools/installer_launcher
 CD ..
+REM in checkout
 
 MKDIR C_library
 CD C_library
+REM in checkout\C_library
 cvs -z3 export -r%COMPILER_RELEASE% -d libpng Src/C_library/libpng
 cvs -z3 export -r%COMPILER_RELEASE% -d zlib Src/C_library/zlib
 CD ..
+REM in checkout
 
 cvs -z3 export -r%COMPILER_RELEASE% -d library free_add_ons/gobo
 CD library
+REM in checkout\library
 tar -xzf gobo_33_win.tgz
 cvs -z3 export -r%COMPILER_RELEASE% -d base Src/library/base
 cvs -z3 export -r%COMPILER_RELEASE% -d wel Src/library/wel
@@ -36,16 +42,20 @@ cvs -z3 export -r%COMPILER_RELEASE% -d keygen Src/library/keygen
 cvs -z3 export -r%COMPILER_RELEASE% -d preferences Src/library/preferences
 cvs -z3 export -r%COMPILER_RELEASE% -d thread Src/library/thread
 CD ..
+REM in checkout
 
 MKDIR dotnet
 CD dotnet
+REM in checkout\dotnet
 cvs -z3 export -r%COMPILER_RELEASE% -d consumer Src/dotnet/consumer
 cvs -z3 export -r%COMPILER_RELEASE% -d helpers Src/dotnet/helpers
 cvs -z3 export -r%COMPILER_RELEASE% -d eac_browser Src/dotnet/eac_browser
 cvs -z3 export -r%CODEDOM_RELEASE% -d codedom_provider Src/dotnet/codedom_provider
 MKDIR VisualStudio\tools
 CD VisualStudio\tools\
+REM in checkout\dotnet\VisualStudio\tools
 cvs -z3 export -r%COMPILER_RELEASE% -d documentation_manager Src/dotnet/VisualStudio/tools/documentation_manager/
 CD ..\..\..
+REM in checkout
 
 CD ..

62dd9580177efbb95ae7c3ed19308135412328be
Jocelyn Fiat <jfiat@eiffel.com>
2004-11-10 11:08:08 +0000

    redesigned synchro between ec's debugger for dotnet and the COM dotnet debugger
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@46412 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/Eiffel/library/cli_writer/Clib/cli_debugger.h
+++ Src/bench/Eiffel/library/cli_writer/Clib/cli_debugger.h
@@ -14,17 +14,19 @@ extern "C" {
 extern EIF_POINTER new_cordebug ();
 */
 extern EIF_INTEGER get_cordebug (LPWSTR a_dbg_version, EIF_POINTER ** );
+extern EIF_INTEGER dbg_continue (void*, BOOL);
 
 extern EIF_INTEGER dbg_timer_id ();
 extern EIF_BOOLEAN dbg_is_synchronizing();
 extern void dbg_init_synchro();
+extern void dbg_notify_from_estudio(char*);
 extern void dbg_terminate_synchro ();
 
 extern void dbg_enable_estudio_callback (EIF_OBJECT estudio_cb_obj, EIF_POINTER estudio_cb_event);
 
 extern void dbg_start_timer();
 extern void dbg_stop_timer();
-//extern void dbg_timer_callback();
+extern void dbg_restore_cb_notification_state ();
 extern void CALLBACK dbg_timer_callback (HWND hwnd, UINT uMsg, UINT idEvent, DWORD dwTime);
 extern void dbg_lock_and_wait_callback (void*);
 extern void dbg_debugger_before_callback(Callback_ids);

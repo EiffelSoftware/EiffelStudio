4afcad0991936ca9f93f8c62556e15d3509b7164
raphaels <raphaels@eiffel.com>
2004-11-18 00:25:29 +0000

    Fixed path to CacheBrowser directory and assembly
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@46539 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/dotnet/codedom_provider/configuration/extract_aces.bat
+++ Src/dotnet/codedom_provider/configuration/extract_aces.bat
@@ -3,19 +3,19 @@
 
 @ECHO OFF
 
-IF EXIST ..\build_studio\EiffelSoftware.CodeDomBase\ace.ace COPY /Y ..\build_studio\EiffelSoftware.CodeDomBase\ace.ace base.ace
-IF EXIST ..\build_studio\EiffelSoftware.CodeDomBase\ace.ace GOTO VISION2
-ECHO Could not find ..\build_studio\EiffelSoftware.CodeDomBase\ace.ace !!
+IF EXIST ..\build_studio\EiffelSoftware.CodeDom.Base\ace.ace COPY /Y ..\build_studio\EiffelSoftware.CodeDom.Base\ace.ace base.ace
+IF EXIST ..\build_studio\EiffelSoftware.CodeDom.Base\ace.ace GOTO VISION2
+ECHO Could not find ..\build_studio\EiffelSoftware.CodeDom.Base\ace.ace !!
 
 :VISION2
-IF EXIST ..\build_studio\EiffelSoftware.CodeDomVision2\ace.ace COPY /Y ..\build_studio\EiffelSoftware.CodeDomVision2\ace.ace vision2.ace
-IF EXIST ..\build_studio\EiffelSoftware.CodeDomVision2\ace.ace GOTO CACHE
-ECHO Could not find ..\build_studio\EiffelSoftware.CodeDomVision2\ace.ace !!
+IF EXIST ..\build_studio\EiffelSoftware.CodeDom.Vision2\ace.ace COPY /Y ..\build_studio\EiffelSoftware.CodeDom.Vision2\ace.ace vision2.ace
+IF EXIST ..\build_studio\EiffelSoftware.CodeDom.Vision2\ace.ace GOTO CACHE
+ECHO Could not find ..\build_studio\EiffelSoftware.CodeDom.Vision2\ace.ace !!
 
 :CACHE
-IF EXIST ..\build_studio\EiffelSoftware.CacheBrowser\ace.ace COPY /Y ..\build_studio\EiffelSoftware.CacheBrowser\ace.ace cache_browser.ace
-IF EXIST ..\build_studio\EiffelSoftware.CacheBrowser\ace.ace GOTO PROVIDER
-ECHO Could not find ..\build_studio\EiffelSoftware.CacheBrowser\ace.ace !!
+IF EXIST ..\build_studio\EiffelSoftware.CodeDom.CacheBrowser\ace.ace COPY /Y ..\build_studio\EiffelSoftware.CodeDom.CacheBrowser\ace.ace cache_browser.ace
+IF EXIST ..\build_studio\EiffelSoftware.CodeDom.CacheBrowser\ace.ace GOTO PROVIDER
+ECHO Could not find ..\build_studio\EiffelSoftware.CodeDom.CacheBrowser\ace.ace !!
 
 :PROVIDER
 IF EXIST ..\build_studio\EiffelSoftware.CodeDom\ace.ace COPY /Y ..\build_studio\EiffelSoftware.CodeDom\ace.ace codedom_provider.ace
@@ -23,24 +23,24 @@ IF EXIST ..\build_studio\EiffelSoftware.CodeDom\ace.ace GOTO END
 ECHO Could not find ..\build_studio\EiffelSoftware.CodeDom\ace.ace !!
 
 :MANAGER
-IF EXIST ..\build_studio\ecd_manager\ace.ace COPY /Y ..\build_studio\ecd_manager\ace.ace manager.ace
-IF EXIST ..\build_studio\ecd_manager\ace.ace GOTO BASEDEBUG
-ECHO Could not find ..\build_studio\ecd_manager\ace.ace !!
+IF EXIST ..\build_studio\ecdpman\ace.ace COPY /Y ..\build_studio\ecdpman\ace.ace manager.ace
+IF EXIST ..\build_studio\ecdpman\ace.ace GOTO BASEDEBUG
+ECHO Could not find ..\build_studio\ecdpman\ace.ace !!
 
 :BASEDEBUG
-IF EXIST ..\build_studio_debug\EiffelSoftware.CodeDomBase\ace.ace COPY /Y ..\build_studio_debug\EiffelSoftware.CodeDomBase\ace.ace base.debug.ace
-IF EXIST ..\build_studio_debug\EiffelSoftware.CodeDomBase\ace.ace GOTO VISION2DEBUG
-ECHO Could not find ..\build_studio_debug\EiffelSoftware.CodeDomBase\ace.ace !!
+IF EXIST ..\build_studio_debug\EiffelSoftware.CodeDom.Base\ace.ace COPY /Y ..\build_studio_debug\EiffelSoftware.CodeDom.Base\ace.ace base.debug.ace
+IF EXIST ..\build_studio_debug\EiffelSoftware.CodeDom.Base\ace.ace GOTO VISION2DEBUG
+ECHO Could not find ..\build_studio_debug\EiffelSoftware.CodeDom.Base\ace.ace !!
 
 :VISION2DEBUG
-IF EXIST ..\build_studio_debug\EiffelSoftware.CodeDomVision2\ace.ace COPY /Y ..\build_studio_debug\EiffelSoftware.CodeDomVision2\ace.ace vision2.debug.ace
-IF EXIST ..\build_studio_debug\EiffelSoftware.CodeDomVision2\ace.ace GOTO CACHEDEBUG
-ECHO Could not find ..\build_studio_debug\EiffelSoftware.CodeDomVision2\ace.ace !!
+IF EXIST ..\build_studio_debug\EiffelSoftware.CodeDom.Vision2\ace.ace COPY /Y ..\build_studio_debug\EiffelSoftware.CodeDom.Vision2\ace.ace vision2.debug.ace
+IF EXIST ..\build_studio_debug\EiffelSoftware.CodeDom.Vision2\ace.ace GOTO CACHEDEBUG
+ECHO Could not find ..\build_studio_debug\EiffelSoftware.CodeDom.Vision2\ace.ace !!
 
 :CACHEDEBUG
-IF EXIST ..\build_studio_debug\EiffelSoftware.CacheBrowser\ace.ace COPY /Y ..\build_studio_debug\EiffelSoftware.CacheBrowser\ace.ace cache_browser.debug.ace
-IF EXIST ..\build_studio_debug\EiffelSoftware.CacheBrowser\ace.ace GOTO PROVIDERDEBUG
-ECHO Could not find ..\build_studio_debug\EiffelSoftware.CacheBrowser\ace.ace !!
+IF EXIST ..\build_studio_debug\EiffelSoftware.CodeDom.CacheBrowser\ace.ace COPY /Y ..\build_studio_debug\EiffelSoftware.CodeDom.CacheBrowser\ace.ace cache_browser.debug.ace
+IF EXIST ..\build_studio_debug\EiffelSoftware.CodeDom.CacheBrowser\ace.ace GOTO PROVIDERDEBUG
+ECHO Could not find ..\build_studio_debug\EiffelSoftware.CodeDom.CacheBrowser\ace.ace !!
 
 :PROVIDERDEBUG
 IF EXIST ..\build_studio_debug\EiffelSoftware.CodeDom\ace.ace COPY /Y ..\build_studio_debug\EiffelSoftware.CodeDom\ace.ace codedom_provider.debug.ace
@@ -48,9 +48,9 @@ IF EXIST ..\build_studio_debug\EiffelSoftware.CodeDom\ace.ace GOTO MANAGERDEBUG
 ECHO Could not find ..\build_studio_debug\EiffelSoftware.CodeDom\ace.ace !!
 
 :MANAGERDEBUG
-IF EXIST ..\build_studio_debug\ecd_manager\ace.ace COPY /Y ..\build_studio_debug\ecd_manager\ace.ace manager.debug.ace
-IF EXIST ..\build_studio_debug\ecd_manager\ace.ace GOTO END
-ECHO Could not find ..\build_studio_debug\ecd_manager\ace.ace !!
+IF EXIST ..\build_studio_debug\ecdpman\ace.ace COPY /Y ..\build_studio_debug\ecdpman\ace.ace manager.debug.ace
+IF EXIST ..\build_studio_debug\ecdpman\ace.ace GOTO END
+ECHO Could not find ..\build_studio_debug\ecdpman\ace.ace !!
 
 :END
 ECHO Done copying Eiffel CodeDom Provider ace files.

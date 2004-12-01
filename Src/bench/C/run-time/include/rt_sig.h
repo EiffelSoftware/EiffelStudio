10afd062312d682dbcae27b8aa072c7b7546a514
Emmanuel Stapf <manus@eiffel.com>
2004-12-01 02:16:56 +0000

    Added copyright information. Fixed bug in preprocessor directive `HAS_SYS_SIGLIST' it should have been   the opposite (not detected before because HAS_SYS_SIGLIST was alway undefined   before.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@46823 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/include/rt_sig.h
+++ Src/bench/C/run-time/include/rt_sig.h
@@ -1,12 +1,23 @@
 /*
+--|----------------------------------------------------------------
+--| Eiffel runtime header file
+--| Copyright (C) 1985-2004 Eiffel Software. All rights reserved.
+--| Duplication and distribution prohibited.  May be used only with
+--| ISE Eiffel, under terms of user license.
+--| Contact Eiffel Software for any other use.
+--|
+--| Interactive Software Engineering Inc.
+--| dba Eiffel Software
+--| 356 Storke Road, Goleta, CA 93117 USA
+--| Telephone 805-685-1006, Fax 805-685-6869
+--| Contact us at: http://www.eiffel.com/general/email.html
+--| Customer support: http://support.eiffel.com
+--| For latest info on our award winning products, visit:
+--|     http://www.eiffel.com
+--|----------------------------------------------------------------
+*/
 
-  ####      #     ####           #    #
- #          #    #    #          #    #
-  ####      #    #               ######
-      #     #    #  ###   ###    #    #
- #    #     #    #    #   ###    #    #
-  ####      #     ####    ###    #    #
-
+/*
 	Private signal handling declarations.
 */
 
@@ -49,7 +60,7 @@ extern char *signame(int sig);			/* Give English description of a signal */
 extern void trapsig(void (*handler) (int));			/* Set a trap for most of the signals */
 extern Signal_t exfpe(int sig);		/* Routine trapped for floating point exception */
 
-#ifdef HAS_SYS_SIGLIST
+#ifndef HAS_SYS_SIGLIST
 	extern char *sys_siglist[];
 #endif
 

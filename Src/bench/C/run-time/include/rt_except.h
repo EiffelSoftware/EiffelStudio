29198c336a2facc399a1c8efae2928a61fc07e00
Emmanuel Stapf <manus@eiffel.com>
2004-12-01 02:11:54 +0000

    Added copyright information. Removed useless `registerX' prefixes as nowadays C compiler are much smarter   than the user.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@46820 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/include/rt_except.h
+++ Src/bench/C/run-time/include/rt_except.h
@@ -1,12 +1,23 @@
 /*
+--|----------------------------------------------------------------
+--| Eiffel runtime header file
+--| Copyright (C) 1985-2004 Eiffel Software. All rights reserved.
+--| Duplication and distribution prohibited.  May be used only with
+--| ISE Eiffel, under terms of user license.
+--| Contact Eiffel Software for any other use.
+--|
+--| Interactive Software Engineering Inc.
+--| dba Eiffel Software
+--| 356 Storke Road, Goleta, CA 93117 USA
+--| Telephone 805-685-1006, Fax 805-685-6869
+--| Contact us at: http://www.eiffel.com/general/email.html
+--| Customer support: http://support.eiffel.com
+--| For latest info on our award winning products, visit:
+--|     http://www.eiffel.com
+--|----------------------------------------------------------------
+*/
 
- #####    #####          ######  #    #   ####   ######  #####    #####         #    #
- #    #     #            #        #  #   #    #  #       #    #     #           #    #
- #    #     #            #####     ##    #       #####   #    #     #           ###### 
- #####      #            #         ##    #       #       #####      #     ###   #    #
- #   #      #            #        #  #   #    #  #       #          #     ###   #    #
- #    #     #   #######  ######  #    #   ####   ######  #          #     ###   #    #
-
+/*
  Exception handling C code.
 */
 
@@ -29,8 +40,8 @@ extern void set_windows_exception_filter(void);
 #endif
 
 extern void xraise(EIF_CONTEXT int code);			/* Raise an exception with no tag */
-extern struct ex_vect *exget(register2 struct xstack *stk);	/* Get a new vector on stack */
-extern struct ex_vect *extop(register1 struct xstack *stk);	/* Top of Eiffel stack */
+extern struct ex_vect *exget(struct xstack *stk);	/* Get a new vector on stack */
+extern struct ex_vect *extop(struct xstack *stk);	/* Top of Eiffel stack */
 extern struct ex_vect *exnext(EIF_CONTEXT_NOARG);	/* Read next eif_trace item from bottom */
 extern void ereturn(EIF_CONTEXT_NOARG);			/* Return to lastly recorded rescue entry */
 extern void excatch(jmp_buf *jmp);			/* Set exception catcher from C to interpret */

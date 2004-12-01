1f43efb2c6483a638c1cb25b11831c0c9305aa8a
Emmanuel Stapf <manus@eiffel.com>
2004-12-01 01:20:08 +0000

    64 bits port: updated data type to avoid warnings. Removed useless `registerX' prefixes as nowadays C compiler are much smarter   than the user. Added copyright information.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@46789 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_plug.h
+++ Src/bench/C/run-time/eif_plug.h
@@ -1,12 +1,23 @@
 /*
+--|----------------------------------------------------------------
+--| Eiffel runtime header file
+--| Copyright (C) 1985-2004 Eiffel Software. All rights reserved.
+--| Duplication and distribution prohibited.  May be used only with
+--| ISE Eiffel, under terms of user license.
+--| Contact Eiffel Software for any other use.
+--|
+--| Interactive Software Engineering Inc.
+--| dba Eiffel Software
+--| 356 Storke Road, Goleta, CA 93117 USA
+--| Telephone 805-685-1006, Fax 805-685-6869
+--| Contact us at: http://www.eiffel.com/general/email.html
+--| Customer support: http://support.eiffel.com
+--| For latest info on our award winning products, visit:
+--|     http://www.eiffel.com
+--|----------------------------------------------------------------
+*/
 
- #####   #       #    #   ####           #    #
- #    #  #       #    #  #    #          #    #
- #    #  #       #    #  #               ######
- #####   #       #    #  #  ###   ###    #    #
- #       #       #    #  #    #   ###    #    #
- #       ######   ####    ####    ###    #    #
-
+/*
 	Declarations for plugging routines and structures.
 */
 
@@ -43,9 +54,9 @@ struct bit {
  * Run time functions used by generated C code.
  */
 
-RT_LNK EIF_REFERENCE makestr_with_hash(register char *s, register int len, register int a_hash);
-RT_LNK EIF_REFERENCE makestr_with_hash_as_old(register char *s, register int len, register int a_hash);
-RT_LNK EIF_REFERENCE makestr(register char *s, register int len);
+RT_LNK EIF_REFERENCE makestr_with_hash(register char *s, register size_t len, register int a_hash);
+RT_LNK EIF_REFERENCE makestr_with_hash_as_old(register char *s, register size_t len, register int a_hash);
+RT_LNK EIF_REFERENCE makestr(register char *s, register size_t len);
 extern EIF_REFERENCE makebit(char *bit, long int bit_count);		/* Build an Eiffel bit object */
 extern EIF_REFERENCE striparr(EIF_REFERENCE curr, int dtype, EIF_REFERENCE *items, long int nbr);			/* Build an Eiffel ARRAY[ANY] object for strip*/
 

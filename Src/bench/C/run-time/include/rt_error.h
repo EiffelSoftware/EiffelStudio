91a6bc5c8c03ab5a06969e3bb75818a2f76c20fb
Emmanuel Stapf <manus@eiffel.com>
2004-12-01 02:11:36 +0000

    Added copyright information. Fixed bug in preprocessor directive `HAS_SYS_ERRLIST' it should have been   the opposite (not detected before because HAS_SYS_ERRLIST was alway undefine   before.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@46819 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/include/rt_error.h
+++ Src/bench/C/run-time/include/rt_error.h
@@ -1,14 +1,24 @@
 /*
+--|----------------------------------------------------------------
+--| Eiffel runtime header file
+--| Copyright (C) 1985-2004 Eiffel Software. All rights reserved.
+--| Duplication and distribution prohibited.  May be used only with
+--| ISE Eiffel, under terms of user license.
+--| Contact Eiffel Software for any other use.
+--|
+--| Interactive Software Engineering Inc.
+--| dba Eiffel Software
+--| 356 Storke Road, Goleta, CA 93117 USA
+--| Telephone 805-685-1006, Fax 805-685-6869
+--| Contact us at: http://www.eiffel.com/general/email.html
+--| Customer support: http://support.eiffel.com
+--| For latest info on our award winning products, visit:
+--|     http://www.eiffel.com
+--|----------------------------------------------------------------
+*/
 
- ######  #####   #####    ####   #####           #    #
- #       #    #  #    #  #    #  #    #          #    #
- #####   #    #  #    #  #    #  #    #          ######
- #       #####   #####   #    #  #####    ###    #    #
- #       #   #   #   #   #    #  #   #    ###    #    #
- ######  #    #  #    #   ####   #    #   ###    #    #
-
+/*
  Private declaration.
-
 */
 
 #ifndef _rt_error_h_
@@ -24,7 +34,7 @@ extern void esys(void);				/* Raise 'Operating system error' exception */
 extern void eise_io(char *tag);		/* Raise an Eiffel 'I/O error' exception with `tag' */
 extern char *error_tag(int code);	/* English description out of errno code */
 
-#ifdef HAS_SYS_ERRLIST
+#ifndef HAS_SYS_ERRLIST
 	extern int sys_nerr;            /* Size of sys_errlist[] */
 	extern char *sys_errlist[];     /* Maps error code to string */
 #endif

e05813af8aacad1c3a3fae01260800efe92e7e30
Emmanuel Stapf <manus@eiffel.com>
2004-12-01 02:13:10 +0000

    Added copyright information. Removed declaration of `eif_typename_len' since not used.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@46821 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/include/rt_gen_types.h
+++ Src/bench/C/run-time/include/rt_gen_types.h
@@ -1,3 +1,22 @@
+/*
+--|----------------------------------------------------------------
+--| Eiffel runtime header file
+--| Copyright (C) 1985-2004 Eiffel Software. All rights reserved.
+--| Duplication and distribution prohibited.  May be used only with
+--| ISE Eiffel, under terms of user license.
+--| Contact Eiffel Software for any other use.
+--|
+--| Interactive Software Engineering Inc.
+--| dba Eiffel Software
+--| 356 Storke Road, Goleta, CA 93117 USA
+--| Telephone 805-685-1006, Fax 805-685-6869
+--| Contact us at: http://www.eiffel.com/general/email.html
+--| Customer support: http://support.eiffel.com
+--| For latest info on our award winning products, visit:
+--|     http://www.eiffel.com
+--|----------------------------------------------------------------
+*/
+
 #ifndef _rt_gen_types_h_
 #define _rt_gen_types_h_
 
@@ -54,7 +73,6 @@ extern "C" {
 /*------------------------------------------------------------------*/
 
 extern char *eif_typename (int16);
-extern int  eif_typename_len (int16);
 extern char eif_gen_typecode_with_dftype (int16 dftype, int pos);
 
 #ifdef __cplusplus

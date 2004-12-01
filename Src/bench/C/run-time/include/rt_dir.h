66f03bac34e0c9db262881163d16dc0016aec1d4
Emmanuel Stapf <manus@eiffel.com>
2004-12-01 02:10:36 +0000

    Added copyright information. Redid preprocessor branching for including the right header files for directories.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@46818 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/include/rt_dir.h
+++ Src/bench/C/run-time/include/rt_dir.h
@@ -1,12 +1,23 @@
 /*
+--|----------------------------------------------------------------
+--| Eiffel runtime header file
+--| Copyright (C) 1985-2004 Eiffel Software. All rights reserved.
+--| Duplication and distribution prohibited.  May be used only with
+--| ISE Eiffel, under terms of user license.
+--| Contact Eiffel Software for any other use.
+--|
+--| Interactive Software Engineering Inc.
+--| dba Eiffel Software
+--| 356 Storke Road, Goleta, CA 93117 USA
+--| Telephone 805-685-1006, Fax 805-685-6869
+--| Contact us at: http://www.eiffel.com/general/email.html
+--| Customer support: http://support.eiffel.com
+--| For latest info on our award winning products, visit:
+--|     http://www.eiffel.com
+--|----------------------------------------------------------------
+*/
 
- #####      #    #####           #    #
- #    #     #    #    #          #    #
- #    #     #    #    #          ######
- #    #     #    #####    ###    #    #
- #    #     #    #   #    ###    #    #
- #####      #    #    #   ###    #    #
-
+/*
 	Private declaration of public directory functions.
 */
 
@@ -15,33 +26,22 @@
 
 #include "eif_dir.h"
 
-#ifdef I_DIRECT
-#include <direct.h>				/* Pseudo symbol for Watcom C */
-#define DIRENTRY struct dirent
-#else
 #ifdef I_DIRENT
 #include <dirent.h>
 #define DIRENTRY struct dirent
-#else
-#ifdef I_SYS_DIR
+#elif defined(I_SYS_DIR)
 #include <sys/dir.h>
 #define DIRENTRY struct direct
-#else
-#ifdef I_SYS_NDIR
+#elif defined(I_SYS_NDIR)
 #include <sys/ndir.h>
 #define DIRENTRY struct direct
-#else
-#ifdef EIF_VMS_V6_ONLY
+#elif defined(EIF_VMS_V6_ONLY)
 #include <descrip.h>
 #include "eif_vmsdirent.h"		/* local to run-time */
 #define DIRENTRY struct dirent
 #elif defined EIF_WIN32
 #else
 	Sorry! You have to find a directory package...
-#endif
-#endif
-#endif
-#endif
 #endif
 
 #ifdef EIF_WIN32

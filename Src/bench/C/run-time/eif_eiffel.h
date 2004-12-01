746bc47317b03d5c29bf324ab8168f7ed6dd4d62
Emmanuel Stapf <manus@eiffel.com>
2004-12-01 00:53:26 +0000

    64 bits port: updated data type to avoid warnings. Added copyright information.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@46785 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_eiffel.h
+++ Src/bench/C/run-time/eif_eiffel.h
@@ -1,12 +1,23 @@
 /*
+--|----------------------------------------------------------------
+--| Eiffel runtime header file
+--| Copyright (C) 1985-2004 Eiffel Software. All rights reserved.
+--| Duplication and distribution prohibited.  May be used only with
+--| ISE Eiffel, under terms of user license.
+--| Contact Eiffel Software for any other use.
+--|
+--| Interactive Software Engineering Inc.
+--| dba Eiffel Software
+--| 356 Storke Road, Goleta, CA 93117 USA
+--| Telephone 805-685-1006, Fax 805-685-6869
+--| Contact us at: http://www.eiffel.com/general/email.html
+--| Customer support: http://support.eiffel.com
+--| For latest info on our award winning products, visit:
+--|     http://www.eiffel.com
+--|----------------------------------------------------------------
+*/
 
- ######     #    ######  ######  ######  #               #    #
- #          #    #       #       #       #               #    #
- #####      #    #####   #####   #####   #               ######
- #          #    #       #       #       #        ###    #    #
- #          #    #       #       #       #        ###    #    #
- ######     #    #       #       ######  ######   ###    #    #
-
+/*
 	Some configured inclusions/definitions.
 */
 
@@ -59,7 +70,7 @@ extern "C" {
 #endif
 
 /* defined in pattern.c */
-RT_LNK int str_str(char *text, char *pattern, int tlen, int plen, int start, int fuzzy);
+RT_LNK EIF_INTEGER str_str(char *text, char *pattern, int tlen, int plen, int start, int fuzzy);
 
 #ifdef __cplusplus
 }
--- Src/bench/C/run-time/eif_malloc.h
+++ Src/bench/C/run-time/eif_malloc.h
@@ -1,12 +1,23 @@
 /*
+--|----------------------------------------------------------------
+--| Eiffel runtime header file
+--| Copyright (C) 1985-2004 Eiffel Software. All rights reserved.
+--| Duplication and distribution prohibited.  May be used only with
+--| ISE Eiffel, under terms of user license.
+--| Contact Eiffel Software for any other use.
+--|
+--| Interactive Software Engineering Inc.
+--| dba Eiffel Software
+--| 356 Storke Road, Goleta, CA 93117 USA
+--| Telephone 805-685-1006, Fax 805-685-6869
+--| Contact us at: http://www.eiffel.com/general/email.html
+--| Customer support: http://support.eiffel.com
+--| For latest info on our award winning products, visit:
+--|     http://www.eiffel.com
+--|----------------------------------------------------------------
+*/
 
- #    #    ##    #       #        ####    ####           #    #
- ##  ##   #  #   #       #       #    #  #    #          #    #
- # ## #  #    #  #       #       #    #  #               ######
- #    #  ######  #       #       #    #  #        ###    #    #
- #    #  #    #  #       #       #    #  #    #   ###    #    #
- #    #  #    #  ######  ######   ####    ####    ###    #    #
-
+/*
 	Declarations for malloc routines
 */
 
@@ -75,7 +86,7 @@ RT_LNK EIF_REFERENCE sp_init (EIF_REFERENCE obj, uint32 dftype, EIF_INTEGER lowe
 
 RT_LNK EIF_REFERENCE strmalloc(unsigned int nbytes);		/* Allocate a string. */
 						/* Set the string header. */
-RT_LNK EIF_REFERENCE cmalloc(unsigned int nbytes);				/* Allocate a C object */
+RT_LNK EIF_REFERENCE cmalloc(size_t nbytes);				/* Allocate a C object */
 RT_LNK EIF_REFERENCE sprealloc(EIF_REFERENCE ptr, unsigned int nbitems);			/* Reallocate an Eiffel special object */
 
 #ifdef __cplusplus

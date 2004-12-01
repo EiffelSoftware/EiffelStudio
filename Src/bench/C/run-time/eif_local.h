94108d6c2f11c38a178dc64f09db15e649383832
Emmanuel Stapf <manus@eiffel.com>
2004-12-01 00:50:42 +0000

    Removed useless `registerX' prefixes as nowadays C compiler are much smarter   than the user. 64 bits port: updated data type to avoid warnings. Added copyright information.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@46783 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/run-time/eif_local.h
+++ Src/bench/C/run-time/eif_local.h
@@ -1,12 +1,23 @@
 /*
+--|----------------------------------------------------------------
+--| Eiffel runtime header file
+--| Copyright (C) 1985-2004 Eiffel Software. All rights reserved.
+--| Duplication and distribution prohibited.  May be used only with
+--| ISE Eiffel, under terms of user license.
+--| Contact Eiffel Software for any other use.
+--|
+--| Interactive Software Engineering Inc.
+--| dba Eiffel Software
+--| 356 Storke Road, Goleta, CA 93117 USA
+--| Telephone 805-685-1006, Fax 805-685-6869
+--| Contact us at: http://www.eiffel.com/general/email.html
+--| Customer support: http://support.eiffel.com
+--| For latest info on our award winning products, visit:
+--|     http://www.eiffel.com
+--|----------------------------------------------------------------
+*/
 
- #        ####    ####     ##    #               #    #
- #       #    #  #    #   #  #   #               #    #
- #       #    #  #       #    #  #               ######
- #       #    #  #       ######  #        ###    #    #
- #       #    #  #    #  #    #  #        ###    #    #
- ######   ####    ####   #    #  ######   ###    #    #
-
+/*
 	Declarations for local variable stack handling.
 */
 
@@ -17,7 +28,7 @@
 extern "C" {
 #endif
 
-RT_LNK void epop(register struct stack *stk, register int nb_items);			/* Pops values off the local stack */
+RT_LNK void epop(struct stack *stk, rt_uint_ptr nb_items);			/* Pops values off the local stack */
 #ifdef ISE_GC
 RT_LNK char **eget(register int num);		/* Get another chunk for local variables */
 RT_LNK void eback(register char **top);		/* Get back to the previous stack chunk */

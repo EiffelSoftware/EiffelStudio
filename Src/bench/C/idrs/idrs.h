d67de487a92f106fd91dbcf377bea58f41fc7027
Emmanuel Stapf <manus@eiffel.com>
2005-02-23 01:55:20 +0000

    Moved `idr_xx' routine to `idrs_helpers.h to avoid useless warnings when   they were defined in `idrs.h' but not used in the C file that included them.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@48648 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/idrs/idrs.h
+++ Src/bench/C/idrs/idrs.h
@@ -68,54 +68,6 @@ extern void idrf_destroy(IDRF *idrf);
 extern void idrf_reset_pos(IDRF *idrf);
 extern bool_t idr_setpos(IDR *idrs, size_t pos);
 
-
-/* Make sure the buffer can hold 'x' bytes, return false if this is not
- * possible. When serializing, this makes sure we'll have enough room for the
- * current type (especially simple types). When deserializing, it helps keeping
- * the consistency (trying to deserialize a longer structure).
- */
-#define CHK_SIZE(i, x) { \
-	if (((i)->i_ptr + (x)) > ((i)->i_buf + (i)->i_size)) \
-		return FALSE; \
-	}
-
-/* Helper macro to avoid copy/pasting. */
-#define EIF_IDR_SERIALIZER(a_buffer, a_value, a_data_type) \
-	if (a_buffer->i_op == IDR_ENCODE) { \
-		memcpy (idrs->i_ptr, a_value, sizeof(a_data_type)); \
-	} else { \
-		memcpy (a_value, idrs->i_ptr, sizeof(a_data_type)); \
-	} \
-	idrs->i_ptr += sizeof(a_data_type); 
-
-rt_private bool_t idr_int(IDR* idrs, int *val) {
-	CHK_SIZE(idrs,sizeof(int));
-	EIF_IDR_SERIALIZER(idrs,val,int);
-	return TRUE;
-}
-rt_private bool_t idr_rt_uint_ptr(IDR* idrs, rt_uint_ptr *val) {
-	CHK_SIZE(idrs,sizeof(rt_uint_ptr));
-	EIF_IDR_SERIALIZER(idrs,val,rt_uint_ptr);
-	return TRUE;
-}
-rt_private bool_t idr_eif_reference(IDR* idrs, EIF_REFERENCE *val) {
-	CHK_SIZE(idrs,sizeof(EIF_REFERENCE));
-	EIF_IDR_SERIALIZER(idrs,val,EIF_REFERENCE);
-	return TRUE;
-}
-rt_private bool_t idr_unsigned_char(IDR* idrs, unsigned char *val) {
-	CHK_SIZE(idrs,sizeof(unsigned char));
-	EIF_IDR_SERIALIZER(idrs,val,unsigned char);
-	return TRUE;
-}
-rt_private bool_t idr_size_t(IDR* idrs, size_t *val) {
-	CHK_SIZE(idrs,sizeof(size_t));
-	EIF_IDR_SERIALIZER(idrs,val,size_t);
-	return TRUE;
-}
-
-extern bool_t idr_string(IDR *idrs, char **sp, int maxlen);
-
 #ifdef __cplusplus
 }
 #endif

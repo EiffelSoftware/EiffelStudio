23a679f0851fae34d385c92191a7af4fb49f32fe
raphaels <raphaels@eiffel.com>
2005-05-10 20:57:52 +0000

    Added missing projects
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@50922 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/dotnet/codedom_provider/setup_envision.bat
+++ Src/dotnet/codedom_provider/setup_envision.bat
@@ -6,26 +6,24 @@ IF EXIST build_envision RD /Q /S build_envision
 
 MKDIR build_envision
 MKDIR build_envision\EiffelSoftware.CodeDomBase
-MKDIR build_envision\EiffelSoftware.CodeDomBase\obj
-MKDIR build_envision\EiffelSoftware.CodeDomBase\obj\Debug
-MKDIR build_envision\EiffelSoftware.CodeDomBase\obj\Release
 MKDIR build_envision\EiffelSoftware.CodeDomVision2
-MKDIR build_envision\EiffelSoftware.CodeDomVision2\obj
-MKDIR build_envision\EiffelSoftware.CodeDomVision2\obj\Debug
-MKDIR build_envision\EiffelSoftware.CodeDomVision2\obj\Release
 MKDIR build_envision\EiffelSoftware.CacheBrowser
-MKDIR build_envision\EiffelSoftware.CacheBrowser\obj
-MKDIR build_envision\EiffelSoftware.CacheBrowser\obj\Debug
-MKDIR build_envision\EiffelSoftware.CacheBrowser\obj\Release
 MKDIR build_envision\EiffelSoftware.CodeDom
-MKDIR build_envision\EiffelSoftware.CodeDom\obj
-MKDIR build_envision\EiffelSoftware.CodeDom\obj\Debug
-MKDIR build_envision\EiffelSoftware.CodeDom\obj\Release
+MKDIR build_envision\EiffelSoftware.CodeDom.Splitter
+MKDIR build_envision\ecdpman
+MKDIR build_envision\esplitter
+MKDIR build_envision\esplit
+MKDIR build_envision\nmap
 
 COPY configuration\base.eifp build_envision\EiffelSoftware.CodeDomBase\EiffelSoftware.CodeDomBase.eifp
 COPY configuration\vision2.eifp build_envision\EiffelSoftware.CodeDomVision2\EiffelSoftware.CodeDomVision2.eifp
 COPY configuration\cache_browser.eifp build_envision\EiffelSoftware.CacheBrowser\EiffelSoftware.CacheBrowser.eifp
 COPY configuration\codedom_provider.eifp build_envision\EiffelSoftware.CodeDom\EiffelSoftware.CodeDom.eifp
+COPY configuration\codedom_provider.eifp build_envision\EiffelSoftware.CodeDom.Splitter\EiffelSoftware.CodeDom.Splitter.eifp
+COPY configuration\ecdpman.eifp build_envision\ecdpman\ecdpman.eifp
+COPY configuration\esplitter.eifp build_envision\esplitter\esplitter.eifp
+COPY configuration\esplit.eifp build_envision\esplit\esplit.eifp
+COPY configuration\nmap.eifp build_envision\nmap\nmap.eifp
 
 COPY configuration\codedom_provider.sln build_envision\
 

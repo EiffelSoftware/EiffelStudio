e67117d114f585ee988d5ce7874caa617d85d8f4
raphaels <raphaels@eiffel.com>
2005-05-10 20:04:07 +0000

    Use command line for new GOBO
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@50914 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/dotnet/codedom_provider/code_generator/entities/inheritance_clauses/make.bat
+++ Src/dotnet/codedom_provider/code_generator/entities/inheritance_clauses/make.bat
@@ -8,8 +8,8 @@ IF EXIST code_inheritance_clause_scanner.e DEL code_inheritance_clause_scanner.e
 IF EXIST code_inheritance_clause_tokens.e DEL code_inheritance_clause_tokens.e
 IF EXIST code_inheritance_clause_parser.e DEL code_inheritance_clause_parser.e
 
-gelex inheritance_clause.l
-geyacc -t CODE_INHERITANCE_CLAUSE_TOKENS -o code_inheritance_clause_parser.e inheritance_clause.y
+gelex -z inheritance_clause.l
+geyacc --new_typing --pragma=noline -x -t CODE_INHERITANCE_CLAUSE_TOKENS -o code_inheritance_clause_parser.e inheritance_clause.y
 
 IF NOT EXIST code_inheritance_clause_scanner.e ECHO Scanner not generated!! (file code_inheritance_clause_scanner.e does not exist)
 IF NOT EXIST code_inheritance_clause_parser.e ECHO Parser not generated!! (file code_inheritance_clause_parser.e does not exist)

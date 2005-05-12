713ee4751e9204f880041f0814f9f0afe9283293
raphaels <raphaels@eiffel.com>
2005-05-12 16:00:35 +0000

    Updated to use new COM wizard.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@51008 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/com_compiler/generate_exe.bat
+++ Src/bench/com_compiler/generate_exe.bat
@@ -2,4 +2,4 @@ if exist generated rd /q /s generated
 
 mkdir generated
 
-%ISE_EIFFEL%\wizards\com\com_wizard_cmd.exe -new_com_project -com_file %EIFFEL_SRC%\com_compiler\idl\EiffelSoftwareCompiler.idl -destination %EIFFEL_SRC%\com_compiler\generated -server -output_all -not_spawn_ebench -proxy_stub "%EIFFEL_SRC%\com_compiler\idl\EiffelSoftware.CompilerPS.dll"
+%ISE_EIFFEL%\wizards\com\com_wizard.exe --server %EIFFEL_SRC%\com_compiler\idl\EiffelSoftwareCompiler.idl --outofprocess --destination %EIFFEL_SRC%\com_compiler\generated --marshaller

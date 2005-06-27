aeec9423aef50cea9e6e65fb4bdbbd2f23d63aa3
Alexander Kogtenkov <alexk@eiffel.com>
2005-06-27 12:54:30 +0000

    Used built-in OS variable to get path to the Windows directory.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@52632 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/tools/doc_builder/setup.bat
+++ Src/tools/doc_builder/setup.bat
@@ -1,4 +1,5 @@
 REM --- Generate WinForms Control for Microsoft Web Browser Active control ---
 REM ------------------------------------------------------------------------------
-aximp c:\windows\system32\shdocvw.dll
+if "%windir%" == "" set windir=c:\windows
+aximp "%windir%\system32\shdocvw.dll"
 REM tlbimp mshtml.tlb  -- Use for DHTML DOM interfaces

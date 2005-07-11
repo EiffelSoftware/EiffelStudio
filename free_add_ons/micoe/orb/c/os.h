6514660164c558f536070578c65fb2114d53f386
Emmanuel Stapf <manus@eiffel.com>
2005-07-11 16:54:38 +0000

    Removed since it has not been updated for a while and we didn't have time to   check that it will work with the 5.6 release.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@53277 8089f293-4706-0410-a29e-feb5c42a2edf

--- free_add_ons/micoe/orb/c/os.h
+++
@@ -1,4 +0,0 @@
-#include "eiffel.h"
-extern EIF_INTEGER MICO_getpid(void);
-extern EIF_POINTER MICO_tempnam(void);
-extern EIF_INTEGER MICO_get_time(void);
--- free_add_ons/micoe/orb/transport/x509.e
+++
@@ -1,4 +0,0 @@
-class X509
-        -- I haven't yet found such a class or struct!
-
-end -- class X509

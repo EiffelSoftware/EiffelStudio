fb87463a22f89ec79fbc4b41be0b0912e2ebe09a
Emmanuel Stapf <manus@eiffel.com>
2005-07-11 16:54:38 +0000

    Removed since it has not been updated for a while and we didn't have time to   check that it will work with the 5.6 release.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@53277 8089f293-4706-0410-a29e-feb5c42a2edf

--- free_add_ons/micoe/acconfig.h
+++
@@ -1,9 +0,0 @@
-/* whether we have byteorder big endian */
-#undef HAVE_BYTEORDER_BE
-
-/* whether we have byteorder little endian */
-#undef HAVE_BYTEORDER_LE
-
-/* whether we have IEEE compliant floating point types */
-#undef HAVE_IEEE_FP
-
--- free_add_ons/micoe/orb/c/constants.c
+++
@@ -1,6 +0,0 @@
-#include "eiffel.h"
-
-EIF_INTEGER MICO_highest_priority (void)
-{
-    return (EIF_INTEGER) 0x7fffffffL;
-}

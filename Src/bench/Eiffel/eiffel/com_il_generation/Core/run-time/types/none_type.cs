5f6135c5765ad23c058dd71116de1beb11617ecb
Emmanuel Stapf <manus@eiffel.com>
2005-07-12 01:18:38 +0000

    Ensured that instances of RT_NONE_TYPE are properly initialized.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@53341 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/Eiffel/eiffel/com_il_generation/Core/run-time/types/none_type.cs
+++ Src/bench/Eiffel/eiffel/com_il_generation/Core/run-time/types/none_type.cs
@@ -12,6 +12,11 @@ namespace EiffelSoftware.Runtime.Types {
 [Serializable]
 sealed public class RT_NONE_TYPE: RT_CLASS_TYPE {
 
+	public RT_NONE_TYPE()
+	{
+		type = typeof(RT_NONE_TYPE).TypeHandle;
+	}
+
 	public override String class_name ()
 		// Name of object's generating type who has Current as an EIFFEL_DERIVATION
 		// (type of which it is a direct instance)

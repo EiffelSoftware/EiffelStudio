d547870823f95d5db7631cdaafa0abaf449649cf
Emmanuel Stapf <manus@eiffel.com>
2005-07-13 18:10:13 +0000

    USe %ISE_PLATFORM% to make script portable from Windows 32 bits to Windows 64 bits.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@53399 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/Eiffel/Ace/build.bat
+++ Src/bench/Eiffel/Ace/build.bat
@@ -8,9 +8,9 @@ if .%2. == .. goto usage
 cdd %1
 rd /q /s EIFGEN
 
-%ISE_EIFFEL%\studio\spec\windows\bin\ec.exe -finalize -project_path %1 -ace %EIFFEL_SRC%\Eiffel\Ace\newbench.mswin.ace -batch -c_compile
+%ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe -finalize -project_path %1 -ace %EIFFEL_SRC%\Eiffel\Ace\newbench.mswin.ace -batch -c_compile
 
-cdd %ISE_EIFFEL%\studio\spec\windows\bin
+cdd %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin
 if not exist ec.exe del old_ec.exe
 ren ec.exe old_ec.exe
 copy %1\EIFGEN\F_code\ec.exe .
@@ -18,7 +18,7 @@ copy %1\EIFGEN\F_code\ec.exe .
 cdd %2
 rd /q /s EIFGEN
 
-%ISE_EIFFEL%\studio\spec\windows\bin\ec.exe -freeze -project_path %2 -ace %EIFFEL_SRC%\Eiffel\Ace\newbench.mswin.ace -batch -c_compile
+%ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe -freeze -project_path %2 -ace %EIFFEL_SRC%\Eiffel\Ace\newbench.mswin.ace -batch -c_compile
 
 goto end
 

9829bb2d1f50b9b79f339dc639e04ba101e49cc1
Emmanuel Stapf <manus@eiffel.com>
2005-11-15 00:30:47 +0000

    New GUIDs for consumer for 5.7 release.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@55730 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/Eiffel/library/cli_writer/Clib/metadata_consumer.h
+++ Src/bench/Eiffel/library/cli_writer/Clib/metadata_consumer.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 6.00.0361 */
-/* at Tue Jun 14 14:13:43 2005
+/* at Mon Nov 14 16:30:05 2005
  */
 /* Compiler settings for metadata_consumer.idl:
     Oicf, W1, Zp8, env=Win32 (32b run)
@@ -95,7 +95,7 @@ EXTERN_C const IID IID_EiffelSoftware_MetadataConsumer_Interop_I_COM_CACHE_MANAG
 
 #if defined(__cplusplus) && !defined(CINTERFACE)
     
-    MIDL_INTERFACE("E1FFE1A4-412E-4517-8BD0-56F31E07E879")
+    MIDL_INTERFACE("E1FFE100-6FE1-4C21-AE1E-01415B20FE30")
     EiffelSoftware_MetadataConsumer_Interop_I_COM_CACHE_MANAGER : public IDispatch
     {
     public:
@@ -518,7 +518,7 @@ EXTERN_C const IID IID_EiffelSoftware_MetadataConsumer_Interop_I_COM_ASSEMBLY_IN
 
 #if defined(__cplusplus) && !defined(CINTERFACE)
     
-    MIDL_INTERFACE("E1FFE140-552A-432A-A066-86E24073A90C")
+    MIDL_INTERFACE("E1FFE100-6EB3-46C8-94CE-208A7B1C79C3")
     EiffelSoftware_MetadataConsumer_Interop_I_COM_ASSEMBLY_INFORMATION : public IDispatch
     {
     public:
@@ -775,7 +775,7 @@ EXTERN_C const CLSID CLSID_EiffelSoftware_MetadataConsumer_Interop_Impl_COM_CACH
 
 #ifdef __cplusplus
 
-class DECLSPEC_UUID("E1FFE1ED-1F90-4D3C-BAC6-68B74B00F5CB")
+class DECLSPEC_UUID("E1FFE1AC-B1AC-436F-948D-20B62A3A87AF")
 EiffelSoftware_MetadataConsumer_Interop_Impl_COM_CACHE_MANAGER;
 #endif
 #endif /* __EiffelSoftware_MetadataConsumer_LIBRARY_DEFINED__ */

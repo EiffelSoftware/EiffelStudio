020fbc090b315667d040d34f5b1eab349d3ac3f6
raphaels <raphaels@eiffel.com>
2005-12-15 19:33:33 +0000

    Added codedom filter.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@56191 8089f293-4706-0410-a29e-feb5c42a2edf

--- Delivery/xmldoc/projects/codedom.dpr
+++ Delivery/xmldoc/projects/codedom.dpr
@@ -1,8 +1,8 @@
 <project>
 	<project_name>EiffelASP.NET Documentation</project_name>
-	<root_directory>E:/xmldoc/technologies/codedom</root_directory>
-	<schema>E:/Sources/tools/doc_builder/resources/xml/schema/document.xsd</schema>
-	<html_css>E:/xmldoc/technologies/codedom/default.css</html_css>
+	<root_directory>C:/Sources/Delivery/xmldoc/technologies/codedom</root_directory>
+	<schema>C:/Sources/tools/doc_builder/resources/xml/schema/document.xsd</schema>
+	<html_css>C:/Sources/Delivery/xmldoc/technologies/codedom/default.css</html_css>
 	<header_override>True</header_override>
 	<use_header_from_file>False</use_header_from_file>
 	<footer_override>True</footer_override>
@@ -13,4 +13,12 @@
 	<process_html_stylesheet>True</process_html_stylesheet>
 	<include_navigation_links>True</include_navigation_links>
 	<generate_dhtml_filter>True</generate_dhtml_filter>
-	<generate_feature_nodes>True</generate_feature_nodes></project>
+	<generate_feature_nodes>True</generate_feature_nodes>
+	<filter>
+		<filter_description>codedom</filter_description>
+		<filter_highlight_color_red>1</filter_highlight_color_red>
+		<filter_highlight_color_green>1</filter_highlight_color_green>
+		<filter_highlight_color_blue>1</filter_highlight_color_blue>
+		<filter_highlight_on>False</filter_highlight_on>
+		<tag>codedom</tag>
+	</filter></project>

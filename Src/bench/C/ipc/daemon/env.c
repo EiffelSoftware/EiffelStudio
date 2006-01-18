4f4aa42609e345517def36be6f07b7c020489396
Emmanuel Stapf <manus@eiffel.com>
2006-01-18 21:44:20 +0000

    Added new copyright for runtime.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@56595 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/bench/C/ipc/daemon/env.c
+++ Src/bench/C/ipc/daemon/env.c
@@ -1,12 +1,41 @@
 /*
-	ENV.C - environment processing functions.
-		This accesses the registry in key
-
-		HKEY_LOCAL_MACHINE\Software\ISE\Eiffel\app
-
-		where app is the name of the started application.
-
-		This is standard Windows registry access.
+	description: "[
+			Environment processing functions. We first try to look up in the environment
+			variable table, then in the HKLM\Software\ISE\EiffelXX\app and if not found then in
+			HKCU\Software\ISE\EiffelXX\app when app is the name of the started application.
+			]"
+	date:		"$Date$"
+	revision:	"$Revision$"
+	copyright:	"Copyright (c) 1985-2006, Eiffel Software."
+	license:	"GPL version 2 see http://www.eiffel.com/licensing/gpl.txt)"
+	licensing_options:	"Commercial license is available at http://www.eiffel.com/licensing"
+	copying: "[
+			This file is part of Eiffel Software's Runtime.
+			
+			Eiffel Software's Runtime is free software; you can
+			redistribute it and/or modify it under the terms of the
+			GNU General Public License as published by the Free
+			Software Foundation, version 2 of the License
+			(available at the URL listed under "license" above).
+			
+			Eiffel Software's Runtime is distributed in the hope
+			that it will be useful,	but WITHOUT ANY WARRANTY;
+			without even the implied warranty of MERCHANTABILITY
+			or FITNESS FOR A PARTICULAR PURPOSE.
+			See the	GNU General Public License for more details.
+			
+			You should have received a copy of the GNU General Public
+			License along with Eiffel Software's Runtime; if not,
+			write to the Free Software Foundation, Inc.,
+			51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+		]"
+	source: "[
+			 Eiffel Software
+			 356 Storke Road, Goleta, CA 93117 USA
+			 Telephone 805-685-1006, Fax 805-685-6869
+			 Website http://www.eiffel.com
+			 Customer support http://support.eiffel.com
+		]"
 */
 
 #include "eif_config.h"
--- Src/bench/C/ipc/shared/uu.h
+++ Src/bench/C/ipc/shared/uu.h
@@ -1,5 +1,37 @@
 /*
-	UU.H - uuencode_str and uudecode_str prototypes.
+	description: "Declarations for `uu.c'."
+	date:		"$Date$"
+	revision:	"$Revision$"
+	copyright:	"Copyright (c) 1985-2006, Eiffel Software."
+	license:	"GPL version 2 see http://www.eiffel.com/licensing/gpl.txt)"
+	licensing_options:	"Commercial license is available at http://www.eiffel.com/licensing"
+	copying: "[
+			This file is part of Eiffel Software's Runtime.
+			
+			Eiffel Software's Runtime is free software; you can
+			redistribute it and/or modify it under the terms of the
+			GNU General Public License as published by the Free
+			Software Foundation, version 2 of the License
+			(available at the URL listed under "license" above).
+			
+			Eiffel Software's Runtime is distributed in the hope
+			that it will be useful,	but WITHOUT ANY WARRANTY;
+			without even the implied warranty of MERCHANTABILITY
+			or FITNESS FOR A PARTICULAR PURPOSE.
+			See the	GNU General Public License for more details.
+			
+			You should have received a copy of the GNU General Public
+			License along with Eiffel Software's Runtime; if not,
+			write to the Free Software Foundation, Inc.,
+			51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+		]"
+	source: "[
+			 Eiffel Software
+			 356 Storke Road, Goleta, CA 93117 USA
+			 Telephone 805-685-1006, Fax 805-685-6869
+			 Website http://www.eiffel.com
+			 Customer support http://support.eiffel.com
+		]"
 */
 
 #ifndef uu_h_

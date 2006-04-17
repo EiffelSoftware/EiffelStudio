6528145b4455b02913baa6e0fbc33ec9eb49708a
Emmanuel Stapf <manus@eiffel.com>
2006-04-17 19:43:06 +0000

    Removed because empty.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@58239 8089f293-4706-0410-a29e-feb5c42a2edf

--- Delivery/dotnet/assemblies/build.bat
+++
@@ -1,7 +0,0 @@
-call emitter.exe /init /nooutput
-call emitter.exe /add /nooutput %WINDIR%\Microsoft.NET\Framework\v1.0.3705\System.Windows.Forms.dll
-call emitter.exe /add /nooutput %WINDIR%\Microsoft.NET\Framework\v1.0.3705\System.Web.Services.dll
-
-copy System.Object.xml mscorlib-1_0_3300_0--b77a5c561934e089\classes
-copy mscorlib-1_0_3300_0--b77a5c561934e089\types.xml mscorlib-1_0_3300_0--b77a5c561934e089\original_types.xml
-copy types.xml mscorlib-1_0_3300_0--b77a5c561934e089

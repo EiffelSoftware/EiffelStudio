99c0bd7289d45390645413b563b7fd7aca837bdd
Emmanuel Stapf <manus@eiffel.com>
2006-04-20 17:26:36 +0000

    Added creation of compiler specific directory in the vision2 library.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@58341 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C_library/libpng/make_msc.bat
+++ Src/C_library/libpng/make_msc.bat
@@ -1,4 +1,6 @@
 nmake -f scripts/makefile.vcwin32
+if not exist ..\..\library\vision2\spec\msc mkdir ..\..\library\vision2\spec\msc
+if not exist ..\..\library\vision2\spec\msc\lib mkdir ..\..\library\vision2\spec\msc\lib
 copy libpng.lib ..\..\library\vision2\spec\msc\lib\
 del *.obj
 del *.lib
--- Src/C_library/zlib/make_msc.bat
+++ Src/C_library/zlib/make_msc.bat
@@ -1,4 +1,6 @@
 nmake /f win32\Makefile.msc
+if not exist ..\..\library\vision2\spec\msc mkdir ..\..\library\vision2\spec\msc
+if not exist ..\..\library\vision2\spec\msc\lib mkdir ..\..\library\vision2\spec\msc\lib
 copy zlib.lib ..\..\library\vision2\spec\msc\lib\
 del *.obj
 del *.lib

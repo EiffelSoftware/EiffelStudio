2bf0036d383b2ebfd90c3a9ac8885a70bc669e9f
Emmanuel Stapf <manus@eiffel.com>
2006-04-20 17:26:36 +0000

    Added creation of compiler specific directory in the vision2 library.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@58341 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C_library/libpng/make_bcb.bat
+++ Src/C_library/libpng/make_bcb.bat
@@ -1,4 +1,6 @@
 %ISE_EIFFEL%\BCC55\bin\make -f scripts/makefile.bcb
+if not exist ..\..\library\vision2\spec\bcb mkdir ..\..\library\vision2\spec\bcb
+if not exist ..\..\library\vision2\spec\bcb\lib mkdir ..\..\library\vision2\spec\bcb\lib
 copy libpng.lib ..\..\library\vision2\spec\bcb\lib\
 del *.obj
 del *.lib
--- Src/C_library/zlib/make_bcb.bat
+++ Src/C_library/zlib/make_bcb.bat
@@ -1,4 +1,6 @@
 %ISE_EIFFEL%\BCC55\bin\make /f win32\Makefile.bor
+if not exist ..\..\library\vision2\spec\bcb mkdir ..\..\library\vision2\spec\bcb
+if not exist ..\..\library\vision2\spec\bcb\lib mkdir ..\..\library\vision2\spec\bcb\lib
 copy zlib.lib ..\..\library\vision2\spec\bcb\lib\
 del *.obj
 del *.lib

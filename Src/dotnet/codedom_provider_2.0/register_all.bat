41e0802bb692e921e645758b04a54a7666bd0645
raphaels <raphaels@eiffel.com>
2006-05-10 01:57:46 +0000

    Updated scripts to use new configuration system.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@58850 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/dotnet/codedom_provider_2.0/register_all.bat
+++ Src/dotnet/codedom_provider_2.0/register_all.bat
@@ -1,55 +1,45 @@
 @ECHO OFF
 
 :START
-IF EXIST build_studio_debug GOTO DEBUG
-CD build_studio
-GOTO BASE
-:DEBUG
-CD build_studio_debug
+CD bin
 
 :BASE
 ECHO Registering EiffelSoftware.CodeDom.Base.dll
-IF EXIST EiffelSoftware.CodeDom.Base\EIFGEN\F_code\EiffelSoftware.CodeDom.Base.dll gacutil -if EiffelSoftware.CodeDom.Base\EIFGEN\F_code\EiffelSoftware.CodeDom.Base.dll /nologo
-IF EXIST EiffelSoftware.CodeDom.Base\EIFGEN\F_code\EiffelSoftware.CodeDom.Base.dll GOTO LIBBASE
-ECHO Could not find EiffelSoftware.CodeDom.Base\EIFGEN\F_code\EiffelSoftware.CodeDom.Base.dll
+IF EXIST Base\EIFGENs\base\F_code\EiffelSoftware.CodeDom.Base.dll gacutil -if Base\EIFGENs\base\F_code\EiffelSoftware.CodeDom.Base.dll /nologo
+IF EXIST Base\EIFGENs\base\F_code\EiffelSoftware.CodeDom.Base.dll GOTO LIBBASE
+ECHO Could not find EiffelSoftware.CodeDom.Base.dll
 
 :LIBBASE
 ECHO Registering libEiffelSoftware.CodeDom.Base.dll
-IF EXIST EiffelSoftware.CodeDom.Base\EIFGEN\F_code\libEiffelSoftware.CodeDom.Base.dll COPY /Y EiffelSoftware.CodeDom.Base\EIFGEN\F_code\libEiffelSoftware.CodeDom.Base.dll %WINDIR%\System32
-IF EXIST EiffelSoftware.CodeDom.Base\EIFGEN\F_code\libEiffelSoftware.CodeDom.Base.dll GOTO VISION2
-ECHO Could not find EiffelSoftware.CodeDom.Base\EIFGEN\F_code\libEiffelSoftware.CodeDom.Base.dll
+IF EXIST Base\EIFGENs\base\F_code\libEiffelSoftware.CodeDom.Base.dll COPY /Y Base\EIFGENs\base\F_code\libEiffelSoftware.CodeDom.Base.dll %WINDIR%\System32
+IF EXIST Base\EIFGENs\base\F_code\libEiffelSoftware.CodeDom.Base.dll GOTO VISION2
+ECHO Could not find libEiffelSoftware.CodeDom.Base.dll
 
 :VISION2
 ECHO Registering EiffelSoftware.CodeDom.Vision2.dll
-IF EXIST EiffelSoftware.CodeDom.Vision2\EIFGEN\F_code\EiffelSoftware.CodeDom.Vision2.dll gacutil -if EiffelSoftware.CodeDom.Vision2\EIFGEN\F_code\EiffelSoftware.CodeDom.Vision2.dll /nologo
-IF EXIST EiffelSoftware.CodeDom.Vision2\EIFGEN\F_code\EiffelSoftware.CodeDom.Vision2.dll GOTO LIBVISION2
-ECHO Could not find EiffelSoftware.CodeDom.Vision2\EIFGEN\F_code\EiffelSoftware.CodeDom.Vision2.dll
+IF EXIST Vision2\EIFGENs\vision2\F_code\EiffelSoftware.CodeDom.Vision2.dll gacutil -if Vision2\EIFGENs\vision2\F_code\EiffelSoftware.CodeDom.Vision2.dll /nologo
+IF EXIST Vision2\EIFGENs\vision2\F_code\EiffelSoftware.CodeDom.Vision2.dll GOTO LIBVISION2
+ECHO Could not find EiffelSoftware.CodeDom.Vision2.dll
 
 :LIBVISION2
 ECHO Registering libEiffelSoftware.CodeDom.Vision2.dll
-IF EXIST EiffelSoftware.CodeDom.Vision2\EIFGEN\F_code\libEiffelSoftware.CodeDom.Vision2.dll COPY /Y EiffelSoftware.CodeDom.Vision2\EIFGEN\F_code\libEiffelSoftware.CodeDom.Vision2.dll %WINDIR%\System32
-IF EXIST EiffelSoftware.CodeDom.Vision2\EIFGEN\F_code\libEiffelSoftware.CodeDom.Vision2.dll GOTO CACHE
+IF EXIST Vision2\EIFGENs\vision2\F_code\libEiffelSoftware.CodeDom.Vision2.dll COPY /Y Vision2\EIFGENs\vision2\F_code\libEiffelSoftware.CodeDom.Vision2.dll %WINDIR%\System32
+IF EXIST Vision2\EIFGENs\vision2\F_code\libEiffelSoftware.CodeDom.Vision2.dll GOTO CACHE
 ECHO Could not find EiffelSoftware.CodeDom.Vision2\EIFGEN\F_code\libEiffelSoftware.CodeDom.Vision2.dll
 
 :CACHE
 ECHO Registering EiffelSoftware.CodeDom.CacheBrowser.dll
-IF EXIST EiffelSoftware.CodeDom.CacheBrowser\EIFGEN\F_code\EiffelSoftware.CodeDom.CacheBrowser.dll gacutil -if EiffelSoftware.CodeDom.CacheBrowser\EIFGEN\F_code\EiffelSoftware.CodeDom.CacheBrowser.dll /nologo
-IF EXIST EiffelSoftware.CodeDom.CacheBrowser\EIFGEN\F_code\EiffelSoftware.CodeDom.CacheBrowser.dll GOTO CODEDOM
-ECHO Could not find EiffelSoftware.CodeDom.CacheBrowser\EIFGEN\F_code\EiffelSoftware.CodeDom.CacheBrowser.dll
+IF EXIST cache_browser\EIFGENs\cache_browser\F_code\EiffelSoftware.CodeDom.CacheBrowser.dll gacutil -if cache_browser\EIFGENs\cache_browser\F_code\EiffelSoftware.CodeDom.CacheBrowser.dll /nologo
+IF EXIST cache_browser\EIFGENs\cache_browser\F_code\EiffelSoftware.CodeDom.CacheBrowser.dll GOTO CODEDOM
+ECHO Could not find EiffelSoftware.CodeDom.CacheBrowser.dll
 
 :CODEDOM
 ECHO Registering EiffelSoftware.CodeDom.dll
-IF EXIST EiffelSoftware.CodeDom\EIFGEN\F_code\EiffelSoftware.CodeDom.dll gacutil -if EiffelSoftware.CodeDom\EIFGEN\F_code\EiffelSoftware.CodeDom.dll /nologo
-IF EXIST EiffelSoftware.CodeDom\EIFGEN\F_code\EiffelSoftware.CodeDom.dll installutil EiffelSoftware.CodeDom\EIFGEN\F_code\EiffelSoftware.CodeDom.dll
-IF EXIST EiffelSoftware.CodeDom\EIFGEN\F_code\EiffelSoftware.CodeDom.dll GOTO PROVIDER
-ECHO Could not find EiffelSoftware.CodeDom\EIFGEN\F_code\EiffelSoftware.CodeDom.dll
-
-:PROVIDER
-CD ..
-ECHO Registering EiffelSoftware.CodedomSerializer.dll
-IF EXIST test\serializer\provider\EIFGEN\F_code\EiffelSoftware.CodedomSerializer.dll gacutil -if test\serializer\provider\EIFGEN\F_code\EiffelSoftware.CodedomSerializer.dll /nologo
-IF EXIST test\serializer\provider\EIFGEN\F_code\EiffelSoftware.CodedomSerializer.dll GOTO END
-ECHO Could not find test\serializer\provider\EIFGEN\F_code\EiffelSoftware.CodedomSerializer.dll
+IF EXIST codedom_provider\EIFGENs\debug\F_code\EiffelSoftware.CodeDom.dll gacutil -if codedom_provider\EIFGENs\debug\F_code\EiffelSoftware.CodeDom.dll /nologo
+IF EXIST codedom_provider\EIFGENs\debug\F_code\EiffelSoftware.CodeDom.dll installutil codedom_provider\EIFGENs\debug\F_code\EiffelSoftware.CodeDom.dll
+IF EXIST codedom_provider\EIFGENs\debug\F_code\EiffelSoftware.CodeDom.dll GOTO END 
+ECHO Could not find EiffelSoftware.CodeDom.dll
 
 :END
+CD ..
 ECHO Done.

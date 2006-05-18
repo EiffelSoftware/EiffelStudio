f53cbaa4a413c3efcf95e61c3195c46bdf699db4
Emmanuel Stapf <manus@eiffel.com>
2006-05-18 00:33:19 +0000

    Updated to new filename extension of configuration file.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@59049 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/examples/vision2/widget_test/retrieve_flat_short.bat
+++ Src/examples/vision2/widget_test/retrieve_flat_short.bat
@@ -1,12 +1,12 @@
 rm -rf %TEMP%\vision2_precompile
 mkdir %TEMP%\vision2_precompile
-%ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec -config vision2.acex -project_path %TEMP%\vision2_precompile
+%ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec -config vision2.ecf -project_path %TEMP%\vision2_precompile
 
 REM Create flatshort directory
 MD .\flatshort
 
 REM now perform generation based on "flatshort_instructions.txt"
-type flatshort_instructions.txt | %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec -config vision2.acex -loop
+type flatshort_instructions.txt | %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec -config vision2.ecf -loop
 
 REM remove vision2_precompile directory.
 rm -rf %TEMP%\vision2_precompile

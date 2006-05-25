477ef4b86d75bb72febba5f4c4333a3191c44c37
Emmanuel Stapf <manus@eiffel.com>
2006-05-25 05:21:14 +0000

    Fixed second problem shown by eweasel test#exec219 where conversion from SK_BOOL, SK_CHAR   or SK_WCHAR to SK_INTXX or SK_NATXX would truncate the value to a 16 bits value because   we forgot some `break;' instructions.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@59191 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C/run-time/interp.c
+++ Src/C/run-time/interp.c
@@ -797,6 +797,7 @@ rt_private void interpret(int flag, int where)
 					default:
 						eif_panic ("Illegal type");
 				}
+				break;
 			case SK_CHAR:
 				switch (offset) {
 					case 8: last->it_uint8 = (EIF_NATURAL_8) last->it_char; break;
@@ -806,6 +807,7 @@ rt_private void interpret(int flag, int where)
 					default:
 						eif_panic ("Illegal type");
 				}
+				break;
 			case SK_WCHAR:
 				switch (offset) {
 					case 8: last->it_uint8 = (EIF_NATURAL_8) last->it_wchar; break;
@@ -815,6 +817,7 @@ rt_private void interpret(int flag, int where)
 					default:
 						eif_panic ("Illegal type");
 				}
+				break;
 			case SK_UINT8:
 				switch (offset) {
 					case 8: break;
@@ -956,6 +959,7 @@ rt_private void interpret(int flag, int where)
 					default:
 						eif_panic ("Illegal type");
 				}
+				break;
 			case SK_CHAR:
 				switch (offset) {
 					case 8: last->it_int8 = (EIF_INTEGER_8) last->it_char; break;
@@ -965,6 +969,7 @@ rt_private void interpret(int flag, int where)
 					default:
 						eif_panic ("Illegal type");
 				}
+				break;
 			case SK_WCHAR:
 				switch (offset) {
 					case 8: last->it_int8 = (EIF_INTEGER_8) last->it_wchar; break;
@@ -974,6 +979,7 @@ rt_private void interpret(int flag, int where)
 					default:
 						eif_panic ("Illegal type");
 				}
+				break;
 			case SK_UINT8:
 				switch (offset) {
 					case 8: last->it_int8 = (EIF_INTEGER_8) last->it_uint8; break;

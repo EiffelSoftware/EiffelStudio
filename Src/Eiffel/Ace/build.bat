52a46e7f41c1b4361af5bed446ae013c1ad8f1ff
Emmanuel Stapf <manus@eiffel.com>
2006-05-27 17:40:07 +0000

    Prevent change of directory.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@59242 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/Eiffel/Ace/build.bat
+++ Src/Eiffel/Ace/build.bat
@@ -5,18 +5,16 @@ echo off
 if .%1. == .. goto usage
 if .%2. == .. goto usage
 
-cdd %1
-rd /q /s EIFGENs\bench
+
+rd /q /s %1\EIFGENs\bench
 
 %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe -finalize -local -project_path %1 -config %EIFFEL_SRC%\Eiffel\Ace\ec.ecf -target bench -batch -c_compile
 
-cdd %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin
-if not exist ec.exe del old_ec.exe
-ren ec.exe old_ec.exe
+if not exist %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe del %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\old_ec.exe
+ren %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\old_ec.exe
 copy %1\EIFGENs\bench\F_code\ec.exe .
 
-cdd %2
-rd /q /s EIFGENs\bench
+rd /q /s %2\EIFGENs\bench
 
 %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe -freeze -local -project_path %2 -config %EIFFEL_SRC%\Eiffel\Ace\ec.ecf -target bench -batch -c_compile
 

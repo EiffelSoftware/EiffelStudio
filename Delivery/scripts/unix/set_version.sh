2d6d071ec4a48c7c95e7149ea41caa9201d815c6
Emmanuel Stapf <manus@eiffel.com>
2006-06-28 22:34:18 +0000

    Changed name of `svn_version' variable.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@60731 8089f293-4706-0410-a29e-feb5c42a2edf

--- Delivery/scripts/unix/set_version.sh
+++ Delivery/scripts/unix/set_version.sh
@@ -5,10 +5,10 @@
 # $2 = pattern to transform
 # $3 = file to process if any, otherwise display version on output
 
-svn_revision=`svn info $1 | grep "Last Changed Rev" | sed -e "s/Last Changed Rev: //"`
+computed_svn_revision=`svn info $1 | grep "Last Changed Rev" | sed -e "s/Last Changed Rev: //"`
 if [[ -z "$2" || -z "$3" ]]; then
-	echo $svn_revision
+	echo $computed_svn_revision
 else
-	sed -e "s/$2/$svn_revision/" $3 > tmp
+	sed -e "s/$2/$computed_svn_revision/" $3 > tmp
 	mv tmp $3
 fi

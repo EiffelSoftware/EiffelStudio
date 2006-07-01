795c9b82c566769a32d782f25be5266008908d7f
Emmanuel Stapf <manus@eiffel.com>
2006-07-01 15:54:25 +0000

    Store version number as part of the MSI file.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@60904 8089f293-4706-0410-a29e-feb5c42a2edf

--- Delivery/scripts/windows/set_version.sh
+++ Delivery/scripts/windows/set_version.sh
@@ -3,8 +3,12 @@
 # set version number in $3 to the latest SVN repository version number.
 # $1 = SVN repository
 # $2 = pattern to transform
-# $3 = file to process
+# $3 = file to process if any, otherwise display version on output
 
-svn_revision=`svn info $1 | grep "Last Changed Rev" | sed -e "s/Last Changed Rev: //"`
-sed -e "s/$2/$svn_revision/" $3 > tmp
-mv tmp $3
+computed_svn_revision=`svn info $1 | grep "Last Changed Rev" | sed -e "s/Last Changed Rev: //"`
+if [[ -z "$2" || -z "$3" ]]; then
+	echo $computed_svn_revision
+else
+	sed -e "s/$2/$computed_svn_revision/" $3 > tmp
+	mv tmp $3
+fi

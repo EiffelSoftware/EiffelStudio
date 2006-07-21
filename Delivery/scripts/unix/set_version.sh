fb99962479e8e1aa99ae23d408d2758684e2fd5d
Emmanuel Stapf <manus@eiffel.com>
2006-07-21 18:37:35 +0000

    Updated test to work with `sh'.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@61547 8089f293-4706-0410-a29e-feb5c42a2edf

--- Delivery/scripts/unix/set_version.sh
+++ Delivery/scripts/unix/set_version.sh
@@ -6,7 +6,7 @@
 # $3 = file to process if any, otherwise display version on output
 
 computed_svn_revision=`svn info $1 | grep "Last Changed Rev" | sed -e "s/Last Changed Rev: //"`
-if [[ -z "$2" || -z "$3" ]]; then
+if [ "$2" = "" -o "$3" = "" ]; then
 	echo $computed_svn_revision
 else
 	sed -e "s/$2/$computed_svn_revision/" $3 > tmp

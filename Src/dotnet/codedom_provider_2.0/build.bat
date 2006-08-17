7a08bd38fcb3c2477f1128de1bcce7223f2c2a00
raphaels <raphaels@eiffel.com>
2006-08-17 22:33:56 +0000

    Now change title of DOS prompt to indicate what is currently being compiled.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@62708 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/dotnet/codedom_provider_2.0/build.bat
+++ Src/dotnet/codedom_provider_2.0/build.bat
@@ -20,6 +20,7 @@ RD /Q /S configuration\EIFGENS
 :BASE
 ECHO -
 ECHO COMPILING CodeDom.Base
+TITLE COMPILING CodeDom.Base
 ECHO -
 ec -config configuration\base.ecf -target base -precompile %EFLAGS%
 IF EXIST configuration\EIFGENS\base\F_code\EiffelSoftware.CodeDom.Base2_0.dll GOTO CACHE
@@ -29,6 +30,7 @@ GOTO END
 :CACHE
 ECHO -
 ECHO COMPILING Eiffel Cache Browser
+TITLE COMPILING Eiffel Cache Browser
 ECHO -
 ec -config configuration\cache_browser.ecf -target cache_browser -precompile %EFLAGS%
 IF EXIST configuration\EIFGENS\cache_browser\F_code\EiffelSoftware.CodeDom.CacheBrowser2_0.dll GOTO PROVIDER
@@ -38,6 +40,7 @@ GOTO END
 :PROVIDER
 ECHO -
 ECHO COMPILING Eiffel CodeDom Provider
+TITLE COMPILING Eiffel CodeDom Provider
 ECHO -
 ec -config configuration\codedom_provider.ecf -target codedom_provider_%TARGET% %EFLAGS%
 IF EXIST configuration\EIFGENs\codedom_provider_%TARGET%\F_code\EiffelSoftware.CodeDom2_0.dll GOTO VISION2
@@ -47,6 +50,7 @@ GOTO END
 :VISION2
 ECHO -
 ECHO COMPILING CodeDom.Vision2
+TITLE COMPILING CodeDom.Vision2
 ECHO -
 ec -config configuration\vision2.ecf -target vision2 -precompile %EFLAGS%
 IF EXIST configuration\EIFGENS\vision2\F_code\EiffelSoftware.CodeDom.Vision2_0.dll GOTO MANAGER
@@ -57,6 +61,7 @@ GOTO END
 :MANAGER
 ECHO -
 ECHO COMPILING Eiffel CodeDom Manager
+TITLE COMPILING Eiffel CodeDom Manager
 ECHO -
 REM FIXME DOESNT WORK YET COPY tools\manager\ecdpman.ico .
 REM FIXME DOESNT WORK YET COPY tools\manager\ecdpman.rc .
@@ -68,6 +73,7 @@ GOTO END
 :SPLITTER
 ECHO -
 ECHO COMPILING eSplitter
+TITLE COMPILING eSplitter
 ECHO -
 ec -config configuration\esplit.ecf -target esplit_graphical_%TARGET% %EFLAGS%
 IF EXIST configuration\EIFGENs\esplit_graphical_%TARGET%\F_code\esplitter2_0.exe GOTO SPLIT
@@ -77,6 +83,7 @@ GOTO END
 :SPLIT
 ECHO -
 ECHO COMPILING eSplit
+TITLE COMPILING eSplit
 ECHO -
 ec -config configuration\esplit.ecf -target esplit_console_%TARGET% %EFLAGS%
 IF EXIST configuration\EIFGENs\esplit_console_%TARGET%\F_code\esplit2_0.exe GOTO NMAP
@@ -86,6 +93,7 @@ GOTO END
 :NMAP
 ECHO -
 ECHO COMPILING Name Mapper
+TITLE COMPILING Name Mapper
 ECHO -
 ec -config configuration\nmap.ecf -target nmap_%TARGET% %EFLAGS%
 IF EXIST configuration\EIFGENs\nmap_%TARGET%\F_code\nmap2_0.exe GOTO END
@@ -93,4 +101,5 @@ ECHO Compilation failed !! (no nmap.exe was generated)
 GOTO END
 
 :END
-ECHO Done compiling EiffelStudio Eiffel CodeDom Provider projects.
+ECHO Done compiling Eiffel Codedom Provider projects.
+TITLE Done compiling Eiffel Codedom Provider projects.

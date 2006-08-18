f3429d24bcdb60345d2c64987d3025cb0c633afc
raphaels <raphaels@eiffel.com>
2006-08-18 19:16:38 +0000

    Updated to work with latest repository and compiler. Now changes DOS prompt title to indicate current status.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@62766 8089f293-4706-0410-a29e-feb5c42a2edf

--- Delivery/codedom_provider/scripts/build_codedom.bat
+++ Delivery/codedom_provider/scripts/build_codedom.bat
@@ -4,28 +4,30 @@ REM Eiffel Codedom Provider delivery build script
 REM *********************************************
 
 ECHO Building Codedom files
+TITLE Building Codedom files
 
 SET EIFFEL_SRC=%CD%\checkout
 SET ISE_EIFFEL=%1
 SET PATH=%INIT_PATH%;C:\ecdpc
 
-CD checkout\dotnet\codedom_provider
-REM in checkout\dotnet\codedom_provider
-sed -e "s/ec /ecdpc /" build_studio.bat > new_build_studio.bat
-IF EXIST old_build_studio.bat DEL old_build_studio.bat
-REN build_studio.bat old_build_studio.bat
-REN new_build_studio.bat build_studio.bat
-CALL build_studio.bat /release
-IF NOT EXIST build_studio GOTO END
-IF NOT EXIST build_studio\EiffelSoftware.Codedom\EIFGEN\F_code\EiffelSoftware.Codedom.dll GOTO END
-IF NOT EXIST build_studio\EiffelSoftware.CodeDom.CacheBrowser\EIFGEN\F_code\EiffelSoftware.CodeDom.CacheBrowser.dll GOTO END
-IF NOT EXIST build_studio\EiffelSoftware.CodeDom.Base\EIFGEN\F_code\EiffelSoftware.CodeDom.Base.dll GOTO END
-IF NOT EXIST build_studio\EiffelSoftware.CodeDom.Vision2\EIFGEN\F_code\EiffelSoftware.CodeDom.Vision2.dll GOTO END
-IF NOT EXIST build_studio\ecdpman\EIFGEN\F_code\ecdpman.exe GOTO END
-IF NOT EXIST build_studio\esplitter\EIFGEN\F_code\esplitter.exe GOTO END
-IF NOT EXIST build_studio\esplit\EIFGEN\F_code\esplit.exe GOTO END
+CD checkout\dotnet\codedom_provider_2.0
+REM in checkout\dotnet\codedom_provider_2.0
+sed -e "s/ec /ecdpc /" build.bat > new_build.bat
+IF EXIST old_build.bat DEL old_build.bat
+REN build.bat old_build.bat
+REN new_build.bat build.bat
+CALL build.bat release
+IF NOT EXIST configuration\EIFGENS\base\F_code\EiffelSoftware.CodeDom.Base2_0.dll GOTO END
+IF NOT EXIST configuration\EIFGENS\cache_browser\F_code\EiffelSoftware.CodeDom.CacheBrowser2_0.dll GOTO END
+IF NOT EXIST configuration\EIFGENs\codedom_provider_release\F_code\EiffelSoftware.CodeDom2_0.dll GOTO END
+IF NOT EXIST configuration\EIFGENS\vision2\F_code\EiffelSoftware.CodeDom.Vision2_0.dll GOTO END
+IF NOT EXIST configuration\EIFGENs\manager_release\F_code\ecdpman2_0.exe GOTO END
+IF NOT EXIST configuration\EIFGENs\esplit_graphical_release\F_code\esplitter2_0.exe GOTO END
+IF NOT EXIST configuration\EIFGENs\esplit_console_release\F_code\esplit2_0.exe GOTO END
+IF NOT EXIST configuration\EIFGENs\nmap_release\F_code\nmap2_0.exe GOTO END
 
 ECHO Copying files
+TITLE Copying files
 CD ..\..\..\delivery
 REM in delivery
 MKDIR codedom
@@ -36,16 +38,16 @@ MKDIR configs
 COPY ..\..\files\default.ecd configs\
 
 MKDIR bin
-COPY ..\..\checkout\dotnet\codedom_provider\build_studio\EiffelSoftware.CodeDom.CacheBrowser\EIFGEN\F_code\EiffelSoftware.CodeDom.CacheBrowser.dll bin\
-COPY ..\..\checkout\dotnet\codedom_provider\build_studio\EiffelSoftware.CodeDom\EIFGEN\F_code\EiffelSoftware.Codedom.dll bin\
-COPY ..\..\checkout\dotnet\codedom_provider\build_studio\EiffelSoftware.CodeDom.Base\EIFGEN\F_code\EiffelSoftware.CodeDom.Base.dll bin\
-COPY ..\..\checkout\dotnet\codedom_provider\build_studio\EiffelSoftware.CodeDom.Base\EIFGEN\F_code\libEiffelSoftware.CodeDom.Base.dll bin\
-COPY ..\..\checkout\dotnet\codedom_provider\build_studio\EiffelSoftware.CodeDom.Vision2\EIFGEN\F_code\EiffelSoftware.CodeDom.Vision2.dll bin\
-COPY ..\..\checkout\dotnet\codedom_provider\build_studio\EiffelSoftware.CodeDom.Vision2\EIFGEN\F_code\libEiffelSoftware.CodeDom.Vision2.dll bin\
-COPY ..\..\checkout\dotnet\codedom_provider\build_studio\ecdpman\EIFGEN\F_code\ecdpman.exe bin\
-COPY ..\..\checkout\dotnet\codedom_provider\build_studio\esplitter\EIFGEN\F_code\esplitter.exe bin\
-COPY ..\..\checkout\dotnet\codedom_provider\build_studio\esplit\EIFGEN\F_code\esplit.exe bin\
-COPY ..\..\checkout\dotnet\codedom_provider\build_studio\nmap\EIFGEN\F_code\nmap.exe bin\
+COPY ..\..\checkout\dotnet\codedom_provider_2.0\configuration\EIFGENs\cache_browser\F_code\EiffelSoftware.CodeDom.CacheBrowser2_0.dll bin\
+COPY ..\..\checkout\dotnet\codedom_provider_2.0\configuration\EIFGENs\codedom_provider_release\F_code\EiffelSoftware.Codedom2_0.dll bin\
+COPY ..\..\checkout\dotnet\codedom_provider_2.0\configuration\EIFGENs\base\F_code\EiffelSoftware.CodeDom.Base2_0.dll bin\
+COPY ..\..\checkout\dotnet\codedom_provider_2.0\configuration\EIFGENs\base\F_code\libEiffelSoftware.CodeDom.Base2_0.dll bin\
+COPY ..\..\checkout\dotnet\codedom_provider_2.0\configuration\EIFGENs\vision2\F_code\EiffelSoftware.CodeDom.Vision2_0.dll bin\
+COPY ..\..\checkout\dotnet\codedom_provider_2.0\configuration\EIFGENs\vision2\F_code\libEiffelSoftware.CodeDom.Vision2_0.dll bin\
+COPY ..\..\checkout\dotnet\codedom_provider_2.0\configuration\EIFGENs\manager_release\EIFGEN\F_code\ecdpman2_0.exe bin\
+COPY ..\..\checkout\dotnet\codedom_provider_2.0\configuration\EIFGENs\esplit_graphical_release\F_code\esplitter.exe2_0 bin\
+COPY ..\..\checkout\dotnet\codedom_provider_2.0\configuration\EIFGENs\esplit_console_release\F_code\esplit2_0.exe bin\
+COPY ..\..\checkout\dotnet\codedom_provider_2.0\configuration\EIFGENs\nmap_release\F_code\nmap2_0.exe bin\
 COPY ..\..\checkout\Eiffel\eiffel\com_il_generation\Core\run-time\EiffelSoftware.Runtime.dll bin\
 
 CD bin
@@ -62,3 +64,4 @@ SET PATH=%INIT_PATH%;
 :END
 IF "%CODEDOM_BUILT%"=="" ECHO COULD NOT BUILD CODEDOM FILES!!
 ECHO Done.
+TITLE Done.
--- Delivery/codedom_provider/scripts/build_docs.bat
+++ Delivery/codedom_provider/scripts/build_docs.bat
@@ -4,6 +4,7 @@ REM Eiffel Codedom Provider documentation build script
 REM **************************************************
 
 ECHO Building Documentation
+TITLE Building Documentation
 
 IF EXIST "delivery\Documentation\Eiffel for ASP.NET.chm" GOTO END
 IF NOT EXIST delivery\Documentation MKDIR delivery\Documentation
@@ -17,14 +18,15 @@ CD tools\doc_builder
 REM in checkout\tools\doc_builder
 
 ECHO Registering initial compiler's metadata consumer
+TITLE Registering initial compiler's metadata consumer
 regasm %ISE_EIFFEL%\studio\spec\windows\bin\EiffelSoftware.MetadataConsumer.dll
 IF EXIST %SystemRoot%\system32\libEiffelSoftware.MetadataConsumer.dll.bak COPY /Y %SystemRoot%\system32\libEiffelSoftware.MetadataConsumer.dll.bak %SystemRoot%\system32\libEiffelSoftware.MetadataConsumer.dll
 IF EXIST EIFGEN RD /Q /S EIFGEN
 IF EXIST *.epr DEL *.epr
-ec -finalize -batch -c_compile -ace docbuilder.mswin.ace
-IF NOT EXIST EIFGEN\F_code\doc_builder.exe ECHO ERROR: Documentation builder could not be built!
-IF NOT EXIST EIFGEN\F_code\doc_builder.exe GOTO END
-COPY EIFGEN\F_Code\doc_builder.exe ..\..\docs\
+ec -finalize -batch -c_compile -config docbuilder.ecf -target doc_builder
+IF NOT EXIST EIFGENs\doc_builder\F_code\doc_builder.exe ECHO ERROR: Documentation builder could not be built!
+IF NOT EXIST EIFGENs\doc_builder\F_code\doc_builder.exe GOTO END
+COPY EIFGENs\doc_builder\F_Code\doc_builder.exe ..\..\docs\
 
 CD ..\..\
 REM in checkout
@@ -49,3 +51,4 @@ SET DOCS_BUILT=1
 SET PATH=%INIT_PATH%
 CD %ECPOriginalPath%
 ECHO Done building documentation.
+TITLE Done building documentation.

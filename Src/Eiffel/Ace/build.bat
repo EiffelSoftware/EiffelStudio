de1d3e019eaeef4bec287fbcd3e740de3a01658f
patrickr <patrickr@eiffel.com>
2006-10-23 23:53:48 +0000

    removed -local switch as this is the default
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@64531 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/Eiffel/Ace/build.bat
+++ Src/Eiffel/Ace/build.bat
@@ -8,7 +8,7 @@ if .%2. == .. goto usage
 
 rd /q /s %1\EIFGENs\bench
 
-%ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe -finalize -local -project_path %1 -config %EIFFEL_SRC%\Eiffel\Ace\ec.ecf -target bench -batch -c_compile
+%ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe -finalize -project_path %1 -config %EIFFEL_SRC%\Eiffel\Ace\ec.ecf -target bench -batch -c_compile
 
 if not exist %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe del %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\old_ec.exe
 ren %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\old_ec.exe
@@ -16,7 +16,7 @@ copy %1\EIFGENs\bench\F_code\ec.exe %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\
 
 rd /q /s %2\EIFGENs\bench
 
-%ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe -freeze -local -project_path %2 -config %EIFFEL_SRC%\Eiffel\Ace\ec.ecf -target bench -batch -c_compile
+%ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe -freeze -project_path %2 -config %EIFFEL_SRC%\Eiffel\Ace\ec.ecf -target bench -batch -c_compile
 
 goto end
 

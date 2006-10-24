e0f3c9fb0762572bf45c16e5d0ad4a3ca2415bbc
paulb <paulb@eiffel.com>
2006-10-24 23:37:31 +0000

    Corrected registration.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@64563 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/dotnet/codedom_provider_2.0/register_all.bat
+++ Src/dotnet/codedom_provider_2.0/register_all.bat
@@ -1,59 +1,51 @@
 @ECHO OFF
 
-:START
-CD bin
+CD configuration
 
-:BASE
 ECHO Registering EiffelSoftware.CodeDom.Base.dll
-IF EXIST Base\EIFGENs\base\F_code\EiffelSoftware.CodeDom.Base.dll (
-	gacutil -if Base\EIFGENs\base\F_code\EiffelSoftware.CodeDom.Base.dll /nologo
-	GOTO LIBBASE
+IF EXIST EIFGENs\base\F_code\EiffelSoftware.CodeDom.Base2_0.dll (
+	gacutil -if EIFGENs\base\F_code\EiffelSoftware.CodeDom.Base2_0.dll /nologo
+) ELSE (
+	ECHO Could not find EiffelSoftware.CodeDom.Base.dll
 )
-ECHO Could not find EiffelSoftware.CodeDom.Base.dll
 
-:LIBBASE
 ECHO Registering libEiffelSoftware.CodeDom.Base.dll
-IF EXIST Base\EIFGENs\base\F_code\libEiffelSoftware.CodeDom.Base.dll (
-	COPY /Y Base\EIFGENs\base\F_code\libEiffelSoftware.CodeDom.Base.dll %WINDIR%\System32
-	IF EXIST %WINDIR%\SysWow64 COPY /Y Base\EIFGENs\base\F_code\libEiffelSoftware.CodeDom.Base.dll %WINDIR%\SysWow64
-	GOTO VISION2
+IF EXIST EIFGENs\base\F_code\libEiffelSoftware.CodeDom.Base2_0.dll (
+	COPY /Y EIFGENs\base\F_code\libEiffelSoftware.CodeDom.Base2_0.dll %WINDIR%\System32
+	IF EXIST %WINDIR%\SysWow64 COPY /Y EIFGENs\base\F_code\libEiffelSoftware.CodeDom.Base2_0.dll %WINDIR%\SysWow64
+) ELSE (
+	ECHO Could not find libEiffelSoftware.CodeDom.Base.dll
 )
-ECHO Could not find libEiffelSoftware.CodeDom.Base.dll
 
-:VISION2
-ECHO Registering EiffelSoftware.CodeDom.Vision2.dll
-IF EXIST Vision2\EIFGENs\vision2\F_code\EiffelSoftware.CodeDom.Vision2.dll (
-	gacutil -if Vision2\EIFGENs\vision2\F_code\EiffelSoftware.CodeDom.Vision2.dll /nologo
-	GOTO LIBVISION2
-)
-ECHO Could not find EiffelSoftware.CodeDom.Vision2.dll
+REM ECHO Registering EiffelSoftware.CodeDom.Vision2.dll
+REM IF EXIST EIFGENs\vision2\F_code\EiffelSoftware.CodeDom.Vision2_0.dll (
+	REM gacutil -if EIFGENs\vision2\F_code\EiffelSoftware.CodeDom.Vision2_0.dll /nologo
+REM ) ELSE (
+	REM ECHO Could not find EiffelSoftware.CodeDom.Vision2.dll
+REM )
+REM 
+REM ECHO Registering libEiffelSoftware.CodeDom.Vision2.dll
+REM IF EXIST EIFGENs\vision2\F_code\libEiffelSoftware.CodeDom.Vision2_0.dll (
+	REM COPY /Y EIFGENs\vision2\F_code\libEiffelSoftware.CodeDom.Vision2_0.dll %WINDIR%\System32
+	REM IF EXIST %WINDIR%\SysWow64 COPY /Y EIFGENs\vision2\F_code\libEiffelSoftware.CodeDom.Vision2_0.dll %WINDIR%\SysWow64
+REM ) ELSE (
+	REM ECHO Could not find EiffelSoftware.CodeDom.Vision2\EIFGEN\F_code\libEiffelSoftware.CodeDom.Vision2.dll
+REM )
 
-:LIBVISION2
-ECHO Registering libEiffelSoftware.CodeDom.Vision2.dll
-IF EXIST Vision2\EIFGENs\vision2\F_code\libEiffelSoftware.CodeDom.Vision2.dll (
-	COPY /Y Vision2\EIFGENs\vision2\F_code\libEiffelSoftware.CodeDom.Vision2.dll %WINDIR%\System32
-	IF EXIST %WINDIR%\SysWow64 COPY /Y Vision2\EIFGENs\vision2\F_code\libEiffelSoftware.CodeDom.Vision2.dll %WINDIR%\SysWow64
-	GOTO CACHE
-)
-ECHO Could not find EiffelSoftware.CodeDom.Vision2\EIFGEN\F_code\libEiffelSoftware.CodeDom.Vision2.dll
-
-:CACHE
 ECHO Registering EiffelSoftware.CodeDom.CacheBrowser.dll
-IF EXIST cache_browser\EIFGENs\cache_browser\F_code\EiffelSoftware.CodeDom.CacheBrowser.dll (
-	gacutil -if cache_browser\EIFGENs\cache_browser\F_code\EiffelSoftware.CodeDom.CacheBrowser.dll /nologo
-	GOTO CODEDOM
-)
+IF EXIST EIFGENs\cache_browser\F_code\EiffelSoftware.CodeDom.CacheBrowser2_0.dll (
+	gacutil -if EIFGENs\cache_browser\F_code\EiffelSoftware.CodeDom.CacheBrowser2_0.dll /nologo
+) ELSE (
 ECHO Could not find EiffelSoftware.CodeDom.CacheBrowser.dll
-
-:CODEDOM
-ECHO Registering EiffelSoftware.CodeDom.dll
-IF EXIST codedom_provider\EIFGENs\debug\F_code\EiffelSoftware.CodeDom.dll (
-	gacutil -if codedom_provider\EIFGENs\debug\F_code\EiffelSoftware.CodeDom.dll /nologo
-	installutil codedom_provider\EIFGENs\debug\F_code\EiffelSoftware.CodeDom.dll
-	GOTO END
 )
-ECHO Could not find EiffelSoftware.CodeDom.dll
 
-:END
+ECHO Registering EiffelSoftware.CodeDom.dll
+IF EXIST EIFGENs\codedom_provider_debug\F_code\EiffelSoftware.CodeDom2_0.dll (
+	gacutil -if EIFGENs\codedom_provider_debug\F_code\EiffelSoftware.CodeDom2_0.dll /nologo
+	installutil EIFGENs\codedom_provider_debug\F_code\EiffelSoftware.CodeDom2_0.dll
+) ELSE (
+	ECHO Could not find EiffelSoftware.CodeDom.dll
+)
+
 CD ..
 ECHO Done.

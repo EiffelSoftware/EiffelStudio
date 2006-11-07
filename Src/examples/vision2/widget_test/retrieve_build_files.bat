e6e708cd4b97f9bbee65beb811953fe8c0077869
patrickr <patrickr@eiffel.com>
2006-11-07 16:20:14 +0000

    Changed vision2_tour into vision2_demo, changed build into esbuilder.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@64842 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/examples/vision2/widget_test/retrieve_build_files.bat
+++ Src/examples/vision2/widget_test/retrieve_build_files.bat
@@ -4,10 +4,10 @@ REM Check out Build source.
 svn co %1/Src/build Src\build
 
 REM Check out bitmaps from Build delivery
-svn co %1/Delivery/build/bitmaps Delivery\build\bitmaps
+svn co %1/Delivery/build/bitmaps Delivery\esbuilder\bitmaps
 
 REM Check out files from vision2_tour
-svn co %1/Delivery/vision2_tour Delivery/vision2_tour
+svn co %1/Delivery/vision2_tour Delivery/vision2_demo
 
 GOTO files_checked_out
 
@@ -17,14 +17,14 @@ REM Check out Build source.
 svn co https://eiffelsoftware.origo.ethz.ch/svn/es/trunk/Src/build Src\build
 
 REM Check out bitmaps from Build delivery
-svn co https://eiffelsoftware.origo.ethz.ch/svn/es/trunk/Delivery/build/bitmaps Delivery\build\bitmaps
+svn co https://eiffelsoftware.origo.ethz.ch/svn/es/trunk/Delivery/build/bitmaps Delivery\esbuilder\bitmaps
 
 REM Check out files from vision2_tour
-svn co https://eiffelsoftware.origo.ethz.ch/svn/es/trunk/Delivery/vision2_tour Delivery\vision2_tour
+svn co https://eiffelsoftware.origo.ethz.ch/svn/es/trunk/Delivery/vision2_tour Delivery\vision2_demo
 
 :files_checked_out
 REM Copy template files
-XCOPY /Y /E /I Delivery\vision2_tour\templates .\templates
+XCOPY /Y /E /I Delivery\vision2_demo\templates .\templates
 
 REM Copy constants and eiffel environment to interface.
 XCOPY /Y Src\build\constants\gb_constants.e .\interface
@@ -36,16 +36,16 @@ XCOPY /Y Src\build\utilities\gb_color_stone.e .\interface
 XCOPY /Y Src\build\utilities\object_default_state_checker.e .\interface
 
 REM Copy icons for different widget types across. The /E option moves the whole directory structure.
-XCOPY /Y /E /I Delivery\build\bitmaps .\bitmaps
+XCOPY /Y /E /I Delivery\esbuilder\bitmaps .\bitmaps
 
 REM Copy icons for standard buttons.
-XCOPY /Y Delivery\vision2_tour\bitmaps\ico\documentation.ico .\bitmaps\ico\
-XCOPY /Y Delivery\vision2_tour\bitmaps\ico\testing.ico .\bitmaps\ico\
-XCOPY /Y Delivery\vision2_tour\bitmaps\ico\properties.ico .\bitmaps\ico\
-XCOPY /Y Delivery\vision2_tour\bitmaps\ico\size_down.ico .\bitmaps\ico\
-XCOPY /Y Delivery\vision2_tour\bitmaps\ico\size_up.ico .\bitmaps\ico\
-XCOPY /Y Delivery\vision2_tour\bitmaps\png\image1.png .\bitmaps\png\
-XCOPY /Y Delivery\vision2_tour\bitmaps\png\image2.png .\bitmaps\png\
+XCOPY /Y Delivery\vision2_demo\bitmaps\ico\documentation.ico .\bitmaps\ico\
+XCOPY /Y Delivery\vision2_demo\bitmaps\ico\testing.ico .\bitmaps\ico\
+XCOPY /Y Delivery\vision2_demo\bitmaps\ico\properties.ico .\bitmaps\ico\
+XCOPY /Y Delivery\vision2_demo\bitmaps\ico\size_down.ico .\bitmaps\ico\
+XCOPY /Y Delivery\vision2_demo\bitmaps\ico\size_up.ico .\bitmaps\ico\
+XCOPY /Y Delivery\vision2_demo\bitmaps\png\image1.png .\bitmaps\png\
+XCOPY /Y Delivery\vision2_demo\bitmaps\png\image2.png .\bitmaps\png\
 
 
 REM Copy all the editor constructor classes into interface.

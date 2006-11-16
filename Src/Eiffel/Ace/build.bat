0ef2db11b984239a88eb3f006b8e4cf254e088b6
Emmanuel Stapf <manus@eiffel.com>
2006-11-16 21:36:17 +0000

    Updated script to take into account the presence of 2 deliveries, one to perform   the initial bootstrap stage, and the second for the bootstrapped compiler.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@65134 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/Eiffel/Ace/build.bat
+++ Src/Eiffel/Ace/build.bat
@@ -5,18 +5,34 @@ echo off
 if .%1. == .. goto usage
 if .%2. == .. goto usage
 
+set OLD_ISE_EIFFEL=%ISE_EIFFEL%
+set NEW_ISE_EIFFEL=%ISE_EIFFEL%_wkbench
+set TMP_PATH=%cd%
 
 rd /q /s %1\EIFGENs\bench
 
-%ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe -finalize -project_path %1 -config %EIFFEL_SRC%\Eiffel\Ace\ec.ecf -target bench -batch -c_compile
+%OLD_ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe -finalize -project_path %1 -config %EIFFEL_SRC%\Eiffel\Ace\ec.ecf -target bench -batch -c_compile
 
-if not exist %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe del %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\old_ec.exe
-ren %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\old_ec.exe
-copy %1\EIFGENs\bench\F_code\ec.exe %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe
+
+rem Use new delivery to compile the workbench compiler
+
+set ISE_EIFFEL=%NEW_ISE_EIFFEL%
+
+if not exist %NEW_ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe del %NEW_ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\old_ec.exe
+ren %NEW_ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe %NEW_ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\old_ec.exe
+copy %1\EIFGENs\bench\F_code\ec.exe %NEW_ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe
 
 rd /q /s %2\EIFGENs\bench
 
-%ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe -freeze -project_path %2 -config %EIFFEL_SRC%\Eiffel\Ace\ec.ecf -target bench -batch -c_compile
+%NEW_ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe -freeze -project_path %2 -config %EIFFEL_SRC%\Eiffel\Ace\ec.ecf -target bench -batch -c_compile
+
+cd /d %ISE_EIFFEL
+cd ..
+ren %OLD_ISE_EIFFEL% %ISE_EIFFEL%_backup
+ren %NEW_ISE_EIFFEL% %ISE_EIFFEL%
+ren %ISE_EIFFEL%_backup %NEW_ISE_EIFFEL%
+
+cd /d %TMP_PATH%
 
 goto end
 

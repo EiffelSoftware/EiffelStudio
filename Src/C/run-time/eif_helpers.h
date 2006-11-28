258c7774058f62b2e498db424594b0a9e7622641
juliant <juliant@eiffel.com>
2006-11-28 17:15:07 +0000

    added macros and functions related to floating point infinity and NaN
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@65261 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C/run-time/eif_helpers.h
+++ Src/C/run-time/eif_helpers.h
@@ -65,6 +65,12 @@ rt_private EIF_REAL_64 eif_uint64_to_real64 (EIF_NATURAL_64 v) {
 #endif
 }
 
+/* NaN test macros */
+#define EIF_IS_NAN_REAL_32(x) ( ((*((EIF_NATURAL_32 *)(&(x))) & 0x7F800000)==0x7F800000) && \
+	                             (*((EIF_NATURAL_32 *)(&(x))) & 0x007FFFFF) )
+#define EIF_IS_NAN_REAL_64(x) ( ((*((EIF_NATURAL_64 *)(&(x))) & 0x7FF0000000000000)==0x7FF0000000000000) && \
+	                             (*((EIF_NATURAL_64 *)(&(x))) & 0x000FFFFFFFFFFFFF) )
+
 /* Absolute value computation */
 rt_private EIF_INTEGER_8 eif_abs_int8 (EIF_INTEGER_8 i) {
 	return (i > 0 ? i : -i);
@@ -199,7 +205,63 @@ rt_private EIF_INTEGER_32 eif_twc_real64 (EIF_REAL_64 i, EIF_REAL_64 j) {
 	return (i < j ? -1 : (j < i) ? 1 : 0);
 }
 
+/* INF and NaN tests */
+rt_private EIF_BOOLEAN eif_is_inf_real_32 (EIF_REAL_32 x) {
+	return ( ((*((EIF_NATURAL_32 *)(&(x))) & 0x7F800000)==0x7F800000) &&
+             ((*((EIF_NATURAL_32 *)(&(x))) & 0x007FFFFF)==0x00000000) );
+}
+rt_private EIF_BOOLEAN eif_is_inf_real_64 (EIF_REAL_64 x) {
+	return ( ((*((EIF_NATURAL_64 *)(&(x))) & 0x7FF0000000000000)==0x7FF0000000000000) &&
+             ((*((EIF_NATURAL_64 *)(&(x))) & 0x000FFFFFFFFFFFFF)==0x0000000000000000) );
+}
+rt_private EIF_BOOLEAN eif_is_nan_real_32 (EIF_REAL_32 x) {
+	return EIF_IS_NAN_REAL_32 (x);
+}
+rt_private EIF_BOOLEAN eif_is_nan_real_64 (EIF_REAL_64 x) {
+	return EIF_IS_NAN_REAL_64 (x);
+}
+rt_private EIF_BOOLEAN eif_is_quiet_nan_real_32 (EIF_REAL_32 x) {
+	return ( ((*((EIF_NATURAL_32 *)(&(x))) & 0x7FC00000)==0x7FC00000) &&
+              (*((EIF_NATURAL_32 *)(&(x))) & 0x007FFFFF) );
+}
+rt_private EIF_BOOLEAN eif_is_quiet_nan_real_64 (EIF_REAL_64 x) {
+	return ( ((*((EIF_NATURAL_64 *)(&(x))) & 0x7FF8000000000000)==0x7FF8000000000000) &&
+              (*((EIF_NATURAL_64 *)(&(x))) & 0x000FFFFFFFFFFFFF) );
+}
+rt_private EIF_BOOLEAN eif_is_signaling_nan_real_32 (EIF_REAL_32 x) {
+	return ( ((*((EIF_NATURAL_32 *)(&(x))) & 0x7FC00000)==0x7F800000) &&
+	          (*((EIF_NATURAL_32 *)(&(x))) & 0x007FFFFF) );
+}
+rt_private EIF_BOOLEAN eif_is_signaling_nan_real_64 (EIF_REAL_64 x) {
+	return ( ((*((EIF_NATURAL_64 *)(&(x))) & 0x7FF8000000000000)==0x7FF0000000000000) &&
+              (*((EIF_NATURAL_64 *)(&(x))) & 0x000FFFFFFFFFFFFF) );
+}
 
+/* Floating point is_equal computation */
+rt_private EIF_BOOLEAN eif_fpeq_real_32 (EIF_REAL_32 i, EIF_REAL_32 j) {
+	return (EIF_IS_NAN_REAL_32(i) ? EIF_IS_NAN_REAL_32(j) : i == j);
+}
+rt_private EIF_BOOLEAN eif_fpeq_real_64 (EIF_REAL_64 i, EIF_REAL_64 j) {
+	return (EIF_IS_NAN_REAL_64(i) ? EIF_IS_NAN_REAL_64(j) : i == j);
+}
+
+/* NaN constants */
+rt_private EIF_REAL_32 eif_signaling_nan_real_32 () {
+	EIF_NATURAL_32 s_nan = 0x7FA00000;
+	return * ((EIF_REAL_32 *) &s_nan);
+}
+rt_private EIF_REAL_64 eif_signaling_nan_real_64 () {
+	EIF_NATURAL_64 s_nan = 0x7FF4000000000000;
+	return * ((EIF_REAL_64 *) &s_nan);
+}
+rt_private EIF_REAL_32 eif_quiet_nan_real_32 () {
+	EIF_NATURAL_32 q_nan = 0x7FC00000;
+	return * ((EIF_REAL_32 *) &q_nan);
+}
+rt_private EIF_REAL_64 eif_quiet_nan_real_64 () {
+	EIF_NATURAL_64 q_nan = 0x7FF8000000000000;
+	return * ((EIF_REAL_64 *) &q_nan);
+}
 
 #ifdef __cplusplus
 }

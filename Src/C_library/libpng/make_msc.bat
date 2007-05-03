7fe9388735f415a31c52299cd7f2d55acf56cf93
Emmanuel Stapf <manus@eiffel.com>
2007-05-03 22:54:11 +0000

    Fixed incorrect creation of spec directory if not present.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@68098 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C_library/libpng/make_msc.bat
+++ Src/C_library/libpng/make_msc.bat
@@ -1,6 +1,6 @@
 nmake -f scripts/makefile.vcwin32
 if not exist ..\..\library\vision2\spec\msc mkdir ..\..\library\vision2\spec\msc
-if not exist ..\..\library\vision2\spec\msc\%ISE_PLATFORM% mkdir ..\..\library\vision2\spec\%ISE_PLATFORM%
+if not exist ..\..\library\vision2\spec\msc\%ISE_PLATFORM% mkdir ..\..\library\vision2\spec\msc\%ISE_PLATFORM%
 if not exist ..\..\library\vision2\spec\msc\%ISE_PLATFORM%\lib mkdir ..\..\library\vision2\spec\msc\%ISE_PLATFORM%\lib
 copy libpng.lib ..\..\library\vision2\spec\msc\%ISE_PLATFORM%\lib\
 del *.obj

a5a45efcb3dc25c2894906f44f9d77dfd0bf1a34
Emmanuel Stapf <manus@eiffel.com>
2007-07-16 23:27:19 +0000

    Removed the code for storing mechanism and retrieving mechanism using a STREAM since it has been   commented out since the beginning (most likely because no performance gain was achieved).
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@69458 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C/bench/pretrieve.c
+++ Src/C/bench/pretrieve.c
@@ -65,13 +65,8 @@ rt_public char *retrieve_all(EIF_INTEGER f_desc, long position);
 rt_public void parsing_retrieve_initialize (void);
 
 /* Private features */
-rt_private void stream_buffer_initialization (EIF_INTEGER file_desc, size_t file_size, long position);
 rt_private size_t parsing_retrieve_read_with_compression (void);
 rt_private int parsing_char_read (char *, int);
-rt_private int parsing_stream_read (char *pointer, int size);	/* store in stream */
-rt_private char *parsing_stream_buffer;
-rt_private int parsing_stream_position;
-rt_private int parsing_stream_size;
 rt_private int file_descriptor;
 
 #define RETRIEVE_BUFFER_SIZE 262144L
@@ -101,11 +96,9 @@ rt_public char *partial_retrieve(EIF_INTEGER f_desc, long position, long nb_obj)
 	EIF_GET_CONTEXT
 	char *result;
 
-/*	parsing_stream_position = 0; */
 	file_descriptor = (int) f_desc;
 	if (lseek (f_desc, position, SEEK_SET) == -1)
 		esys ();
-/* 	stream_buffer_initialization (f_desc, file_size, position); */
 	current_position = 0;
 	end_of_buffer = 0;
 
@@ -126,11 +119,9 @@ rt_public char *retrieve_all(EIF_INTEGER f_desc, long position)
 	 * position. */
 	char *result;
 
-/*	parsing_stream_position = 0; */
 	file_descriptor = (int)f_desc;
 	if (lseek(file_descriptor, position, SEEK_SET) == -1)
 		esys();   /* bail out */
-/*	stream_buffer_initialization (f_desc, file_size, position); */
 
 	current_position = 0;
 	end_of_buffer = 0;
@@ -146,33 +137,6 @@ rt_public char *retrieve_all(EIF_INTEGER f_desc, long position)
 rt_private int parsing_char_read (char *pointer, int size)
 {
 	return read(file_descriptor, pointer, size);
-}
-
-int parsing_stream_read(char *pointer, int size)
-{
-	memcpy (pointer, (parsing_stream_buffer + parsing_stream_position), size);
-	parsing_stream_position += size;
-	return size;
-}
-
-rt_private void stream_buffer_initialization (EIF_INTEGER file_desc, size_t file_size, long position)
-{
-	long number_left = (long) (file_size - position);
-	long number_read;
-
-	REQUIRE("file not too big", file_size <= 0x7FFFFFF);
-
-	if (number_left > parsing_stream_size) {
-		parsing_stream_size = number_left; 
-		parsing_stream_buffer = (char *) realloc (parsing_stream_buffer, parsing_stream_size);
-	}
-
-	if (lseek (file_desc, position, SEEK_SET) == -1)
-		esys ();
-
-	number_read = read (file_desc, parsing_stream_buffer, number_left);
-	if ((number_read <= 0) || (number_read != number_left))
-			eio ();
 }
 
 rt_private size_t parsing_retrieve_read_with_compression (void)

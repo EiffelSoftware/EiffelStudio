d15dfd0ec11118576a4847452178ebf4d389a356
Emmanuel Stapf <manus@eiffel.com>
2007-07-20 16:38:45 +0000

    Added option to specify which C compiler we want. Removed creation of layout, we found a way to do this in the Makefile-win.SH which is at least   portable between VC++, Borland and MinGW.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@69574 8089f293-4706-0410-a29e-feb5c42a2edf

--- Delivery/studio/spec/windows/compile_library.bat
+++ Delivery/studio/spec/windows/compile_library.bat
@@ -3,6 +3,16 @@ rem Eiffel Software script utility to compile the C code of some Eiffel librarie
 rem It assumes a standard layout which is that you are in a Clib directory and that 
 rem the C libraries will be created under ../spec/$ISE_C_COMPILER/$ISE_PLATFORM/lib
 
+rem Store old value for ISE_C_COMPILER
+set OLD_C_COMPILER=%ISE_C_COMPILER%
+
+rem Parse command line argument values
+IF "%~1" NEQ "" (
+	set ISE_C_COMPILER=%~1
+)
+
+echo Compiling C code for %ISE_C_COMPILER%
+
 rem Check environment
 if not exist "%ISE_EIFFEL%\studio\config\%ISE_PLATFORM%\%ISE_C_COMPILER%\config.sh" (
 	echo Missing config.sh file in "%ISE_EIFFEL%\studio\config\%ISE_PLATFORM%\%ISE_C_COMPILER%"
@@ -16,12 +26,6 @@ if not exist "%ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\sed.exe" (
 	echo Missing sed command "%ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\sed.exe"
 	goto error
 )
-
-rem Create the layout
-if not exist ..\spec mkdir ..\spec
-if not exist ..\spec\%ISE_C_COMPILER% mkdir ..\spec\%ISE_C_COMPILER%
-if not exist ..\spec\%ISE_C_COMPILER%\%ISE_PLATFORM% mkdir ..\spec\%ISE_C_COMPILER%\%ISE_PLATFORM%
-if not exist ..\spec\%ISE_C_COMPILER%\%ISE_PLATFORM%\lib mkdir ..\spec\%ISE_C_COMPILER%\%ISE_PLATFORM%\lib
 
 rem Copy the platform/compiler configuration file
 if not exist config.sh copy "%ISE_EIFFEL%\studio\config\%ISE_PLATFORM%\%ISE_C_COMPILER%\config.sh" config.sh
@@ -64,3 +68,4 @@ goto end
 echo Exiting...
 
 :end
+set ISE_C_COMPILER=%OLD_C_COMPILER%

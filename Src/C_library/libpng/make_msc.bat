5f8015a1ef39ec5259e3c8f396d432e4291a0593
Emmanuel Stapf <manus@eiffel.com>
2007-07-25 17:08:37 +0000

    Use `Makefile-win.SH' to compile libpng now.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@69677 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C_library/libpng/make_msc.bat
+++
@@ -1,7 +0,0 @@
-nmake -f scripts/makefile.vcwin32
-if not exist ..\..\library\vision2\spec\msc mkdir ..\..\library\vision2\spec\msc
-if not exist ..\..\library\vision2\spec\msc\%ISE_PLATFORM% mkdir ..\..\library\vision2\spec\msc\%ISE_PLATFORM%
-if not exist ..\..\library\vision2\spec\msc\%ISE_PLATFORM%\lib mkdir ..\..\library\vision2\spec\msc\%ISE_PLATFORM%\lib
-copy libpng.lib ..\..\library\vision2\spec\msc\%ISE_PLATFORM%\lib\
-del *.obj
-del *.lib

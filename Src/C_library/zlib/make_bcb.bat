999a39d69d8a7c3c8a70ac27858e1d57e1380595
Emmanuel Stapf <manus@eiffel.com>
2007-07-25 17:02:55 +0000

    Use Makefile-win.SH to compile zlib.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@69676 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C_library/zlib/make_bcb.bat
+++
@@ -1,6 +0,0 @@
-%ISE_EIFFEL%\BCC55\bin\make /f win32\Makefile.bor
-if not exist ..\..\library\vision2\spec\bcb mkdir ..\..\library\vision2\spec\bcb
-if not exist ..\..\library\vision2\spec\bcb\lib mkdir ..\..\library\vision2\spec\bcb\lib
-copy zlib.lib ..\..\library\vision2\spec\bcb\lib\
-del *.obj
-del *.lib

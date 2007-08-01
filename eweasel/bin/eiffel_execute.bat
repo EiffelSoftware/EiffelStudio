7ddeecfc46836b3cf7058984f857d64480198c59
Emmanuel Stapf <manus@eiffel.com>
2007-08-01 01:12:00 +0000

    Added support for running executable under the `mono.exe' engine.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@69764 8089f293-4706-0410-a29e-feb5c42a2edf

--- eweasel/bin/eiffel_execute.bat
+++ eweasel/bin/eiffel_execute.bat
@@ -1,4 +1,11 @@
 @echo off
+
+if NOT .%MONO%. == .. (
+	rem We need to set MONO_PATH and PATH for executing under Mono
+	set MONO_PATH=%1\Assemblies
+	set PATH=%1\Assemblies
+)
+
 cd /d %1
 shift
 
@@ -14,4 +21,8 @@ shift
 goto loop
 :exit
 
-call %ARGS% && echo Execution completed|| echo Execution failed
+if .%MONO%. == .. (
+	call %ARGS% && echo Execution completed|| echo Execution failed
+) else (
+	call "%MONO%\bin\mono.exe" %ARGS% && echo Execution completed|| echo Execution failed
+)

a1d3177ded24da7a15eb823cb00175546d7b9755
Alexander Kogtenkov <alexk@eiffel.com>
2007-08-03 08:29:07 +0000

    Ensured there is no difference when processing expanded object allocated in a heap or as a local (this eliminates the differences in behaviour of C and byte code for test#exec273).
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@69853 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C/run-time/eif_garcol.h
+++ Src/C/run-time/eif_garcol.h
@@ -84,6 +84,7 @@ RT_LNK struct stack oms_set;	/* Once manifest strings */
  */
 #define eif_is_nested_expanded(flags) (((flags) & (EO_EXP | EO_REF)) == (EO_EXP))
 #define eif_is_boxed_expanded(flags)  (((flags) & (EO_EXP | EO_REF)) == (EO_EXP | EO_REF))
+#define eif_is_expanded(flags)        (((flags) & (EO_EXP)) == (EO_EXP))
 
 /* Exported data-structure declarations */
 RT_LNK EIF_REFERENCE root_obj;	/* Address of `root' object */	

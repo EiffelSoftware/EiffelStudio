9fd46dc9578b4a8cec6be43f1791a5b1f6447aa5
larryl <larryl@eiffel.com>
2007-11-09 08:36:46 +0000

    Added class description and license information.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@71100 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/cURL/spec/include/eiffel_curl.h
+++ Src/library/cURL/spec/include/eiffel_curl.h
@@ -1,3 +1,17 @@
+/*
+indexing
+	description: "Functions used by the class CURL_FUNCTION."
+	copyright:	"Copyright (c) 1984-2006, Eiffel Software and others"
+	license:	"Eiffel Forum License v2 (see http://www.eiffel.com/licensing/forum.txt)"
+	source: "[
+			 Eiffel Software
+			 356 Storke Road, Goleta, CA 93117 USA
+			 Telephone 805-685-1006, Fax 805-685-6869
+			 Website http://www.eiffel.com
+			 Customer support http://support.eiffel.com
+		]"
+*/
+
 #include <stdio.h>
 #include <string.h>
 

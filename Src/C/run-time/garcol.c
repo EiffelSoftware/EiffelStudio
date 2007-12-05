95f345d44621cc07c7ba2c35dde3ecbe246639d1
Emmanuel Stapf <manus@eiffel.com>
2007-12-05 18:43:01 +0000

    Fixed a bug where a SIGBLOCK was not balanced with SIGRESUME in case one could not extend   the stack. (Thanks for DH to spot this).
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@71368 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C/run-time/garcol.c
+++ Src/C/run-time/garcol.c
@@ -4906,8 +4906,10 @@ rt_shared int epush(register struct stack *stk, register void *value)
 		 */
 		SIGBLOCK;							/* Critical section */
 		if (stk->st_cur == stk->st_tl) {	/* Reached last chunk */
-			if (-1 == st_extend(stk, eif_stack_chunk))
+			if (-1 == st_extend(stk, eif_stack_chunk)) {
+				SIGRESUME;
 				return -1;			/* Could not extend stack */
+			}
 			top = stk->st_top;		/* New top */
 		} else {
 			struct stchunk *current;		/* New current chunk */

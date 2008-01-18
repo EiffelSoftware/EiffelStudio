45fceefb4e5aecae739eb67a34b49e78ed423ca3
Emmanuel Stapf <manus@eiffel.com>
2008-01-18 20:43:04 +0000

    Use the real names of `eif_adopt', `eif_wean', `eif_protect', `eif_freeze' and `eif_unfreeze'   for the function name. Removed `spfreeze', `spufreeze' and `hfree': the first two because they did not work anymore, one   has to use `eif_freeze/eif_unfreeze' instead, the last one because no one I know of is using it   (most people use `eif_wean'). Updated runtime to new calls.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@71972 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C/run-time/eif_hector.h
+++ Src/C/run-time/eif_hector.h
@@ -57,22 +57,10 @@ RT_LNK struct stack hec_stack;	/* Indirection table "hector" */
  */
 #if defined(ISE_GC) && !defined(EIF_IL_DLL)
 #define eif_access(_x)	(*(EIF_REFERENCE *) (_x))	/* Access object through hector */
-#define eif_freeze		efreeze				/* Freeze memory address */
-#define eif_adopt		eadopt				/* The C adopts an object */
-#define	eif_protect		henter				/* The C protects an object */
-#define eif_wean		ewean				/* The C weans adopted object */
-#define eif_unfreeze	eufreeze			/* Forget a frozen memory address */
 #define eif_frozen(object)	(HEADER(object)->ov_size & B_C)			/* Is object frozen? */
-#define spfrozen(object)	(HEADER(object)->ov_size & B_C) /* Is special object frozen */
 #else
 #define eif_access(obj)		(obj)
-#define eif_freeze(obj)		(obj)
-#define eif_adopt(obj)		(obj)
-#define eif_protect(obj)	(obj)
-#define eif_wean(obj)		(obj)
-#define eif_unfreeze(obj)
 #define eif_frozen(obj)		1		/* Always frozen since they do not move */
-#define spfrozen(obj)		1		/* Always frozen since they do not move */
 #endif
 
 /* Declaration of hector routines */
@@ -82,25 +70,19 @@ extern int stck_nb_items_free_stack ();
 #endif
 
 #if defined(ISE_GC) && !defined(EIF_IL_DLL)
-RT_LNK EIF_REFERENCE efreeze(EIF_OBJECT object);	/* Freeze object's address (no more move) */
-RT_LNK EIF_OBJECT eadopt(EIF_OBJECT object);		/* The C wants to keep the reference */
-RT_LNK EIF_REFERENCE ewean(EIF_OBJECT object);		/* Weans a previously adopted reference */
-extern void eufreeze(EIF_REFERENCE object);			/* Forget a frozen memory address */
-RT_LNK EIF_OBJECT hrecord(EIF_REFERENCE object);	/* Record entry in hector table */
-RT_LNK EIF_OBJECT henter(EIF_REFERENCE object);		/* Low-level entry in hector table */
-RT_LNK void hfree(EIF_OBJECT address);				/* Low-level release from hector table */
-RT_LNK EIF_REFERENCE spfreeze(EIF_REFERENCE object);/* Freeze special object's address */
-RT_LNK void spufreeze(EIF_REFERENCE object);		/* Put frozen spec obj back to GC control */
+RT_LNK EIF_REFERENCE eif_freeze(EIF_OBJECT object);	/* Freeze object's address (no more move) */
+RT_LNK EIF_OBJECT eif_adopt(EIF_OBJECT object);		/* The C wants to keep the reference */
+RT_LNK EIF_REFERENCE eif_wean(EIF_OBJECT object);		/* Weans a previously adopted reference */
+extern void eif_unfreeze(EIF_REFERENCE object);			/* Forget a frozen memory address */
+RT_LNK EIF_OBJECT eif_protect (EIF_REFERENCE object);		/* Low-level entry in hector table */
+RT_LNK EIF_OBJECT hrecord(EIF_REFERENCE object);	/* Record entry in hector table, only for code generation */
 #else
-#define efreeze(object)	(object)
-#define eadopt(object)	(object)
-#define ewean(object)	(object)
-#define eufreeze(object)
+#define eif_freeze(object)	(object)
+#define eif_adopt(object)	(object)
+#define eif_wean(object)	(object)
+#define eif_unfreeze(object)
+#define eif_protect(object)	(object)
 #define hrecord(object)	(object)
-#define henter(object)	(object)
-#define	hfree(object)
-#define spfreeze(object)	(object)
-#define spufreeze(object)
 #endif
 
 #ifdef __cplusplus

0b4963e4ee92fd391645b8befad97a20e7f17473
Emmanuel Stapf <manus@eiffel.com>
2008-01-18 21:08:41 +0000

    Updated to new location of the xslt tranform binary.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@71973 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C/doc/generate_runtime_doc.sh
+++ Src/C/doc/generate_runtime_doc.sh
@@ -7,5 +7,5 @@ echo Get XML data related to the runtime documentation
 bash document.sh .. doc_runtime.xml
 
 echo Transform the XML doc into HTML doc
-$GOBO/bin/gexslt --file=c_code.xsl --file=doc_runtime.xml --output=doc_runtime.html
+$ISE_EIFFEL/library/gobo/spec/$ISE_PLATFORM/bin/gexslt --file=c_code.xsl --file=doc_runtime.xml --output=doc_runtime.html
 

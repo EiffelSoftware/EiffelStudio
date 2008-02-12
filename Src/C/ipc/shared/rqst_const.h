1e5fb95a1f88ce69cee1135ad71c5e074fe994d5
Jocelyn Fiat <jfiat@eiffel.com>
2008-02-12 16:49:31 +0000

    Fixed issue with IPC (raised by trying to use execution replay) fixed bug#13951: Operating system signal in [DEBUG_EXT.send_rqst_3_integer] in EiffelStudio.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@72284 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C/ipc/shared/rqst_const.h
+++ Src/C/ipc/shared/rqst_const.h
@@ -39,59 +39,59 @@
 
 /* Request types */
 /* Don't forget to update u_Request array in rqst_idrs.c. */
-#define EIF_OPAQUE		1		/* Opaque data request */
-#define ACKNLGE			2		/* Acknowledgment (positive/negative) */
-#define TRANSFER		3		/* Data transfer via daemon */
-#define HELLO			4		/* Application's handshake with ewb */
-#define STOPPED			5		/* Stop notification from app -> ewb */
-#define NOTIFIED		6		/* Notification/event data request (no answer is needed) */
-#define INSPECT			7		/* Object inspection */
-#define DUMP_THREADS	8		/* Send the threads */
-#define DUMP_STACK		9		/* A general stack dump request */
-#define DUMP_VARIABLES	10		/* Send the locals and the arguments of a feature on the call stack */
-#define DUMPED			11		/* A dumped stack item */
-#define MOVE			12		/* Change active routine pointer */
-#define BREAK			13		/* Add/delete breakpoint */
-#define RESUME			14		/* Resume execution */
-#define QUIT			15		/* Application must die immediately */
-#define CMD				16		/* Run a shell command (for ised) */
-#define APPLICATION		17		/* Start up application (for ised) */
-#define KPALIVE			18		/* Dummy request to keep connection alive */
-#define ASYNCMD			19		/* Run command asynchronously */
-#define ASYNACK			20		/* Status of the asynchronous job */
-#define DEAD			21		/* Application is dead */
-#define LOAD			22		/* Load byte code information */
-#define BYTECODE		23		/* Byte code transfer */
-#define KILL			24		/* Kill application asynchronously */
-#define ADOPT			25		/* Adopt object */
-#define ACCESS			26		/* Access object through hector */
-#define WEAN			27		/* Wean adopted object */
-#define ONCE			28		/* Once routines inspection */
-#define EWB_INTERRUPT	29		/* Debugger asking interruption of application*/
-#define APP_INTERRUPT	30		/* Application wondering if it has to stop */
-#define INTERRUPT_OK	31		/* Application must stop its execution */
-#define INTERRUPT_NO	32		/* Application can resume execution */
-#define SP_LOWER		33		/* Bounds for special objects inspection */
+#define EIF_OPAQUE			1		/* Opaque data request */
+#define ACKNLGE				2		/* Acknowledgment (positive/negative) */
+#define TRANSFER			3		/* Data transfer via daemon */
+#define HELLO				4		/* Application's handshake with ewb */
+#define STOPPED				5		/* Stop notification from app -> ewb */
+#define NOTIFIED			6		/* Notification/event data request (no answer is needed) */
+#define INSPECT				7		/* Object inspection */
+#define DUMP_THREADS		8		/* Send the threads */
+#define DUMP_STACK			9		/* A general stack dump request */
+#define DUMP_VARIABLES		10		/* Send the locals and the arguments of a feature on the call stack */
+#define DUMPED				11		/* A dumped stack item */
+#define MOVE				12		/* Change active routine pointer */
+#define BREAK				13		/* Add/delete breakpoint */
+#define RESUME				14		/* Resume execution */
+#define QUIT				15		/* Application must die immediately */
+#define CMD					16		/* Run a shell command (for ised) */
+#define APPLICATION			17		/* Start up application (for ised) */
+#define KPALIVE				18		/* Dummy request to keep connection alive */
+#define ASYNCMD				19		/* Run command asynchronously */
+#define ASYNACK				20		/* Status of the asynchronous job */
+#define DEAD				21		/* Application is dead */
+#define LOAD				22		/* Load byte code information */
+#define BYTECODE			23		/* Byte code transfer */
+#define KILL				24		/* Kill application asynchronously */
+#define ADOPT				25		/* Adopt object */
+#define ACCESS				26		/* Access object through hector */
+#define WEAN				27		/* Wean adopted object */
+#define ONCE				28		/* Once routines inspection */
+#define EWB_INTERRUPT		29		/* Debugger asking interruption of application*/
+#define APP_INTERRUPT		30		/* Application wondering if it has to stop */
+#define INTERRUPT_OK		31		/* Application must stop its execution */
+#define INTERRUPT_NO		32		/* Application can resume execution */
+#define SP_LOWER			33		/* Bounds for special objects inspection */
 #define METAMORPHOSE		34		/* Convert the top-level item in the operational stack from a basic type to a reference */
-#define APP_INTERRUPT_FLAG	35	/* Application sends the address of its interruption flag to daemon */
-#define EWB_NEWBREAKPOINT	36	/* Application sends the address of its interruption flag to daemon */
-#define MODIFY_LOCAL	37		/* `ec' asks the æpplication to change the value of a local variable */
-#define MODIFY_ATTR		38		/* `ec' asks the application to change the value of an object attribute */
-#define DYNAMIC_EVAL	39		/* `ec' asks the application to execute a given feature on a given object */
-#define APPLICATION_CWD	40		/* Send current directory to launch application */
-#define OVERFLOW_DETECT	41		/* Specify the stack overflow management: set a depth at which the application raises an exception */
-#define CHANGE_THREAD	42		/* Thread id used for inspection and other */
+#define APP_INTERRUPT_FLAG	35		/* Application sends the address of its interruption flag to daemon */
+#define EWB_NEWBREAKPOINT	36		/* Application sends the address of its interruption flag to daemon */
+#define MODIFY_LOCAL		37		/* `ec' asks the æpplication to change the value of a local variable */
+#define MODIFY_ATTR			38		/* `ec' asks the application to change the value of an object attribute */
+#define DYNAMIC_EVAL		39		/* `ec' asks the application to execute a given feature on a given object */
+#define APPLICATION_CWD		40		/* Send current directory to launch application */
+#define OVERFLOW_DETECT		41		/* Specify the stack overflow management: set a depth at which the application raises an exception */
+#define CHANGE_THREAD		42		/* Thread id used for inspection and other */
 
-#define EWB_SET_ASSERTION_CHECK 43		/* Change assertion checking on the application being debugged */
-#define CLOSE_DBG 44			/* Close ecdbgd */
-#define SET_IPC_PARAM 45		/* set IPC parameters value (pid, timeout in seconds, ...) */
-#define CLEAR_BREAKPOINTS 46	/* Clear breakpoints table */
-#define LAST_EXCEPTION	 47		/* Get last_exception object */
-#define APPLICATION_ENV	48		/* Send current env to launch application */
-#define NEW_INSTANCE	49		/* Create new instance of class */
-#define RT_OPERATION	50		/* Invoke an `RT_EXTENSION' operation */
+#define EWB_SET_ASSERTION_CHECK 43	/* Change assertion checking on the application being debugged */
+#define CLOSE_DBG 			44		/* Close ecdbgd */
+#define SET_IPC_PARAM 		45		/* set IPC parameters value (pid, timeout in seconds, ...) */
+#define CLEAR_BREAKPOINTS 	46		/* Clear breakpoints table */
+#define LAST_EXCEPTION	 	47		/* Get last_exception object */
+#define APPLICATION_ENV		48		/* Send current env to launch application */
+#define NEW_INSTANCE		49		/* Create new instance of class */
+#define RT_OPERATION		50		/* Invoke an `RT_EXTENSION' operation */
 
-#define MAX_REQUEST_TYPE 50		/* To update with new value of max request type. */
+#define MAX_REQUEST_TYPE 	50		/* To update with new value of max request type. */
 
 
 /* Acknowledgments codes */

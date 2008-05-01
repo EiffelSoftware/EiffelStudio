fc047cb818ed9128f574e75cc49608856489003e
fivaa <fivaa@eiffel.com>
2008-05-01 16:11:23 +0000

    Simplified and cleaned up packaging scripts/files for debian.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@73267 8089f293-4706-0410-a29e-feb5c42a2edf

--- Delivery/scripts/unix/packaging/rpm/eiffelstudio.spec.sh
+++ Delivery/scripts/unix/packaging/rpm/eiffelstudio.spec.sh
@@ -1,10 +1,10 @@
 case $CONFIG in
 '')
-        if test ! -f $PACKAGING_DIR/config.sh; then
-                (echo "Can't find config.sh."; exit 1)
-        fi 2>/dev/null
-        . $PACKAGING_DIR/config.sh
-        ;;
+	if test ! -f $PACKAGING_DIR/config.sh; then
+		(echo "Can't find config.sh."; exit 1)
+	fi 2>/dev/null
+	. $PACKAGING_DIR/config.sh
+	;;
 esac
 case "$O" in
 */*) cd `expr X$0 : 'X\(.*\)/'` ;;

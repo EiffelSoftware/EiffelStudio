89cab3f69fb25119f3fa18a9b3d9d9aefca49dff
Jocelyn Fiat <jfiat@eiffel.com>
2008-09-12 15:35:36 +0000

    update versions
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@74687 8089f293-4706-0410-a29e-feb5c42a2edf

--- Documentation/tools/web/trunk/bin/eiffeldoc_install.sh
+++ Documentation/tools/web/trunk/bin/eiffeldoc_install.sh
@@ -38,9 +38,18 @@ getdrupmod()
   wgetit http://ftp.drupal.org/files/projects $URLNAME
   unzipit $URLNAME $DRUPALDIR/sites/all/modules
 }
+getdruptheme()
+{
+  URLNAME=$1
+  wgetit http://ftp.drupal.org/files/projects $URLNAME
+  unzipit $URLNAME $DRUPALDIR/sites/all/themes
+}
 
+getdrupal_themes(){
+	getdruptheme zen-6.x-1.0-beta2.tar.gz
+}
 getdrupal_modules(){
-	getdrupmod cck-6.x-2.0-rc6.tar.gz
+	getdrupmod cck-6.x-2.0-rc7.tar.gz
 	getdrupmod diff-6.x-2.0.tar.gz
 	getdrupmod freelinking-6.x-1.4.tar.gz
 	getdrupmod geshifilter-6.x-1.1.tar.gz
@@ -51,22 +60,26 @@ getdrupal_modules(){
 	getdrupmod pathauto-6.x-1.1.tar.gz
 	getdrupmod pearwiki_filter-6.x-1.0-beta1.tar.gz
 	getdrupmod persistent_login-6.x-1.4-beta2.tar.gz
-	getdrupmod print-6.x-1.0-rc9.tar.gz
+	getdrupmod print-6.x-1.0.tar.gz
 	getdrupmod recent_changes-6.x-1.x-dev.tar.gz
 	getdrupmod tagadelic-6.x-1.0.tar.gz
+	getdrupmod community_tags-6.x-1.0-beta1.tar.gz
 	getdrupmod talk-6.x-1.4.tar.gz
 	getdrupmod textareatabs-6.x-0.1.tar.gz
 	getdrupmod token-6.x-1.11.tar.gz
 	getdrupmod trash-6.x-1.x-dev.tar.gz
 	getdrupmod upload_image-6.x-1.x-dev.tar.gz
-	getdrupmod views-6.x-2.0-rc1.tar.gz
+	getdrupmod views-6.x-2.0-rc2.tar.gz
 	getdrupmod wikitools-6.x-1.0.tar.gz
 	getdrupmod flexifilter-6.x-1.1-rc1.tar.gz
 	getdrupmod tableofcontents-6.x-2.2.tar.gz
 	getdrupmod fckeditor-6.x-1.3-rc1.tar.gz
 	getdrupmod opensearchplugin-6.x-1.1.tar.gz
 	getdrupmod jtooltips-6.x-1.8.tar.gz
-	getdrupmod xmlcontent-6.x-1.x-dev.tar.gz
+	#getdrupmod xmlcontent-6.x-1.x-dev.tar.gz
+	#getdrupmod nice_menus-6.x-1.1.tar.gz
+	getdrupmod captcha-6.x-1.0-rc2.tar.gz
+	getdrupmod captcha_pack-6.x-1.0-beta2.tar.gz
 
 
 	# For development purpose
@@ -135,6 +148,11 @@ fi
 
 cd ..
 
+#Get themes
+safemkdir themes
+cd themes
+getdrupal_themes
+cd ..
 
 #Get modules
 safemkdir modules

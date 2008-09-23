3e7e4887c00379d98b2bf3e7d2d3f3a081728f45
Jocelyn Fiat <jfiat@eiffel.com>
2008-09-23 09:26:21 +0000

    add specific eiffeldoc theme
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@74787 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Documentation/tools/web/trunk/drupal/sites/all/themes/eiffeldoc/README.txt
@@ -0,0 +1,16 @@
+ABOUT ZEN CLASSIC
+
+  The Zen Classic theme contains the old stylesheets from Zen 5.x-0.7. It was
+  made a sub-theme of Zen because, while the theme looked good as-is, everyone
+  was tired of undoing all that CSS when using it to develop a new theme. See
+  http://groups.drupal.org/node/6353 and http://drupal.org/node/171464
+
+SUPPORT
+
+  The Zen Classic theme is left as-is for historical purposes and as a reference
+  for people who used Zen 5.x-0.7 and earlier and have gotten used to themeing
+  with it.
+
+  However, since most developers dislike using it as a starting point for theme
+  development, fewer people will be available to help with themeing issues you
+  may encounter.
---
+++ Documentation/tools/web/trunk/drupal/sites/all/themes/eiffeldoc/page.tpl.php
@@ -0,0 +1,214 @@
+<?php
+// $Id: page.tpl.php,v 1.10 2008/09/15 08:35:47 johnalbin Exp $
+
+/**
+ * @file page.tpl.php
+ *
+ * Theme implementation to display a single Drupal page.
+ *
+ * Available variables:
+ *
+ * General utility variables:
+ * - $base_path: The base URL path of the Drupal installation. At the very
+ *   least, this will always default to /.
+ * - $css: An array of CSS files for the current page.
+ * - $directory: The directory the theme is located in, e.g. themes/garland or
+ *   themes/garland/minelli.
+ * - $is_front: TRUE if the current page is the front page. Used to toggle the mission statement.
+ * - $logged_in: TRUE if the user is registered and signed in.
+ * - $is_admin: TRUE if the user has permission to access administration pages.
+ *
+ * Page metadata:
+ * - $language: (object) The language the site is being displayed in.
+ *   $language->language contains its textual representation.
+ *   $language->dir contains the language direction. It will either be 'ltr' or 'rtl'.
+ * - $head_title: A modified version of the page title, for use in the TITLE tag.
+ * - $head: Markup for the HEAD section (including meta tags, keyword tags, and
+ *   so on).
+ * - $styles: Style tags necessary to import all CSS files for the page.
+ * - $scripts: Script tags necessary to load the JavaScript files and settings
+ *   for the page.
+ * - $body_classes: A set of CSS classes for the BODY tag. This contains flags
+ *   indicating the current layout (multiple columns, single column), the current
+ *   path, whether the user is logged in, and so on.
+ *
+ * Site identity:
+ * - $front_page: The URL of the front page. Use this instead of $base_path,
+ *   when linking to the front page. This includes the language domain or prefix.
+ * - $logo: The path to the logo image, as defined in theme configuration.
+ * - $site_name: The name of the site, empty when display has been disabled
+ *   in theme settings.
+ * - $site_slogan: The slogan of the site, empty when display has been disabled
+ *   in theme settings.
+ * - $mission: The text of the site mission, empty when display has been disabled
+ *   in theme settings.
+ *
+ * Navigation:
+ * - $search_box: HTML to display the search box, empty if search has been disabled.
+ * - $primary_links (array): An array containing primary navigation links for the
+ *   site, if they have been configured.
+ * - $secondary_links (array): An array containing secondary navigation links for
+ *   the site, if they have been configured.
+ *
+ * Page content (in order of occurrance in the default page.tpl.php):
+ * - $left: The HTML for the left sidebar.
+ *
+ * - $breadcrumb: The breadcrumb trail for the current page.
+ * - $title: The page title, for use in the actual HTML content.
+ * - $help: Dynamic help text, mostly for admin pages.
+ * - $messages: HTML for status and error messages. Should be displayed prominently.
+ * - $tabs: Tabs linking to any sub-pages beneath the current page (e.g., the view
+ *   and edit tabs when displaying a node).
+ *
+ * - $content: The main content of the current Drupal page.
+ *
+ * - $right: The HTML for the right sidebar.
+ *
+ * Footer/closing data:
+ * - $feed_icons: A string of all feed icons for the current page.
+ * - $footer_message: The footer message as defined in the admin settings.
+ * - $footer : The footer region.
+ * - $closure: Final closing markup from any modules that have altered the page.
+ *   This variable should always be output last, after all other dynamic content.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_page()
+ */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
+
+<head>
+  <title><?php print $head_title; ?></title>
+  <?php print $head; ?>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <script type="text/javascript"><?php /* Needed to avoid Flash of Unstyled Content in IE */ ?> </script>
+</head>
+<body class="<?php print $body_classes; ?>">
+  <div id="page">
+    <div id="header">
+
+      <div id="skip-nav"><a href="#content"><?php print t('Skip to Main Content'); ?></a></div>
+
+      <div id="user-info">@
+        <ul>
+        <?php if ($logged_in) { ?>
+          <li><?php print t("Signed in as "); print l($user->name, 'user/'.$user->uid); ?></li>
+          <li><a href="<?php print url('logout'); ?>"><? print t("Sign out") ?></a></li>
+        <?php } else { ?>
+          <li><a href="<?php print url('user', drupal_get_destination()); ?>"><?php print t("Sign in") ?></a></li>
+          <li><a href="<?php print url('user/register', drupal_get_destination()); ?>"><?php print t("Register") ?></a></li>
+        <?php } ?>
+        </ul>
+      </div>
+      <div id="logo-title">
+
+        <?php print $search_box; ?>
+        <?php if (!empty($logo)): ?>
+          <a href="<?php print $base_path; ?>" title="<?php print t('Home'); ?>" rel="home">
+            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" id="logo" />
+          </a>
+        <?php endif; ?>
+
+        <div id="name-and-slogan">Test
+
+        <?php if (!empty($site_name)): ?>
+          <div id="site-name"><strong>
+            <a href="<?php print $base_path; ?>" title="<?php print t('Home'); ?>" rel="home">
+              <?php print $site_name; ?>
+            </a>
+          </strong></div>
+        <?php endif; ?>
+
+        <?php if (!empty($site_slogan)): ?>
+          <div id="site-slogan">
+            <?php print $site_slogan; ?>
+          </div>
+        <?php endif; ?>
+
+        </div> <!-- /name-and-slogan -->
+
+      </div> <!-- /logo-title -->
+
+      <div id="navigation" class="menu<?php if ($primary_links) { print " withprimary"; } if ($secondary_links) { print " withsecondary"; } ?> ">
+        <?php if (!empty($primary_links)): ?>
+          <div id="primary" class="clear-block">
+            <?php print theme('links', $primary_links); ?>
+          </div>
+        <?php endif; ?>
+
+        <?php if (!empty($secondary_links)): ?>
+          <div id="secondary" class="clear-block">
+            <?php print theme('links', $secondary_links); ?>
+          </div>
+        <?php endif; ?>
+      </div> <!-- /navigation -->
+
+      <?php if (!empty($header) || !empty($breadcrumb)): ?>
+        <div id="header-region">
+          <?php print $breadcrumb; ?>
+          <?php print $header; ?>
+        </div> <!-- /header-region -->
+      <?php endif; ?>
+
+    </div> <!-- /header -->
+
+    <div id="container" class="clear-block">
+
+      <?php if (!empty($left)): ?>
+        <div id="sidebar-left" class="column sidebar">
+          <?php print $left; ?>
+        </div> <!-- /sidebar-left -->
+      <?php endif; ?>
+
+      <div id="main" class="column"><div id="squeeze" class="clear-block">
+        <?php if (!empty($mission)): ?>
+          <div id="mission"><?php print $mission; ?></div>
+        <?php endif; ?>
+        <?php if (!empty($content_top)): ?>
+          <div id="content-top"><?php print $content_top; ?></div>
+        <?php endif; ?>
+        <div id="content">
+          <?php if (!empty($title)): ?>
+            <h1 class="title"><?php print $title; ?></h1>
+          <?php endif; ?>
+          <?php if (!empty($tabs)): ?>
+            <div class="tabs"><?php print $tabs; ?></div>
+          <?php endif; ?>
+          <?php print $help; ?>
+          <?php print $messages; ?>
+          <?php print $content; ?>
+          <?php if (!empty($feed_icons)): ?>
+            <div class="feed-icons"><?php print $feed_icons; ?></div>
+          <?php endif; ?>
+        </div> <!-- /content -->
+        <?php if (!empty($content_bottom)): ?>
+          <div id="content-bottom"><?php print $content_bottom; ?></div>
+        <?php endif; ?>
+      </div></div> <!-- /squeeze /main -->
+
+      <?php if (!empty($right)): ?>
+        <div id="sidebar-right" class="column sidebar">
+          <?php print $right; ?>
+        </div> <!-- /sidebar-right -->
+      <?php endif; ?>
+
+    </div> <!-- /container -->
+
+    <div id="footer-wrapper">
+      <div id="footer">
+        <?php print $footer_message; ?>
+      </div> <!-- /footer -->
+    </div> <!-- /footer-wrapper -->
+
+    <?php if ($closure_region): ?>
+      <div id="closure-blocks"><?php print $closure_region; ?></div>
+    <?php endif; ?>
+
+    <?php print $closure; ?>
+
+  </div> <!-- /page -->
+
+</body>
+</html>

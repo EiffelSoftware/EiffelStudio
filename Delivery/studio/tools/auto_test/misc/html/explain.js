6f81573e96bb08fcee6e54166097f2b1740bc623
fivaa <fivaa@eiffel.com>
2008-10-30 23:21:19 +0000

    Added AutoTest specific files (images, html) to delivery
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@75380 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Delivery/studio/tools/auto_test/misc/html/explain.js
@@ -0,0 +1,4 @@
+function explain(url)
+{
+  window.open(url,'explain','toolbar=no,location=no,directories=no,status=no,menubar=no,resizable=yes,copyhistory=no,scrollbars=yes,width=600,height=300');
+}

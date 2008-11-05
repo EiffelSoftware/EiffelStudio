b0d033ad0dd086f5f9525aa6ce2672d892f903f7
mev <mev@eiffel.com>
2008-11-05 08:25:38 +0000

    revert change because the redefinition of the FD_SETSIZE is required even though it generates the compilation warning
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@75426 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/net_ipv6/Clib/network.c
+++ Src/library/net_ipv6/Clib/network.c
@@ -28,6 +28,7 @@ indexing
 #ifdef EIF_WINDOWS
 #include <Winsock2.h>
 #include <ws2tcpip.h>
+#define FD_SETSIZE 256
 #endif
 
 #include "eif_portable.h" 	/* required for VMS, recommended for others */

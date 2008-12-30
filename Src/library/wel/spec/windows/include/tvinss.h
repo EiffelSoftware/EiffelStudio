07c99cc74fb950c4b1bf8479711c346d28626dd5
Emmanuel Stapf <manus@eiffel.com>
2008-12-30 20:26:01 +0000

    Use the nameless union access to the TV_INSERTSTRUCT fields. It was done before because borland does   not support it, but now that Borland is not supported anymore, we can use the namelessunion.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@76453 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/library/wel/spec/windows/include/tvinss.h
+++ Src/library/wel/spec/windows/include/tvinss.h
@@ -25,21 +25,10 @@ extern "C" {
 
 #define cwel_insertstruct_set_hparent(_ptr_, _value_) (((TV_INSERTSTRUCT *) _ptr_)->hParent = (HTREEITEM) (_value_))
 #define cwel_insertstruct_set_hinsertafter(_ptr_, _value_) (((TV_INSERTSTRUCT *) _ptr_)->hInsertAfter = (HTREEITEM) (_value_))
-
-#if (_WIN32_IE >= 0x0400)
-#define cwel_insertstruct_set_item(_ptr_, _value_) (((TV_INSERTSTRUCT *) _ptr_)->DUMMYUNIONNAME.item = (*(TV_ITEM *) (_value_)))
-#else
 #define cwel_insertstruct_set_item(_ptr_, _value_) (((TV_INSERTSTRUCT *) _ptr_)->item = (*(TV_ITEM *) (_value_)))
-#endif
-
 #define cwel_insertstruct_get_hparent(_ptr_) ((((TV_INSERTSTRUCT *) _ptr_)->hParent))
 #define cwel_insertstruct_get_hinsertafter(_ptr_) ((((TV_INSERTSTRUCT *) _ptr_)->hInsertAfter))
-
-#if (_WIN32_IE >= 0x0400)
-#define cwel_insertstruct_get_item(_ptr_) (&(((TV_INSERTSTRUCT *) _ptr_)->DUMMYUNIONNAME.item))
-#else
 #define cwel_insertstruct_get_item(_ptr_) (&(((TV_INSERTSTRUCT *) _ptr_)->item))
-#endif
 
 #ifdef __cplusplus
 }

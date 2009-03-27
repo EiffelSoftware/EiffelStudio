7e2d46a42420cc987a3df9020a1a671fa2840ae3
Emmanuel Stapf <manus@eiffel.com>
2009-03-27 16:07:30 +0000

    Added builtin for vxworks support.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@77942 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C/run-time/eif_built_in.h
+++ Src/C/run-time/eif_built_in.h
@@ -133,6 +133,7 @@ extern "C" {
 #define eif_builtin_PLATFORM_is_windows 				EIF_IS_WINDOWS
 #define eif_builtin_PLATFORM_is_unix 					EIF_TEST(!(EIF_IS_VMS || EIF_IS_WINDOWS))
 #define eif_builtin_PLATFORM_is_mac						EIF_OS==EIF_OS_DARWIN
+#define eif_builtin_PLATFORM_is_vxworks					EIF_OS==EIF_OS_VXWORKS
 #ifdef EIF_IL_DLL
 #define eif_builtin_PLATFORM_is_dotnet					EIF_TRUE
 #else

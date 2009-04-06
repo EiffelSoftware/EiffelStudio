e1ea0fe113b67b36cc9defcc00b36a02f631a402
fzuend <fzuend@eiffel.com>
2009-04-06 22:06:26 +0000

    Reverted errors.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@78104 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/framework/web/xebra/eiffel_projects/xebra_translator/generate-webapp.sh
+++ Src/framework/web/xebra/eiffel_projects/xebra_translator/generate-webapp.sh
@@ -2,10 +2,9 @@
 echo ">>>>>>>>>>Compile xebra_translator"
 $ISE_EIFFEL/studio/spec/$ISE_PLATFORM/bin/ecb -c_compile -config $XEBRA_DEV/eiffel_projects/xebra_translator/xebra_translator.ecf
 echo ">>>>>>>>>>Generate servlet_gen"
-$ISE_EIFFEL/studio/spec/$ISE_PLATFORM/bin/ecb -loop -config $XEBRA_DEV/eiffel_projects/xebra_translator/xebra_translator.ecf < input_translator.txt >
+$ISE_EIFFEL/studio/spec/$ISE_PLATFORM/bin/ecb -loop -config $XEBRA_DEV/eiffel_projects/xebra_translator/xebra_translator.ecf < input_translator.txt
 echo ">>>>>>>>>>Compile servlet_gen"
 $ISE_EIFFEL/studio/spec/$ISE_PLATFORM/bin/ecb -c_compile -config $XEBRA_DEV/websites/$1/servlet_gen/servlet_gen.ecf
 echo ">>>>>>>>>>Generate servlets"
-$ISE_EIFFEL/studio/spec/$ISE_PLATFORM/bin/ecb -loop -config $XEBRA_DEV/websites/$1/servlet_gen/servlet_gen.ecf < input_servlet_gen.txt >
+$ISE_EIFFEL/studio/spec/$ISE_PLATFORM/bin/ecb -loop -config $XEBRA_DEV/websites/$1/servlet_gen/servlet_gen.ecf < input_servlet_gen.txt
 echo "done"
-

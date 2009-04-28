73571c6bc0292d376fee7f3033e1f4cb98b5506f
Emmanuel Stapf <manus@eiffel.com>
2009-04-28 17:38:06 +0000

    We do not use any xmldoc anymore since our documentation is online using drupal at http://doc.eiffel.com.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@78417 8089f293-4706-0410-a29e-feb5c42a2edf

--- Documentation/xmldoc/images/eiffelstudio/icons/generate_icons.cmd
+++
@@ -1,9 +0,0 @@
-del /q 10x10\*
-del /q 12x12\*
-del /q 16x16\*
-del /q cursors\*
-set PNGDIR="%EIFFEL_SRC%/Delivery/studio/bitmaps/png/"
-emcgen -slice "%PNGDIR%/10x10.png" "%PNGDIR%/10x10.ini" -pngs 10x10
-emcgen -slice "%PNGDIR%/12x12.png" "%PNGDIR%/12x12.ini" -pngs 12x12
-emcgen -slice "%PNGDIR%/16x16.png" "%PNGDIR%/16x16.ini" -pngs 16x16
-emcgen -slice "%PNGDIR%/cursors.png" "%PNGDIR%/cursors.ini" -pngs cursors

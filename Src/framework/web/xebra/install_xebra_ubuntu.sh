0a927163283c356792521bad0f96c9b0e1935694
fzuend <fzuend@eiffel.com>
2009-05-16 22:53:39 +0000

    Updated install scripts. The '_alternative' script uses the apache from ubuntu.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@78729 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/framework/web/xebra/install_xebra_ubuntu.sh
+++ Src/framework/web/xebra/install_xebra_ubuntu.sh
@@ -27,8 +27,12 @@ fi;
 
 
 sudo apache2ctl stop
+
+# Checkout xebra
 cd $XEBRA_DEV
 svn co https://svn.origo.ethz.ch/eiffelstudio/trunk/Src/framework/web/xebra/
+
+# Install apache
 mkdir httpd
 mkdir httpd_tmp
 cd $XEBRA_DEV/httpd_tmp
@@ -39,6 +43,8 @@ cd $XEBRA_DEV/httpd_tmp/httpd-2.2.11
 make
 make install
 rm -Rf $XEBRA_DEV/httpd_tmp
+
+# Compile and install xebra module
 cd $XEBRA_DEV/c_projects/mod_xebra
 $XEBRA_DEV/httpd/bin/apxs -c -I$ISE_EIFFEL/studio/spec/$ISE_PLATFORM/include mod_xebra.c mod_xebra.h
 $XEBRA_DEV/httpd/bin/apxs -i mod_xebra.la
@@ -48,9 +54,11 @@ echo "XebraServer_port \"55000\"" >> $XEBRA_DEV/httpd/conf/httpd.conf
 echo "XebraServer_host \"localhost\"" >> $XEBRA_DEV/httpd/conf/httpd.conf 
 echo "LogLevel debug" >> $XEBRA_DEV/httpd/conf/httpd.conf 
 sudo $XEBRA_DEV/httpd/bin/apachectl start
+
+# Compile xebra translator
 cd $XEBRA_DEV/eiffel_projects/xebra_translator/
 ec -config xebra_translator-voidunsafe.ecf  -target xebra_translator -c_compile
-echo "Installation compelte. Please edit $XEBRA_DEV/eiffel_projects/xebra_server/config.ini to match your configuration."
+echo "Installation complete. Please edit $XEBRA_DEV/eiffel_projects/xebra_server/config.ini to match your configuration."
 
 
 

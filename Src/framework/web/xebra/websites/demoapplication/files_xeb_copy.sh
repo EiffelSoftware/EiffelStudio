ad8ce0581922b6d4f73e56c0fa2efd086632824b
fzuend <fzuend@eiffel.com>
2009-05-18 17:54:42 +0000

    Updated demoapp with new template system.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@78748 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/framework/web/xebra/websites/demoapplication/files_xeb_copy.sh
+++ Src/framework/web/xebra/websites/demoapplication/files_xeb_copy.sh
@@ -9,5 +9,6 @@ cp html/logout.html logout.xeb
 cp html/reservations.html reservations.xeb
 cp html/delete.html delete.xeb
 cp html/insert.html insert.xeb
+cp html/master_template.html  master_template.xeb
 
 

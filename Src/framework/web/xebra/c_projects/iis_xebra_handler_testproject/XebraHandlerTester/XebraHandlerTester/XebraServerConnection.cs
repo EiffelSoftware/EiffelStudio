7269c5b6d66c5bf7a894b96320b7106d9acb988d
fzuend <fzuend@eiffel.com>
2009-05-26 16:33:41 +0000

    - Added new features to iis_handler - Corrected include paths for settable_types in all projects  (ISE_LIBRARY -> EIFFEL_SRC) - Updated tutorial
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@78867 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/framework/web/xebra/c_projects/iis_xebra_handler_testproject/XebraHandlerTester/XebraHandlerTester/XebraServerConnection.cs
+++ Src/framework/web/xebra/c_projects/iis_xebra_handler_testproject/XebraHandlerTester/XebraHandlerTester/XebraServerConnection.cs
@@ -13,7 +13,8 @@ namespace XebraHandlerTester
         #region Fields
 
         TcpClient socket;
-        int FRAG_SIZE = 15;//65536;
+        int FRAG_SIZE = 65536;
+        int MAX_FRAGS = 1000;
 
         public delegate void Outputter(String s);
         Outputter outputter;
@@ -55,13 +56,13 @@ namespace XebraHandlerTester
             int numBytes = 0;
             byte[] messageByte = Encoding.UTF8.GetBytes(message);
             uint totalMessageLength = (uint)messageByte.Length;
+            string oute;
 
             Debug("About to send message '" + message + "'. Length is " + totalMessageLength + "bytes");
 
-                
             while (bytesSent < totalMessageLength)
             {
-                if ((messageByte.Length - bytesSent) > FRAG_SIZE)
+                if ((totalMessageLength - bytesSent) > FRAG_SIZE)
                 {
                     encodedMsgLengthByte = System.BitConverter.GetBytes((int)Encoder.encode_natural((uint)FRAG_SIZE, 1));
                     //Convert to bigendian
@@ -70,8 +71,10 @@ namespace XebraHandlerTester
                     System.Buffer.BlockCopy(encodedMsgLengthByte, 0, msgFragAndLength, 0, encodedMsgLengthByte.Length);
                     System.Buffer.BlockCopy(messageByte, bytesSent, msgFragAndLength, encodedMsgLengthByte.Length, FRAG_SIZE);
 
-
-                    numBytes = socket.Client.Send(msgFragAndLength, bytesSent, msgFragAndLength.Length, SocketFlags.None);
+                    oute = System.Text.ASCIIEncoding.ASCII.GetString(msgFragAndLength);
+                    Debug("--senging fragment " + FRAG_SIZE + " (+4) bytes : '" + oute + "'");
+                    numBytes = socket.Client.Send(msgFragAndLength, msgFragAndLength.Length, SocketFlags.None);
+                    Debug(numBytes + " bytes sent.");
                 }
                 else
                 {
@@ -80,70 +83,110 @@ namespace XebraHandlerTester
                     Array.Reverse(encodedMsgLengthByte);
 
                     System.Buffer.BlockCopy(encodedMsgLengthByte, 0, msgFragAndLength, 0, encodedMsgLengthByte.Length);
-                    System.Buffer.BlockCopy(messageByte, bytesSent, msgFragAndLength, encodedMsgLengthByte.Length, messageByte.Length - (int)bytesSent);
+                    System.Buffer.BlockCopy(messageByte, bytesSent, msgFragAndLength, encodedMsgLengthByte.Length, (int)totalMessageLength - (int)bytesSent);
 
-
-                    numBytes = socket.Client.Send(msgFragAndLength, bytesSent, msgFragAndLength.Length - bytesSent, SocketFlags.None);
+                    oute = System.Text.ASCIIEncoding.ASCII.GetString(msgFragAndLength);
+                    Debug("--senging LAST fragment " + msgFragAndLength.Length + " (+4) bytes : '" + oute + "'");
+                    numBytes = socket.Client.Send(msgFragAndLength, (int)totalMessageLength - (int)bytesSent + 4, SocketFlags.None);
+                    Debug(numBytes + " bytes sent.");
                 }
                 if (numBytes < 5)
                 {
                     Error("Failed to send!");
                     return false;
-                }       
+                }
                 else
                 {
                     bytesSent += numBytes - 4;
                 }
             }
-
-
-            //while (bytesSent < totalMessageLength)
-            //{
-            //    if (message.Length <= FRAG_SIZE)
-            //    {
-            //        msgFrag = message.ToCharArray();
-            //        encodedMsgLengthByte = intToByteArray(encode_natural(msgFrag.Length * SOC, 0));
-            //        Debug(" -About to send last fragment. Length is " + msgFrag.Length * SOC + " bytes");
-            //    }
-            //    else
-            //    {
-            //        msgFrag = message.Substring(0, FRAG_SIZE).ToCharArray();
-            //        message = message.Substring(FRAG_SIZE, message.Length);
-            //        encodedMsgLengthByte = intToByteArray(encode_natural(msgFrag.Length, 1));
-            //        Debug("-About to send fragment. Length is " + msgFrag.Length * SOC + " bytes");
-            //    }
-            //    //Debug("-Sending length...");
-            //    ////Send msg length
-            //    //numBytes = socket.Client.Send(encodedMsgLengthByte, 4, SocketFlags.None);
-
-            //    //if (numBytes < 1)
-            //    //{
-            //    //    Error("Failed to send encodedMsgLengthByte");
-            //    //    return false;
-            //    //}
-            //    //Debug("Length sent.");
-            //    Debug("-Sending fragment...: '" + msgFrag.ToString() + "'");
-
-            //    //Copy length and msg into one byte array
-            //    System.Buffer.BlockCopy(encodedMsgLengthByte, 0, msgFragAndLength, 0, encodedMsgLengthByte.Length);
-            //    System.Buffer.BlockCopy(msgFrag, 0, msgFragAndLength, encodedMsgLengthByte.Length - 1, msgFrag.Length);
-
-
-            //    //Send Length and Msg
-            //    numBytes = socket.Client.Send(msgFragAndLength, msgFragAndLength.Length, SocketFlags.None);
-            //    if (numBytes < 1)
-            //    {
-            //        Error("Failed to send encodedMsgLengthByte");
-            //        return false;
-            //    }
-
-            //    bytesSent += msgFrag.Length * SOC;
-            //    Debug("Fragment sent.");
-
-            //}
             Debug("Message sent.");
             return true;
         }
+
+        public String receiveMessage()
+        {
+            bool flag = false;
+            byte[] encodedLengthByte = new byte[4];
+            uint encodedLength;
+            uint length;
+            int fragCounter = 0;
+            int numBytes;
+            string message = "";
+            byte[] msgBuf = new byte[FRAG_SIZE + 4];
+
+            Debug("Receiving message...");
+            do
+            {
+                //Read Length
+                Debug("Reading msg length...");
+                numBytes = socket.Client.Receive(encodedLengthByte, 4, SocketFlags.None);
+                if (numBytes != 4)
+                {
+                    Error("Could not receive 4 bytes for length.");
+                    return "";
+                }
+                //Convert to littleEndian
+                Array.Reverse(encodedLengthByte);
+
+                encodedLength = System.BitConverter.ToUInt32(encodedLengthByte, 0);
+                length = Encoder.decode_natural(encodedLength);
+                flag = Encoder.decode_flag(encodedLength);
+
+                if (flag)
+                    Debug("Incoming message fragment " + length + " bytes...");
+                else
+                    Debug("Incoming (last) message fragment " + length + " bytes...");
+
+
+                if (length < 1 || length > FRAG_SIZE)
+                {
+                    Error("Illegal msg length " + length);
+                    return "";
+                }
+
+                //Read Msg
+                Debug("Reading msg...");
+                numBytes = socket.Client.Receive(msgBuf, (int)length, SocketFlags.None);
+                message += System.Text.ASCIIEncoding.ASCII.GetString(msgBuf).Substring(0,numBytes);
+                Debug("Read: '" + System.Text.ASCIIEncoding.ASCII.GetString(msgBuf) + "'");
+
+                if (fragCounter > MAX_FRAGS)
+                {
+                    Error ("Maximum fragments reached, aborting.");
+                    return "";
+                }
+
+                fragCounter++;
+            } while (flag == true);
+            return message;
+        }
+
+        //from
+        //    l_i := 1
+        //    l_frag_counter := 0
+        //    l_msg.flag := True
+        //    l_error := False
+        //until
+        //    not l_msg.flag or l_error
+        //loop
+        //    a_http_socket.read_natural_32
+        //    l_msg.length := encoder.decode_natural (a_http_socket.last_natural_32)
+        //    l_msg.flag := encoder.decode_flag (a_http_socket.last_natural_32)
+        //    if not l_msg.is_length_valid then
+        //        l_error := True
+        //        o.eprint ("Could not decode msg length", generating_type)
+        //    else
+        //        l_buf := read_string (a_http_socket, l_msg.length)
+        //        l_msg.append_string(l_buf)
+        //        l_i := l_i + 1
+        //    end
+        //    if (l_frag_counter >= Max_fragments) then
+        //        l_error := True
+        //        o.eprint ("Maximumg fragments reached", generating_type)
+        //    end
+        //    l_frag_counter := l_frag_counter + 1
+        //end
 
         //private String receiveMessage()
         //{

375f522e27207c2c808faf52b034a4518c66a214
fzuend <fzuend@eiffel.com>
2009-05-27 00:27:47 +0000

    - Added new features to xebra iis handler
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@78884 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/framework/web/xebra/c_projects/iis_xebra_handler_testproject/XebraHandlerTester/XebraHandlerTester/Program.cs
+++ Src/framework/web/xebra/c_projects/iis_xebra_handler_testproject/XebraHandlerTester/XebraHandlerTester/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace XebraHandlerTester
@@ -15,7 +13,7 @@ namespace XebraHandlerTester
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new XebraHandlerTesterMainForm());
         }
     }
 }

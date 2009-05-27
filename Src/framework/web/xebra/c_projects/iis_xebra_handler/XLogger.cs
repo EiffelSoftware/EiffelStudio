837a8f73b1f926e6abf26a90c701f4e66c107a95
fzuend <fzuend@eiffel.com>
2009-05-27 00:31:51 +0000

    - Added files
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@78885 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/framework/web/xebra/c_projects/iis_xebra_handler/XLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xebra
+{
+    interface XLogger
+    {
+        void Debug(String msg);
+        void Error(String msg);
+        void Warning(String msg);
+        void Info(String msg);            
+    }
+}
---
+++ Src/framework/web/xebra/c_projects/iis_xebra_handler/XWindowsLogger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace Xebra
+{
+    class XWindowsLogger : XLogger
+    {
+        #region XLogger Members
+
+        public void Debug(string msg)
+        {
+            Info(msg);
+        }
+
+        public void Error(string msg)
+        {
+            Log(msg, EventLogEntryType.Error);
+        }
+
+        public void Info(string msg)
+        {
+            Log(msg, EventLogEntryType.Information);
+        }
+
+        public void Warning(string msg)
+        {
+            Log(msg, EventLogEntryType.Warning);
+        }
+     
+        private void Log(String msg, EventLogEntryType type)
+        {
+            if (msg.Length > 32766)
+                EventLog.WriteEntry("XebraHandler", msg.Substring(0,32766), type);
+            else
+                EventLog.WriteEntry("XebraHandler", msg, type);
+        }
+        #endregion
+    }
+}

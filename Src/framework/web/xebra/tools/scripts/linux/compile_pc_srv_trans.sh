82db859e43e9df745861b52981b5a64a12360476
fzuend <fzuend@eiffel.com>
2009-09-01 22:41:42 +0000

    - Deleted old scripts - Added new windows installer
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@80578 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/framework/web/xebra/tools/scripts/linux/compile_pc_srv_trans.sh
+++
@@ -1,3 +0,0 @@
-ec -experiment -config $XEBRA_DEV/eiffel_projects/library/xebra_precompile/xebra_precompile.ecf -target xebra_precompile -c_compile -clean -precompile
-ec -experiment -config $XEBRA_DEV/eiffel_projects/xebra_translator/xebra_translator-voidunsafe.ecf -target xebra_translator -c_compile -clean -finalize
-ec -experiment -config $XEBRA_DEV/eiffel_projects/xebra_server/xebra_server-voidunsafe.ecf -target xebra_server -c_compile -clean 
--- Src/framework/web/xebra/tools/scripts/linux/superclean.sh
+++
@@ -1,5 +0,0 @@
-echo "Deleting all .generated folders..."
-find -type d -name .generated | xargs rm -rf
-echo "Deleting all EIFGENs folders..."
-find -type d -name EIFGENs | xargs rm -rf
-echo "Done"

3f3874726db1ed007923cf5b49e0f0bb1fd704ec
Emmanuel Stapf <manus@eiffel.com>
2009-10-16 21:23:47 +0000

    Previous fix was actually not correct for modal dialog because we incorrectly interpreted the return value of `SetCapture'. Now instead, we check if the top level window is a dialog or not, and if it is a dialog we make the hook active, otherwise the hook does nothing. Fixed issue on Windows Vista and Windows 7 with aeroglass effect enabled where the pick and drop was actually quite slow (it slowed down the movement of the mouse) by moving the hook to its own thread. That way the mouse movement was not impacted by the drawing code. Because the drawing code is slow with the effect enabled we have added detection of the presence of the effect and in that case we only post WM_MOUSEMOVE message every 150ms which showed a quite good performance (only estimated by testing).
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@81185 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/experimental/library/wel/Clib/capture.c
+++ Src/experimental/library/wel/Clib/capture.c
@@ -1,6 +1,13 @@
 /*
 indexing
-	description: "Functions used to implement capture beyond the boundaries of the process."
+	description: "[
+		Functions used to implement capture beyond the boundaries of the process.
+		Because the hook processing WH_MOUSE_LL is done in the thread that installed it,
+		it means that by default it would be in the GUI thread. That would slow down
+		mouse movement if the application responds to move events (such as drawing the
+		line for pick and drop under Vista/Windows 7 and the desktop composition).
+		Therefore we install the hook in its own thread.
+		]"
 	copyright:	"Copyright (c) 1984-2006, Eiffel Software and others"
 	license:	"Eiffel Forum License v2 (see http://www.eiffel.com/licensing/forum.txt)"
 	source: "[
@@ -22,10 +29,35 @@ indexing
 #include <windows.h>
 #include "wel_capture.h"
 
-static HWND hook_window;
-static HHOOK mouse_hook;
+static HWND hook_window = NULL;
+static HWND captured_window = NULL;
+static int is_top_dialog = 0;
+static HANDLE hThread = NULL;
+static DWORD dwThreadId = 0;
 static DWORD hook_process_id;
-static int already_captured = 0;
+static int is_desktop_composition_active = 0;
+
+/* This variable is only accessed from the spawned thread. */
+static HHOOK mouse_hook;
+static DWORD tick_count;
+
+/* Find out if desktop windows composition is active or not. This is done dynamically since
+ * the DLL does not exist on older version of Windows. */
+void compute_desktop_composition () {
+	HMODULE dwmapi;
+	is_desktop_composition_active = 0;
+	dwmapi = LoadLibrary(L"dwmapi.dll");
+	if (dwmapi) {
+		FARPROC query = GetProcAddress(dwmapi, "DwmIsCompositionEnabled");
+		if (query) {
+			BOOL val;
+			HRESULT hr;
+			hr = ((FUNCTION_CAST_TYPE (HRESULT, WINAPI, (BOOL *)) query) (&val));
+			is_desktop_composition_active = (hr == S_OK) && (val == TRUE);
+		}
+		FreeLibrary (dwmapi);
+	}
+}
 
 /*---------------------------------------------------------------------------*/
 /* FUNC: cwel_mouse_hook_proc                                                */
@@ -47,9 +79,8 @@ LRESULT CALLBACK cwel_mouse_hook_proc(int nCode, WPARAM wParam, LPARAM lParam)
 	if (nCode < 0) {
 		return CallNextHookEx(mouse_hook, nCode, wParam, lParam);
 	} else if (hook_window == NULL) {
-			/* Something wrong happened... We don't have any window target So we stop the hook. */
-		cwel_release_capture();
-		return 1;
+			/* Something wrong happened... We don't have any window target So we just forward the hook. */
+		return CallNextHookEx(mouse_hook, nCode, wParam, lParam);
 	} else {
 		/* Transform & redirect the mouse message to the target window. */
 		switch (Msg) {
@@ -68,7 +99,7 @@ LRESULT CALLBACK cwel_mouse_hook_proc(int nCode, WPARAM wParam, LPARAM lParam)
 						 * called `SetCapture'. */
 					if (hover) {
 						(void) GetWindowThreadProcessId(hover, &l_process_id);
-						if (already_captured || (l_process_id != hook_process_id)) {
+						if (is_top_dialog || (l_process_id != hook_process_id)) {
 								/* The current window is not the target
 								 * window, so we simply post the message nothing the system to
 								 * forward the message to the current window
@@ -80,7 +111,18 @@ LRESULT CALLBACK cwel_mouse_hook_proc(int nCode, WPARAM wParam, LPARAM lParam)
 								 * to the target window. */
 							GetWindowRect(hook_window, &rect);
 							dwMousePos = MAKELONG(pInfo->pt.x - rect.left, pInfo->pt.y - rect.top);
-							PostMessage(hook_window, Msg, 0, dwMousePos);
+								/* Do not send all messages when DWM is active as if your
+								 * code involves drawing like in pick and drop it is quite slow to draw
+								 * directly on the desktop. */
+							if (is_desktop_composition_active && Msg == WM_MOUSEMOVE) {
+								DWORD new_tick_count = GetTickCount();
+								if (abs(new_tick_count - tick_count) > 150) {
+									tick_count = new_tick_count;
+									PostMessage(hook_window, Msg, 0, dwMousePos);
+								}
+							} else {
+								PostMessage(hook_window, Msg, 0, dwMousePos);
+							}
 						}
 					}
 				}
@@ -92,6 +134,46 @@ LRESULT CALLBACK cwel_mouse_hook_proc(int nCode, WPARAM wParam, LPARAM lParam)
 		return CallNextHookEx(mouse_hook, nCode, wParam, lParam);
 	}
 }
+
+/*---------------------------------------------------------------------------*/
+/* FUNC: cwel_main_for_capture                                               */
+/*---------------------------------------------------------------------------*/
+/* Main routine for thread that will handle the capture. It installs the hook*/
+/* and process events for the thread until it gets the WM_QUIT message where */
+/* it removes the hook and exit gracefully.                                  */
+/*---------------------------------------------------------------------------*/
+rt_private DWORD WINAPI cwel_main_for_capture (LPVOID param)
+{
+	MSG Msg;
+
+		/* Get Current number of ticks to reduce the number of PostMessage posted when mouse is moving
+		 * on Windows platform with the desktop composition effect. */
+	tick_count = GetTickCount();
+
+		/* Hook the mouse messages */
+	mouse_hook = SetWindowsHookEx (
+		WH_MOUSE_LL,			/* hook type */
+		cwel_mouse_hook_proc,	/* hook procedure */
+		GetModuleHandle(NULL),	/* handle to application instance */
+		0	/* thread identifier */
+		);
+
+		/* If we succeed in creating the hook, that's good otherwise it means that
+		 * capture will be limited to the current application. */
+	if (mouse_hook) {
+		while (GetMessage(&Msg, NULL, 0, 0) > 0) {
+			TranslateMessage(&Msg);
+			DispatchMessage(&Msg);
+		}
+
+		UnhookWindowsHookEx(mouse_hook);
+		mouse_hook = NULL;
+	}
+
+	ExitThread(0);
+	return 0;
+}
+
 
 /*---------------------------------------------------------------------------*/
 /* FUNC: cwel_captured_window                                                */
@@ -110,28 +192,47 @@ HWND cwel_captured_window(void)
 /* Capture all mouse messages and redirect them to `hWnd'.                   */
 /*---------------------------------------------------------------------------*/
 void cwel_capture (HWND hWnd) {
+
 	if (hWnd && IsWindow(hWnd)) {
-		if (hook_window || mouse_hook) {
+		HWND top_window;
+
+			/* Find out if we are on Vista/Windows 7 with desktop composition. */
+		compute_desktop_composition();
+
+		if (hook_window) {
 				/* The Caller did not yet call `cwel_release_capture', we do it for him. */
 			cwel_release_capture();
 		}
 
 		hook_window = hWnd;
-		if (!SetCapture(hook_window)) {
-			already_captured = 1;
+		top_window = GetAncestor (hWnd, GA_ROOT);
+		if (top_window && GetWindowLongPtr(top_window, DWLP_DLGPROC)) {
+			is_top_dialog = 1;
 		} else {
-			already_captured = 0;
+			is_top_dialog = 0;
+		}
+		captured_window = SetCapture (hook_window);
+			/* The code below is to restore the capture if there was already one. */
+		if (captured_window) {
+			if (!ReleaseCapture()) {
+					/* We cannot release the capture from `captured_window', we gave up
+					 * as it simply means that underlying widgets will also get the events. */
+				captured_window = NULL;
+			} else if (SetCapture (hook_window)) {
+					/* This routine should not fail, but it does, it is like above and 
+					 * underlying widgets will also get the events. */
+			}
 		}
 
 		(void) GetWindowThreadProcessId (hook_window, &hook_process_id);
 
-			/* Hook the mouse messages */
-		mouse_hook = SetWindowsHookEx (
-			WH_MOUSE_LL,			/* hook type */
-			cwel_mouse_hook_proc,	/* hook procedure */
-			GetModuleHandle(NULL),	/* handle to application instance */
-			0						/* thread identifier */
-			);
+		hThread = CreateThread (
+			NULL,			/* default security attributes */
+			0,				/* use default stack size */
+			cwel_main_for_capture,	/* thread function */
+			NULL,			/* argument to thread function */
+			0,				/* use default creation flags */
+			&dwThreadId);	/* returns the thread identifier */
 	}
 }
 
@@ -143,9 +244,18 @@ void cwel_capture (HWND hWnd) {
 void cwel_release_capture() {
 	hook_window = NULL;
 		/* If the hook was successful in `cwel_capture' we should release it. */
-	if (mouse_hook) {
-		UnhookWindowsHookEx(mouse_hook);
-		mouse_hook = NULL;
+	if (hThread) {
+		PostThreadMessage (dwThreadId, WM_QUIT, (WPARAM) 0, (LPARAM) 0);
+		WaitForSingleObject (hThread, INFINITE);
+		CloseHandle (hThread);
+		hThread = NULL;
 	}
+		/* This call may fail if we initially failed at the SetCapture call in `cwel_capture' but regardless
+		 * we do not need to check for errors. */
 	ReleaseCapture();
+		/* Restore the initial capture. */
+	if (captured_window) {
+		SetCapture (captured_window);
+		captured_window = NULL;
+	}
 }

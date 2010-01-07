3fed4d23a83319628d6de0a416780af76faf26a8
tedf <tedf@eiffel.com>
2010-01-07 09:48:30 +0000

    Added the first version of Eiffel libevent based on libevent-2.0.3-alpha. This is a basic version of event part which is only is tested on Ubuntu. On other platforms, some tunings are needed. C library libevent should be updated when libevent2 release is out.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@82014 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/experimental/library/libevent/Clib/eiffel_libevent.c
@@ -0,0 +1,44 @@
+/*
+indexing
+	description: "Functions used by the class LIBEVENT_HANDLER."
+	copyright:	"Copyright (c) 1984-2006, Eiffel Software and others"
+	license:	"Eiffel Forum License v2 (see http://www.eiffel.com/licensing/forum.txt)"
+	source: "[
+			 Eiffel Software
+			 356 Storke Road, Goleta, CA 93117 USA
+			 Telephone 805-685-1006, Fax 805-685-6869
+			 Website http://www.eiffel.com
+			 Customer support http://support.eiffel.com
+		]"
+*/
+
+#include "eiffel_libevent.h"
+
+typedef void (* EIF_EVCB_FUCTION) (
+#ifndef EIF_IL_DLL
+	EIF_REFERENCE,  /* Eiffel object */
+#endif
+	EIF_INTEGER
+	);
+
+/* Release Eiffel CURL_FUNCTION object address */
+//void c_release_object()
+//{
+	//eif_wean (eiffel_function_object);
+//}
+
+/* Direct C callback */
+void c_event_callback(int fd, short event, void *arg)
+{
+	EIF_EVCBDATAP l_cb_data = (EIF_EVCBDATAP)arg;
+	EIF_EVCB_FUCTION l_func = (EIF_EVCB_FUCTION)l_cb_data->func;
+	if (l_func != NULL) {
+		(l_func (
+#ifndef EIF_IL_DLL
+				eif_access (l_cb_data->o),
+				event
+#endif
+			)
+		);
+	}
+}
---
+++ Src/experimental/library/libevent/buffer_event.e
@@ -0,0 +1,13 @@
+note
+	description: "bufferevent presentation in libevent. To be implemented."
+	date: "$Date$"
+	revision: "$Revision$"
+
+class
+	BUFFER_EVENT
+
+inherit
+	EVENT_ANY
+
+
+end

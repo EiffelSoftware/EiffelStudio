06d065032aa6a085386987083f8fea27f3c71d71
Emmanuel Stapf <manus@eiffel.com>
2010-03-17 15:58:08 +0000

    Replaced ec by ecb.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@82593 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/examples/vision2/widget_test/retrieve_flat_short.bat
+++ Src/examples/vision2/widget_test/retrieve_flat_short.bat
@@ -1,12 +1,12 @@
 rd /Q /S %TEMP%\vision2_precompile
 mkdir %TEMP%\vision2_precompile
-%ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec -config vision2.ecf -local -project_path %TEMP%\vision2_precompile
+%ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ecb -config vision2.ecf -local -project_path %TEMP%\vision2_precompile
 
 REM Create flatshort directory
 MD .\flatshort
 
 REM now perform generation based on "flatshort_instructions.txt"
-type flatshort_instructions.txt | %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec -config vision2.ecf -local -project_path %TEMP%\vision2_precompile -loop
+type flatshort_instructions.txt | %ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ecb -config vision2.ecf -local -project_path %TEMP%\vision2_precompile -loop
 
 REM remove vision2_precompile directory.
 rd /Q /S %TEMP%\vision2_precompile

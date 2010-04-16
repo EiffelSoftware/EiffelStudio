4fb12eae8873e6549fcc67fb7df065d59d60692e
Jocelyn Fiat <jfiat@eiffel.com>
2010-04-16 20:56:32 +0000

    Added 2 (dos) scripts to build and run specific tests
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@82858 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/framework/xml/tests/build.bat
@@ -0,0 +1,2 @@
+ec -config test_xml_parser.ecf -full -finalize -c_compile -project_path bin -clean -target xml_parser
+ec -config test_xml_parser.ecf -full -finalize -c_compile -project_path bin -clean -target gobo_xml_parser

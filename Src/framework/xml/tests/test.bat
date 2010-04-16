96c1f521868a1d115b9cfe89eba46ad3f74b0189
Jocelyn Fiat <jfiat@eiffel.com>
2010-04-16 20:56:32 +0000

    Added 2 (dos) scripts to build and run specific tests
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@82858 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/framework/xml/tests/test.bat
@@ -0,0 +1,99 @@
+setlocal
+@echo off
+set FILENAME=C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\en\mscorlib.xml
+
+set ECHOTEST=echo [test_xml] 
+chdir %~dp0\bin\EIFGENs\xml_parser\F_code
+set TEST=test_xml.exe
+
+rem From FILE
+set TEST_XML_PARSER=file
+
+%ECHOTEST%  FILE+NULL ------------------------------------------
+set TEST_XML_PARSER=null
+%TEST% %FILENAME%
+
+rem %ECHOTEST%  FILE+DEBUG -----------------------------------------
+rem set TEST_XML_PARSER=debug
+rem %TEST% %FILENAME%
+
+%ECHOTEST%  FILE+TREE  -----------------------------------------
+set TEST_XML_PARSER=tree
+%TEST% %FILENAME%
+
+%ECHOTEST%  FILE+XML_TREE --------------------------------------
+set TEST_XML_PARSER=xml_tree
+%TEST% %FILENAME%
+
+rem From STRING
+set TEST_XML_PARSER_KIND=string
+
+%ECHOTEST%  STRING+NULL ----------------------------------------
+set TEST_XML_PARSER=null
+%TEST% %FILENAME%
+
+rem %ECHOTEST%  STRING+DEBUG ---------------------------------------
+rem set TEST_XML_PARSER=debug
+rem %TEST% %FILENAME%
+
+%ECHOTEST%  STRING+TREE ----------------------------------------
+set TEST_XML_PARSER=tree
+%TEST% %FILENAME%
+
+%ECHOTEST%  STRING+XML_TREE ------------------------------------
+set TEST_XML_PARSER=xml_tree
+%TEST% %FILENAME%
+
+rem %ECHOTEST%  STRING+XML_TREE_VIS --------------------------------
+rem set TEST_XML_PARSER=xml_tree_vis
+rem %TEST% %FILENAME%
+
+echo ==========================================================
+set ECHOTEST=echo [test_gobo_xml] 
+chdir %~dp0\bin\EIFGENs\gobo_xml_parser\F_code
+set TEST=test_gobo_xml.exe
+
+rem From FILE
+set TEST_XML_PARSER_KIND=file
+
+%ECHOTEST%  FILE+NULL ------------------------------------------
+set TEST_XML_PARSER=null
+%TEST% %FILENAME%
+
+rem %ECHOTEST%  FILE+DEBUG -----------------------------------------
+rem set TEST_XML_PARSER=debug
+rem %TEST% %FILENAME%
+
+%ECHOTEST%  FILE+TREE  -----------------------------------------
+set TEST_XML_PARSER=tree
+%TEST% %FILENAME%
+
+%ECHOTEST%  FILE+XML_TREE --------------------------------------
+set TEST_XML_PARSER=xml_tree
+%TEST% %FILENAME%
+
+rem From STRING
+set TEST_XML_PARSER_KIND=string
+
+%ECHOTEST%  STRING+NULL ----------------------------------------
+set TEST_XML_PARSER=null
+%TEST% %FILENAME%
+
+rem %ECHOTEST%  STRING+DEBUG ---------------------------------------
+rem set TEST_XML_PARSER=debug
+rem %TEST% %FILENAME%
+
+%ECHOTEST%  STRING+TREE ----------------------------------------
+set TEST_XML_PARSER=tree
+%TEST% %FILENAME%
+
+%ECHOTEST%  STRING+XML_TREE ------------------------------------
+set TEST_XML_PARSER=xml_tree
+%TEST% %FILENAME%
+
+rem %ECHOTEST%  STRING+XML_TREE_VIS --------------------------------
+rem set TEST_XML_PARSER=xml_tree_vis
+rem %TEST% %FILENAME%
+
+
+endlocal

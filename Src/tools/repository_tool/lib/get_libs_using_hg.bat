3ce70b259a816c88e901b8e8cc1f15cf5c8e8199
Jocelyn Fiat <jfiat@eiffel.com>
2010-08-16 10:59:31 +0000

    now eapml and eel are hosted on github.com , and thus using git and not anymore hg (mercurial)
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@84181 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/tools/repository_tool/lib/get_libs_using_hg.bat
+++
@@ -1,2 +0,0 @@
-hg clone https://eapml.googlecode.com/hg/ eapml
-hg clone https://eiffel-encryption-library.googlecode.com/hg/ eel
---
+++ Src/tools/repository_tool/lib/get_ligs_using_git.bat
@@ -0,0 +1,3 @@
+git clone http://github.com/clemahieu/eapml.git eapml
+git clone http://github.com/clemahieu/eel.git eel
+

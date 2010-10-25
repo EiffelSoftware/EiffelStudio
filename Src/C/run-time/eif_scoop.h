b90725caec27bddc04bc19036a2f944652c3f1c5
Alexander Kogtenkov <alexk@eiffel.com>
2010-10-25 13:23:29 +0000

    Added functions to handle separate calls.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@84702 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/C/run-time/eif_scoop.h
@@ -0,0 +1,59 @@
+/*
+	description:	"SCOOP support."
+	date:		"$Date$"
+	revision:	"$Revision$"
+	copyright:	"Copyright (c) 2010, Eiffel Software."
+	license:	"GPL version 2 see http://www.eiffel.com/licensing/gpl.txt)"
+	licensing_options:	"Commercial license is available at http://www.eiffel.com/licensing"
+	copying: "[
+			This file is part of Eiffel Software's Runtime.
+			
+			Eiffel Software's Runtime is free software; you can
+			redistribute it and/or modify it under the terms of the
+			GNU General Public License as published by the Free
+			Software Foundation, version 2 of the License
+			(available at the URL listed under "license" above).
+			
+			Eiffel Software's Runtime is distributed in the hope
+			that it will be useful,	but WITHOUT ANY WARRANTY;
+			without even the implied warranty of MERCHANTABILITY
+			or FITNESS FOR A PARTICULAR PURPOSE.
+			See the	GNU General Public License for more details.
+			
+			You should have received a copy of the GNU General Public
+			License along with Eiffel Software's Runtime; if not,
+			write to the Free Software Foundation, Inc.,
+			51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+		]"
+	source: "[
+			 Eiffel Software
+			 356 Storke Road, Goleta, CA 93117 USA
+			 Telephone 805-685-1006, Fax 805-685-6869
+			 Website http://www.eiffel.com
+			 Customer support http://support.eiffel.com
+		]"
+*/
+
+#ifndef _eif_scoop_h_
+#define _eif_scoop_h_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+	EIF_REFERENCE   target;       /* Target of a call */
+	BODY_INDEX      body_index;   /* Routine to be called */
+	EIF_NATURAL_32  count;        /* Number of arguments excluding target object */
+	EIF_TYPED_VALUE argument [1]; /* Arguments excluding target object */
+} call_data;
+
+rt_public void eif_log_procedure (int static_type_id, int feature_id, uint16 current_pid, call_data * data);
+
+#ifdef __cplusplus
+}
+
+#endif
+
+#endif	/* _eif_scoop_h_ */
+
---
+++ Src/C/run-time/scoop.c
@@ -0,0 +1,90 @@
+/*
+	description:	"SCOOP support."
+	date:		"$Date$"
+	revision:	"$Revision$"
+	copyright:	"Copyright (c) 2010, Eiffel Software."
+	license:	"GPL version 2 see http://www.eiffel.com/licensing/gpl.txt)"
+	licensing_options:	"Commercial license is available at http://www.eiffel.com/licensing"
+	copying: "[
+			This file is part of Eiffel Software's Runtime.
+
+			Eiffel Software's Runtime is free software; you can
+			redistribute it and/or modify it under the terms of the
+			GNU General Public License as published by the Free
+			Software Foundation, version 2 of the License
+			(available at the URL listed under "license" above).
+
+			Eiffel Software's Runtime is distributed in the hope
+			that it will be useful,	but WITHOUT ANY WARRANTY;
+			without even the implied warranty of MERCHANTABILITY
+			or FITNESS FOR A PARTICULAR PURPOSE.
+			See the	GNU General Public License for more details.
+
+			You should have received a copy of the GNU General Public
+			License along with Eiffel Software's Runtime; if not,
+			write to the Free Software Foundation, Inc.,
+			51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+		]"
+	source: "[
+			 Eiffel Software
+			 356 Storke Road, Goleta, CA 93117 USA
+			 Telephone /R805-685-1006, Fax 805-685-6869
+			 Website http://www.eiffel.com
+			 Customer support http://support.eiffel.com
+		]"
+*/
+
+/*
+doc:<file name="scoop.c" header="eif_scoop.h" version="$Id$" summary="SCOOP support.">
+*/
+
+#include "rt_globals.h"
+#include "rt_struct.h"
+#include "rt_wbench.h"
+
+
+#ifdef WORKBENCH
+rt_public void eif_log_procedure (int s, int f, uint16 p, call_data * a)
+{
+	BODY_INDEX body_id;
+	EIF_REFERENCE t = a -> target;
+
+	CBodyId(body_id,Routids(s)[f],Dtype(t));
+	a -> body_index = body_id;
+	RTS_TCB(scoop_task_add_command,p,RTS_PID(t),body_id,a,EIFNULL);
+}
+ 
+rt_public void eif_try_call (call_data * a)
+{
+	RT_GET_CONTEXT
+	uint32            pid = 0; /* Pattern id of the frozen feature */
+	EIF_NATURAL_32    i;
+	EIF_NATURAL_32    n;
+	BODY_INDEX        body_id = a -> body_index;
+	EIF_TYPED_VALUE * v;
+
+		/* Push arguments to the evaluation stack */
+	for (n = a -> count, i = 0; i < n; i++) {
+		* (iget ()) = a -> argument [i];
+	}
+		/* Push current to the evaluation stack */
+	v = iget ();
+	v -> it_r = a -> target;
+	v -> type = SK_REF;
+		/* Make a call */
+	if (egc_frozen [body_id]) {		/* We are below zero Celsius, i.e. ice */
+		pid = (uint32) FPatId(body_id);
+		(pattern[pid].toc)(egc_frozen[body_id]); /* Call pattern */
+	} else {
+		/* The proper way to start the interpretation of a melted feature is to call `xinterp'
+		 * in order to initialize the calling context (which is not done by `interpret').
+		 * `tagval' will therefore be set, but we have to resynchronize the registers anyway.
+		 */
+		xinterp(MTC melt[body_id], 0);
+	}
+}
+#endif
+
+/*
+doc:</file>
+*/

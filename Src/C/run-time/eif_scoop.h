cc63b09a76a3f416e8454b19f0e94e7dc92a9502
king <king@eiffel.com>
2010-11-07 08:33:14 +0000

    Refactored atomic operations for gcc
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@84854 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C/run-time/eif_scoop.h
+++ Src/C/run-time/eif_scoop.h
@@ -49,8 +49,57 @@ typedef struct {
 	EIF_TYPED_VALUE   argument [1]; /* Arguments excluding target object */
 } call_data;
 
+#ifdef WORKBENCH
 rt_public void eif_log_call (int static_type_id, int feature_id, EIF_SCP_PID current_pid, call_data * data);
 rt_public void eif_log_callp (int origin, int offset, EIF_SCP_PID current_pid, call_data * data);
+#endif
+
+/* Atomic Operations */
+
+#define EIF_ATOMIC_INLINE inline
+
+static EIF_ATOMIC_INLINE EIF_INTEGER_32 eif_atomic_compare_and_swap_integer_32 (EIF_INTEGER_32 *dest, EIF_INTEGER_32 setter, EIF_INTEGER_32 compare)
+	{
+		// Atomically update dest contents to setter if original contents were equal to compare, return original value, caller checks if value equals compare value, if so then dest must now equal setter, if not then no operation occurred.
+#if defined(_WIN32)
+		return (EIF_INTEGER_32) _InterlockedCompareExchange((long *)dest, setter, compare);
+#elif defined (__GNUC__)
+		return __sync_val_compare_and_swap (dest, compare, setter);
+#else
+		return (EIF_INTEGER_32)0;
+#endif
+	}
+
+static EIF_ATOMIC_INLINE EIF_INTEGER_32 eif_atomic_swap_integer_32 (EIF_INTEGER_32 *dest, EIF_INTEGER_32 setter)
+	{
+		// Atomically update contents of dest to setter, return original value of dest.
+#if defined(_WIN32)
+		return (EIF_INTEGER_32) _InterlockedExchange((long *)dest, setter);
+#elif defined (__GNUC__)
+		return (EIF_INTEGER_32) __sync_val_compare_and_swap (dest, *dest, setter);
+#else
+		return (EIF_INTEGER_32)0;
+#endif
+	}
+
+static EIF_ATOMIC_INLINE EIF_INTEGER_32 eif_atomic_add_integer_32 (EIF_INTEGER_32 *dest, EIF_INTEGER_32 val)
+	{
+		// Atomically update dest target to original value plus val, return this new value.
+#if defined(_WIN32)
+		return (EIF_INTEGER_32) (_InterlockedExchangeAdd((long *)dest, val) + val); // Add val to return new value.
+#elif defined (__GNUC__)
+		return (EIF_INTEGER_32) __sync_add_and_fetch (dest, val);
+#else
+		return (EIF_INTEGER_32)0;
+#endif
+	}
+
+#define RTS_ACAS_I32(dest,setter,compare) eif_atomic_compare_and_swap_integer_32((EIF_INTEGER_32 *)dest,setter,compare)
+#define RTS_AS_I32(dest, setter) eif_atomic_swap_integer_32((EIF_INTEGER_32 *)dest,setter)
+#define RTS_AI_I32(dest) eif_atomic_add_integer_32((EIF_INTEGER_32 *)dest, 1)
+#define RTS_AD_I32(dest) eif_atomic_add_integer_32((EIF_INTEGER_32 *)dest, -1)
+#define RTS_AA_I32(dest, val) eif_atomic_add_integer_32((EIF_INTEGER_32 *)dest,val)
+
 
 #ifdef __cplusplus
 }

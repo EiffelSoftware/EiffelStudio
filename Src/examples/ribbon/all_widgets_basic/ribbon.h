8000a656ea7f5b6bd64241161de54a697a93e5e4
larryl <larryl@eiffel.com>
2011-03-14 08:48:49 +0000

    Added example to demonstrate all supported ribbon widgets basically. The features of ribbon widgets (such as setting label, image or select_action etc) are not demonstrated in this example.
    Most of codes in this example are generated by EiffelRibbon tool. Using EiffelRibbon tool to open "ribbon_project.er".
    
    
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@85861 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/examples/ribbon/all_widgets_basic/ribbon.h
@@ -0,0 +1,33 @@
+// *****************************************************************************
+// * This is an automatically generated header file for UI Element definition  *
+// * resource symbols and values. Please do not modify manually.               *
+// *****************************************************************************
+
+#pragma once
+
+#define tab_1 2 
+#define tab_1_LabelTitle_RESID 60001
+#define group_1 3 
+#define button_1 4 
+#define button_1_LabelTitle_RESID 60002
+#define button_1_SmallImages_RESID 60003
+#define toggle_button_1 5 
+#define toggle_button_1_LabelTitle_RESID 60004
+#define checkbox_1 6 
+#define checkbox_1_LabelTitle_RESID 60005
+#define spinner_1 7 
+#define spinner_1_LabelTitle_RESID 60006
+#define combobox_1 8 
+#define combobox_1_LabelTitle_RESID 60007
+#define split_button_1 9 
+#define split_button_1_LabelTitle_RESID 60008
+#define button_3 10 
+#define button_3_LabelTitle_RESID 60009
+#define button_4 11 
+#define button_4_LabelTitle_RESID 60010
+#define drop_down_gallery_1 12 
+#define drop_down_gallery_1_LabelTitle_RESID 60011
+#define button_2 13 
+#define button_2_LabelTitle_RESID 60012
+#define button_2_LargeImages_RESID 60013
+#define InternalCmd2_LabelTitle_RESID 60014

7c75aea915db060fb17ab071c053b06fa6d9b0a8
Jocelyn Fiat <jfiat@eiffel.com>
2011-04-04 15:23:34 +0000

    Fixed and updated script to work with various structure of the working copy
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@86121 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/Eiffel/interface/new_graphical/shared/build_pixmaps.cmd
+++ Src/Eiffel/interface/new_graphical/shared/build_pixmaps.cmd
@@ -1,6 +1,9 @@
 set FRAMEFILE="%EIFFEL_SRC%\tools\eiffel_matrix_code_generator\frames\studio.e.frame"
+
 set INIDIR="%EIFFEL_SRC%\Delivery\studio\bitmaps\png"
-emcgen -frame "%FRAMEFILE%" "%INIDIR%\10x10.ini"
-emcgen -frame "%FRAMEFILE%" "%INIDIR%\12x12.ini
-emcgen -frame "%FRAMEFILE%" "%INIDIR%\16x16.ini
-emcgen -frame "%FRAMEFILE%" "%INIDIR%\cursors.ini
+if NOT EXIST %INIDIR% set INIDIR="%EIFFEL_SRC%\..\Delivery\studio\bitmaps\png"
+
+emcgen --frame "%FRAMEFILE%" "%INIDIR%\10x10.ini"
+emcgen --frame "%FRAMEFILE%" "%INIDIR%\12x12.ini
+emcgen --frame "%FRAMEFILE%" "%INIDIR%\16x16.ini
+emcgen --frame "%FRAMEFILE%" "%INIDIR%\cursors.ini

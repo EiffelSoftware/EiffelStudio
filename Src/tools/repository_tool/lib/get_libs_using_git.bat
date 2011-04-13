d2d39c2a6bbbb1c2ed0de9fa0549d9f40c93cc2d
Jocelyn Fiat <jfiat@eiffel.com>
2011-04-13 19:06:13 +0000

    Fixed dos batch to get source using git
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@86246 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/tools/repository_tool/lib/get_libs_using_git.bat
+++ Src/tools/repository_tool/lib/get_libs_using_git.bat
@@ -1,3 +1,3 @@
-git clone http://github.com/clemahieu/eapml.git eapml
-git clone http://github.com/clemahieu/eel.git eel
+@call git clone http://github.com/clemahieu/eapml.git eapml
+@call git clone http://github.com/clemahieu/eel.git eel
 

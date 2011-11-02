7ee9303252f0e10b6408851da4ca82f82cc71850
Emmanuel Stapf <manus@eiffel.com>
2011-11-02 23:37:45 +0000

    Removed building of documentation package since we don't offer it anymore.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@87700 8089f293-4706-0410-a29e-feb5c42a2edf

--- Delivery/scripts/unix/packaging/debian/control.sh
+++ Delivery/scripts/unix/packaging/debian/control.sh
@@ -14,14 +14,3 @@ Description: $SUMMARY
 sed -e "s/^\(.\+\)$/ \1/" -e "s/^$/ ./" $PACKAGING_DIR/description >> $DEBIAN_DIR/control
 
 $SPITSHELL >> $DEBIAN_DIR/control <<!GROK!THIS!
-
-
-Package: $PRODUCT-doc
-Section: doc
-Priority: optional
-Architecture: all
-Depends: 
-Description: $SUMMARY (Documentation)
-!GROK!THIS!
-
-sed -e "s/^\(.\+\)$/ \1/" -e "s/^$/ ./" $PACKAGING_DIR/description >> $DEBIAN_DIR/control

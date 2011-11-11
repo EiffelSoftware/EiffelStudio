3fbb5aa44863d9d8afec29212f5ba0efc1f4ea18
Jocelyn Fiat <jfiat@eiffel.com>
2011-11-11 17:18:39 +0000

    Using new location for eel and eapml   (in $ISE_LIBRARY/contrib/library/....)
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@87788 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/tools/repository_tool/lib/get_libs_from_7z.bat
+++
@@ -1,5 +0,0 @@
-wget http://github.com/clemahieu/eapml/zipball/master -O eapml.zip
-7z x eapml.zip
-
-wget http://github.com/clemahieu/eel/zipball/master -O eel.zip
-7z x eel.zip
--- Src/tools/repository_tool/lib/get_libs_using_git.bat
+++
@@ -1,3 +0,0 @@
-@call git clone http://github.com/clemahieu/eapml.git eapml
-@call git clone http://github.com/clemahieu/eel.git eel
-

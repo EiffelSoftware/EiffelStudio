bd5dc3e7ed7fcf63d8423815916d98ac3b8049a1
Emmanuel Stapf <manus@eiffel.com>
2012-01-12 07:24:07 +0000

    Updated to the 7.1 release.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@88168 8089f293-4706-0410-a29e-feb5c42a2edf

--- Delivery/scripts/unix/packaging/debian/changelog.sh
+++ Delivery/scripts/unix/packaging/debian/changelog.sh
@@ -1,7 +1,7 @@
 $SPITSHELL >$DEBIAN_DIR/changelog <<!GROK!THIS!
 $PRODUCT ($VERSION-1) optional; urgency=low
 
-  * EiffelStudio 7.0 release.
+  * EiffelStudio 7.1 release.
 
  -- $NAME  `date -R`
 

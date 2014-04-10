ff3edf0109b7afd09d3a8bf9650326bc86fbccd2
Jocelyn Fiat <jfiat@eiffel.com>
2014-04-10 21:10:51 +0000

    Updated install.bat Windows scripts (to install quickly the iron tools for testing in current delivery)
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@94821 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/tools/iron/client/commands/install.bat
+++ Src/tools/iron/client/commands/install.bat
@@ -1,17 +1,22 @@
 setlocal
 
 mkdir %cd%\.comp
-ecb -config %cd%\update_ecf\update_ecf.ecf -finalize -c_compile -project_path %cd%\.comp
+ecb -config %~dp0\update_ecf\update_ecf.ecf -finalize -c_compile -project_path %cd%\.comp
 move %cd%\.comp\EIFGENs\update_ecf\F_code\update_ecf.exe update_ecf.exe
 rd /q/s %cd%\.comp\EIFGENs\update_ecf
 
-ecb -config %cd%\update_iron\update_iron.ecf -finalize -c_compile -project_path %cd%\.comp
+ecb -config %~dp0\update_iron\update_iron.ecf -finalize -c_compile -project_path %cd%\.comp
 move %cd%\.comp\EIFGENs\update_iron\F_code\update_iron.exe update_iron.exe
 rd /q/s %cd%\.comp\EIFGENs\update_iron
 
 
 rd /q/s %cd%\.comp
+mkdir %ISE_EIFFEL%\tools\iron\spec\%ISE_PLATFORM%\bin\commands
+
+echo install update_ecf.exe
 move update_ecf.exe %ISE_EIFFEL%\tools\iron\spec\%ISE_PLATFORM%\bin\commands\update_ecf.exe
+
+echo install update_iron.exe
 move update_iron.exe %ISE_EIFFEL%\tools\iron\spec\%ISE_PLATFORM%\bin\commands\update_iron.exe
 
 endlocal

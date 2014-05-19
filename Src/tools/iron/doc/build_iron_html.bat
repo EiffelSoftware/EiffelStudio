91082bfdf20cead98941a45fb8cfb7a79a940f88
Jocelyn Fiat <jfiat@eiffel.com>
2014-05-19 16:43:32 +0000

    update iron documentation to remove unwanted link.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@95112 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/tools/iron/doc/build_iron_html.bat
+++ Src/tools/iron/doc/build_iron_html.bat
@@ -1,1 +1,1 @@
-%~dp0_tools\wiki2xhtml --header header.html.tpl --footer footer.html.tpl --to-file iron.html iron.wiki
+%~dp0_tools\wiki2xhtml --header header.html.tpl --footer footer.html.tpl --to-file index.html iron.wiki

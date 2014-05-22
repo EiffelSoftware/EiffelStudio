240cbe58fe93c89d03467fecfeb6fc967f3167f9
Jocelyn Fiat <jfiat@eiffel.com>
2014-05-22 13:01:13 +0000

    Added install_iron_doc.bat for convenience.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@95161 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/tools/iron/doc/install_iron_doc.bat
@@ -0,0 +1,3 @@
+copy %~dp0index.html ..\delivery\resources\node\doc\index.html
+copy %~dp0iron.wiki ..\delivery\resources\node\doc\iron.wiki
+copy %~dp0iron.css ..\delivery\resources\node\doc\iron.css

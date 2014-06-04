7f33dc3a019e87d105e11d89ab742295aa588da3
Jocelyn Fiat <jfiat@eiffel.com>
2014-06-04 08:48:44 +0000

    Updated script to upload ISE packages to iron.eiffel.com server
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@95227 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/tools/iron/delivery/repository_cfg.py
+++ Src/tools/iron/delivery/repository_cfg.py
@@ -4,7 +4,5 @@ def version():
 	return "14.05"
 
 def repository():
-	#return "http://localhost:9099"
-	return "http://iron.eiffel.com"
-	#return "http://192.168.1.20:9090"
+	return "https://iron.eiffel.com"
 

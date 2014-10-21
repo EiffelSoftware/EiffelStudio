e611c0f62140ab0c046e1e92d3f4f82800122808
Alexander Kogtenkov <alexk@eiffel.com>
2014-10-21 12:28:53 +0000

    Added a class for unique locks.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@95962 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/C/run-time/concurrency/rt_unique_lock.hpp
@@ -0,0 +1,38 @@
+#ifndef _RT_UNIQUE_LOCK_HPP
+#define _RT_UNIQUE_LOCK_HPP
+
+namespace eiffel_run_time
+{
+
+template <class mutex_type>
+class unique_lock
+{
+public:
+	explicit unique_lock (mutex_type& m):
+			mutex_pointer (&m), is_owned (false)
+		{	// construct and lock
+			mutex_pointer -> lock();
+			is_owned = true;
+		}
+
+	~unique_lock ()
+		{	// clean up
+			if (is_owned)
+				mutex_pointer -> unlock();
+		}
+
+	unique_lock (const unique_lock &);	// not defined
+	unique_lock & operator = (const unique_lock &);	// not defined
+
+	mutex_type * mutex () const _NOEXCEPT
+		{	// return pointer to managed mutex
+			return mutex_pointer;
+		}
+private:
+	mutex_type * mutex_pointer;
+	bool is_owned;
+}; // class unique_lock
+
+} // namespace eiffel_run_time
+
+#endif

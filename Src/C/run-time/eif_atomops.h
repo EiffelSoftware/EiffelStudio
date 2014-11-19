721377117bc0ee4a98c3c50bcd45a10fc8d54da7
Alexander Kogtenkov <alexk@eiffel.com>
2014-11-19 12:16:15 +0000

    Cleaned the code and added some more conditions to allow for more cases when GCC-specific atomic operations can be used.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@96116 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C/run-time/eif_atomops.h
+++ Src/C/run-time/eif_atomops.h
@@ -2,7 +2,7 @@
 	description:	"Atomic Memory Operations Support"
 	date:		"$Date$"
 	revision:	"$Revision: 86017 $"
-	copyright:	"Copyright (c) 2010, Eiffel Software."
+	copyright:	"Copyright (c) 2010-2014, Eiffel Software."
 	license:	"GPL version 2 see http://www.eiffel.com/licensing/gpl.txt)"
 	licensing_options:	"Commercial license is available at http://www.eiffel.com/licensing"
 	copying: "[
@@ -70,67 +70,37 @@ extern "C" {
 
 #endif
 
+/*
+* Atomic operations:
+* RTS_ACAS_I32(dest,setter,compare) - compare-exchange
+* RTS_AS_I32(dest, setter)          - exchange
+* RTS_AA_I32(dest, val)             - add-fetch
+* RTS_AS_PTR(dest, setter)          - exchange for pointers
+* RTS_AI_I32(dest)                  - increment-fetch
+* RTS_AD_I32(dest)                  - decrement-fetch
+*/
 
 #if defined(_WIN32)
-#define RTS_ACAS_I32(dest,setter,compare) InterlockedCompareExchange((long *)dest, setter, compare)
-#elif defined (__EIF_GNUC_ATOMOPS__)
-#define RTS_ACAS_I32(dest,setter,compare) __sync_val_compare_and_swap (dest, compare, setter)
+#	define RTS_ACAS_I32(dest,setter,compare) InterlockedCompareExchange ((long *) dest, setter, compare)
+#	define RTS_AS_I32(dest, setter)          InterlockedExchange ((long *) dest, setter)
+#	define RTS_AA_I32(dest, val)             (InterlockedExchangeAdd ((long *) dest, val) + val) // Add val to return new value.
+#	define RTS_AS_PTR(dest, setter)          InterlockedExchangePointer (dest, setter)
+#elif defined (__EIF_GNUC_ATOMOPS__) || defined (__GCC_HAVE_SYNC_COMPARE_AND_SWAP_4) || (defined (__clang__) && (defined (__x86_64__) || defined (__i386__)))
+#	define RTS_ACAS_I32(dest,setter,compare) __sync_val_compare_and_swap (dest, compare, setter)
+#	define RTS_AS_I32(dest, setter)          __sync_val_compare_and_swap (dest, *dest, setter)
+#	define RTS_AA_I32(dest, val)             __sync_add_and_fetch (dest, val)
+#	define RTS_AS_PTR(dest, setter)          __sync_val_compare_and_swap (dest, *dest, setter)
 #elif defined (__EIF_SUNPRO_C_ATOMOPS__)
-#define RTS_ACAS_I32(dest,setter,compare) atomic_cas_32 ((volatile uint32_t *)dest, compare, setter)
-#else
-#define RTS_ACAS_I32(dest,setter,compare) 0
-#endif
-
-
-#if defined(_WIN32) 
-#define RTS_AS_I32(dest, setter) InterlockedExchange((long *)dest, setter)
-#elif defined (__EIF_GNUC_ATOMOPS__)
-#define RTS_AS_I32(dest, setter) __sync_val_compare_and_swap (dest, *dest, setter)
-#elif defined (__EIF_SUNPRO_C_ATOMOPS__)
-#define RTS_AS_I32(dest, setter) atomic_swap_32 ((volatile uint32_t*)dest, setter)
-#else
-#define RTS_AS_I32(dest, setter) 0
-#endif
-
-
-#if defined(_WIN32)
-#define RTS_AA_I32(dest, val) (InterlockedExchangeAdd((long *)dest, val) + val) // Add val to return new value.
-#elif defined (__EIF_GNUC_ATOMOPS__)
-#define RTS_AA_I32(dest, val) __sync_add_and_fetch (dest, val)
-#elif defined (__EIF_SUNPRO_C_ATOMOPS__)
-#define RTS_AA_I32(dest, val) atomic_add_32_nv ((volatile uint32_t*)dest, val)
-#else  
-#define RTS_AA_I32(dest, val) 0
-#endif
-
-#define RTS_AI_I32(dest) RTS_AA_I32(dest, 1)
-#define RTS_AD_I32(dest) RTS_AA_I32(dest, -1)
-
-
-#if defined(_WIN32)
-#	define RTS_ACAS_PTR(dest,setter,compare) InterlockedCompareExchangePointer(dest, setter, compare)
-#elif defined (__EIF_GNUC_ATOMOPS__)
-#	define RTS_ACAS_PTR(dest,setter,compare) __sync_val_compare_and_swap (dest, compare, setter)
-#elif defined (__EIF_SUNPRO_C_ATOMOPS__)
-#	define RTS_ACAS_PTR(dest,setter,compare) atomic_cas_ptr (dest, compare, setter)
+#	define RTS_ACAS_I32(dest,setter,compare) atomic_cas_32 ((volatile uint32_t *) dest, compare, setter)
+#	define RTS_AS_I32(dest, setter)          atomic_swap_32 ((volatile uint32_t*) dest, setter)
+#	define RTS_AA_I32(dest, val)             atomic_add_32_nv ((volatile uint32_t*) dest, val)
+#	define RTS_AS_PTR(dest, setter)          atomic_swap_ptr (dest, setter)
 #else
 #	error "Missing atomic compare-exchange functionality."
 #endif
 
-
-#if defined(_WIN32) 
-#	define RTS_AS_PTR(dest, setter) InterlockedExchangePointer(dest, setter)
-#elif defined (__EIF_GNUC_ATOMOPS__)
-#	define RTS_AS_PTR(dest, setter) __sync_val_compare_and_swap (dest, *dest, setter)
-#elif defined (__EIF_SUNPRO_C_ATOMOPS__)
-#	define RTS_AS_PTR(dest, setter) atomic_swap_ptr (dest, setter)
-#else
-#	error "Missing atomic exchange functionality."
-#endif
-
-
-#define RTS_AI_PTR(dest) RTS_AI_I32(dest)
-#define RTS_AD_PTR(dest) RTS_AD_I32(dest)
+#define RTS_AI_I32(dest) RTS_AA_I32 (dest, 1)
+#define RTS_AD_I32(dest) RTS_AA_I32 (dest, -1)
 
 
 #ifdef __cplusplus
@@ -139,4 +109,3 @@ extern "C" {
 #endif
 
 #endif	/* _eif_atomops_h_ */
-

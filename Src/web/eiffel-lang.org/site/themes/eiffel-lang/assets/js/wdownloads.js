18e02fdde171bb206442d6077b3e137f741bde48
Javier Velilla <javierv@eiffel.com>
2014-12-10 10:42:29 +0000

    Removed download configuration file from the root folder. Updated code to handle historical releases, updated CSS to handle the new download page.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@96305 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/web/eiffel-lang.org/site/themes/eiffel-lang/assets/js/wdownloads.js
+++ Src/web/eiffel-lang.org/site/themes/eiffel-lang/assets/js/wdownloads.js
@@ -1,11 +1,37 @@
+var WDOWNLOAD = WDOWNLOAD || { };
+
+WDOWNLOAD.toggle = function () {
+    $(".more").hide();
+    $('.button-read-more').click(function () {
+        $(this).closest('.less').addClass('active');
+        $(this).closest(".less").prev().stop(true).slideDown("1000");
+    });
+    $('.button-read-less').click(function () {
+        $(this).closest('.less').removeClass('active');
+        $(this).closest(".less").prev().stop(true).slideUp("1000");
+    });
+};
+
+WDOWNLOAD.toggle2 = function () {
+    $(".more").hide();
+    $('.button-read-more').click(function () {
+        $(this).closest('.less').addClass('active');
+        $(this).closest(".less").next().stop(true).slideDown("1000");
+    });
+    $('.button-read-less').click(function () {
+        $(this).closest('.less').removeClass('active');
+        $(this).closest(".less").next().stop(true).slideUp("1000");
+    });
+};
+
+
+
+
 
 $(document).ready(function() {
 		/* toogle EiffelSutdio Product*/
-	$('#EiffelStudio').click(function() {
-	    $('.toggle2').toggle();
-	    return false;
-	});
 
+	WDOWNLOAD.toggle2 ();
 		/* Download a particular EiffelStudio version */
 	$('.download_link').click(function(e) {
 		e.preventDefault();	

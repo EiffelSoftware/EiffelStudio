c62b6b73e2285c59f04c9804ab92c5e3ded195d3
Alexander Kogtenkov <alexk@eiffel.com>
2014-12-15 14:00:26 +0000

    Added a dummy version of unordered_map for SunOS.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@96337 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/C/run-time/scoop/unordered_map.hpp
@@ -0,0 +1,125 @@
+#include <cstddef>
+#include <utility>
+
+namespace eiffel_run_time
+{
+
+template <class Value> class unordered_map_const_iterator
+{	
+public:
+	unordered_map_const_iterator ()
+		{	
+		}
+
+	const Value & operator*() const
+		{	
+			return value; // (*this->_Ptr);
+		}
+
+
+	Value * operator->()
+		{
+			return & value;
+			// return (&**this);
+		}
+
+	unordered_map_const_iterator& operator++()
+		{	
+/*
+		++this->_Ptr;
+*/
+			return (*this);
+		}
+
+	unordered_map_const_iterator operator++(int)
+		{
+		/*
+		_Myiter _Tmp = *this;
+		++*this;
+		return (_Tmp); */
+		}
+
+	bool operator==(const unordered_map_const_iterator& _Right) const
+		{	
+			return false;	
+/*		return (this->_Ptr == _Right._Ptr); */
+		}
+
+	bool operator!=(const unordered_map_const_iterator& _Right) const
+		{
+			return false;	
+/*		return (!(*this == _Right)); */
+		}
+
+private:
+	Value value;
+};
+
+template<class Key, class Value> class unordered_map
+{
+
+public: // Creation/destruction
+
+	unordered_map ()
+	{
+	}
+
+	~ unordered_map ()
+	{
+	}
+
+public: // Access
+
+	typedef Value value_type;
+
+  	Value & operator [] (const Key & key)
+  	{
+  		return value;
+  	}
+
+public: // Removal
+
+	std::size_t erase(const Key& key)
+	{
+		return 0;
+	}
+
+public: // Iteration
+
+	typedef unordered_map_const_iterator <std::pair <Key, Value> > const_iterator;
+	typedef unordered_map_const_iterator <std::pair <Key, Value> > iterator;
+
+	iterator find (const Key & key)
+	{
+		return const_iterator ();
+	}
+
+	const_iterator find (const Key & key) const
+	{
+		return const_iterator ();
+	}
+
+	const_iterator begin () const
+	{
+		return const_iterator ();
+	}
+
+	const_iterator end () const
+	{
+		return const_iterator ();
+	}
+
+public: // Modification
+
+	std::pair<iterator, bool> insert (const value_type& val)
+	{
+		return std::pair<iterator, bool> ();
+	}
+
+private: // TODO: remove
+
+	value_type value;
+
+}; // class unordered_map
+
+}

5626332ed799917dfc3603ac69bf2d6ddda6bde2
Alexander Kogtenkov <alexk@eiffel.com>
2014-12-16 11:51:44 +0000

    Removed unnecessary include.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@96357 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C/run-time/scoop/eveqs.cpp
+++ Src/C/run-time/scoop/eveqs.cpp
@@ -40,9 +40,8 @@ doc:<file name="eveqs.cpp" header="eif_scoop.h" version="$Id$" summary="SCOOP su
 */
 
 #include "eif_utils.hpp"
-#include "internal.hpp"
 #include "processor_registry.hpp"
-#include "processor.hpp"
+#include "internal.hpp"
 #include "eif_interp.h"
 #include "rt_wbench.h"
 #include "rt_struct.h"

aae9da94068129348f1e06c935a0a22ff14f8396
Jocelyn Fiat <jfiat@eiffel.com>
2014-12-19 18:33:59 +0000

    Improved the wdocs tree menu with arrow, and no flashing annoying effect.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@96405 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/web/eiffel-lang.org/site/themes/eiffel-lang/assets/js/wdocs.js
+++ Src/web/eiffel-lang.org/site/themes/eiffel-lang/assets/js/wdocs.js
@@ -1,53 +1,28 @@
 
 var WDOCSMOD = WDOCSMOD || { };
 WDOCSMOD.prepareTreeMenuItem = function (li) {
+	li.prepend("<span class=\"arrow \"/>");
+	li.addClass("arrowed");
 	if (li.hasClass("expandable")) {
-		if (li.hasClass("expanded")) {
-			li.prepend("<span class=\"treeitemcontrol\">-</span> ");
-		} else {
-			li.prepend("<span class=\"treeitemcontrol\">+</span> ");
-		}
-
-		li.children("span.treeitemcontrol").click(function () {
-				WDOCSMOD.toggleTreeMenuItem($(this).parent("li.expandable"));
+		li.children("span.arrow").click(function () {
+				WDOCSMOD.toggleTreeMenuItem(li);
 			}
 		);
-	} else {
-		li.prepend("&middot; ");
 	}
-}
-WDOCSMOD.refreshTreeMenuItem = function (li) {
-	li.children("span.treeitemcontrol").each (function () {
-		var ctrl = $(this);
-		var li = ctrl.parent("li.expandable");
-		if (li.hasClass("expandable")) {
-			if (li.hasClass("expanded")) {
-				ctrl.text("-");
-			} else {
-				ctrl.text("+");
-			}
-		}
-	});
 }
 
 WDOCSMOD.toggleTreeMenuItem = function (li) {
-	if (li.hasClass("expandable")) {
-		if (li.hasClass("expanded")) {
-			li.removeClass("expanded");
-		} else {
-			li.addClass("expanded");
-		}
-		WDOCSMOD.refreshTreeMenuItem(li);
+	if (li.hasClass("expanded")) {
+		li.removeClass("expanded");
 	} else {
-		alert(li.prop('outerHTML'));
-		li.stopPropagation();
+		li.addClass("expanded");
 	}
 }
 
 $(document).ready(function() {
-		// ....
 	$("#wdocs-tree.menu li").each(function() { WDOCSMOD.prepareTreeMenuItem($(this));});
 
+	/* Prepare <code> for prettyPrint */
 	$('code').each(function(i, block) {
 		var codelang = $(block).attr("lang");
 		if (codelang !== undefined) {
@@ -60,24 +35,5 @@ $(document).ready(function() {
 
 	prettyPrint();
 
-//	$("#wdocs-tree.menu li.expandable>a").click(function() {
-//		WDOCSMOD.toggleTreeMenuItem($(this).parent("li.expandable"));
-//		return false;
-//	});
-//	$("#wdocs-tree.menu li:not(.expandable)>a").click(function() {
-//		alert($(this).prop('outerHTML'));
-//		return false;
-//	});
-//	$("#wdocs-tree.menu li.expandable:not(.expanded)").click(function() {
-//		$(this).addClass("expanded");
-////		alert("not expanded");
-//		return false;
-//		}
-//		);
-//	$("#wdocs-tree.menu li.expandable.expanded").click(function() {
-//		$(this).removeClass("expanded");
-////		alert("expanded");
-//		return false;
-//	});
-}
+	}
 )

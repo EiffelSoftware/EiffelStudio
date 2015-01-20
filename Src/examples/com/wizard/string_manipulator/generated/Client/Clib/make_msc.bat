ed72ac28c5e9d07eee5944c5affd51fe27c3b629
Emmanuel Stapf <manus@eiffel.com>
2015-01-20 10:08:39 +0000

    Removed reference to a specific version of EiffelStudio. Renamed ECF and system name to drop the `client' part.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@96492 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/examples/com/wizard/string_manipulator/generated/Client/Clib/make_msc.bat
+++ Src/examples/com/wizard/string_manipulator/generated/Client/Clib/make_msc.bat
@@ -1,8 +1,4 @@
 @ECHO OFF
-IF "%ISE_EIFFEL%." == "." SET ISE_EIFFEL=C:\apps\Eiffel_14.05
-IF "%ISE_PLATFORM%." == "." SET ISE_PLATFORM=win64
-IF "%ISE_LIBRARY%." == "." SET ISE_LIBRARY=C:\work\14.05_dev
-SET ISE_C_COMPILER=msc
 
 nmake /f Makefile.msc
 nmake /f Makefile-mt.msc

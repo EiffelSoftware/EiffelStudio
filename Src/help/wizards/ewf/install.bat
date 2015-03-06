fdc8539b08acf6123cdb7f30a77747dd5602a9c2
Jocelyn Fiat <jfiat@eiffel.com>
2015-03-06 13:59:29 +0000

    Updated location of EWF estudio wizard to put it aside the others estudio wizards.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@96786 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/help/wizards/ewf/install.bat
+++ Src/help/wizards/ewf/install.bat
@@ -1,7 +1,7 @@
 setlocal
 
 set TMP_ROOTDIR=%~dp0rootdir
-set TMP_TARGETNAME=gui_wizard
+set TMP_TARGETNAME=wizard
 
 set WIZ_TARGET=%ISE_EIFFEL%\studio\wizards\new_projects\ewf
 rd /q/s %WIZ_TARGET%
@@ -12,7 +12,7 @@ move %WIZ_TARGET%\ewf.dsc %WIZ_TARGET%\..\ewf.dsc
 
 rd /q/s tmp
 mkdir tmp
-ecb -config ewf_ise_wizard-safe.ecf -target %TMP_TARGETNAME% -finalize -c_compile -project_path tmp
+ecb -config wizard.ecf -target %TMP_TARGETNAME% -finalize -c_compile -project_path tmp
 rd /q/s %WIZ_TARGET%\spec
 mkdir %WIZ_TARGET%\spec
 mkdir %WIZ_TARGET%\spec\%ISE_PLATFORM%

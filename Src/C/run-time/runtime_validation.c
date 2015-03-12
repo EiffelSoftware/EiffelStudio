47d71ea9a84e748b340d62f271b0b3fd98cc7d49
Emmanuel Stapf <manus@eiffel.com>
2015-03-12 15:00:23 +0000

    Updated runtime for supporting a macro to initialize a union using designated initializers if supported or a trick made just for `desc_info' to initialize the non_generic member. Added building of `runtime_validation' in workbench mode.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@96820 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C/run-time/runtime_validation.c
+++ Src/C/run-time/runtime_validation.c
@@ -113,27 +113,22 @@ rt_private rt_inline void double_alignment_speed_test (void) {
 }
 
 /* Test to ensure that union initialization works on all our supported platforms. */
-struct union_struct {
-	union {
-		const short *generic;
-		uintptr_t non_generic;
-	} type;
-	int body_index;
+#ifdef WORKBENCH
+static const EIF_TYPE_INDEX gen_type_1 [2] = {1, 0x0fff};
+static struct desc_info desc_35[] = {
+	{EIF_NON_GENERIC(40), 1},
+	{EIF_GENERIC(NULL), 2},
+	{EIF_GENERIC(gen_type_1), 3},
+	{EIF_NON_GENERIC(40), 4}
 };
-
-static const short gen_type_1 [2] = {1, 0x0fff};
-static struct union_struct desc_35[] = {
-	{.type.non_generic = 40, 1},
-	{.type.generic = NULL, 2},
-	{.type.generic = gen_type_1, 3},
-	{.type.non_generic = 40, 4}
-};
+#endif
 
 int main(int argc, char **argv)
 {
 	struct eif_ex_type t3;
 
 	print_info();
+
 
 	double_alignment_speed_test ();
 

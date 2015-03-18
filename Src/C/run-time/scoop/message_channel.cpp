f0c6d1bb62b0751cf974b5cd984342713c1a7a4f
romanschmocker <romans@eiffel.com>
2015-03-18 13:20:14 +0000

    Improved documentation and contracts in rt_message_channel.cpp/hpp
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@96866 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C/run-time/scoop/message_channel.cpp
+++ Src/C/run-time/scoop/message_channel.cpp
@@ -35,23 +35,72 @@
 		]"
 */
 
+/*
+doc:<file name="message_channel.cpp" header="rt_message_channel.hpp" version="$Id$" summary="A channel for rt_message structs between processors.">
+*/
+
 #include "rt_message_channel.hpp"
 #include "spsc.hpp"
 #include "rt_message.h"
 
+/*
+doc:	<routine name="rt_message_channel_send" return_type="void" export="shared">
+doc:		<summary> Send the message 'message_type' over channel 'self'. </summary>
+doc:		<param name="self" type="struct rt_message_channel*"> The channel on which to send the message. Must not be NULL. </param>
+doc:		<param name="message_type" type="enum scoop_message_type"> The type of the message. </param>
+doc:		<param name="sender" type="struct processor*"> The sender processor. Must not be NULL for SCOOP_MESSAGE_EXECUTE and SCOOP_MESSAGE_CALLBACK. </param>
+doc:		<param name="call" type="struct call_data*"> Information about a call to be executed. Must not be NULL for SCOOP_MESSAGE_EXECUTE and SCOOP_MESSAGE_CALLBACK. </param>
+doc:		<thread_safety> Safe for exactly one sender thread and one receiver thread. </thread_safety>
+doc:		<synchronization> None required. </synchronization>
+doc:	</routine>
+*/
 rt_shared void rt_message_channel_send (struct rt_message_channel* self, enum scoop_message_type message_type, processor* sender, struct call_data* call)
 {
 	struct rt_message message;
+
+	REQUIRE ("self_not_null", self);
+	REQUIRE ("sender_not_null", a_sender || (a_message != SCOOP_MESSAGE_EXECUTE && a_message != SCOOP_MESSAGE_CALLBACK));
+	REQUIRE ("call_not_null", a_call || (a_message != SCOOP_MESSAGE_EXECUTE && a_message != SCOOP_MESSAGE_CALLBACK));
+
 	rt_message_init (&message, message_type, sender, call);
 	self->impl.push (message);
 }
 
+/*
+doc:	<routine name="rt_message_channel_receive" return_type="void" export="shared">
+doc:		<summary> Receive a message on channel 'self' and store the result in 'message'. </summary>
+doc:		<param name="self" type="struct rt_message_channel*"> The channel on which to receive the message. Must not be NULL. </param>
+doc:		<param name="message" type="struct rt_message*"> A pointer to a rt_message struct where the result shall be stored. Must not be NULL. </param>
+doc:		<thread_safety> Safe for exactly one sender thread and one receiver thread. </thread_safety>
+doc:		<synchronization> None required. </synchronization>
+doc:	</routine>
+*/
 rt_shared void rt_message_channel_receive (struct rt_message_channel* self, struct rt_message* message)
 {
+	REQUIRE ("self_not_null", self);
+	REQUIRE ("message_not_null", message);
+
+		/* TODO: In theory we could allow 'message' to be NULL.
+		 * There's one use case in processor_registry that is not interested in the result. */
 	self->impl.pop (*message, self->spin);
 }
 
+/*
+doc:	<routine name="rt_message_channel_mark" return_type="void" export="shared">
+doc:		<summary> Mark all call_data structs during garbage collection in message channel 'self'. </summary>
+doc:		<param name="self" type="struct rt_message_channel*"> The rt_message_channel struct. Must not be NULL. </param>
+doc:		<param name="marking" type="MARKER"> The marker function. Must not be NULL. </param>
+doc:		<thread_safety> Not safe. </thread_safety>
+doc:		<synchronization> Only call during GC phase. </synchronization>
+doc:	</routine>
+*/
 rt_shared void rt_message_channel_mark (struct rt_message_channel* self, MARKER marking)
 {
+	REQUIRE ("self_not_null", self);
+	REQUIRE ("marking_not_null", marking);
 	self->impl.mark (marking);
 }
+
+/*
+doc:</file>
+*/

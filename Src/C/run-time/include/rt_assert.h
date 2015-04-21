734ec236af450d8a7f92f0ca14d58f24106ac31e
romanschmocker <romans@eiffel.com>
2015-04-21 11:37:58 +0000

    Added file rt_portable.h with the macro rt_unused. This macro is needed to mark static inline functions in header files that may not always be used and can therefore trigger compiler warnings in clang and GCC.
    Added rt_unused to ise_printf, rt_valid_type_index and most
    features in rt_vector.h.
    
    
    
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@97146 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C/run-time/include/rt_assert.h
+++ Src/C/run-time/include/rt_assert.h
@@ -49,6 +49,7 @@
 
 #include "eif_config.h"
 #include "eif_confmagic.h"
+#include "rt_portable.h"
 #include <stdio.h>
 #ifdef EIF_ASSERTIONS
 #include <stdarg.h>
@@ -67,7 +68,7 @@ extern "C" {
 	 * when assertion violation is reported. To do so, we have created
 	 * `ise_printf' so that we can simply set a breakpoint in it.
 	 */
-rt_private int ise_printf (char *StrFmt, ...)
+rt_unused rt_private int ise_printf (char *StrFmt, ...)
 	/* To put a breakpoint when an assertion violation occurs. */
 {
 	va_list ap;

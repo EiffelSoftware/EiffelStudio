ec431ab3c795658225abc1d66a5d16664c4cc191
Emmanuel Stapf <manus@eiffel.com>
2015-10-18 09:32:01 +0000

    Let's restore _WINSOCKAPI_ in case it is not restored by including <winsock2.h>.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@98007 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/unstable/library/network/socket/netssl/spec/include/eif_openssl.h
+++ Src/unstable/library/network/socket/netssl/spec/include/eif_openssl.h
@@ -1,8 +1,21 @@
 #ifdef EIF_WINDOWS
+/* To avoid a warning because _WINSOCKAPI_ is already defined, we remove its definition temporarely. */
 #ifdef _WINSOCKAPI_
 #undef _WINSOCKAPI_
+#define EIF_WINSOCKAPI_OVERRIDE
 #endif
+
 #include <winsock2.h>
+
+/* If we have disabled _WINSOCKAPI, we have to restore it in case <winsock2.h> didn't.
+ * Also remove EIF_WINSOCKAPI_OVERRIDE as it was just for this current header file. */
+#ifdef EIF_WINSOCKAPI_OVERRIDE
+#ifndef _WINSOCKAPI_
+#define _WINSOCKAPI_
+#endif
+#undef EIF_WINSOCKAPI_OVERRIDE
+#endif
+
 #endif
 
 # include <openssl/bio.h>

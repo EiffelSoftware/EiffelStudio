76bd82dd3501df3dcb0ce252af8aaa6460c7a954
Jocelyn Fiat <jfiat@eiffel.com>
2015-12-11 13:50:05 +0000

    Renamed ecf files under "alter" by adding ".tpl", to avoid issue with compile_all and update related script to take this extension into account for *.e and *.ecf files.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@98243 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/tools/iron/delivery/ise_upload_version.py
+++ Src/tools/iron/delivery/ise_upload_version.py
@@ -39,6 +39,7 @@ def get_ise_libraries(basedir, br, v):
 		call(["svn", "checkout", "%s/Src/unstable" % (branch_dir), d ])
 	alter_folder_with (basedir, os.path.join (basedir, "..", "..", "alter"))
 	alter_folder_with (basedir, os.path.join (basedir, "..", "alter"))
+
 def alter_folder_with (a_source, a_alter):
 	print "Altering %s with %s." % (a_source, a_alter)
 	if os.path.exists (a_alter):
@@ -46,7 +47,10 @@ def alter_folder_with (a_source, a_alter):
 			names = os.listdir(a_alter)
 			for name in names:
 				srcname = os.path.join (a_alter, name)
-				dstname = os.path.join (a_source, name)
+				if name.endswith(".ecf.tpl") or name.endswith(".e.tpl"):
+					dstname = os.path.join (a_source, name[:-3])
+				else:
+					dstname = os.path.join (a_source, name)
 				if os.path.isdir(srcname):
 					alter_folder_with (dstname, srcname)
 				else:
@@ -74,6 +78,8 @@ def main():
 	get_ise_libraries(l_sources_dir, repository_cfg.branch(), repository_cfg.version())
 	print "Updating the ecf files for iron packaging ..."
 	upload_version(l_sources_dir)
+
+
 	#process_iron_package_files (os.path.join (l_sources_dir, "library"), l_sources_dir, l_packages_dir, l_login, l_password, repo, repository_cfg.version())
 	#process_iron_package_files (os.path.join (l_sources_dir, "unstable", "library"), l_sources_dir, l_packages_dir, l_login, l_password, repo, repository_cfg.version())
 	#process_iron_package_files (os.path.join (l_sources_dir, "contrib", "library"), l_sources_dir, l_packages_dir, l_login, l_password, repo, repository_cfg.version())

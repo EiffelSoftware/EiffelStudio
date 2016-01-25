81ce5b2cd7896316a10736fb1abfc73181fb899e
Jocelyn Fiat <jfiat@eiffel.com>
2016-01-25 16:45:41 +0000

    Added a "are you sure you want to leave this page" solution when the user changed an input or textarea field from the "main" div.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@98441 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/web/eiffel-org/site/themes/eiffel-org/assets/js/home.js
+++ Src/web/eiffel-org/site/themes/eiffel-org/assets/js/home.js
@@ -1,7 +1,13 @@
 var EHOME = EHOME || { };
 
 $(document).ready(function() {
-      $('.fipoweredby').remove();
-     }
-);
+    $("main input, main textarea").on("input change", function() {
+        window.onbeforeunload = window.onbeforeunload || function (e) {
+            return "You have unsaved changes.  Do you want to leave this page and lose your changes?";
+        };
+    });
+    $("form").on("submit", function() {
+        window.onbeforeunload = null;
+    });
+});
 

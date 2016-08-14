6b0c0dcdbed81a5ae8a99a35708a1128bbdfaf2a
Alexander Kogtenkov <alexk@eiffel.com>
2016-08-14 09:20:39 +0000

    Added batch files for a regular shell with the same functionality as BTM versions.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@99108 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/help/wizards/make_a_wizard.bat
@@ -0,0 +1,38 @@
+@echo off
+echo -----------------------------------------------------------------
+echo ----------------- Preparing Temporary directory -----------------
+echo -----------------------------------------------------------------
+pushd
+mkdir %DELIVERY%\studio\wizards\%1\%2\spec
+mkdir %DELIVERY%\studio\wizards\%1\%2\spec\%ISE_PLATFORM%
+rd /Q /S %TEMP%\%3
+mkdir %TEMP%\%3
+echo -----------------------------------------------------------------
+echo ----------------- Retrieving last sources -----------------------
+echo -----------------------------------------------------------------
+cd/d %EIFFEL_SRC%\help\wizards
+call svn up
+echo -----------------------------------------------------------------
+echo ----------------- Compilation -----------------------------------
+echo -----------------------------------------------------------------
+cd/d %TEMP%\%3
+copy/b %EIFFEL_SRC%\help\wizards\wizard.ico .
+copy/b %EIFFEL_SRC%\help\wizards\wizard.rc .
+%ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec -finalize -config %EIFFEL_SRC%\help\wizards\%3.ecf -project_path %TEMP%\%3 -c_compile
+echo -----------------------------------------------------------------
+echo ------- Compress generated executable file ----------------------
+echo -----------------------------------------------------------------
+cd/d %TEMP%\%3\EIFGENs\wizard\F_code
+iff not defined WIN64 then
+	upx wizard.exe
+endiff
+echo -----------------------------------------------------------------
+echo ------- Copying generated executable file into Delivery ---------
+echo -----------------------------------------------------------------
+copy/b wizard.exe %DELIVERY%\studio\wizards\%1\%2\spec\%ISE_PLATFORM%
+cd %TEMP%
+popd
+echo -----------------------------------------------------------------
+echo ------- Deleting temporary files --------------------------------
+echo -----------------------------------------------------------------
+rd /Q /S %TEMP%\%3

6342b4bece7af63900f982fc851d8966afa03904
Alexander Kogtenkov <alexk@eiffel.com>
2016-08-14 09:20:39 +0000

    Added batch files for a regular shell with the same functionality as BTM versions.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@99108 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/help/wizards/make_wizards.bat
@@ -0,0 +1,36 @@
+@echo off
+
+if not defined INSTALL_LOG set INSTALL_LOG=CON
+
+if not defined TEMP (echo Variable TEMP is not set && exit 0)
+if not defined DELIVERY (echo Variable DELIVERY is not set && exit 0)
+
+echo *****************************************************************
+echo *                 Compiling WEL Wizard                          *
+echo *****************************************************************
+call make_a_wizard.bat new_projects wel wel_wizard >> %INSTALL_LOG% 2>>&1
+
+echo *****************************************************************
+echo *               Compiling Vision2 Wizard                        *
+echo *****************************************************************
+call make_a_wizard.bat new_projects vision2 vision2_wizard >> %INSTALL_LOG% 2>>&1
+
+echo *****************************************************************
+echo *                Compiling Wizard Wizard                        *
+echo *****************************************************************
+REM call make_a_wizard.bat new_projects wizard wizard_wizard >> %INSTALL_LOG% 2>>&1
+
+echo *****************************************************************
+echo *              Compiling Precompilation Wizard                  *
+echo *****************************************************************
+call make_a_wizard.bat others precompile precompile_wizard >> %INSTALL_LOG% 2>>&1
+
+echo *****************************************************************
+echo *                    Compiling .NET Wizard                      *
+echo *****************************************************************
+call make_a_wizard.bat new_projects dotnet dotnet_wizard >> %INSTALL_LOG% 2>>&1
+
+echo *****************************************************************
+echo *                 Compiling EWF Wizard                          *
+echo *****************************************************************
+call make_a_wizard.bat new_projects ewf ewf_wizard >> %INSTALL_LOG% 2>>&1

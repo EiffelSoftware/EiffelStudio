29edb691c7b24ce63724733299a32b226e23e852
Alexander Kogtenkov <alexk@eiffel.com>
2017-01-25 12:55:47 +0000

    Supported paths with spaces.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@99756 8089f293-4706-0410-a29e-feb5c42a2edf

--- eweasel/compilation/install_eweasel.bat
+++ eweasel/compilation/install_eweasel.bat
@@ -5,25 +5,25 @@ if '%EWEASEL%' == '' echo Environment variable EWEASEL is missing
 if '%EWEASEL%' == '' GOTO END
 
 rem Compiling eweasel (single-threaded)
-%ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe -finalize -config %EWEASEL%/source/eweasel.ecf -target eweasel_st -c_compile
+"%ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe" -finalize -config "%EWEASEL%/source/eweasel.ecf" -target eweasel_st -c_compile
 
 rem Compiling eweasel (multi-threaded)
-%ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe -finalize -config %EWEASEL%/source/eweasel.ecf -target eweasel_mt -c_compile
+"%ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin\ec.exe" -finalize -config "%EWEASEL%/source/eweasel.ecf" -target eweasel_mt -c_compile
 
 rem Create delivery structure
-if not exist %EWEASEL%\spec (
-	mkdir %EWEASEL%\spec
+if not exist "%EWEASEL%\spec" (
+	mkdir "%EWEASEL%\spec"
 )
-if not exist %EWEASEL%\spec\%ISE_PLATFORM% (
-	mkdir %EWEASEL%\spec\%ISE_PLATFORM%
+if not exist "%EWEASEL%\spec\%ISE_PLATFORM%" (
+	mkdir "%EWEASEL%\spec\%ISE_PLATFORM%"
 )
-if not exist %EWEASEL%\spec\%ISE_PLATFORM%\bin (
-	mkdir %EWEASEL%\spec\%ISE_PLATFORM%\bin
+if not exist "%EWEASEL%\spec\%ISE_PLATFORM%\bin" (
+	mkdir "%EWEASEL%\spec\%ISE_PLATFORM%\bin"
 )
 
 rem Copy executables
-copy EIFGENs\eweasel_st\F_code\eweasel.exe %EWEASEL%\spec\%ISE_PLATFORM%\bin
-copy EIFGENs\eweasel_mt\F_code\eweasel-mt.exe %EWEASEL%\spec\%ISE_PLATFORM%\bin
+copy EIFGENs\eweasel_st\F_code\eweasel.exe "%EWEASEL%\spec\%ISE_PLATFORM%\bin"
+copy EIFGENs\eweasel_mt\F_code\eweasel-mt.exe "%EWEASEL%\spec\%ISE_PLATFORM%\bin"
 rd /q /s EIFGENs
 del eweasel.rc
 del eweasel-mt.rc

e10652f672dea1c920514fa2b3074ece6b4878d3
Emmanuel Stapf <manus@eiffel.com>
2017-05-01 21:09:23 +0000

    Address comment from bug#19354 in eif_pthread_cond_wait_with_timeout
    When calling eif_pthread_sem_wait_with_timeout in eif_pthread_cond_wait_with_timeout we should check the Result status before continuing.
    
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@100279 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C/run-time/posix_threads.c
+++ Src/C/run-time/posix_threads.c
@@ -1376,29 +1376,31 @@ rt_public int eif_pthread_cond_wait_with_timeout (EIF_COND_TYPE *condvar, EIF_MU
 	eif_pthread_mutex_unlock(mutex);
 
 	for (;;) {
-		Result = eif_pthread_sem_wait_with_timeout(condvar->semaphore, timeout);	
-
-		(void) eif_pthread_cs_lock(condvar->csection);
-
-		if (condvar->num_wake) {
-			if (condvar->generation != generation) {
-				condvar->num_wake--;
-				condvar->num_waiting--;
-				Result = T_OK;
-				break;
-			} else {
-				wake = 1;
-			}
-		} else if (Result == T_TIMEDOUT) {
-			condvar->num_waiting--;
+		Result = eif_pthread_sem_wait_with_timeout(condvar->semaphore, timeout);
+		if (Result != T_OK && Result != T_TIMEDOUT) {
+			// An error occurred. We have to cancel the wait.
 			break;
-		}
+		} else {
+			(void) eif_pthread_cs_lock(condvar->csection);
+			if (condvar->num_wake) {
+				if (condvar->generation != generation) {
+					condvar->num_wake--;
+					condvar->num_waiting--;
+					Result = T_OK;
+					break;
+				} else {
+					wake = 1;
+				}
+			} else if (Result == T_TIMEDOUT) {
+				condvar->num_waiting--;
+				break;
+			}
+			(void) eif_pthread_cs_unlock(condvar->csection);
 
-		(void) eif_pthread_cs_unlock(condvar->csection);
-
-		if (wake) {
-			wake = 0;
-			eif_pthread_sem_post(condvar->semaphore);
+			if (wake) {
+				wake = 0;
+				eif_pthread_sem_post(condvar->semaphore);
+			}
 		}
 	}
 

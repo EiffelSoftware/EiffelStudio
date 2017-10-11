61ad0d33df3644108c5092deff1cbe20f6a8fdd3
Jocelyn Fiat <jfiat@eiffel.com>
2017-10-11 20:00:19 +0000

    Fixed sql for user profiles
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@100896 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/web/eiffel-cloud/site/modules/core/scripts/user_profile.sql
+++ Src/web/eiffel-cloud/site/modules/core/scripts/user_profile.sql
@@ -2,5 +2,5 @@ CREATE TABLE user_profiles(
   `uid` INTEGER NOT NULL CHECK("uid">=0),
   `key` VARCHAR(255) NOT NULL,
   `value` TEXT,
-  CONSTRAINT PK_uid_key PRIMARY KEY (uid,key)
+  CONSTRAINT PK_uid_key PRIMARY KEY (`uid`,`key`)
 );

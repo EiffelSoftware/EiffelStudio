7cad8064a2d274d9e1d3ab67e3c6be8a27f63a72
Jocelyn Fiat <jfiat@eiffel.com>
2019-03-21 17:45:24 +0000

    Updated to allow building the porterpackage for enterprise too, and for a specific branch.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@103001 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/Delivery/scripts/docker/unix/build_gpl_deliveries.sh
+++ Src/Delivery/scripts/docker/unix/build_gpl_deliveries.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 #docker ps -a | awk '{ print $1,$2 }' | grep eiffel_deliv | awk '{print $1 }' | xargs -I {} docker rm {}
 docker ps -a -q --filter=ancestor=unix_eiffel_deliv | xargs -I {} docker rm {}
@@ -8,3 +8,4 @@ docker rmi unix_eiffel_deliv_32bits
 
 docker-compose -f docker-compose.yml up --build
 docker-compose -f docker-compose-32bits.yml up --build
+
--- Src/Delivery/scripts/docker/unix/src/build_delivery.sh
+++ Src/Delivery/scripts/docker/unix/src/build_delivery.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
 export DELIV_DIR=$1
-export ORIGO_SVN_REVISION=$SVN_EIFFELSTUDIO_REPO_REVISION
-
-# Initialization
-export PATH=$PATH:.
-export DEFAULT_ORIGO_SVN_ROOT=$SVN_EIFFELSTUDIO_REPO
-export DEFAULT_ISE_SVN=$SVN_ISE_REPO/trunk
-
-echo ONLY_PORTERPACKAGE=$ONLY_PORTERPACKAGE
-if [ "$ONLY_PORTERPACKAGE" = "true" ]; then
-	echo Build PorterPackage for $STUDIO_VERSION_MAJOR_MINOR in $DELIV_DIR
-else
-	echo Build delivery $STUDIO_VERSION_MAJOR_MINOR in $DELIV_DIR
-fi
-echo Use EiffelStudio svn repository: $DEFAULT_ORIGO_SVN_ROOT
-
-# Main
 if [ ! -d "$DELIV_DIR" ]; then
 	echo Create $DELIV_DIR
 	mkdir -p $DELIV_DIR
@@ -26,6 +10,57 @@ if [ ! -d "$DELIV_DIR/output" ]; then
 	mkdir -p $DELIV_DIR/output
 fi
 
+
+echo DELIV_DIR=$DELIV_DIR > $DELIV_DIR/output/info.log
+
+echo Install EiffelStudio
+if [ -z "$EIFFEL_SETUP_CHANNEL" ]; then
+	export EIFFEL_SETUP_CHANNEL=nightly
+fi
+if [ -d /home/eiffel/Eiffel-${EIFFEL_SETUP_CHANNEL} ]; then
+	echo "Already installed!"
+else
+	curl -sSL https://www.eiffel.org/setup/install.sh | bash -s -- --channel ${EIFFEL_SETUP_CHANNEL} --platform ${ISE_PLATFORM} --install-dir /home/eiffel/Eiffel-${EIFFEL_SETUP_CHANNEL} --dir /home/eiffel
+fi
+
+# Define Eiffel environment variables
+export ISE_EIFFEL=/home/eiffel/Eiffel-${EIFFEL_SETUP_CHANNEL}
+export ISE_LIBRARY=$ISE_EIFFEL
+export PATH=$PATH:$ISE_EIFFEL/studio/spec/$ISE_PLATFORM/bin:$ISE_EIFFEL/tools/spec/$ISE_PLATFORM/bin:$ISE_EIFFEL/library/gobo/spec/$ISE_PLATFORM/bin:$ISE_EIFFEL/esbuilder/spec/$ISE_PLATFORM/bin
+
+export ORIGO_SVN_REVISION=$SVN_EIFFELSTUDIO_REPO_REVISION
+if [ -z "$SVN_EIFFELSTUDIO_BRANCH" ]; then 
+	export SVN_EIFFELSTUDIO_BRANCH=trunk
+fi
+
+echo SVN_EIFFELSTUDIO_BRANCH=$SVN_EIFFELSTUDIO_BRANCH
+echo ONLY_PORTERPACKAGE=$ONLY_PORTERPACKAGE
+echo INCLUDE_ENTERPRISE=$INCLUDE_ENTERPRISE
+
+# Initialization
+export PATH=$PATH:.
+if [ -n "$SVN_EIFFELSTUDIO_REPO" ]; then
+	export DEFAULT_ORIGO_SVN_ROOT=$SVN_EIFFELSTUDIO_REPO
+fi
+if [ -n "$SVN_ISE_REPO" ]; then
+	if [ -n "$SVN_ISE_BRANCH" ]; then
+		export DEFAULT_ISE_SVN=$SVN_ISE_REPO$SVN_ISE_BRANCH
+	else
+		export DEFAULT_ISE_SVN=$SVN_ISE_REPO/trunk
+	fi
+	echo DEFAULT_ISE_SVN=$DEFAULT_ISE_SVN
+fi
+
+
+if [ "$ONLY_PORTERPACKAGE" = "true" ]; then
+	echo Build PorterPackage in $DELIV_DIR
+else
+	echo Build delivery in $DELIV_DIR
+fi
+echo Use EiffelStudio svn repository: $DEFAULT_ORIGO_SVN_ROOT
+
+# Main
+
 if [ -d "$DELIV_DIR/scripts-unix" ]; then
 	echo Clean $DELIV_DIR/scripts-unix
 	\rm -rf $DELIV_DIR/scripts-unix
@@ -33,8 +68,8 @@ fi
 
 echo Build delivery for $ISE_PLATFORM 
 echo revision: $ORIGO_SVN_REVISION
-echo svn checkout --config-option config:miscellany:use-commit-times=yes $SVN_EIFFELSTUDIO_REPO/trunk/Src/Delivery/scripts/unix $DELIV_DIR/scripts-unix
-svn checkout --config-option config:miscellany:use-commit-times=yes $SVN_EIFFELSTUDIO_REPO/trunk/Src/Delivery/scripts/unix $DELIV_DIR/scripts-unix
+echo svn checkout --config-option config:miscellany:use-commit-times=yes $SVN_EIFFELSTUDIO_REPO$SVN_EIFFELSTUDIO_BRANCH/Src/Delivery/scripts/unix $DELIV_DIR/scripts-unix
+svn checkout --config-option config:miscellany:use-commit-times=yes $SVN_EIFFELSTUDIO_REPO$SVN_EIFFELSTUDIO_BRANCH/Src/Delivery/scripts/unix $DELIV_DIR/scripts-unix
 
 echo Get revision from source code
 if [ ! "$ORIGO_SVN_REVISION" ]; then
@@ -56,8 +91,14 @@ if [ -f "$STUDIO_PORTERPACKAGE_TAR" ]; then
 	tar xf "$STUDIO_PORTERPACKAGE_TAR"
 fi
 if [ ! -d "PorterPackage" ]; then
-	echo Build PorterPackage ...
-	./make_delivery
+	if [ "$INCLUDE_ENTERPRISE" == "true" ]; then
+		echo Build GPL+ENT PorterPackage ...
+		./make_delivery all
+	else
+		echo Build GPL PorterPackage ...
+		./make_delivery
+	fi
+	cp ./*.log $DELIV_DIR/output/.
 fi
 if [ -d "PorterPackage" ]; then
 	if [ ! -f "$STUDIO_PORTERPACKAGE_TAR" ]; then

bfa29fbe769bbf23a695215bd5e4e28e58c64a30
Jocelyn Fiat <jfiat@eiffel.com>
2019-03-21 08:20:57 +0000

    Updated scripts
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@102988 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/Delivery/scripts/docker/unix/build_gpl_deliveries.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+#docker ps -a | awk '{ print $1,$2 }' | grep eiffel_deliv | awk '{print $1 }' | xargs -I {} docker rm {}
+docker ps -a -q --filter=ancestor=unix_eiffel_deliv | xargs -I {} docker rm {}
+
+docker rmi unix_eiffel_deliv
+docker rmi unix_eiffel_deliv_32bits
+
+docker-compose -f docker-compose.yml up --build
+docker-compose -f docker-compose-32bits.yml up --build

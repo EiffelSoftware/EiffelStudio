6b73708eb47d871a15d9991be6d6f90fc461d4b2
Jocelyn Fiat <jfiat@eiffel.com>
2019-05-15 15:09:26 +0000

    Updated scripts to generate icons from svg.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@103160 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/Delivery/studio/bitmaps/src/build_all_icons_matrix.sh
+++ Src/Delivery/studio/bitmaps/src/build_all_icons_matrix.sh
@@ -3,26 +3,31 @@
 ./build_overlays.sh
 
 ./build_icons.sh
-./build_svg_matrix.sh icons icons/icons.svg
-./build_svg_matrix.sh icons icons/icons_16x16.svg
-./build_svg_matrix.sh icons icons/icons_20x20.svg
-./build_svg_matrix.sh icons icons/icons_24x24.svg
-./build_svg_matrix.sh icons icons/icons_32x32.svg
+cd icons
+./build_svg_matrix.sh . ./icons.svg
+./build_svg_matrix.sh . ./icons_16x16.svg
+./build_svg_matrix.sh . ./icons_20x20.svg
+./build_svg_matrix.sh . ./icons_24x24.svg
+./build_svg_matrix.sh . ./icons_32x32.svg
+cd ..
 
 ./build_small_icons.sh
-./build_svg_matrix.sh small small/small_icons.svg
-./build_svg_matrix.sh small small/small_12x12.svg
-./build_svg_matrix.sh small small/small_15x15.svg
-./build_svg_matrix.sh small small/small_18x18.svg
-./build_svg_matrix.sh small small/small_24x24.svg
+cd small
+./build_svg_matrix.sh . ./small_icons.svg
+./build_svg_matrix.sh . ./small_12x12.svg
+./build_svg_matrix.sh . ./small_15x15.svg
+./build_svg_matrix.sh . ./small_18x18.svg
+./build_svg_matrix.sh . ./small_24x24.svg
+cd ..
 
 ./build_mini_icons.sh
-./build_svg_matrix.sh mini mini/mini_icons.svg
-./build_svg_matrix.sh mini mini/mini_10x10.svg
-./build_svg_matrix.sh mini mini/mini_12x12.svg
-./build_svg_matrix.sh mini mini/mini_15x15.svg
-./build_svg_matrix.sh mini mini/mini_20x20.svg
-
+cd mini
+./build_svg_matrix.sh . ./mini_icons.svg
+./build_svg_matrix.sh . ./mini_10x10.svg
+./build_svg_matrix.sh . ./mini_12x12.svg
+./build_svg_matrix.sh . ./mini_15x15.svg
+./build_svg_matrix.sh . ./mini_20x20.svg
+cd ..
 
 ./inc/matrix_from_subpixmaps.sh icons icons/16x16.png
 ./inc/matrix_from_subpixmaps.sh small small/12x12.png

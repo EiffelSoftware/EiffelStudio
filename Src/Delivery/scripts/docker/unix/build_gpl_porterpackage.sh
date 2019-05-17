862b6fa9ba06fe24c66d166e7427d531fc546070
Jocelyn Fiat <jfiat@eiffel.com>
2019-05-17 15:38:17 +0000

    Expose also the SVN_*_BRANCH environment variables.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@103177 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/Delivery/scripts/docker/unix/build_gpl_porterpackage.sh
+++ Src/Delivery/scripts/docker/unix/build_gpl_porterpackage.sh
@@ -9,7 +9,7 @@ docker_image_name=local/eiffel-deliv-porterpackage
 docker rmi ${docker_image_name}
 docker build -t ${docker_image_name} -f ./porterpackage/dockerfile porterpackage
 if [ -d "$var_dir/eiffel" ]; then
-	echo USe Eiffel installation from $var_dir/eiffel/...
+	echo Use Eiffel installation from $var_dir/eiffel/...
         t_docker_vol_opts="-v $var_dir/eiffel:/home/eiffel/Eiffel"
 fi
 docker run --rm \
@@ -17,9 +17,11 @@ docker run --rm \
         -v $var_dir/deliv-output:/home/eiffel/deliv/output \
         ${t_docker_vol_opts} \
         --network host \
-        -e SVN_EIFFELSTUDIO_REPO=svn://svn.ise/mirrors/eiffelstudio \
         -e SVN_ISE_REPO=svn://svn.ise/ise_svn \
+        -e SVN_ISE_BRANCH=$SVN_ISE_BRANCH \
+        -e SVN_EIFFELSTUDIO_REPO=svn://svn.ise/mirrors/eiffelstudio \
         -e SVN_EIFFELSTUDIO_REPO_REVISION=$SVN_EIFFELSTUDIO_REPO_REVISION \
+        -e SVN_EIFFELSTUDIO_BRANCH=$SVN_EIFFELSTUDIO_BRANCH \
         ${docker_image_name}
 #docker rmi ${docker_image_name}
 

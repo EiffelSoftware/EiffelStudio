c566cc988242d1d44c57569df8e98dafd516725f
Javier Velilla <javierv@eiffel.com>
2019-05-20 17:59:00 +0000

    Added new wel_scaling_api.h header Updated class WEL_SCALING_EXTERNALS to use the new wel_scaling_api.h header, to avoid issues with Mingw.
    git-svn-id: https://svn.eiffel.com/eiffelstudio/trunk@103185 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/library/wel/spec/windows/include/wel_scaling_api.h
@@ -0,0 +1,34 @@
+/*
+indexing
+description: "WEL: library of reusable components for Windows."
+	copyright:	"Copyright (c) 1984-2019, Eiffel Software and others"
+	license:	"Eiffel Forum License v2 (see http://www.eiffel.com/licensing/forum.txt)"
+	source: "[
+				Eiffel Software
+				5949 Hollister Ave., Goleta, CA 93117 USA
+				Telephone 805-685-1006, Fax 805-685-6869
+				Website https://www.eiffel.com
+				Customer support https://support.eiffel.com
+		]"
+*/
+
+/*****************************************************************************/
+/* wel_scaling_api.h                                                         */
+/*****************************************************************************/
+/* Used to add HighDPI support	(only for MSC compiler: 2019-05-20  )         */
+/*****************************************************************************/
+
+
+#ifndef _wel_scaling_api_h_
+#define _wel_scaling_api_h_
+
+#ifdef _MSC_VER 
+#	include <shellscalingapi.h>
+#endif
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#endif /* _wel_scaling_api_h_ */

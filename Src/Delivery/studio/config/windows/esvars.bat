50c6f82bf016343319d994e7afdc9afe368e5a52
Jocelyn Fiat <jfiat@eiffel.com>
2019-11-01 14:57:56 +0000

    Fixed mingw path ( see bug#19593 )
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@103643 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/Delivery/studio/config/windows/esvars.bat
+++ Src/Delivery/studio/config/windows/esvars.bat
@@ -14,6 +14,6 @@ REM Set PATH so that one can easily access to our compiler and associated tools.
 set PATH=%PATH%;%ISE_EIFFEL%\studio\spec\%ISE_PLATFORM%\bin;%ISE_EIFFEL%\tools\spec\%ISE_PLATFORM%\bin;%ISE_EIFFEL%\library\gobo\spec\%ISE_PLATFORM%\bin
 
 REM Set mingw path if currently set C compiler.
-if %ISE_C_COMPILER%==mingw set PATH=%ISE_EIFFEL%\gcc\windows\mingw\bin;%ISE_EIFFEL%\gcc\windows\msys\1.0\bin;%PATH%;
+if %ISE_C_COMPILER%==mingw set PATH=%ISE_EIFFEL%\gcc\%ISE_PLATFORM%\mingw\bin;%ISE_EIFFEL%\gcc\%ISE_PLATFORM%\msys\1.0\bin;%PATH%;
 
 cd "%ISE_EIFFEL%"

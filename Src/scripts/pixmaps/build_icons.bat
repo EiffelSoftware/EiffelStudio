88fe8a1707ae338992e6542268e17afcfeb5523b
Jocelyn Fiat <jfiat@eiffel.com>
2019-11-06 14:30:28 +0000

    Updated script to generate the icons Eiffel classes.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@103657 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/scripts/pixmaps/build_icons.bat
+++ Src/scripts/pixmaps/build_icons.bat
@@ -1,12 +1,12 @@
-echo off
+:echo off
 setlocal
 
 set t_output=%EIFFEL_SRC%\Eiffel\interface\new_graphical\shared\es_icons.e
 emcgen %EIFFEL_SRC%\Delivery\studio\bitmaps\png\icons.ini -f %EIFFEL_SRC%\tools\eiffel_matrix_code_generator\frames\studio_dpi.e.frame --output_file %t_output%
 
 rem Update output file with expected command line.
-for /d %f in (%EIFFEL_SRC%) do set t_drive=%~df
-sed -i -- 's/%t_drive%.eiffelstudio.trunk.Src/%%EIFFEL_SRC%%/g' %t_output%
+set t_drive=%EIFFEL_SRC:~0,2%
+sed -i -- 's/%t_drive%\S*.Src/%%EIFFEL_SRC%%/g' %t_output%
 head %t_output% | grep emcgen
 
 endlocal
--- Src/scripts/pixmaps/build_mini_icons.bat
+++ Src/scripts/pixmaps/build_mini_icons.bat
@@ -4,7 +4,10 @@ setlocal
 set t_output=%EIFFEL_SRC%\Eiffel\interface\new_graphical\shared\es_mini_icons.e
 emcgen %EIFFEL_SRC%\Delivery\studio\bitmaps\png\mini_icons.ini -f %EIFFEL_SRC%\tools\eiffel_matrix_code_generator\frames\studio_dpi.e.frame --output_file %t_output%
 
-sed -i -- 's/E:.eiffelstudio.trunk.Src/%%EIFFEL_SRC%%/g' %t_output%
-head %t_output% | grep command_line
+
+rem Update output file with expected command line.
+set t_drive=%EIFFEL_SRC:~0,2%
+sed -i -- 's/%t_drive%\S*.Src/%%EIFFEL_SRC%%/g' %t_output%
+head %t_output% | grep emcgen
 
 endlocal

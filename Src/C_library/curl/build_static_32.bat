26f1868d9f7903a00059b3bbcc04d6ed93c939f7
Javier Velilla <javierv@eiffel.com>
2020-04-08 21:10:54 +0000

    Added build scripts to compile cURL statically with defaults values for 32 and 64 bits. Updated clibs_libcurl.ecf to include the external_object instead of library/curl/cURL.ecf.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@104038 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/C_library/curl/build_static_32.bat
@@ -0,0 +1,27 @@
+@echo on
+title Script to compile cURL on Windows 32 bits statically
+rem Clean the folders under directy builds
+rem Compiles cURL static library 
+rem Copy the static library libcurl_a.lib to 
+rem cURL/spe/static/lib/libcurl_a.lib
+
+
+set current_dir = %~dp0
+
+rem clean build directory.
+IF exist %current_dir%builds\nul ( 
+	cd %current_dir%builds/
+	rd /s /q . 2>nul 
+	cd ..
+)
+
+cd %cd%/winbuild
+
+@echo Compiling cURL 32bits statically
+
+espawn --x86 "nmake RTLIBCFG=static /f Makefile.vc mode=static GEN_PDB=no DEBUG=no MACHINE=x86"
+
+
+cd ..
+mkdir spec\windows\static\lib
+copy %cd%\builds\libcurl-vc-x86-release-static-ipv6-sspi-winssl\lib\libcurl_a.lib %cd%\spec\windows\static\lib
---
+++ Src/C_library/curl/build_static_64.bat
@@ -0,0 +1,28 @@
+@echo on
+title Script to compile cURL on Windows 64 bits statically
+rem Clean the folders under directy builds
+rem Compiles cURL static library 
+rem Copy the static library libcurl_a.lib to 
+rem cURL/spec/win64/static/lib/libcurl_a.lib
+
+
+set current_dir = %~dp0
+
+rem clean build directory.
+IF exist %current_dir%builds\nul ( 
+	cd %current_dir%builds/
+	rd /s /q . 2>nul 
+	cd ..
+)
+
+rem go to the winbuild directory in the Curl
+cd %cd%/winbuild
+
+@echo Compiling cURL 64bits statically
+
+espawn "nmake RTLIBCFG=static /f Makefile.vc mode=static GEN_PDB=no DEBUG=no MACHINE=x64"
+
+
+cd ..
+mkdir spec\win64\static\lib
+copy %cd%\builds\libcurl-vc-x64-release-static-ipv6-sspi-winssl\lib\libcurl_a.lib  %cd%\spec\win64\static\lib

632059feb9cc8dd34936fcd2c0819e5eff524918
Jocelyn Fiat <jfiat@eiffel.com>
2020-05-27 08:39:53 +0000

    minor changes
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@104347 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/Delivery/scripts/windows/clean_delivery.bat
+++ Src/Delivery/scripts/windows/clean_delivery.bat
@@ -1,9 +1,6 @@
+@echo off
+setlocal
+set TCCLECMD="C:\Program Files\JPSoft\TCCLE14x64\tcc.exe"
 
-echo reset environment
-if "%ISE_PLATFORM%" == "" set ISE_PLATFORM=win64
-if "%ISE_PLATFORM%" == "windows" Call %~dp0bin\reset_eiffel_env.bat windows
-if "%ISE_PLATFORM%" == "win64" Call %~dp0bin\reset_eiffel_env.bat win64
+%TCCLECMD% /C inc\clean_delivery.btm
 
-echo Remove build folder
-call %~dp0init.btm
-if EXIST "%DELIV_DIR%" rd /q/s "%DELIV_DIR%"

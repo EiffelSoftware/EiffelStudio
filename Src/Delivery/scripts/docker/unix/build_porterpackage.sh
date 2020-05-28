035df9a763adb0065e39085a6253a62643197671
Jocelyn Fiat <jfiat@eiffel.com>
2020-05-28 18:13:46 +0000

    Allow to override default repo url.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@104379 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/Delivery/scripts/docker/unix/build_porterpackage.sh
+++ Src/Delivery/scripts/docker/unix/build_porterpackage.sh
@@ -18,14 +18,21 @@ if [ -d "$var_dir/eiffel" ]; then
 	echo Use Eiffel installation from $var_dir/eiffel/...
         t_docker_vol_opts="-v $var_dir/eiffel:/home/eiffel/Eiffel"
 fi
+if [ "$SVN_ISE_REPO" = "" ]; then
+	export SVN_ISE_REPO=svn://svn.ise/ise_svn
+fi
+if [ "$SVN_EIFFELSTUDIO_REPO" = "" ]; then
+        export SVN_EIFFELSTUDIO_REPO=svn://svn.ise/mirrors/eiffelstudio
+fi
+
 docker run --rm \
         --name=eiffel-deliv-pp \
         -v $output_dir:/home/eiffel/deliv/output \
         ${t_docker_vol_opts} \
         --network host \
-        -e SVN_ISE_REPO=svn://svn.ise/ise_svn \
+        -e SVN_ISE_REPO=$SVN_ISE_REPO \
         -e SVN_ISE_BRANCH=$SVN_ISE_BRANCH \
-        -e SVN_EIFFELSTUDIO_REPO=svn://svn.ise/mirrors/eiffelstudio \
+        -e SVN_EIFFELSTUDIO_REPO=$ISE_EIFFELSTUDIO_REPO \
         -e SVN_EIFFELSTUDIO_REPO_REVISION=$SVN_EIFFELSTUDIO_REPO_REVISION \
         -e SVN_EIFFELSTUDIO_BRANCH=$SVN_EIFFELSTUDIO_BRANCH \
 	-e INCLUDE_ENTERPRISE=$include_enterprise \

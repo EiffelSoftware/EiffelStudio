a997fdc45329c4bedf0e014de8980ac7a1f3f1fd
Jocelyn Fiat <jfiat@eiffel.com>
2020-05-29 10:35:02 +0000

    Update make_installation script for windows
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@104390 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/Delivery/scripts/windows/notify.bat
+++ Src/Delivery/scripts/windows/notify.bat
@@ -2,6 +2,7 @@
 setlocal
 set mesg=%~1
 
-echo MESSAGE=%mesg%
+echo NOTIFICATION=%mesg%
+rem curl -X POST -H "Content-type: application/json" --data "{\"text\":\"%mesg%\"}" https://hooks.slack.com/services/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
 
 endlocal

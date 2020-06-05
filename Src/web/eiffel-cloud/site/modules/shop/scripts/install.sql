fdf63a8593d478cfda39914b6278c7466f4f9c6e
Jocelyn Fiat <jfiat@eiffel.com>
2020-06-05 13:21:36 +0000

    Updated installation script for the shop module.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@104426 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/web/eiffel-cloud/site/modules/shop/scripts/install.sql
+++ Src/web/eiffel-cloud/site/modules/shop/scripts/install.sql
@@ -12,6 +12,7 @@ CREATE TABLE shop_orders(
   `oid` INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
   `name` TEXT NOT NULL,
   `uid` INTEGER NOT NULL,
+  `email` TEXT,
   `created` DATETIME NOT NULL,
   `data`	TEXT NOT NULL
 );

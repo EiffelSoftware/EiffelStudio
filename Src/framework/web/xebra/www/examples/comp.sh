a7ef6833407259fd496e1a8317c85b40af793d10
Jocelyn Fiat <jfiat@eiffel.com>
2020-08-25 10:40:14 +0000

    Removed usage of -experiment in xebra project.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@104581 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/framework/web/xebra/www/examples/comp.sh
+++ Src/framework/web/xebra/www/examples/comp.sh
@@ -7,10 +7,10 @@ echo TRANSLATE
 $XEBRA_DEV/eiffel_projects/xebra_translator/EIFGENs/xebra_translator/$EC_MODE/xebra_translator -n $MY_WAPP -i . -o . -l $XEBRA_DEV/eiffel_projects/xebra_translator -f
  
 echo COMPILE servlet_gen
-ec -experiment -config .generated/servlet_gen/servlet_gen.ecf -target servlet_gen -c_compile -stop -project_path .generated/servlet_gen -clean
+ec -config .generated/servlet_gen/servlet_gen.ecf -target servlet_gen -c_compile -stop -project_path .generated/servlet_gen -clean
 
 echo Execute servlet_gen
 .generated/servlet_gen/EIFGENs/servlet_gen/W_code/servlet_gen -o .
 
 echo COMPILE webapp
-ec -experiment -config $MY_WAPP.ecf -target $MY_WAPP -c_compile -stop -project_path . -clean
+ec -config $MY_WAPP.ecf -target $MY_WAPP -c_compile -stop -project_path . -clean

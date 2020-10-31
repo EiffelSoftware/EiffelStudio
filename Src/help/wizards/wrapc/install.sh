139d53866c7eb15b47be5653216604235c11fbab
Javier Velilla <javierv@eiffel.com>
2020-10-31 14:49:18 +0000

    Fixed wrapc wizard install for Linux.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@104851 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/help/wizards/wrapc/install.sh
+++ Src/help/wizards/wrapc/install.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 mkdir tmp
-ecb -config wrapc_wizard.ecf -target wrapc_wizard -finalize -c_compile -project_path tmp
+ecb -config wizard.ecf -target wrapc_wizard -finalize -c_compile -project_path tmp
 mkdir -p spec/$ISE_PLATFORM
-mv tmp/EIFGENs/wizard/F_code/wizard spec/$ISE_PLATFORM/wizard
+mv tmp/EIFGENs/wrapc_wizard/F_code/wizard spec/$ISE_PLATFORM/wizard
 rm -rf tmp
 
 WIZ_TARGET=$ISE_EIFFEL/studio/wizards/new_projects/wrapc
 rm -rf $WIZ_TARGET
 mkdir $WIZ_TARGET
-cp -r resources $WIZ_TARGET/resources
+cp -r rootdir/resources $WIZ_TARGET/resources
 cp -r spec $WIZ_TARGET/spec
-cp wrapc.dsc $WIZ_TARGET/../wrapc.dsc
+cp rootdir/wrapc.dsc $WIZ_TARGET/../wrapc.dsc
 rm -rf spec

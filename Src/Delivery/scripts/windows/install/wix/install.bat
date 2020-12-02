2bbe79e2d259dc0dd53be2246632f0ee5b89cd71
Jocelyn Fiat <jfiat@eiffel.com>
2020-12-02 13:58:19 +0000

    Fixed the onetime install wix script
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@104963 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/Delivery/scripts/windows/install/wix/install.bat
+++ Src/Delivery/scripts/windows/install/wix/install.bat
@@ -1,4 +1,4 @@
-mklink /J "C:\Program Files (x86)\Windows Installer XML v3" %~dp0install\wix\v3.0
-mklink /J "C:\Program Files (x86)\MSBuild\Microsoft\WiX" %~dp0install\wix
+mklink /J "C:\Program Files (x86)\Windows Installer XML v3" %~dp0v3.0
+mklink /J "C:\Program Files (x86)\MSBuild\Microsoft\WiX" %~dp0
 cd v3.0
 mklink /J bin %~dp0

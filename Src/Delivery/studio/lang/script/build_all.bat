514e9f5e969e44a1830ba44e40c4569ff21cb1ec
Alexander Kogtenkov <alexk@eiffel.com>
2020-12-04 12:26:43 +0000

    Checked error level to prevent corrupted output.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@104983 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/Delivery/studio/lang/script/build_all.bat
+++ Src/Delivery/studio/lang/script/build_all.bat
@@ -1,2 +1,10 @@
+@echo off
 %EIFFEL_SRC%/tools/po_generation_tool/EIFGENs/po_generation_tool/F_code/po_generation_tool -D %EIFFEL_SRC%/Eiffel %EIFFEL_SRC%/../research/extension/autoproof/Eiffel %EIFFEL_SRC%/framework %EIFFEL_SRC%/../research/extension/autoproof/framework %EIFFEL_SRC%/help/wizards %EIFFEL_SRC%/library/wizard -o %EIFFEL_SRC%/Delivery/studio/lang/po_files/estudio.pot
+if ERRORLEVEL 1 then goto error
+
 %EIFFEL_SRC%/Delivery/studio/lang/script/build_misc_entries.bat
+if ERRORLEVEL 1 then goto error
+goto :eof
+
+:error
+echo FAILED!

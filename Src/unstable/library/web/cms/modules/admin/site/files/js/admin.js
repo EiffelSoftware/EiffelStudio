bf2be9e9fbc014fd555b11f838e536b27352bb24
Jocelyn Fiat <jfiat@eiffel.com>
2021-01-26 13:51:26 +0000

    fix: use double click to expand/collapse log entry, instead of single click.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@105133 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/unstable/library/web/cms/modules/admin/site/files/js/admin.js
+++ Src/unstable/library/web/cms/modules/admin/site/files/js/admin.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 	$("ul.logs li.log").each(function() { 
 		console.log("log...");
-		this.addEventListener("click", function() {
+		//this.addEventListener("dblclick", function() {
+		$(this).dblclick(function() {
     		this.classList.toggle("active");
-    	});
+		});
 	});
 })

4f22f87a6c7b0bf37717f3145777eae5ed79ef5b
Jocelyn Fiat <jfiat@eiffel.com>
2021-01-27 14:30:29 +0000

    style: Code cleaning
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@105146 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/unstable/library/web/cms/modules/admin/site/files/js/admin.js
+++ Src/unstable/library/web/cms/modules/admin/site/files/js/admin.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 	$("ul.logs li.log").each(function() { 
-		console.log("log...");
 		//this.addEventListener("dblclick", function() {
 		$(this).dblclick(function() {
     		this.classList.toggle("active");

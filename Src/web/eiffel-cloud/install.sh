a42a4ee516e43cde0f61f1662118712a2fd9a433
Jocelyn Fiat <jfiat@eiffel.com>
2021-10-19 11:53:03 +0000

    Updated for email storage capabilities
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@105885 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/web/eiffel-cloud/install.sh
+++ Src/web/eiffel-cloud/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-eiffel --target roc $ISE_LIBRARY/unstable/library/web/cms/tools/roc/roc.ecf install --config roc.cfg
+eiffel --target roc $ISE_LIBRARY/unstable/library/web/cms/tools/roc/roc.ecf install --config roc.cfg $*
--- Src/web/eiffel-cloud/site/modules/core/scripts/updates/core-1.0.0.1.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE users ADD profile_name VARCHAR(250) NULL;
-

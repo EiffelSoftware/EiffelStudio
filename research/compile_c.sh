d7519a318d1848992e34ba8df30a4de4c831df3e
Generic Eiffel Software User <svn@eiffel.com>
2022-01-18 07:43:34 +0000

    Updating the C compilation script for Linux
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@106115 8089f293-4706-0410-a29e-feb5c42a2edf

--- research/compile_c.sh
+++ research/compile_c.sh
@@ -8,3 +8,4 @@ cd ${EIFFEL_SRC}/library/cURL/Clib                             && ${ISE_EIFFEL}/
 cd ${EIFFEL_SRC}/library/net/Clib                              && ${ISE_EIFFEL}/studio/spec/${ISE_PLATFORM}/bin/finish_freezing -library
 cd ${EIFFEL_SRC}/library/vision2/Clib                          && ${ISE_EIFFEL}/studio/spec/${ISE_PLATFORM}/bin/finish_freezing -library
 cp -p ${ISE_EIFFEL}/library/vision2/spec/${ISE_PLATFORM}/lib/gtk3_eiffel.o ${EIFFEL_SRC}/library/vision2/spec/${ISE_PLATFORM}/lib/
+cd ${EIFFEL_SRC}/C							&& ./quick_configure

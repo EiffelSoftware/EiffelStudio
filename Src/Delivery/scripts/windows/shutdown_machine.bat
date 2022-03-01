a2c8c677d5ab96eaa08b09cfb6e89bc960481ad9
Jocelyn Fiat <jfiat@eiffel.com>
2022-03-01 10:08:04 +0000

    when arg is now, set sleep to 0
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@106187 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/Delivery/scripts/windows/shutdown_machine.bat
+++ Src/Delivery/scripts/windows/shutdown_machine.bat
@@ -4,16 +4,19 @@ if "%1" EQU "cancel" (
 	shutdown -a
 	goto EOF
 )
+if "%1" EQU "now" (
+	set tmp_delay=0
+)
 if "%1" EQU "" (
 	set tmp_delay=7200
 	rem 2 hours=2 * 60 * 60 seconds
 ) else (
 	set tmp_delay=%1
 )
-if "%1" EQU "0" (
-   rem No need to notify it
+if "%tmp_delay%" EQU "0" (
+	rem No notification when shutdown is immediate
 ) else (
-call %~dp0notify.bat "Going to shutdown Windows deliv AWS machine in %tmp_delay% seconds!"
+	call %~dp0notify.bat "Going to shutdown Windows deliv AWS machine in %tmp_delay% seconds!"
 )
 
 echo Shutdown the machine %COMPUTERNAME% in %tmp_delay% seconds

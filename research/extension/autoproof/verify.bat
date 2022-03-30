1dc5c0909ff1ff52cc6c568b30d2b4190b0bc46c
Alexander Kogtenkov <alexk@eiffel.com>
2022-03-30 17:11:40 +0000

    Added a script to verify multiple projects in batch mode.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@106216 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ research/extension/autoproof/verify.bat
@@ -0,0 +1,31 @@
+@echo off
+
+rem
+rem A script to verify projects recursively reachable from the current directory.
+rem
+rem Every project should have a single target. Verification applies to the clusters of this target.
+rem
+rem Usage:
+rem 	verify.bat
+rem
+rem Output: projects that fail to compile or verify are reported to the standard output.
+rem         The compilation/verification errors are logged in the project directories.
+rem
+rem Note: the script recompiles projects from scratch and does not keep them.
+rem       In particular, it removes `EIFGENs` from the current directory.
+rem
+
+setlocal enabledelayedexpansion
+
+for /r %%# in (*.ecf) do (
+	%EC% -batch -clean -verify collection:cluster -verify printtime:false -config %%# 2> %%~dpn#.log
+	call set "ECF=%%#"
+	if ERRORLEVEL 1 (
+		call echo Verification failed: %%ECF:CD\=%%
+	) else if ERRORLEVEL 0 (
+		del %%~dpn#.log
+	) else if ERRORLEVEL -1 (
+		call echo Verification failed: %%ECF:CD\=%%
+	)
+	rd/s/q EIFGENs
+)

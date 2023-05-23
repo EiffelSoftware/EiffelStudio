3bb834477e9be84510cc1672068170c9143f0c30
Javier Velilla <javierv@eiffel.com>
2023-05-23 13:54:17 +0000

    Refactor rename folder .NetC# to cs Added C# property_access example.
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@106869 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/framework/dotnet/il_emitter/testing/cs/property_access/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+    // A private field name
+    private string name;
+
+    // A public property Name with get and set accessors
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public static void Main()
+    {
+        Program program = new Program();
+
+        // Set the Name property
+        program.Name = "John Doe";
+
+        // Get the Name property and output it to the console
+        Console.WriteLine("Name: " + program.Name);
+    }
+}

f91fd24650f186585783c4c1c52cec82d709efae
Javier Velilla <javierv@eiffel.com>
2023-05-30 20:24:55 +0000

    Added C# interface example to compare the generated assembly with the Eiffel test_define_interface
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@106892 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/framework/dotnet/il_emitter/testing/cs/interface/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+// Define an interface
+interface IPrintable
+{
+    void Print();
+}
+
+public class Program : IPrintable
+{
+    // A private field name
+    private string name;
+
+    // A public property Name with get and set accessors
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    // Implement the Print method from the IPrintable interface
+    public void Print()
+    {
+        Console.WriteLine(Name);
+    }
+
+    public static void Main()
+    {
+        Program program = new Program();
+
+        // Set the Name property
+        program.Name = "John Doe";
+
+        // Call the Print method to output the Name property to the console
+        program.Print();
+    }
+}

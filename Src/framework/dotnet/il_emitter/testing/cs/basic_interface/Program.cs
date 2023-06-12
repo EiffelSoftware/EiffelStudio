32206ed8b21b739ac3399d2c38e73a9b7cfe6108
Javier Velilla <javierv@eiffel.com>
2023-06-12 19:38:24 +0000

    Added C# example with a basic interface
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@106958 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/framework/dotnet/il_emitter/testing/cs/basic_interface/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+// Define an interface
+interface IPrintable
+{
+    void Print();
+}
+interface IMark
+{
+
+}
+
+public class Program : IPrintable, IMark
+{
+
+    // Implement the Print method from the IPrintable interface
+    public void Print()
+    {
+        Console.WriteLine("Testing Basic Interfaces");
+    }
+
+    public static void Main()
+    {
+        Program program = new Program();
+
+        // Call the Print method to output the console
+        program.Print();
+    }
+}

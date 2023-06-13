d391d2aca983fbbffee921bd8c5acfde5c45bc0a
Javier Velilla <javierv@eiffel.com>
2023-06-13 19:51:04 +0000

    Added C# class example with multiple methods.
    The example will be used to compare the generated output with the equivalent Eiffel code.
    
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@106972 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/framework/dotnet/il_emitter/testing/cs/class/Class.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+ public class MyClass
+    {
+        public void A()
+        {
+            Console.WriteLine("A");
+        }
+
+        public void B()
+        {
+            Console.WriteLine("B");
+        }
+
+        public void C()
+        {
+            Console.WriteLine("C");
+        }
+
+        public void D()
+        {
+            Console.WriteLine("D");
+        }
+
+        public void E()
+        {
+            Console.WriteLine("E");
+        }
+
+        public void F()
+        {
+            Console.WriteLine("F");
+        }
+
+        public void G()
+        {
+            Console.WriteLine("G");
+        }
+
+        public void H()
+        {
+            Console.WriteLine("H");
+        }
+
+        public void I()
+        {
+            Console.WriteLine("I");
+        }
+
+        public void J()
+        {
+            Console.WriteLine("J");
+        }
+
+        public void K()
+        {
+            Console.WriteLine("K");
+        }
+
+    static void Main()
+        {
+            MyClass myClass = new MyClass();
+            myClass.A(); 
+            myClass.B(); 
+            myClass.C(); 
+            myClass.D(); 
+            myClass.E(); 
+            myClass.F(); 
+            myClass.G();
+            myClass.H(); 
+            myClass.I(); 
+            myClass.J(); 
+            myClass.K(); 
+        }
+ }

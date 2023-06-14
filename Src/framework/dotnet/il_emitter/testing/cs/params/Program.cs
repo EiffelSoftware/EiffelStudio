f38e3fe60dfd5533019c5d63c827ed3a69c5e3f4
Javier Velilla <javierv@eiffel.com>
2023-06-14 17:24:31 +0000

    Added C# Params Example.
    Added a C# example code with a few functions with a different number of parameters.
    
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@106985 8089f293-4706-0410-a29e-feb5c42a2edf

---
+++ Src/framework/dotnet/il_emitter/testing/cs/params/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+ public class Program
+    {
+        public void F1(int i)
+        {
+            Console.WriteLine(i);
+        }
+
+        public void F2(string str, int i)
+        {
+            Console.WriteLine(str);
+            Console.WriteLine(i);
+        }
+
+        public void F3(string str, int i, string str2)
+        {
+            Console.WriteLine(str);
+            Console.WriteLine(i);
+            Console.WriteLine(str2);
+
+        }
+
+
+    static void Main()
+        {
+            Program myProgram = new Program();
+            Console.WriteLine("Function F1");
+            myProgram.F1(10);
+            Console.WriteLine("Function F2");
+            myProgram.F2("Hello", 11);
+            Console.WriteLine("Function F3");
+            myProgram.F3("Hello", 12, "World");
+        }
+ }

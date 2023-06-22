5de69c082e82b82aab932958f69a6f8ea305736e
Jocelyn Fiat <jfiat@eiffel.com>
2023-06-22 14:14:17 +0000

    Use version 23.06.10.7006 for netcore version of the EiffelSoftware.Runtime.dll
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@107007 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/Eiffel/eiffel/com_il_generation/Core/run-time/runtime_assembly_info.cs
+++ Src/Eiffel/eiffel/com_il_generation/Core/run-time/runtime_assembly_info.cs
@@ -65,7 +65,11 @@ using System.Runtime.CompilerServices;
 // You can specify all the value or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
+#if NET5_0_OR_GREATER
+[assembly: AssemblyVersion("23.6.10.7006")]
+#else
 [assembly: AssemblyVersion("7.3.9.2490")]
+#endif
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 

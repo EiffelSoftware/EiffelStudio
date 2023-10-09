5830a05ffe7e0854964a799f7945496735bac487
Jocelyn Fiat <jfiat@eiffel.com>
2023-10-09 16:29:14 +0000

    Updated script to put the PorterPackage including enterprise files ... in expected "ent" folder
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@107343 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/Delivery/scripts/docker/unix/build_deliveries.sh
+++ Src/Delivery/scripts/docker/unix/build_deliveries.sh
@@ -19,9 +19,9 @@
 #################################
 # Do not modify following lines #
 #################################
-if test -z "$SVN_EIFFELSTUDIO_REPO_REVISION" 
+if test -z "$SVN_EIFFELSTUDIO_REPO_REVISION"
 then
-	if ! test -z "$1" 
+	if ! test -z "$1"
 	then
 		export SVN_EIFFELSTUDIO_REPO_REVISION=$1
 	fi
@@ -86,9 +86,12 @@ if [ -e "$output_dir/last_revision_built" ]; then
 			echo "ERROR: File size of $porterpackage_tar sounds too small to be valid (size=$pp_filesize), stop delivery!"
 			rm "$porterpackage_tar"
 		else
-			if [ "$edition_name" = "ent" ]; then
+			if [ "$include_enterprise" = "true" ]; then
+				echo "Sharing the $edition_name PorterPackage (including enterprise) to (ent)"
+				share_deliv_file $output_dir/${deliv_revision}/PorterPackage_${deliv_revision}.tar "New $edition_name Porterpackage for revision ${deliv_revision}" ent
+			else if [ "$edition_name" = "ent" ]; then
 				echo "Sharing the $edition_name PorterPackage to (ent)"
-				share_deliv_file $output_dir/${deliv_revision}/PorterPackage_ent_${deliv_revision}.tar "New $edition_name Porterpackage for revision ${deliv_revision}" ${edition_name}
+				share_deliv_file $output_dir/${deliv_revision}/PorterPackage_${deliv_revision}.tar "New $edition_name Porterpackage for revision ${deliv_revision}" ${edition_name}
 			else
 				echo "Sharing the $edition_name PorterPackage to (std)"
 				share_deliv_file $output_dir/${deliv_revision}/PorterPackage_${deliv_revision}.tar "New $edition_name Porterpackage for revision ${deliv_revision}" ${edition_name}
@@ -110,11 +113,11 @@ if [ -e "$output_dir/last_revision_built" ]; then
 			echo "===================================="
 			sh ./build_images_from.sh $porterpackage_tar $output_dir/${deliv_revision} 32 $edition_name
 			share_deliv_folder $output_dir/${deliv_revision} "New $edition_name linux-x86 release for revision ${deliv_revision}"
-		fi 
+		fi
 	else
 		echo ERROR: missing porterpackage "$porterpackage_tar" !
 	fi
 else
-	echo ERROR: issue with previous porterpackage building! 
+	echo ERROR: issue with previous porterpackage building!
 fi
 

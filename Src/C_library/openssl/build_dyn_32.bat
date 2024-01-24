25dc3ed97e4790286bb6bf9846d37ff4436cd663
Javier Velilla <javierv@eiffel.com>
2024-01-24 16:27:34 +0000

    Updated C_library/openssl
    Removed unneeded folders apps and engines.
    Updated build scripts with minimal build options.
    
    git-svn-id: https://svn.eiffel.com/eiffelstudio-public/trunk@107509 8089f293-4706-0410-a29e-feb5c42a2edf

--- Src/C_library/openssl/build_dyn_32.bat
+++ Src/C_library/openssl/build_dyn_32.bat
@@ -1,6 +1,11 @@
 @echo on
-title Script to compile OpenSSL on Windows 32 bits dynamically
 
+rem Get start time:
+for /F "tokens=1-4 delims=:.," %%a in ("%time%") do (
+  set /A "start=((((%%a*60)+1%%b %% 100)*60+1%%c %% 100)*100+1%%d %% 100)"
+)
+
+title Script to compile OpenSSL on Windows 32 bits dynamically
 set current_dir=%~dp0
 
 rem Remove the builds directory if it exists
@@ -16,9 +21,24 @@ cd ..
 @echo Building a 32-Bit dynamic library (/MD) using the option `shared` with VC-WIN32
 @echo Compiling OpenSSL 32bits dynamic
 
-perl Configure VC-WIN32 shared --prefix=%current_dir%builds\dynamic_32 --openssldir=%current_dir%builds\dynamic_32
+perl Configure no-apps no-docs no-asm no-ssl3 no-zlib no-comp no-autoload-config no-engine VC-WIN32 shared --prefix=%current_dir%builds\dynamic_32 --openssldir=%current_dir%builds\dynamic_32
 nmake 
 nmake install_sw
+
+rem Get end time:
+for /F "tokens=1-4 delims=:.," %%a in ("%time%") do (
+  set /A "end=((((%%a*60)+1%%b %% 100)*60+1%%c %% 100)*100+1%%d %% 100)"
+)
+
+rem Get elapsed time:
+set /A elapsed=end-start
+rem Show elapsed time:
+set /A hh=elapsed/(60*60*100), rest=elapsed%%(60*60*100), mm=rest/(60*100), rest%%=60*100, ss=rest/100, cc=rest%%100
+if %mm% lss 10 set mm=0%mm%
+if %ss% lss 10 set ss=0%ss%
+if %cc% lss 10 set cc=0%cc%
+echo Elapsed Time: %hh%:%mm%:%ss%.%cc%
+
 
 
 
--- Src/C_library/openssl/build_dyn_64.bat
+++ Src/C_library/openssl/build_dyn_64.bat
@@ -1,6 +1,11 @@
 @echo on
-title Script to compile OpenSSL on Windows 64 bits dinamycally
 
+rem Get start time:
+for /F "tokens=1-4 delims=:.," %%a in ("%time%") do (
+  set /A "start=((((%%a*60)+1%%b %% 100)*60+1%%c %% 100)*100+1%%d %% 100)"
+)
+
+title Script to compile OpenSSL on Windows 64 bits dinamycally
 set current_dir=%~dp0
 
 rem Remove the builds directory if it exists
@@ -16,9 +21,25 @@ cd ..
 @echo Building a 64-Bit dynamic library (/MD) using the option `shared` with VC-WIN64A
 @echo Compiling OpenSSL 64bits dynamic
 
-perl Configure VC-WIN64A shared --prefix=%current_dir%builds\dynamic_64 --openssldir=%current_dir%builds\dynamic_64
+perl Configure no-apps no-docs no-asm no-ssl3 no-zlib no-comp no-autoload-config no-engine no-quic VC-WIN64A shared --release --prefix=%current_dir%builds\dynamic_64 --openssldir=%current_dir%builds\dynamic_64
 nmake 
 nmake install_sw
+
+
+rem Get end time:
+for /F "tokens=1-4 delims=:.," %%a in ("%time%") do (
+  set /A "end=((((%%a*60)+1%%b %% 100)*60+1%%c %% 100)*100+1%%d %% 100)"
+)
+
+rem Get elapsed time:
+set /A elapsed=end-start
+rem Show elapsed time:
+set /A hh=elapsed/(60*60*100), rest=elapsed%%(60*60*100), mm=rest/(60*100), rest%%=60*100, ss=rest/100, cc=rest%%100
+if %mm% lss 10 set mm=0%mm%
+if %ss% lss 10 set ss=0%ss%
+if %cc% lss 10 set cc=0%cc%
+echo Elapsed Time: %hh%:%mm%:%ss%.%cc%
+
 
 
 
